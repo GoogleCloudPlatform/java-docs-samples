dcf91cc4fcdf9988018aa0a6eba721f2009f7332
Jerjou Cheng <jerjou@google.com>
2015-05-22 13:44:56 -0700

    Update to use App Default Creds.
    + some other cleanup.

--- cloud-storage/xml-api/serviceaccount-cmdline-sample/README.md
+++ cloud-storage/xml-api/serviceaccount-cmdline-sample/README.md
@@ -15,23 +15,23 @@ for setting up a service account.
 Checkout Instructions
 ---------------------
 
-**Prerequisites:** install the latest version of [Java](http//java.com) and [Maven](http://maven.apache.org/download.html). You must also be able to work with qcGitHub repository (see e.g.,
-https://help.github.com/articles/set-up-git).
-
-You may need to set your `JAVA_HOME`.
+1. **Prerequisites:**
+    1. install the latest version of [Java](http//java.com) and [Maven](http://maven.apache.org/download.html).
+    1. [Create](https://cloud.google.com/storage/docs/cloud-console#_creatingbuckets) a Google Cloud Storage bucket
+    1. You must also be able to work with [GitHub](https://help.github.com/articles/set-up-git) repositories.
+    1. You may need to set your `JAVA_HOME`.
 
 1. Clone repository.
 
-        cd [someDirectory]
         git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
         cd java-docs-samples/cloud-storage/xml-api/serviceaccount-cmdline-sample
 
-2. Update key.p12 file.
-3. Edit `StorageServiceAccountSample.java` to add your bucket and service account email.
-4. Compile and run
+1. Update key.json file.
+1. Compile and run
 
         mvn compile install
-        mvn -q exec:java
+        export GOOGLE_APPLICATION_CREDENTIALS=key.json
+        mvn -q exec:java -Dexec.args="your-bucket-name"
 
 
 To enable logging of HTTP requests and responses (highly recommended when 
@@ -40,8 +40,9 @@ developing), please take a look at logging.properties.
 Set Up a Project in Eclipse
 ---------------------------
 
-**Prerequisites:** install [Eclipse](http://www.eclipse.org/downloads/), the [Maven plugin](http://m2eclipse.sonatype.org/installing-m2eclipse.html), and optionally the 
-[GitHub plugin](http://eclipse.github.com/).
+* **Prerequisites:**
+    * Install [Eclipse](http://www.eclipse.org/downloads/), the [Maven plugin](http://eclipse.org/m2e/), and optionally the [GitHub plugin](http://eclipse.github.com/).
+    * [Create](https://cloud.google.com/storage/docs/cloud-console#_creatingbuckets) a Google Cloud Storage bucket
 
 * Set up Eclipse Preferences
 
@@ -63,9 +64,11 @@ to work with GitHub repositories.
     * Make sure the Eclipse package name matches the package name used in the
 code file `com.google.api.services.samples.storage.serviceaccount.cmdline`.
     * Select the project and **Convert to Maven Project** to add Maven Dependencies.
-    * Edit `StorageServiceAccountSample.java` and specify your SERVICE_ACCOUNT_EMAIL and
-BUCKET_NAME.
-    * Update the key.p12 file.
+    * Update the key.json file.
+    * Click on Run > Run configurations
+        * Navigate to your **Java Application**'s configuration section
+        * In the **Arguments** tab, add your bucket name as a **Program argument**
+        * In the **Environment** tab, create a variable `GOOGLE_APPLICATION_CREDENTIALS` and set it to `key.json`
 
 * Run
 
--- cloud-storage/xml-api/serviceaccount-cmdline-sample/src/main/java/StorageServiceAccountSample.java
+++ cloud-storage/xml-api/serviceaccount-cmdline-sample/src/main/java/StorageServiceAccountSample.java
@@ -27,12 +27,12 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.Preconditions;
 import com.google.common.io.Files;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.util.Collections;
+import java.net.URLEncoder;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
@@ -43,12 +43,6 @@ import javax.xml.transform.stream.StreamSource;
 
 public class StorageServiceAccountSample {
 
-  /** E-mail address of the service account. */
-  private static final String SERVICE_ACCOUNT_EMAIL = "[[INSERT_SERVICE_ACCOUNT_EMAIL_HERE]]";
-
-  /** Bucket to list. */
-  private static final String BUCKET_NAME = "[[INSERT_YOUR_BUCKET_NAME_HERE]]";
-
   /** Global configuration of Google Cloud Storage OAuth 2.0 scope. */
   private static final String STORAGE_SCOPE =
       "https://www.googleapis.com/auth/devstorage.read_write";
@@ -56,63 +50,48 @@ public class StorageServiceAccountSample {
   /** Global instance of the HTTP transport. */
   private static HttpTransport httpTransport;
 
-  /** Global instance of the JSON factory. */
-  private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-
   public static void main(String[] args) {
     try {
-      try {
-        httpTransport = GoogleNetHttpTransport.newTrustedTransport();
-        // Check for valid setup.
-        Preconditions.checkArgument(!SERVICE_ACCOUNT_EMAIL.startsWith("[["),
-            "Please enter your service account e-mail from the Google APIs "
-            + "Console to the SERVICE_ACCOUNT_EMAIL constant in %s",
-            StorageServiceAccountSample.class.getName());
-        Preconditions.checkArgument(!BUCKET_NAME.startsWith("[["),
-            "Please enter your desired Google Cloud Storage bucket name "
-            + "to the BUCKET_NAME constant in %s", StorageServiceAccountSample.class.getName());
-        String p12Content = Files.readFirstLine(new File("key.p12"), Charset.defaultCharset());
-        Preconditions.checkArgument(!p12Content.startsWith("Please"), p12Content);
+      httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+      // Check for valid setup.
+      Preconditions.checkArgument(args.length == 1,
+          "Please pass in the Google Cloud Storage bucket name to display");
+      String bucketName = args[0];
 
-        //[START snippet]
-        // Build a service account credential.
-        GoogleCredential credential = new GoogleCredential.Builder().setTransport(httpTransport)
-            .setJsonFactory(JSON_FACTORY)
-            .setServiceAccountId(SERVICE_ACCOUNT_EMAIL)
-            .setServiceAccountScopes(Collections.singleton(STORAGE_SCOPE))
-            .setServiceAccountPrivateKeyFromP12File(new File("key.p12"))
-            .build();
+      //[START snippet]
+      // Build a service account credential.
+      GoogleCredential credential = GoogleCredential.getApplicationDefault()
+          .createScoped(Collections.singleton(STORAGE_SCOPE));
 
-        // Set up and execute a Google Cloud Storage request.
-        String URI = "https://storage.googleapis.com/" + BUCKET_NAME;
-        HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential);
-        GenericUrl url = new GenericUrl(URI);
-        HttpRequest request = requestFactory.buildGetRequest(url);
-        HttpResponse response = request.execute();
-        String content = response.parseAsString();
-       //[END snippet]
+      // Set up and execute a Google Cloud Storage request.
+      String URI = "https://storage.googleapis.com/" + URLEncoder.encode(bucketName, "UTF-8");
+      HttpRequestFactory requestFactory = httpTransport.createRequestFactory(credential);
+      GenericUrl url = new GenericUrl(URI);
+      HttpRequest request = requestFactory.buildGetRequest(url);
+      HttpResponse response = request.execute();
+      String content = response.parseAsString();
+     //[END snippet]
 
-        // Instantiate transformer input.
-        Source xmlInput = new StreamSource(new StringReader(content));
-        StreamResult xmlOutput = new StreamResult(new StringWriter());
+      // Instantiate transformer input.
+      Source xmlInput = new StreamSource(new StringReader(content));
+      StreamResult xmlOutput = new StreamResult(new StringWriter());
 
-        // Configure transformer.
-        Transformer transformer = TransformerFactory.newInstance().newTransformer(); // An identity
-                                                                                     // transformer
-        transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "testing.dtd");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-        transformer.transform(xmlInput, xmlOutput);
+      // Configure transformer.
+      Transformer transformer = TransformerFactory.newInstance().newTransformer(); // An identity
+                                                                                   // transformer
+      transformer.setOutputProperty(OutputKeys.DOCTYPE_SYSTEM, "testing.dtd");
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+      transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+      transformer.transform(xmlInput, xmlOutput);
 
-        // Pretty print the output XML.
-        System.out.println("\nBucket listing for " + BUCKET_NAME + ":\n");
-        System.out.println(xmlOutput.getWriter().toString());
-        System.exit(0);
+      // Pretty print the output XML.
+      System.out.println("\nBucket listing for " + bucketName + ":\n");
+      System.out.println(xmlOutput.getWriter().toString());
+      System.exit(0);
 
-      } catch (IOException e) {
+    } catch (IOException e) {
         System.err.println(e.getMessage());
-      }
     } catch (Throwable t) {
       t.printStackTrace();
     }
