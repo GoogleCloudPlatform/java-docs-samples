bce221c0cd40862855e889dc3b7436e178850c01
Shun Fan <sfan@google.com>
2015-11-24 15:42:22 -0800

    Update to conform to appengine java runtime

--- appengine/helloworld/src/main/java/com/example/appengine/helloworld/HelloServlet.java
+++ appengine/helloworld/src/main/java/com/example/appengine/helloworld/HelloServlet.java
@@ -14,21 +14,18 @@
  * limitations under the License.
  */
 
-package com.example.appengine.gettingstartedjava.helloworld;
+package com.example.appengine.helloworld;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // [START example]
-@WebServlet(name = "helloworld", urlPatterns = { "/*" })
+@SuppressWarnings("serial")
 public class HelloServlet extends HttpServlet {
-
-  private static final long serialVersionUID = 1L;
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
