d289deb55ee53f54b2c12c0f5ca93d05d539932a
Tim Swast <swast@google.com>
2016-01-06 15:23:57 -0800

    Removed pointless try/catch from App Identity sample.
    Also, fixes typo (two spaces by Bearer).

--- appengine/appidentity/src/main/java/com/example/appengine/appidentity/UrlShortener.java
+++ appengine/appidentity/src/main/java/com/example/appengine/appidentity/UrlShortener.java
@@ -18,6 +18,7 @@ package com.example.appengine.appidentity;
 import com.google.appengine.api.appidentity.AppIdentityService;
 import com.google.appengine.api.appidentity.AppIdentityServiceFactory;
 import com.google.common.io.CharStreams;
+
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
@@ -32,46 +33,46 @@ import java.util.ArrayList;
 @SuppressWarnings("serial")
 class UrlShortener {
   // [START asserting_identity_to_Google_APIs]
+  /**
+   * Returns a shortened URL by calling the Google URL Shortener API.
+   *
+   * <p>Note: Error handling elided for simplicity.
+   */
   public String createShortUrl(String longUrl) throws Exception {
-    try {
-      ArrayList<String> scopes = new ArrayList<String>();
-      scopes.add("https://www.googleapis.com/auth/urlshortener");
-      AppIdentityService appIdentity = AppIdentityServiceFactory.getAppIdentityService();
-      AppIdentityService.GetAccessTokenResult accessToken = appIdentity.getAccessToken(scopes);
-      // The token asserts the identity reported by appIdentity.getServiceAccountName()
-      JSONObject request = new JSONObject();
-      request.put("longUrl", longUrl);
+    ArrayList<String> scopes = new ArrayList<String>();
+    scopes.add("https://www.googleapis.com/auth/urlshortener");
+    AppIdentityService appIdentity = AppIdentityServiceFactory.getAppIdentityService();
+    AppIdentityService.GetAccessTokenResult accessToken = appIdentity.getAccessToken(scopes);
+    // The token asserts the identity reported by appIdentity.getServiceAccountName()
+    JSONObject request = new JSONObject();
+    request.put("longUrl", longUrl);
 
-      URL url = new URL("https://www.googleapis.com/urlshortener/v1/url?pp=1");
-      HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-      connection.setDoOutput(true);
-      connection.setRequestMethod("POST");
-      connection.addRequestProperty("Content-Type", "application/json");
-      connection.addRequestProperty("Authorization", "Bearer  " + accessToken.getAccessToken());
+    URL url = new URL("https://www.googleapis.com/urlshortener/v1/url?pp=1");
+    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+    connection.setDoOutput(true);
+    connection.setRequestMethod("POST");
+    connection.addRequestProperty("Content-Type", "application/json");
+    connection.addRequestProperty("Authorization", "Bearer " + accessToken.getAccessToken());
 
-      OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
-      request.write(writer);
-      writer.close();
+    OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+    request.write(writer);
+    writer.close();
 
-      if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-        // Note: Should check the content-encoding.
-        //       Any JSON parser can be used; this one is used for illustrative purposes.
-        JSONTokener response_tokens = new JSONTokener(connection.getInputStream());
-        JSONObject response = new JSONObject(response_tokens);
-        return (String) response.get("id");
-      } else {
-        try (InputStream s = connection.getErrorStream();
-            InputStreamReader r = new InputStreamReader(s, StandardCharsets.UTF_8)) {
-          throw new RuntimeException(String.format(
-              "got error (%d) response %s from %s",
-              connection.getResponseCode(),
-              CharStreams.toString(r),
-              connection.toString()));
-        }
+    if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+      // Note: Should check the content-encoding.
+      //       Any JSON parser can be used; this one is used for illustrative purposes.
+      JSONTokener response_tokens = new JSONTokener(connection.getInputStream());
+      JSONObject response = new JSONObject(response_tokens);
+      return (String) response.get("id");
+    } else {
+      try (InputStream s = connection.getErrorStream();
+          InputStreamReader r = new InputStreamReader(s, StandardCharsets.UTF_8)) {
+        throw new RuntimeException(String.format(
+            "got error (%d) response %s from %s",
+            connection.getResponseCode(),
+            CharStreams.toString(r),
+            connection.toString()));
       }
-    } catch (Exception e) {
-      // Error handling elided.
-      throw e;
     }
   }
   // [END asserting_identity_to_Google_APIs]
