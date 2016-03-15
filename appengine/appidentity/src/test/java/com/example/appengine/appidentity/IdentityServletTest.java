1ee524b4b52a3a08a88a7949a4fc04fdb7f22449
Tim Swast <swast@google.com>
2016-03-15 11:15:11 -0700

    Checkstyle fixes for App Identity sample.
    Getting ready to use shared java-repo-tools configuration.

--- appengine/appidentity/src/test/java/com/example/appengine/appidentity/IdentityServletTest.java
+++ appengine/appidentity/src/test/java/com/example/appengine/appidentity/IdentityServletTest.java
@@ -20,13 +20,14 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -34,7 +35,9 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Unit tests for {@link IdentityServlet}. */
+/**
+ * Unit tests for {@link IdentityServlet}.
+ */
 @RunWith(JUnit4.class)
 public class IdentityServletTest {
 
--- appengine/appidentity/src/test/java/com/example/appengine/appidentity/SignForAppServletTest.java
+++ appengine/appidentity/src/test/java/com/example/appengine/appidentity/SignForAppServletTest.java
@@ -1,31 +1,33 @@
-/**
+/*
  * Copyright 2016 Google Inc. All Rights Reserved.
  *
- * <p>Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * <p>http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * <p>Unless required by applicable law or agreed to in writing, software
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.appengine.appidentity;
 
 import static com.google.common.truth.Truth.assertThat;
 import static org.mockito.Mockito.when;
 
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
@@ -33,7 +35,9 @@ import java.io.StringWriter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Unit tests for {@link SignForAppServlet}. */
+/**
+ * Unit tests for {@link SignForAppServlet}.
+ */
 @RunWith(JUnit4.class)
 public class SignForAppServletTest {
 
