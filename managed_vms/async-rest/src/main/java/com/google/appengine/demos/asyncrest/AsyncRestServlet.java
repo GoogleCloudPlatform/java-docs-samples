e51a33b11e04868b9d3076a5306277872eb506b3
Tim Swast <swast@google.com>
2016-03-16 16:50:21 -0700

    Revert "Checkstyle fixes for async-rest sample."

--- managed_vms/async-rest/src/main/java/com/google/appengine/demos/asyncrest/AsyncRestServlet.java
+++ managed_vms/async-rest/src/main/java/com/google/appengine/demos/asyncrest/AsyncRestServlet.java
@@ -1,29 +1,4 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.google.appengine.demos.asyncrest;
-
-import org.eclipse.jetty.client.HttpClient;
-import org.eclipse.jetty.client.api.Response;
-import org.eclipse.jetty.client.api.Result;
-import org.eclipse.jetty.http.HttpMethod;
-import org.eclipse.jetty.util.BufferUtil;
-import org.eclipse.jetty.util.Utf8StringBuilder;
-import org.eclipse.jetty.util.ajax.JSON;
-import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -39,10 +14,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.Response;
+import org.eclipse.jetty.client.api.Result;
+import org.eclipse.jetty.http.HttpMethod;
+import org.eclipse.jetty.util.BufferUtil;
+import org.eclipse.jetty.util.Utf8StringBuilder;
+import org.eclipse.jetty.util.ajax.JSON;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
 /**
- * Servlet which makes REST calls asynchronously.
- *
- * <p>May be configured with init parameters:
+ * AsyncRestServlet. May be configured with init parameters:
  * <dl>
  * <dt>appid</dt>
  * <dd>The Google app key to use</dd>
@@ -51,9 +33,9 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class AsyncRestServlet extends AbstractRestServlet {
 
-  static final String RESULTS_ATTR = "com.google.appengine.demos.asyncrest.client";
-  static final String DURATION_ATTR = "com.google.appengine.demos.asyncrest.duration";
-  static final String START_ATTR = "com.google.appengine.demos.asyncrest.start";
+  final static String RESULTS_ATTR = "com.google.appengine.demos.asyncrest.client";
+  final static String DURATION_ATTR = "com.google.appengine.demos.asyncrest.duration";
+  final static String START_ATTR = "com.google.appengine.demos.asyncrest.start";
 
   HttpClient client;
 
@@ -82,22 +64,22 @@ public class AsyncRestServlet extends AbstractRestServlet {
     Long start = System.nanoTime();
 
     // Do we have results yet?
-    Queue<Map<String, Object>> results =
-        (Queue<Map<String, Object>>) request.getAttribute(RESULTS_ATTR);
+    Queue<Map<String, Object>> results = (Queue<Map<String, Object>>) request.getAttribute(RESULTS_ATTR);
 
-    // If no results, this must be the first dispatch, so send the REST request(s).
+    // If no results, this must be the first dispatch, so send the REST
+    // request(s)
     if (results == null) {
       // define results data structures
       final Queue<Map<String, Object>> resultsQueue = new ConcurrentLinkedQueue<>();
       request.setAttribute(RESULTS_ATTR, results = resultsQueue);
 
-      // Suspend the request.
+      // suspend the request
       // This is done before scheduling async handling to avoid race of
       // dispatch before startAsync!
       final AsyncContext async = request.startAsync();
       async.setTimeout(30000);
 
-      // Extract keywords to search for.
+      // extract keywords to search for
       String lat = sanitize(request.getParameter(LATITUDE_PARAM));
       String longitude = sanitize(request.getParameter(LONGITUDE_PARAM));
       String radius = sanitize(request.getParameter(RADIUS_PARAM));
@@ -105,19 +87,17 @@ public class AsyncRestServlet extends AbstractRestServlet {
 
       final AtomicInteger outstanding = new AtomicInteger(keywords.length);
 
-      // Send request each keyword.
+      // Send request each keyword
       for (final String item : keywords) {
-        client.newRequest(restQuery(lat + "," + longitude, radius, item))
-            .method(HttpMethod.GET)
-            .send(
-                new AsyncRestRequest() {
+        client.newRequest(restQuery(lat + "," + longitude, radius, item)).method(HttpMethod.GET)
+                .send(new AsyncRestRequest() {
                   @Override
                   void onLocationFound(Map<String, Object> result) {
                     resultsQueue.add(result);
                   }
 
                   @Override
-                  void doComplete() {
+                  void onComplete() {
                     if (outstanding.decrementAndGet() <= 0) {
                       async.dispatch();
                     }
@@ -151,28 +131,20 @@ public class AsyncRestServlet extends AbstractRestServlet {
     long thread = initial + generate;
 
     String loc = sanitize(request.getParameter(LOC_PARAM));
-    out.print(
-        "<b>Asynchronous: Requesting " + sanitize(request.getParameter(ITEMS_PARAM)) + " near "
+    out.print("<b>Asynchronous: Requesting " + sanitize(request.getParameter(ITEMS_PARAM)) + " near "
             + (loc != null ? loc
                     : "lat=" + sanitize(request.getParameter(LATITUDE_PARAM)) + " long="
                     + sanitize(request.getParameter(LONGITUDE_PARAM)))
             + "</b><br/>");
     out.print("Total Time: " + ms(total) + "ms<br/>");
 
-    out.print(
-        "Thread held (<span class='red'>red</span>): "
-            + ms(thread) + "ms (" + ms(initial)
-            + " initial + " + ms(generate) + " generate )<br/>");
+    out.print("Thread held (<span class='red'>red</span>): " + ms(thread) + "ms (" + ms(initial) + " initial + "
+            + ms(generate) + " generate )<br/>");
     out.print("Async wait (<span class='green'>green</span>): " + ms(total - thread) + "ms<br/>");
 
-    out.println(
-        "<img border='0px' src='asyncrest/red.png' height='20px' width='" + width(initial) + "px'>"
-            + "<img border='0px' src='asyncrest/green.png'"
-            + " height='20px'"
-            + " width='" + width(total - thread) + "px'>"
-            + "<img border='0px' src='asyncrest/red.png'"
-            + " height='20px'"
-            + " width='" + width(generate) + "px'>");
+    out.println("<img border='0px' src='asyncrest/red.png'   height='20px' width='" + width(initial) + "px'>"
+            + "<img border='0px' src='asyncrest/green.png' height='20px' width='" + width(total - thread) + "px'>"
+            + "<img border='0px' src='asyncrest/red.png'   height='20px' width='" + width(generate) + "px'>");
 
     out.println("<br/>");
     out.print("First 5 results of " + results.size() + ":<br/>");
@@ -189,7 +161,7 @@ public class AsyncRestServlet extends AbstractRestServlet {
 
   private abstract class AsyncRestRequest extends Response.Listener.Adapter {
 
-    final Utf8StringBuilder utf8Content = new Utf8StringBuilder();
+    final Utf8StringBuilder _content = new Utf8StringBuilder();
 
     AsyncRestRequest() {
     }
@@ -197,13 +169,13 @@ public class AsyncRestServlet extends AbstractRestServlet {
     @Override
     public void onContent(Response response, ByteBuffer content) {
       byte[] bytes = BufferUtil.toArray(content);
-      utf8Content.append(bytes, 0, bytes.length);
+      _content.append(bytes, 0, bytes.length);
     }
 
     @Override
     public void onComplete(Result result) {
-      // Extract results.
-      Map<String, Object> data = (Map<String, Object>) JSON.parse(utf8Content.toString());
+      // extract results
+      Map<String, Object> data = (Map<String, Object>) JSON.parse(_content.toString());
       if (data != null) {
         Object[] results = (Object[]) data.get("results");
         if (results != null) {
@@ -212,13 +184,13 @@ public class AsyncRestServlet extends AbstractRestServlet {
           }
         }
       }
-      doComplete();
+      onComplete();
 
     }
 
     abstract void onLocationFound(Map<String, Object> details);
 
-    abstract void doComplete();
+    abstract void onComplete();
 
   }
 
