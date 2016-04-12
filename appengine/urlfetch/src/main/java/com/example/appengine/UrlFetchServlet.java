eafd2dc41426c4869ecc4b6271ea6d6a964647bc
Les Vogel <lesv@google.com>
2016-04-12 15:25:05 -0700

    Add a more complex example as well.

--- appengine/urlfetch/src/main/java/com/example/appengine/UrlFetchServlet.java
+++ appengine/urlfetch/src/main/java/com/example/appengine/UrlFetchServlet.java
@@ -18,40 +18,85 @@ package com.example.appengine;
 
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.URL;
-
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 @SuppressWarnings("serial")
 public class UrlFetchServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest req, HttpServletResponse resp)
-      throws IOException {
-    PrintWriter out = resp.getWriter();
-    out.println("<html><body>");
+      throws IOException, ServletException {
 
 // [START example]
     URL url = new URL("http://api.icndb.com/jokes/random");
     BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
-    String json = "";
+    StringBuffer json = new StringBuffer();
     String line;
 
     while ((line = reader.readLine()) != null) {
-      json += line;
+      json.append(line);
     }
     reader.close();
 // [END example]
-    JSONObject jo = new JSONObject(json);
-    out.println("<h2>"
-        + jo.getJSONObject("value").getString("joke")
-        + "</h2>");
-    out.println("</body></html>");
+    JSONObject jo = new JSONObject(json.toString());
+
+    req.setAttribute("joke", jo.getJSONObject("value").getString("joke"));
+    req.getRequestDispatcher("/main.jsp").forward(req, resp);
   }
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException, ServletException {
+
+    String id = req.getParameter("id");
+    String text = req.getParameter("text");
+
+    if (id == null || text == null || id == "" || text == "") {
+      req.setAttribute("error", "invalid input");
+      req.getRequestDispatcher("/main.jsp").forward(req,resp);
+      return;
+    }
+
+    JSONObject jsonObj = new JSONObject()
+        .put("userId", 33)
+        .put("id", id)
+        .put("title", text)
+        .put("body", text);
+
+    // [START complex]
+    URL url = new URL("http://jsonplaceholder.typicode.com/posts/"+id);
+    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+    conn.setDoOutput(true);
+    conn.setRequestMethod("PUT");
+
+    OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+    writer.write(URLEncoder.encode(jsonObj.toString(), "UTF-8"));
+    writer.close();
+
+    int respCode = conn.getResponseCode();  // New items get NOT_FOUND on PUT
+    if (respCode == HttpURLConnection.HTTP_OK || respCode == HttpURLConnection.HTTP_NOT_FOUND) {
+      req.setAttribute("error", "" );
+      StringBuffer response = new StringBuffer();
+      String line;
+
+      BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+      while ((line = reader.readLine()) != null) {
+        response.append(line);
+      }
+      reader.close();
+      req.setAttribute("response", response.toString());
+    } else {
+      req.setAttribute("error", conn.getResponseCode()+" "+conn.getResponseMessage());
+    }
+  // [END complex]
+    req.getRequestDispatcher("/main.jsp").forward(req, resp);
+  }
+
 }
