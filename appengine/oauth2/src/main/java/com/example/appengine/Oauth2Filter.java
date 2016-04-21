26167f26538b4232b783a7a82acf0132289eb78a
Les Vogel <lesv@google.com>
2016-04-20 17:55:27 -0700

    Oauth2 updates, Guestbook
    Oauth2 - Tim and Brett’s comments
    - Mostly nits
    - HelloServlet now uses User
    GuestBook - Initial checkin, updated README
    remove .gitignore

--- appengine/oauth2/src/main/java/com/example/appengine/Oauth2Filter.java
+++ appengine/oauth2/src/main/java/com/example/appengine/Oauth2Filter.java
@@ -13,18 +13,14 @@
  */
 package com.example.appengine;
 
-import static com.google.appengine.api.utils.SystemProperty.environment;
-
 import com.google.appengine.api.oauth.OAuthRequestException;
 import com.google.appengine.api.oauth.OAuthService;
 import com.google.appengine.api.oauth.OAuthServiceFactory;
 import com.google.appengine.api.oauth.OAuthServiceFailureException;
-import com.google.appengine.api.users.User;
 import com.google.appengine.api.utils.SystemProperty;
+import com.google.common.collect.ImmutableSet;
 
 import java.io.IOException;
-import java.util.HashSet;
-import java.util.Set;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -35,12 +31,14 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import static com.google.appengine.api.utils.SystemProperty.environment;
+
 /**
  *  Filter to verify that request has a "Authorization: Bearer xxxx" header,
  *  and check if xxxx is authorized to use this app.
  *
- *  Note - this is to demonstrate the OAuth2 APIs, as it is possible to lockdown some
- *  of your app's URL's using cloud console by adding service accounts to the project.
+ *  <p>Note - this is to demonstrate the OAuth2 APIs, as it is possible to lockdown some
+ *  of your app's URL's using cloud console by adding service accounts to the project.</p>
  */
 public class Oauth2Filter implements Filter {
 
@@ -53,23 +51,23 @@ public class Oauth2Filter implements Filter {
 
   // [START oauth2]
   @Override
-  public void doFilter(final ServletRequest servletReq, final ServletResponse servletResp,
-                             final FilterChain chain) throws IOException, ServletException {
+  public void doFilter(
+      final ServletRequest servletReq, final ServletResponse servletResp,  final FilterChain chain)
+      throws IOException, ServletException {
     final String scope = "https://www.googleapis.com/auth/userinfo.email";
-    Set<String> allowedClients = new HashSet<>();
+    ImmutableSet<String> allowedClients = new ImmutableSet.Builder<String>()
+        .add("407408718192.apps.googleusercontent.com")
+        .add("755878275993-j4k7emq6rlupctce1c28enpcrr50vfo1.apps.googleusercontent.com")
+        .build();
 
     HttpServletResponse resp = (HttpServletResponse) servletResp;
 
     OAuthService oauth = OAuthServiceFactory.getOAuthService();
 
-    allowedClients.add("407408718192.apps.googleusercontent.com"); // list of client ids to allow
-    allowedClients.add("755878275993-j4k7emq6rlupctce1c28enpcrr50vfo1.apps.googleusercontent.com");
-
     // Only check Oauth2 when in production, skip if run in development.
     SystemProperty.Environment.Value env = environment.value();
     if (env == SystemProperty.Environment.Value.Production) { // APIs only work in Production
       try {
-        User user = oauth.getCurrentUser(scope);
         String tokenAudience = oauth.getClientId(scope);
 
           // The line below is commented out for privacy.
