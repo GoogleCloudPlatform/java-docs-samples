faed5ad5f8b98d54bfe07d2b9791befa615a4f09
carieshmarie <leecaroline@google.com>
2016-05-08 13:10:02 -0700

    Update MailjetServlet.java (#231)
    Add region tags for documentation.

--- appengine/mailjet/src/main/java/com/example/appengine/mailjet/MailjetServlet.java
+++ appengine/mailjet/src/main/java/com/example/appengine/mailjet/MailjetServlet.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// [START mailjet_imports]
 package com.example.appengine.mailjet;
 
 import com.mailjet.client.MailjetClient;
@@ -21,6 +21,7 @@ import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.resource.Email;
+// [END mailjet_imports]
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,6 +33,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+// [START app]
 @SuppressWarnings("serial")
 public class MailjetServlet extends HttpServlet {
   private static final String MAILJET_API_KEY = System.getenv("MAILJET_API_KEY");
@@ -65,3 +67,4 @@ public class MailjetServlet extends HttpServlet {
     }
   }
 }
+// [END app]
