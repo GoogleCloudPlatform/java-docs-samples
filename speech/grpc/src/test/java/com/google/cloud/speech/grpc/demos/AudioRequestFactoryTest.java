80517281c2a7fe4f26f70060acbfec8af7d38b9a
Puneith Kaul <puneith@google.com>
2016-05-30 21:07:51 -0700

    added test file

---
+++ speech/grpc/src/test/java/com/google/cloud/speech/grpc/demos/AudioRequestFactoryTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.speech.grpc.demos;
+
+import com.google.cloud.speech.v1.AudioRequest;
+import static org.junit.Assert.assertSame;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * Unit tests for {@link AudioRequestFactory}.
+ */
+@RunWith(JUnit4.class)
+public class AudioRequestFactoryTest {
+
+  @Test
+  public void verifyBytesInSizeFromLocalFile() throws IOException {
+    URI uri = new File("speech/grpc/resources/audio.raw").toURI();
+    AudioRequest audio = AudioRequestFactory.createRequest(uri);
+
+    int numBytes = audio.getContent().toByteArray().length();
+
+    //assert the number of bytes in the audio as 57958
+    assertSame(numBytes, 57958);
+  }
+
+  @Test
+  public void verifyBytesInSizeFromGoogleStorageFile() throws IOException {
+    URI uri = URI.create("gs://cloud-samples-test/speech/audio.raw");
+    AudioRequest audio = AudioRequestFactory.createRequest(uri);
+
+    int numBytes = audio.getContent().toByteArray().length();
+
+    //assert the number of bytes in the audio as 57958
+    assertSame(numBytes, 57958);
+  }
+}
