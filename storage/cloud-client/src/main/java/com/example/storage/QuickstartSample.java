1f0c8a5df12f26320095bf20796e6baf0b85ee4d
Tim Swast <swast@google.com>
2016-10-25 16:19:28 -0700

    Add quickstarts to root module.
    Fixes checkstyle errors in samples.

--- storage/cloud-client/src/main/java/com/example/storage/QuickstartSample.java
+++ storage/cloud-client/src/main/java/com/example/storage/QuickstartSample.java
@@ -18,10 +18,10 @@ package com.example.storage;
 
 // [START storage_quickstart]
 // Imports the Google Cloud client library
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 
 public class QuickstartSample {
   public static void main(String... args) throws Exception {
