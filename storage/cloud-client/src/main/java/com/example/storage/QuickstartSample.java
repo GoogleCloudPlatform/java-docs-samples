5da7970f221cd4524e59deddc81b42795b1b4902
Tim Swast <swast@google.com>
2016-10-26 10:37:55 -0700

    Add system tests to Translate sample.

--- storage/cloud-client/src/main/java/com/example/storage/QuickstartSample.java
+++ storage/cloud-client/src/main/java/com/example/storage/QuickstartSample.java
@@ -29,7 +29,7 @@ public class QuickstartSample {
     Storage storage = StorageOptions.defaultInstance().service();
 
     // The name for the new bucket
-    String bucketName = args[0];
+    String bucketName = args[0];  // "my-new-bucket";
 
     // Creates the new bucket
     Bucket bucket = storage.create(BucketInfo.of(bucketName));
