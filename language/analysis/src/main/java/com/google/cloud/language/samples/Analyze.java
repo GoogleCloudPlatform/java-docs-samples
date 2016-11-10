80764fdc7a220c7409b8c1dc368b48cf78c4c808
Gus Class <class@google.com>
2016-11-10 12:26:14 -0800

    Adds part of speech morphologies

--- language/analysis/src/main/java/com/google/cloud/language/samples/Analyze.java
+++ language/analysis/src/main/java/com/google/cloud/language/samples/Analyze.java
@@ -137,6 +137,17 @@ public class Analyze {
       out.printf("\tBeginOffset: %d\n", token.getText().getBeginOffset());
       out.printf("Lemma: %s\n", token.getLemma());
       out.printf("PartOfSpeechTag: %s\n", token.getPartOfSpeech().getTag());
+      out.printf("\tAspect: %s\n",token.getPartOfSpeech().getAspect());
+      out.printf("\tCase: %s\n", token.getPartOfSpeech().getCase());
+      out.printf("\tForm: %s\n", token.getPartOfSpeech().getForm());
+      out.printf("\tGender: %s\n",token.getPartOfSpeech().getGender());
+      out.printf("\tMood: %s\n", token.getPartOfSpeech().getMood());
+      out.printf("\tNumber: %s\n", token.getPartOfSpeech().getNumber());
+      out.printf("\tPerson: %s\n", token.getPartOfSpeech().getPerson());
+      out.printf("\tProper: %s\n", token.getPartOfSpeech().getProper());
+      out.printf("\tReciprocity: %s\n", token.getPartOfSpeech().getReciprocity());
+      out.printf("\tTense: %s\n", token.getPartOfSpeech().getTense());
+      out.printf("\tVoice: %s\n", token.getPartOfSpeech().getVoice());
       out.println("DependencyEdge");
       out.printf("\tHeadTokenIndex: %d\n", token.getDependencyEdge().getHeadTokenIndex());
       out.printf("\tLabel: %s\n", token.getDependencyEdge().getLabel());
