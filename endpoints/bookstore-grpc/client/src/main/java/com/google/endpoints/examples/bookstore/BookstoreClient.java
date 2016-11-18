047e7be5a951baa99721abfafe83ea89683ca708
Wencheng Lu <wlu@google.com>
2016-11-17 17:01:49 -0800

    fix typo: logger -> LOGGER add gradle-wrapper.jar required for gradlew build

--- endpoints/bookstore-grpc/client/src/main/java/com/google/endpoints/examples/bookstore/BookstoreClient.java
+++ endpoints/bookstore-grpc/client/src/main/java/com/google/endpoints/examples/bookstore/BookstoreClient.java
@@ -156,14 +156,14 @@ public final class BookstoreClient {
     @Override
     public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
         MethodDescriptor<ReqT,RespT> method, CallOptions callOptions, Channel next) {
-      logger.info("Intercepted " + method.getFullMethodName());
+      LOGGER.info("Intercepted " + method.getFullMethodName());
       ClientCall<ReqT, RespT> call = next.newCall(method, callOptions);
 
       call = new ForwardingClientCall.SimpleForwardingClientCall<ReqT, RespT>(call) {
         @Override
         public void start(Listener<RespT> responseListener, Metadata headers) {
           if (apiKey != null && !apiKey.isEmpty()) {
-            logger.info("Attaching API Key: " + apiKey);
+            LOGGER.info("Attaching API Key: " + apiKey);
             headers.put(API_KEY_HEADER, apiKey);
           }
           if (authToken != null && !authToken.isEmpty()) {
