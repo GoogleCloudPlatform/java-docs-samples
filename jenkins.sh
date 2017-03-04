d549a57e53bf18f84377095631da792c9fcd4ee4
Jerjou Cheng <jerjou@google.com>
2017-03-03 21:22:47 -0800

    Disable some shellchecks.

--- jenkins.sh
+++ jenkins.sh
@@ -19,6 +19,7 @@ shopt -s globstar
 
 app_version=""
 
+# shellcheck disable=SC2120
 delete_app_version() {
   if [ -n "${app_version}" ] || [ $# -gt 0 ]; then
     yes | gcloud --project="${GOOGLE_PROJECT_ID}" \
@@ -29,7 +30,7 @@ handle_error() {
   errcode=$? # Remember the error code so we can exit with it after cleanup
 
   # Clean up
-  delete_app_version
+  delete_app_version # shellcheck disable=SC2119
 
   exit ${errcode}
 }
