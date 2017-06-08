32b0b70eaa5d60c0476ef621721a2d923747fc64
Jisha Abubaker <jishaa@google.com>
2017-06-08 10:26:22 -0700

    Iap sample using jjwt (#694)
    * switching to jjwt library
    * adding known issues to README reg auth0 jwt on OpenJDK.

--- iap/src/test/java/com/example/iap/BuildAndVerifyIapRequestIT.java
+++ iap/src/test/java/com/example/iap/BuildAndVerifyIapRequestIT.java
@@ -1,32 +1,29 @@
 /**
  * Copyright 2017 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.iap;
 
 import static com.example.iap.BuildIapRequest.buildIAPRequest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.google.api.client.http.GenericUrl;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
 import com.google.api.client.http.HttpResponseException;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import io.jsonwebtoken.Jwt;
 import org.apache.http.HttpStatus;
 import org.junit.Before;
 import org.junit.Test;
@@ -70,7 +67,7 @@ public class BuildAndVerifyIapRequestIT {
     assertNotNull(split);
     assertEquals(split.length, 2);
     assertEquals(split[0].trim(), "x-goog-authenticated-user-jwt");
-    DecodedJWT decodedJWT = verifyIapRequestHeader.verifyJWTToken(split[1].trim(), iapProtectedUrl);
+    Jwt decodedJWT = verifyIapRequestHeader.verifyJWTToken(split[1].trim(), iapProtectedUrl);
     assertNotNull(decodedJWT);
   }
 }
