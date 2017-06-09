86f851284cefbbc14199525c78e85dbd94b2f5a9
Lawrence L <llatif@users.noreply.github.com>
2017-06-09 14:37:59 -0700

    Region tags for documentation

--- appengine/taskqueue-push/src/main/java/com/example/appengine/taskqueue/push/Enqueue.java
+++ appengine/taskqueue-push/src/main/java/com/example/appengine/taskqueue/push/Enqueue.java
@@ -14,9 +14,11 @@
  */
 package com.example.appengine.taskqueue.push;
 
+// [START import]
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
 import com.google.appengine.api.taskqueue.TaskOptions;
+// [END import]
 
 import java.io.IOException;
 
@@ -33,9 +35,11 @@ public class Enqueue extends HttpServlet {
     String key = request.getParameter("key");
 
     // Add the task to the default queue.
+    // [START addQueue]
     Queue queue = QueueFactory.getDefaultQueue();
     queue.add(TaskOptions.Builder.withUrl("/worker").param("key", key));
-
+    // [END addQueue]
+    
     response.sendRedirect("/");
   }
 }
