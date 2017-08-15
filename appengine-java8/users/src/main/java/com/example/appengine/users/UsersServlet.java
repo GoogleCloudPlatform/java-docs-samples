c12ba1013f2368571465da34f07d8b1d50361a9b
Les Vogel <lesv@users.noreply.github.com>
2017-08-15 16:56:13 -0700

    fix a comment (#810)
    1. fix poster comment
    2. run through google-java-format, (wasn’t totally happy)

--- appengine-java8/users/src/main/java/com/example/appengine/users/UsersServlet.java
+++ appengine-java8/users/src/main/java/com/example/appengine/users/UsersServlet.java
@@ -18,38 +18,39 @@ package com.example.appengine.users;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
-
 import java.io.IOException;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 // With @WebServlet annotation the webapp/WEB-INF/web.xml is no longer required.
-@WebServlet(name = "UserAPI", description = "UserAPI: Login / Logout with UserService",
-    urlPatterns = "/userapi")
+@WebServlet(
+  name = "UserAPI",
+  description = "UserAPI: Login / Logout with UserService",
+  urlPatterns = "/userapi"
+)
 public class UsersServlet extends HttpServlet {
   @Override
-  public void doGet(HttpServletRequest req, HttpServletResponse resp)
-        throws IOException {
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
     UserService userService = UserServiceFactory.getUserService();
 
     String thisUrl = req.getRequestURI();
 
     resp.setContentType("text/html");
     if (req.getUserPrincipal() != null) {
-      resp.getWriter().println("<p>Hello, "
-          + req.getUserPrincipal().getName()
-          + "!  You can <a href=\""
-          + userService.createLogoutURL(thisUrl)
-          + "\">sign out</a>.</p>");
+      resp.getWriter()
+          .println(
+              "<p>Hello, "
+                  + req.getUserPrincipal().getName()
+                  + "!  You can <a href=\""
+                  + userService.createLogoutURL(thisUrl)
+                  + "\">sign out</a>.</p>");
     } else {
-      resp.getWriter().println("<p>Please <a href=\""
-            + userService.createLoginURL(thisUrl)
-            + "\">sign in</a>.</p>");
+      resp.getWriter()
+          .println(
+              "<p>Please <a href=\"" + userService.createLoginURL(thisUrl) + "\">sign in</a>.</p>");
     }
   }
 }
 // [END users_API_example]
-
