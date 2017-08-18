513173a7bf768c32e2f1cbe0a4559b1e10c4424c
Atanas Damyanliev <adamyanliev@mailjet.com>
2017-08-18 22:07:00 +0300

    updating Mailjet code samples for Send API v3.1 (#811)
    * update MailjetServlet.java
    
    updating MailjetServlet.java to match Send API v3.1 configuration
    
    * update MailjetServlet.java
    
    updated MailjetServlet.java to match Send API v3.1 configuration
    
    * update MailjetServlet.java
    
    updated to match Send API v3.1 config
    
    * fixing spacing
    
    * fixing spacing
    
    * fixing spacing

--- flexible/mailjet/src/main/java/com/example/mailjet/MailjetServlet.java
+++ flexible/mailjet/src/main/java/com/example/mailjet/MailjetServlet.java
@@ -22,7 +22,8 @@ import com.mailjet.client.MailjetRequest;
 import com.mailjet.client.MailjetResponse;
 import com.mailjet.client.errors.MailjetException;
 import com.mailjet.client.errors.MailjetSocketTimeoutException;
-import com.mailjet.client.resource.Email;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.resource.Emailv31;
 // [END mailjet_imports]
 
 import org.json.JSONArray;
@@ -42,7 +43,7 @@ import javax.servlet.http.HttpServletResponse;
 public class MailjetServlet extends HttpServlet {
   private static final String MAILJET_API_KEY = System.getenv("MAILJET_API_KEY");
   private static final String MAILJET_SECRET_KEY = System.getenv("MAILJET_SECRET_KEY");
-  private MailjetClient client = new MailjetClient(MAILJET_API_KEY, MAILJET_SECRET_KEY);
+  private MailjetClient client = new MailjetClient(MAILJET_API_KEY, MAILJET_SECRET_KEY, new ClientOptions("v3.1"));
 
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException,
@@ -50,15 +51,18 @@ public class MailjetServlet extends HttpServlet {
     String recipient = req.getParameter("to");
     String sender = req.getParameter("from");
 
-    MailjetRequest email = new MailjetRequest(Email.resource)
-        .property(Email.FROMEMAIL, sender)
-        .property(Email.FROMNAME, "pandora")
-        .property(Email.SUBJECT, "Your email flight plan!")
-        .property(Email.TEXTPART,
-            "Dear passenger, welcome to Mailjet! May the delivery force be with you!")
-        .property(Email.HTMLPART,
-            "<h3>Dear passenger, welcome to Mailjet!</h3><br/>May the delivery force be with you!")
-        .property(Email.RECIPIENTS, new JSONArray().put(new JSONObject().put("Email", recipient)));
+    MailjetRequest email = new MailjetRequest(Emailv31.resource)
+      .property(Emailv31.MESSAGES, new JSONArray()
+        .put(new JSONObject()
+          .put(Emailv31.Message.FROM, new JSONObject()
+            .put("Email", sender)
+            .put("Name", "Mailjet Pilot"))
+          .put(Emailv31.Message.TO, new JSONArray()
+            .put(new JSONObject()
+              .put("Email", recipient)))
+          .put(Emailv31.Message.SUBJECT, "Your email flight plan!")
+          .put(Emailv31.Message.TEXTPART, "Dear passenger, welcome to Mailjet! May the delivery force be with you!")
+          .put(Emailv31.Message.HTMLPART, "<h3>Dear passenger, welcome to Mailjet!</h3><br />May the delivery force be with you!")));
 
     try {
       // trigger the API call
