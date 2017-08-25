c79fdb03186e22407983d8778eb1873125a2cb8b
Jisha Abubaker <jishaa@google.com>
2017-08-25 14:46:17 -0700

    typo fix

--- dlp/src/test/java/com/example/dlp/MetadataIT.java
+++ dlp/src/test/java/com/example/dlp/MetadataIT.java
@@ -53,7 +53,7 @@ public class MetadataIT {
   public void testInfoTypesAreRetrieved() throws Exception {
     Metadata.main(new String[] {"-category", "GOVERNMENT"});
     String output = bout.toString();
-    assertTrue(output.contains("Name") && output.contains("Display name")));
+    assertTrue(output.contains("Name") && output.contains("Display name"));
   }
 
   @After
