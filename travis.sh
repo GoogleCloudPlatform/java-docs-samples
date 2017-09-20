51a80cdf2156a0252e081ba8b29180ab8e04b401
Les Vogel <lesv@google.com>
2017-09-19 19:00:36 -0700

    fixup stuff.
    1. Bigtable uses 1.1.33.Final
    2. Bigtable should use 1.0.0-pre3
    3. Try and fix logging
    4. remove a bunch of <prerequsites> (which is both required and annoys
    Maven - the versions plugin is confused, recommends using enforcer,
    then doesn’t care.
    5. update to guava 23.0 where possible.
    6. requires ignoring guava-jdk5
    7. Storage sample would never compile.
    8. add —fail-at-end so we see more of what’s wrong.

--- travis.sh
+++ travis.sh
@@ -79,7 +79,7 @@ echo "Common Dir: ${common_dir}"
 
 # Give Maven a bit more memory
 export MAVEN_OPTS='-Xmx800m -Xms400m'
-mvn --batch-mode clean verify -e \
+mvn --batch-mode --fail-at-end clean verify -e \
     -DskipTests=$SKIP_TESTS \
     -Dbigtable.projectID="${GOOGLE_CLOUD_PROJECT}" \
     -Dbigtable.instanceID=instance | \
