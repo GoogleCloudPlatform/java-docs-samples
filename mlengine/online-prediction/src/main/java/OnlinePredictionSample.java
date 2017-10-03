e98c820ca1cfaa249976a185a82d19779ee81ddb
Lun-Kai Hsu <lunkai@google.com>
2017-10-03 20:20:08 +0000

    add licence and some comments

--- mlengine/online-prediction/src/main/java/OnlinePredictionSample.java
+++ mlengine/online-prediction/src/main/java/OnlinePredictionSample.java
@@ -1,5 +1,17 @@
 /*
- * Sample code for doing Cloud Machine Learning Engine online prediction in Java.
+ * Copyright 2017 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
@@ -19,6 +31,10 @@ import com.google.api.services.discovery.model.RestDescription;
 import com.google.api.services.discovery.model.RestMethod;
 import java.io.File;
 
+/*
+ * Sample code for doing Cloud Machine Learning Engine online prediction in Java.
+ */
+
 public class OnlinePredictionSample {
   public static void main(String[] args) throws Exception {
     HttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
@@ -30,6 +46,8 @@ public class OnlinePredictionSample {
 
     JsonSchema param = new JsonSchema();
     String projectId = "YOUR_PROJECT_ID";
+    // You should have already deployed a model and a version.
+    // For reference, see https://cloud.google.com/ml-engine/docs/how-tos/deploying-models.
     String modelId = "YOUR_MODEL_ID";
     String versionId = "YOUR_VERSION_ID";
     param.set(
