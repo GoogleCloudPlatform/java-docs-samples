e33f93042a6434938ea0436a0bf8762ebb4645e9
Jisha Abubaker <jishaa@google.com>
2017-11-22 08:12:23 -0800

    updating pub/sub samples

--- pubsub/cloud-client/src/main/java/com/example/pubsub/PublisherExample.java
+++ pubsub/cloud-client/src/main/java/com/example/pubsub/PublisherExample.java
@@ -18,52 +18,67 @@ package com.example.pubsub;
 // [START pubsub_quickstart_publisher]
 
 import com.google.api.core.ApiFuture;
+import com.google.api.core.ApiFutureCallback;
 import com.google.api.core.ApiFutures;
+import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.ServiceOptions;
 import com.google.cloud.pubsub.v1.Publisher;
 import com.google.protobuf.ByteString;
 import com.google.pubsub.v1.PubsubMessage;
 import com.google.pubsub.v1.TopicName;
-import java.util.ArrayList;
-import java.util.List;
 
 public class PublisherExample {
-
-  static final int MESSAGE_COUNT = 5;
 
   // use the default project id
   private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
 
-  //schedule a message to be published, messages are automatically batched
-  private static ApiFuture<String> publishMessage(Publisher publisher, String message)
-      throws Exception {
-    // convert message to bytes
-    ByteString data = ByteString.copyFromUtf8(message);
-    PubsubMessage pubsubMessage = PubsubMessage.newBuilder().setData(data).build();
-    return publisher.publish(pubsubMessage);
-  }
-
-  /** Publish messages to a topic. */
+  /** Publish messages to a topic.
+   * @param args topic name, number of messages
+   */
   public static void main(String... args) throws Exception {
     // topic id, eg. "my-topic"
     String topicId = args[0];
-    TopicName topicName = TopicName.create(PROJECT_ID, topicId);
+    int messageCount = Integer.parseInt(args[1]);
+    TopicName topicName = TopicName.of(PROJECT_ID, topicId);
     Publisher publisher = null;
-    List<ApiFuture<String>> apiFutures = new ArrayList<>();
     try {
       // Create a publisher instance with default settings bound to the topic
-      publisher = Publisher.defaultBuilder(topicName).build();
-      for (int i = 0; i < MESSAGE_COUNT; i++) {
+      publisher = Publisher.newBuilder(topicName).build();
+
+      for (int i = 0; i < messageCount; i++) {
         String message = "message-" + i;
-        ApiFuture<String> messageId = publishMessage(publisher, message);
-        apiFutures.add(messageId);
+
+        // convert message to bytes
+        ByteString data = ByteString.copyFromUtf8(message);
+        PubsubMessage pubsubMessage = PubsubMessage.newBuilder()
+            .setData(data)
+            .build();
+
+        //schedule a message to be published, messages are automatically batched
+        ApiFuture<String> future = publisher.publish(pubsubMessage);
+
+        // add an asynchronous callback to handle success / failure
+        ApiFutures.addCallback(future, new ApiFutureCallback<String>() {
+
+          @Override
+          public void onFailure(Throwable throwable) {
+            if (throwable instanceof ApiException) {
+              ApiException apiException = ((ApiException) throwable);
+              // details on the API exception
+              System.out.println(apiException.getStatusCode().getCode());
+              System.out.println(apiException.isRetryable());
+            }
+            System.out.println("Error publishing message : " + message);
+          }
+
+          @Override
+          public void onSuccess(String messageId) {
+            // Once published, returns server-assigned message ids (unique within the topic)
+            System.out.println(messageId);
+          }
+        });
       }
     } finally {
-      // Once published, returns server-assigned message ids (unique within the topic)
-      List<String> messageIds = ApiFutures.allAsList(apiFutures).get();
-      for (String messageId : messageIds) {
-        System.out.println(messageId);
-      }
       if (publisher != null) {
         // When finished with the publisher, shutdown to free up resources.
         publisher.shutdown();
