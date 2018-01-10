c3536c45155a7b1703777c908cdc6e84c3b004d8
Kurtis Van Gent <kvg@google.com>
2018-01-10 11:24:08 -0800

    Updated datastore.

--- datastore/cloud-client/src/test/java/com/example/datastore/QuickstartSampleIT.java
+++ datastore/cloud-client/src/test/java/com/example/datastore/QuickstartSampleIT.java
@@ -21,14 +21,13 @@ import static com.google.common.truth.Truth.assertThat;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.Key;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 
 /**
  * Tests for quickstart sample.
