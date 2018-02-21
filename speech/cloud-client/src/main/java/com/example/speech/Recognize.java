053293614d644e382e227b9d21e10994b52c070c
Noah Negrey <nnegrey@google.com>
2018-02-21 15:25:49 -0800

    Update streamingRecognizeFile with try-catch based on PR feedback

--- speech/cloud-client/src/main/java/com/example/speech/Recognize.java
+++ speech/cloud-client/src/main/java/com/example/speech/Recognize.java
@@ -342,78 +342,78 @@ public class Recognize {
     byte[] data = Files.readAllBytes(path);
 
     // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
-    SpeechClient speech = SpeechClient.create();
+    try (SpeechClient speech = SpeechClient.create()) {
 
-    // Configure request with local raw PCM audio
-    RecognitionConfig recConfig = RecognitionConfig.newBuilder()
-        .setEncoding(AudioEncoding.LINEAR16)
-        .setLanguageCode("en-US")
-        .setSampleRateHertz(16000)
-        .build();
-    StreamingRecognitionConfig config = StreamingRecognitionConfig.newBuilder()
-        .setConfig(recConfig)
-        .build();
+      // Configure request with local raw PCM audio
+      RecognitionConfig recConfig = RecognitionConfig.newBuilder()
+          .setEncoding(AudioEncoding.LINEAR16)
+          .setLanguageCode("en-US")
+          .setSampleRateHertz(16000)
+          .build();
+      StreamingRecognitionConfig config = StreamingRecognitionConfig.newBuilder()
+          .setConfig(recConfig)
+          .build();
 
-    class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
-      private final SettableFuture<List<T>> future = SettableFuture.create();
-      private final List<T> messages = new java.util.ArrayList<T>();
+      class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
+        private final SettableFuture<List<T>> future = SettableFuture.create();
+        private final List<T> messages = new java.util.ArrayList<T>();
 
-      @Override
-      public void onNext(T message) {
-        messages.add(message);
+        @Override
+        public void onNext(T message) {
+          messages.add(message);
+        }
+
+        @Override
+        public void onError(Throwable t) {
+          future.setException(t);
+        }
+
+        @Override
+        public void onCompleted() {
+          future.set(messages);
+        }
+
+        // Returns the SettableFuture object to get received messages / exceptions.
+        public SettableFuture<List<T>> future() {
+          return future;
+        }
       }
 
-      @Override
-      public void onError(Throwable t) {
-        future.setException(t);
-      }
+      ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
+          new ResponseApiStreamingObserver<>();
 
-      @Override
-      public void onCompleted() {
-        future.set(messages);
-      }
+      BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable =
+          speech.streamingRecognizeCallable();
 
-      // Returns the SettableFuture object to get received messages / exceptions.
-      public SettableFuture<List<T>> future() {
-        return future;
+      ApiStreamObserver<StreamingRecognizeRequest> requestObserver =
+          callable.bidiStreamingCall(responseObserver);
+
+      // The first request must **only** contain the audio configuration:
+      requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
+          .setStreamingConfig(config)
+          .build());
+
+      // Subsequent requests must **only** contain the audio data.
+      requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
+          .setAudioContent(ByteString.copyFrom(data))
+          .build());
+
+      // Mark transmission as completed after sending the data.
+      requestObserver.onCompleted();
+
+      List<StreamingRecognizeResponse> responses = responseObserver.future().get();
+
+      for (StreamingRecognizeResponse response : responses) {
+        // For streaming recognize, the results list has one is_final result (if available) followed
+        // by a number of in-progress results (if iterim_results is true) for subsequent utterances.
+        // Just print the first result here.
+        StreamingRecognitionResult result = response.getResultsList().get(0);
+        // There can be several alternative transcripts for a given chunk of speech. Just use the
+        // first (most likely) one here.
+        SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+        System.out.printf("Transcript : %s\n", alternative.getTranscript());
       }
     }
-
-    ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
-        new ResponseApiStreamingObserver<>();
-
-    BidiStreamingCallable<StreamingRecognizeRequest,StreamingRecognizeResponse> callable =
-        speech.streamingRecognizeCallable();
-
-    ApiStreamObserver<StreamingRecognizeRequest> requestObserver =
-        callable.bidiStreamingCall(responseObserver);
-
-    // The first request must **only** contain the audio configuration:
-    requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
-        .setStreamingConfig(config)
-        .build());
-
-    // Subsequent requests must **only** contain the audio data.
-    requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
-        .setAudioContent(ByteString.copyFrom(data))
-        .build());
-
-    // Mark transmission as completed after sending the data.
-    requestObserver.onCompleted();
-
-    List<StreamingRecognizeResponse> responses = responseObserver.future().get();
-
-    for (StreamingRecognizeResponse response: responses) {
-      // For streaming recognize, the results list has one is_final result (if available) followed
-      // by a number of in-progress results (if iterim_results is true) for subsequent utterances.
-      // Just print the first result here.
-      StreamingRecognitionResult result = response.getResultsList().get(0);
-      // There can be several alternative transcripts for a given chunk of speech. Just use the
-      // first (most likely) one here.
-      SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-      System.out.printf("Transcript : %s\n", alternative.getTranscript());
-    }
-    speech.close();
   }
 
   // [START speech_transcribe_model_selection]
