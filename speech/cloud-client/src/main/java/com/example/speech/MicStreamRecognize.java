6d0641e6a5b1e701750dca48fb4a192fe067b906
nirupa-kumar <nirupa.kumar@gmail.com>
2018-06-14 10:48:38 -0700

    Add streaming microphone sample for Speech - multi-threaded

--- speech/cloud-client/src/main/java/com/example/speech/MicStreamRecognize.java
+++ speech/cloud-client/src/main/java/com/example/speech/MicStreamRecognize.java
@@ -16,8 +16,8 @@
 
 package com.example.speech;
 
-//Imports the Google Cloud client library
-
+// [START speech_mic_streaming]
+// Imports the Google Cloud client library
 import com.google.api.gax.rpc.ApiStreamObserver;
 import com.google.api.gax.rpc.BidiStreamingCallable;
 import com.google.cloud.speech.v1p1beta1.RecognitionConfig;
@@ -28,6 +28,7 @@ import com.google.cloud.speech.v1p1beta1.StreamingRecognitionConfig;
 import com.google.cloud.speech.v1p1beta1.StreamingRecognitionResult;
 import com.google.cloud.speech.v1p1beta1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1p1beta1.StreamingRecognizeResponse;
+
 import com.google.common.util.concurrent.SettableFuture;
 import com.google.protobuf.ByteString;
 
@@ -35,26 +36,31 @@ import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.TargetDataLine;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.List;
 
 /**
  * Google Cloud SpeechToText API sample application. Example usage: mvn package exec:java
- * -Dexec.mainClass='com.example.speech.MicStreamRecognize' -Dexec.args="micstreamrecognize <duration>"
+ * -Dexec.mainClass='com.example.speech.MicStreamRecognize' -Dexec.args="micstreamrecognize
+ * <duration>"
  */
 public class MicStreamRecognize {
 
-  // [START speech_mic_streaming]
   /**
    * Demonstrates using the Speech to Text client to convert Microphone streaming speech to text.
    *
    * @throws Exception on SpeechToTextClient Errors.
    */
+  // Microphone audio format specification
+  private static AudioFormat format = new AudioFormat(16000, 16, 1, true, false);
 
-  private static final int BYTES_PER_BUFFER = 6400; //buffer size in bytes
-
+  private static DataLine.Info targetInfo = new DataLine.Info(TargetDataLine.class, format);
+  private static TargetDataLine targetLine;
+  private static int BYTES_PER_BUFFER = 6400; // buffer size in bytes
+  // Creating shared object
+  private static volatile BlockingQueue<byte[]> sharedQueue = new LinkedBlockingQueue();
 
   public static void main(String... args) throws Exception {
     if (args.length < 1) {
@@ -81,113 +87,127 @@ public class MicStreamRecognize {
    *
    * @param duration the time duration for the microphone streaming
    */
-  public static void micRecognize(Integer duration) throws InterruptedException, IOException {
-      //Microphone audio format specification
-      AudioFormat format = new AudioFormat(16000, 16, 1, true, false);
-      DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-      // checks if system supports the data line
-      if (!AudioSystem.isLineSupported(info)) {
-          System.out.println("Line not supported");
-          System.exit(0);
-      }
-      byte[] data = new byte[BYTES_PER_BUFFER];
+  public static void micRecognize(Integer duration) throws Exception {
+    // Creating microphone input buffer thread
+    micBuffer micrunnable = new micBuffer();
+    Thread micThread = new Thread(micrunnable);
+    int durationMillSec = duration * 1000;
+    if (!AudioSystem.isLineSupported(targetInfo)) {
+      System.out.println("Microphone not supported");
+      System.exit(0);
+    }
+    // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
+    try (SpeechClient speech = SpeechClient.create()) {
 
-      // Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
-      try (SpeechClient speech = SpeechClient.create()) {
-
-          //Configure request with microphone audio
-          RecognitionConfig recConfig = RecognitionConfig.newBuilder()
+      // Configure request with local raw PCM audio
+      RecognitionConfig recConfig =
+          RecognitionConfig.newBuilder()
               .setEncoding(AudioEncoding.LINEAR16)
               .setLanguageCode("en-US")
               .setSampleRateHertz(16000)
               .setModel("default")
               .build();
-          StreamingRecognitionConfig config = StreamingRecognitionConfig.newBuilder()
-              .setConfig(recConfig)
-                  .setInterimResults(true)
-              .build();
+      StreamingRecognitionConfig config =
+          StreamingRecognitionConfig.newBuilder().setConfig(recConfig).build();
 
-          class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
-              private final SettableFuture<List<T>> future = SettableFuture.create();
-              private final List<T> messages = new java.util.ArrayList<T>();
+      class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
+        private final SettableFuture<List<T>> future = SettableFuture.create();
+        private final List<T> messages = new java.util.ArrayList<T>();
 
-              @Override
-              public void onNext(T message) { messages.add(message); }
+        @Override
+        public void onNext(T message) {
+          messages.add(message);
+        }
 
-              @Override
-              public void onError(Throwable t) {
-                  future.setException(t);
-              }
+        @Override
+        public void onError(Throwable t) {
+          future.setException(t);
+        }
 
-              @Override
-              public void onCompleted() { future.set(messages); }
+        @Override
+        public void onCompleted() {
+          future.set(messages);
+        }
 
-              // Returns the SettableFuture object to get received messages / exceptions.
-              public SettableFuture<List<T>> future() {
-                  return future;
-              }
-          }
-
-          ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
-              new ResponseApiStreamingObserver<>();
-
-          BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable =
-              speech.streamingRecognizeCallable();
-
-          ApiStreamObserver<StreamingRecognizeRequest> requestObserver =
-              callable.bidiStreamingCall(responseObserver);
-
-          TargetDataLine targetLine = (TargetDataLine) AudioSystem.getLine(info);
-          targetLine.open(format);
-
-          long startTime = System.currentTimeMillis();
-          ByteArrayOutputStream out  = new ByteArrayOutputStream();
-
-          // The first request must **only** contain the audio configuration:
-          requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
-              .setStreamingConfig(config)
-              .build());
-          System.out.println("Start speaking...");
-
-          while (true) {
-              targetLine.start();
-              int numBytesRead = targetLine.read(data, 0, data.length);
-
-              if (numBytesRead <= 0) continue;
-
-              out.write(data,0,data.length);
-              // Subsequent requests must **only** contain the audio data.
-              requestObserver.onNext(StreamingRecognizeRequest.newBuilder()
-                  .setAudioContent(ByteString.copyFrom(out.toByteArray()))
-                  .build());
-              Thread.sleep(1000);
-              long estimatedTime = System.currentTimeMillis() - startTime;
-              out.flush();
-              if (estimatedTime > (duration*1000)) {
-                  targetLine.stop();
-                  targetLine.close();
-                  out.close();
-                  System.out.println("Stop speaking");
-                  break;
-              }
-          }
-          // Mark transmission as completed after sending the data.
-          requestObserver.onCompleted();
-          List<StreamingRecognizeResponse> responses = responseObserver.future().get();
-          for (StreamingRecognizeResponse response : responses) {
-              // For streaming recognize, the results list has one is_final result (if available) followed
-              // by a number of in-progress results (if interim_results is true) for subsequent utterances.
-              // Just print the first result here.
-              StreamingRecognitionResult result = response.getResultsList().get(0);
-              // There can be several alternative transcripts for a given chunk of speech. Just use the
-              // first (most likely) one here.
-              SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-              System.out.printf("Transcript : %s\n", alternative.getTranscript());
-          }
-
-      } catch (Exception e) {
-          System.err.println(e);
+        // Returns the SettableFuture object to get received messages / exceptions.
+        public SettableFuture<List<T>> future() {
+          return future;
+        }
       }
+
+      ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
+          new ResponseApiStreamingObserver<>();
+
+      BidiStreamingCallable<StreamingRecognizeRequest, StreamingRecognizeResponse> callable =
+          speech.streamingRecognizeCallable();
+
+      ApiStreamObserver<StreamingRecognizeRequest> requestObserver =
+          callable.bidiStreamingCall(responseObserver);
+      targetLine = (TargetDataLine) AudioSystem.getLine(targetInfo);
+      targetLine.open(format);
+
+      // The first request must **only** contain the audio configuration:
+      requestObserver.onNext(
+          StreamingRecognizeRequest.newBuilder().setStreamingConfig(config).build());
+      micThread.start();
+      try {
+        long startTime = System.currentTimeMillis();
+        while (true) {
+          Thread.sleep(100);
+          long estimatedTime = System.currentTimeMillis() - startTime;
+          if (estimatedTime > durationMillSec) {
+            System.out.println("Stop speaking.");
+            targetLine.stop();
+            targetLine.close();
+            break;
+          }
+          // Subsequent requests must **only** contain the audio data.
+          requestObserver.onNext(
+              StreamingRecognizeRequest.newBuilder()
+                  .setAudioContent(ByteString.copyFrom(sharedQueue.take()))
+                  .build());
+        }
+      } catch (Exception e) {
+        System.out.println("Error in MicrophoneStreamRecognize : " + e.getMessage());
+      }
+      // Mark transmission as completed after sending the data.
+      requestObserver.onCompleted();
+
+      List<StreamingRecognizeResponse> responses = responseObserver.future().get();
+
+      for (StreamingRecognizeResponse response : responses) {
+        // For streaming recognize, the results list has one is_final result (if available) followed
+        // by a number of in-progress results (if interim_results is true) for subsequent
+        // utterances.
+        // Just print the first result here.
+        StreamingRecognitionResult result = response.getResultsList().get(0);
+        // There can be several alternative transcripts for a given chunk of speech. Just use the
+        // first (most likely) one here.
+        SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+        System.out.printf("Transcript : %s\n", alternative.getTranscript());
+      }
+    }
   }
 
+  // Microphone Input buffering
+  static class micBuffer implements Runnable {
+
+    @Override
+    public void run() {
+      System.out.println("Start speaking...");
+      targetLine.start();
+      byte[] data = new byte[BYTES_PER_BUFFER];
+      while (targetLine.isOpen()) {
+        try {
+          int numBytesRead = targetLine.read(data, 0, data.length);
+          if (numBytesRead <= 0) continue;
+          sharedQueue.put(data.clone());
+
+        } catch (InterruptedException e) {
+          System.out.println("Microphone input buffering interrupted : " + e.getMessage());
+        }
+      }
+    }
+  }
+  // [END speech_mic_streaming]
 }
