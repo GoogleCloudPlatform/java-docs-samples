1f3c4c1ba63c25fad27f615d40b29f1ef11e679c
nirupa-kumar <nirupakumar@google.com>
2018-06-18 15:35:10 -0700

    Fixing as per pull request review comments

--- speech/cloud-client/src/main/java/com/example/speech/MicStreamRecognize.java
+++ speech/cloud-client/src/main/java/com/example/speech/MicStreamRecognize.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google Inc.
+ * Copyright 2018 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,31 +110,6 @@ public class MicStreamRecognize {
       StreamingRecognitionConfig config =
           StreamingRecognitionConfig.newBuilder().setConfig(recConfig).build();
 
-      class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
-        private final SettableFuture<List<T>> future = SettableFuture.create();
-        private final List<T> messages = new java.util.ArrayList<T>();
-
-        @Override
-        public void onNext(T message) {
-          messages.add(message);
-        }
-
-        @Override
-        public void onError(Throwable t) {
-          future.setException(t);
-        }
-
-        @Override
-        public void onCompleted() {
-          future.set(messages);
-        }
-
-        // Returns the SettableFuture object to get received messages / exceptions.
-        public SettableFuture<List<T>> future() {
-          return future;
-        }
-      }
-
       ResponseApiStreamingObserver<StreamingRecognizeResponse> responseObserver =
           new ResponseApiStreamingObserver<>();
 
@@ -186,6 +161,31 @@ public class MicStreamRecognize {
         SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
         System.out.printf("Transcript : %s\n", alternative.getTranscript());
       }
+    }
+  }
+
+  static class ResponseApiStreamingObserver<T> implements ApiStreamObserver<T> {
+    private final SettableFuture<List<T>> future = SettableFuture.create();
+    private final List<T> messages = new java.util.ArrayList<T>();
+
+    @Override
+    public void onNext(T message) {
+      messages.add(message);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+      future.setException(t);
+    }
+
+    @Override
+    public void onCompleted() {
+      future.set(messages);
+    }
+
+    // Returns the SettableFuture object to get received messages / exceptions.
+    public SettableFuture<List<T>> future() {
+      return future;
     }
   }
 
