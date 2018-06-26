93b9d6a13790c488d449ef9f71cf5a0087a478ec
xinyunh0929 <xinyunh@google.com>
2018-06-26 10:56:46 -0700

    separate the sample tests

--- jobs/cjd_sample/src/main/java/com/google/samples/CommuteSearchSample.java
+++ jobs/cjd_sample/src/main/java/com/google/samples/CommuteSearchSample.java
@@ -17,58 +17,30 @@
 package com.google.samples;
 
 import com.google.api.services.jobs.v2.JobService;
+import com.google.api.services.jobs.v2.model.CommutePreference;
 import com.google.api.services.jobs.v2.model.Company;
 import com.google.api.services.jobs.v2.model.Job;
 import com.google.api.services.jobs.v2.model.JobQuery;
+import com.google.api.services.jobs.v2.model.LatLng;
 import com.google.api.services.jobs.v2.model.RequestMetadata;
 import com.google.api.services.jobs.v2.model.SearchJobsRequest;
 import com.google.api.services.jobs.v2.model.SearchJobsResponse;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.Random;
 
 /**
- * The sample in this file introduce featured job, including:
+ * The samples in this file introduce how to do a commute search.
  *
- * - Construct a featured job
- *
- * - Search featured job
+ * Note: Commute Search is different from location search. It only take latitude and longitude as
+ * the start location.
  */
-public final class FeaturedJobsSearchSample {
+public final class CommuteSearchSample {
 
   private static JobService jobService = JobServiceQuickstart.getJobService();
 
-  // [START featured_job]
+  // [START commute_search]
 
-  /**
-   * Creates a job as featured.
-   */
-  public static Job generateFeaturedJob(String companyName) throws IOException {
-    // requisition id should be a unique Id in your system.
-    String requisitionId =
-        "featuredJob:" + String.valueOf(new Random().nextLong());
-
-    Job job =
-        new Job()
-            .setRequisitionId(requisitionId)
-            .setJobTitle("Software Engineer")
-            .setCompanyName(companyName)
-            .setApplicationUrls(Arrays.asList("http://careers.google.com"))
-            .setDescription(
-                "Design, develop, test, deploy, maintain and improve software.")
-            // Featured job is the job with positive promotion value
-            .setPromotionValue(2);
-    System.out.println("Job generated: " + job);
-    return job;
-  }
-  // [END featured_job]
-
-  // [START search_featured_job]
-
-  /**
-   * Searches featured jobs.
-   */
-  public static void searchFeaturedJobs(String companyName) throws IOException {
+  public static void commuteSearch(String companyName) throws IOException {
     // Make sure to set the requestMetadata the same as the associated search request
     RequestMetadata requestMetadata =
         new RequestMetadata()
@@ -78,34 +50,41 @@ public final class FeaturedJobsSearchSample {
             .setSessionId("HashedSessionID")
             // Domain of the website where the search is conducted
             .setDomain("www.google.com");
-
-    JobQuery jobQuery = new JobQuery().setQuery("Software Engineer");
+    JobQuery jobQuery =
+        new JobQuery()
+            .setCommuteFilter(
+                new CommutePreference()
+                    .setRoadTraffic("TRAFFIC_FREE")
+                    .setMethod("TRANSIT")
+                    .setTravelTime("1000s")
+                    .setStartLocation(
+                        new LatLng().setLatitude(37.422408)
+                            .setLongitude(-122.085609)));
     if (companyName != null) {
       jobQuery.setCompanyNames(Arrays.asList(companyName));
     }
-
-    SearchJobsRequest request =
+    SearchJobsRequest searchJobsRequest =
         new SearchJobsRequest()
-            .setRequestMetadata(requestMetadata)
             .setQuery(jobQuery)
-            // Set the search mode to a featured search,
-            // which would only search the jobs with positive promotion value.
-            .setMode("FEATURED_JOB_SEARCH");
-    SearchJobsResponse response = jobService.jobs().search(request).execute();
+            .setRequestMetadata(requestMetadata)
+            .setJobView("FULL")
+            .setEnablePreciseResultSize(true);
+    SearchJobsResponse response = jobService.jobs().search(searchJobsRequest).execute();
     System.out.println(response);
   }
-  // [END search_featured_job]
+  // [END commute_search]
 
   public static void main(String... args) throws Exception {
     Company companyToBeCreated = BasicCompanySample.generateCompany();
     String companyName = BasicCompanySample.createCompany(companyToBeCreated).getName();
 
-    Job jobToBeCreated = generateFeaturedJob(companyName);
+    Job jobToBeCreated = BasicJobSample.generateJobWithRequiredFields(companyName)
+        .setLocations(Arrays.asList("1600 Amphitheatre Pkwy, Mountain View, CA 94043"));
     String jobName = BasicJobSample.createJob(jobToBeCreated).getName();
 
     // Wait several seconds for post processing
     Thread.sleep(10000);
-    searchFeaturedJobs(companyName);
+    commuteSearch(companyName);
 
     BasicJobSample.deleteJob(jobName);
     BasicCompanySample.deleteCompany(companyName);
