949ebf896a189585bf535416bebe0a62dc434064
nirupa-kumar <nirupa.kumar@gmail.com>
2018-07-12 13:02:06 -0700

    Speech beta samples: Diarization,Multi-channel, Multi-language and Word level confidence

--- speech/cloud-client/src/test/java/com/example/speech/RecognizeIT.java
+++ speech/cloud-client/src/test/java/com/example/speech/RecognizeIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google Inc.
+ * Copyright 2018 Google LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -26,9 +26,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-/**
- * Tests for speech recognize sample.
- */
+/** Tests for speech recognize sample. */
 @RunWith(JUnit4.class)
 @SuppressWarnings("checkstyle:abbreviationaswordinname")
 public class RecognizeIT {
@@ -40,7 +38,10 @@ public class RecognizeIT {
 
   // The path to the audio file to transcribe
   private String audioFileName = "./resources/audio.raw";
+  private String multiChannelAudioFileName = "./resources/commercial_stereo.wav";
+  private String gcsMultiChannelAudioPath = "gs://" + BUCKET + "/speech/commercial_stereo.wav";
   private String gcsAudioPath = "gs://" + BUCKET + "/speech/brooklyn.flac";
+  private String gcsDiarizationAudioPath = "gs://" + BUCKET + "/speech/commercial_mono.wav";
 
   // The path to the video file to transcribe
   private String videoFileName = "./resources/Google_Gnome.wav";
@@ -160,5 +161,63 @@ public class RecognizeIT {
     Recognize.transcribeFileWithMetadata(recognitionAudioFile);
     String got = bout.toString();
     assertThat(got).contains("Chrome");
+  }
+
+  @Test
+  public void testTranscribeDiarization() throws Exception {
+    Recognize.transcribeDiarization(recognitionAudioFile);
+    String got = bout.toString();
+    assertThat(got).contains("Speaker Tag : 1");
+  }
+
+  @Test
+  public void testTranscribeDiarizationGcs() throws Exception {
+    Recognize.transcribeDiarizationGcs(gcsDiarizationAudioPath);
+    String got = bout.toString();
+    assertThat(got).contains("Speaker Tag : 1");
+  }
+
+  @Test
+  public void testTranscribeMultiChannel() throws Exception {
+    Recognize.transcribeMultiChannel(multiChannelAudioFileName);
+    String got = bout.toString();
+    assertThat(got).contains("Channel Tag : 1");
+  }
+
+  @Test
+  public void testTranscribeMultiChannelGcs() throws Exception {
+    Recognize.transcribeMultiChannelGcs(gcsMultiChannelAudioPath);
+    String got = bout.toString();
+    assertThat(got).contains("Channel Tag : 1");
+  }
+
+  @Test
+  public void testTranscribeMultiLanguage() throws Exception {
+    Recognize.transcribeMultiLanguage(videoFileName);
+    String got = bout.toString();
+    assertThat(got).contains("Transcript : OK Google");
+  }
+
+  @Test
+  public void testTranscribeMultiLanguageGcs() throws Exception {
+    Recognize.transcribeMultiLanguageGcs(gcsVideoPath);
+    String got = bout.toString();
+    assertThat(got).contains("Transcript : OK Google");
+  }
+
+  @Test
+  public void testTranscribeWordLevelConfidence() throws Exception {
+    Recognize.transcribeWordLevelConfidence(audioFileName);
+    String got = bout.toString();
+    assertThat(got).contains("Transcript : how old is the Brooklyn Bridge");
+    assertThat(got).contains("First Word and Confidence : how");
+  }
+
+  @Test
+  public void testTranscribeWordLevelConfidenceGcs() throws Exception {
+    Recognize.transcribeWordLevelConfidenceGcs(gcsAudioPath);
+    String got = bout.toString();
+    assertThat(got).contains("Transcript : how old is the Brooklyn Bridge");
+    assertThat(got).contains("First Word and Confidence : how");
   }
 }
