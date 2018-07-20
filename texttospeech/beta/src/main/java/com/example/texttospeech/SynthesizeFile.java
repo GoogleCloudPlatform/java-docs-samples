3f9af3c24bad559e9455c9f6a09a5a703422cef1
Nirupa Anantha Kumar <nirupa-kumar@users.noreply.github.com>
2018-07-20 10:51:23 -0700

    Adding TTS Beta samples : Audio profile (#1152)
    * Adding TTS Beta samples : Audio profile
    
    * Updated based on comments : Adding TTS Beta samples - Audio profile
    
    * Updated based on comments : TTS Beta samples - Audio profile
    
    * Updated based on comments : TTS Beta samples - Audio profile
    
    * Updates after review
    
    * Updates after review
    
    * Updates after review : Please let this be the last one :)
    
    * Update to released client library
    
    * Update SynthesizeText.java
    
    Need to update the verification script to allow LLC.
    
    * Update SynthesizeText.java
    
    Need to update the verification script to allow LLC.

--- texttospeech/beta/src/main/java/com/example/texttospeech/SynthesizeFile.java
+++ texttospeech/beta/src/main/java/com/example/texttospeech/SynthesizeFile.java
@@ -17,59 +17,64 @@
 package com.example.texttospeech;
 
 // Imports the Google Cloud client library
-import com.google.cloud.texttospeech.v1.AudioConfig;
-import com.google.cloud.texttospeech.v1.AudioEncoding;
-import com.google.cloud.texttospeech.v1.SsmlVoiceGender;
-import com.google.cloud.texttospeech.v1.SynthesisInput;
-import com.google.cloud.texttospeech.v1.SynthesizeSpeechResponse;
-import com.google.cloud.texttospeech.v1.TextToSpeechClient;
-import com.google.cloud.texttospeech.v1.VoiceSelectionParams;
+import com.google.cloud.texttospeech.v1beta1.AudioConfig;
+import com.google.cloud.texttospeech.v1beta1.AudioEncoding;
+import com.google.cloud.texttospeech.v1beta1.SsmlVoiceGender;
+import com.google.cloud.texttospeech.v1beta1.SynthesisInput;
+import com.google.cloud.texttospeech.v1beta1.SynthesizeSpeechResponse;
+import com.google.cloud.texttospeech.v1beta1.TextToSpeechClient;
+import com.google.cloud.texttospeech.v1beta1.VoiceSelectionParams;
 import com.google.protobuf.ByteString;
 
 import java.io.FileOutputStream;
 import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import net.sourceforge.argparse4j.ArgumentParsers;
 import net.sourceforge.argparse4j.inf.ArgumentParser;
 import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import net.sourceforge.argparse4j.inf.MutuallyExclusiveGroup;
 import net.sourceforge.argparse4j.inf.Namespace;
 
+
 /**
  * Google Cloud TextToSpeech API sample application.
- * Example usage: mvn package exec:java -Dexec.mainClass='com.example.texttospeech.SynthesizeText'
- *                                      -Dexec.args='--text "hello"'
+ * Example usage: mvn package exec:java -Dexec.mainClass='com.example.texttospeech.SynthesizeFile'
+ *                                      -Dexec.args='--text resources/hello.txt'
  */
-public class SynthesizeText {
+public class SynthesizeFile {
 
-  // [START tts_synthesize_text]
+  // [START tts_synthesize_text_file]
   /**
-   * Demonstrates using the Text to Speech client to synthesize text or ssml.
-   *
-   * @param text the raw text to be synthesized. (e.g., "Hello there!")
+   * Demonstrates using the Text to Speech client to synthesize a text file or ssml file.
+   * @param textFile the text file to be synthesized. (e.g., hello.txt)
    * @throws Exception on TextToSpeechClient Errors.
    */
-  public static void synthesizeText(String text) throws Exception {
+  public static void synthesizeTextFile(String textFile)
+      throws Exception {
     // Instantiates a client
     try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
+      // Read the file's contents
+      String contents = new String(Files.readAllBytes(Paths.get(textFile)));
       // Set the text input to be synthesized
-      SynthesisInput input = SynthesisInput.newBuilder().setText(text).build();
+      SynthesisInput input = SynthesisInput.newBuilder()
+          .setText(contents)
+          .build();
 
       // Build the voice request
-      VoiceSelectionParams voice =
-          VoiceSelectionParams.newBuilder()
-              .setLanguageCode("en-US") // languageCode = "en_us"
-              .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
-              .build();
+      VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+          .setLanguageCode("en-US") // languageCode = "en_us"
+          .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
+          .build();
 
       // Select the type of audio file you want returned
-      AudioConfig audioConfig =
-          AudioConfig.newBuilder()
-              .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
-              .build();
+      AudioConfig audioConfig = AudioConfig.newBuilder()
+          .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
+          .build();
 
       // Perform the text-to-speech request
-      SynthesizeSpeechResponse response =
-          textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+      SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice,
+          audioConfig);
 
       // Get the audio contents from the response
       ByteString audioContents = response.getAudioContent();
@@ -81,40 +86,40 @@ public class SynthesizeText {
       }
     }
   }
-  // [END tts_synthesize_text]
+  // [END tts_synthesize_text_file]
 
-  // [START tts_synthesize_ssml]
+
+  // [START tts_synthesize_ssml_file]
   /**
-   * Demonstrates using the Text to Speech client to synthesize text or ssml.
-   *
-   * <p>Note: ssml must be well-formed according to: (https://www.w3.org/TR/speech-synthesis/
-   * Example: <speak>Hello there.</speak>
-   *
-   * @param ssml the ssml document to be synthesized. (e.g., "<?xml...")
+   * Demonstrates using the Text to Speech client to synthesize a text file or ssml file.
+   * @param ssmlFile the ssml document to be synthesized. (e.g., hello.ssml)
    * @throws Exception on TextToSpeechClient Errors.
    */
-  public static void synthesizeSsml(String ssml) throws Exception {
+  public static void synthesizeSsmlFile(String ssmlFile)
+      throws Exception {
     // Instantiates a client
     try (TextToSpeechClient textToSpeechClient = TextToSpeechClient.create()) {
+      // Read the file's contents
+      String contents = new String(Files.readAllBytes(Paths.get(ssmlFile)));
       // Set the ssml input to be synthesized
-      SynthesisInput input = SynthesisInput.newBuilder().setSsml(ssml).build();
+      SynthesisInput input = SynthesisInput.newBuilder()
+          .setSsml(contents)
+          .build();
 
       // Build the voice request
-      VoiceSelectionParams voice =
-          VoiceSelectionParams.newBuilder()
-              .setLanguageCode("en-US") // languageCode = "en_us"
-              .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
-              .build();
+      VoiceSelectionParams voice = VoiceSelectionParams.newBuilder()
+          .setLanguageCode("en-US") // languageCode = "en_us"
+          .setSsmlGender(SsmlVoiceGender.FEMALE) // ssmlVoiceGender = SsmlVoiceGender.FEMALE
+          .build();
 
       // Select the type of audio file you want returned
-      AudioConfig audioConfig =
-          AudioConfig.newBuilder()
-              .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
-              .build();
+      AudioConfig audioConfig = AudioConfig.newBuilder()
+          .setAudioEncoding(AudioEncoding.MP3) // MP3 audio.
+          .build();
 
       // Perform the text-to-speech request
-      SynthesizeSpeechResponse response =
-          textToSpeechClient.synthesizeSpeech(input, voice, audioConfig);
+      SynthesizeSpeechResponse response = textToSpeechClient.synthesizeSpeech(input, voice,
+          audioConfig);
 
       // Get the audio contents from the response
       ByteString audioContents = response.getAudioContent();
@@ -126,16 +131,12 @@ public class SynthesizeText {
       }
     }
   }
-  // [END tts_synthesize_ssml]
+  // [END tts_synthesize_ssml_file]
 
   public static void main(String... args) throws Exception {
-
-    ArgumentParser parser =
-        ArgumentParsers.newFor("SynthesizeText")
-            .build()
-            .defaultHelp(true)
-            .description("Synthesize a text or ssml.");
-
+    ArgumentParser parser = ArgumentParsers.newFor("SynthesizeFile").build()
+        .defaultHelp(true)
+        .description("Synthesize a text file or ssml file.");
     MutuallyExclusiveGroup group = parser.addMutuallyExclusiveGroup().required(true);
     group.addArgument("--text").help("The text file from which to synthesize speech.");
     group.addArgument("--ssml").help("The ssml file from which to synthesize speech.");
@@ -144,9 +145,9 @@ public class SynthesizeText {
       Namespace namespace = parser.parseArgs(args);
 
       if (namespace.get("text") != null) {
-        synthesizeText(namespace.getString("text"));
+        synthesizeTextFile(namespace.getString("text"));
       } else {
-        synthesizeSsml(namespace.getString("ssml"));
+        synthesizeSsmlFile(namespace.getString("ssml"));
       }
     } catch (ArgumentParserException e) {
       parser.handleError(e);
