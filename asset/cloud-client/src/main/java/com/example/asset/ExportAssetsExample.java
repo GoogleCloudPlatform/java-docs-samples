5e37300e8c2026e1e80a546fba55880102bdd06f
Peter Zheng <peterzh@google.com>
2018-11-12 10:58:46 -0800

    [Asset] Minor update on comment.

--- asset/cloud-client/src/main/java/com/example/asset/ExportAssetsExample.java
+++ asset/cloud-client/src/main/java/com/example/asset/ExportAssetsExample.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google LLC.
+ * Copyright 2018 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 package com.example.asset;
 
 // [START asset_quickstart_export_assets]
-
 // Imports the Google Cloud client library
 import com.google.cloud.ServiceOptions;
 import com.google.cloud.asset.v1beta1.AssetServiceClient;
