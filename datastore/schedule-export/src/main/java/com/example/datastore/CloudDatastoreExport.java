0a60a1bdac502de0eced68d40a3138e20222518e
Juan Lara <1543140+jlara310@users.noreply.github.com>
2018-11-28 16:44:32 -0800

    Run java linter

--- datastore/schedule-export/src/main/java/com/example/datastore/CloudDatastoreExport.java
+++ datastore/schedule-export/src/main/java/com/example/datastore/CloudDatastoreExport.java
@@ -2,34 +2,23 @@ package com.google.example.datastore;
 
 import com.google.appengine.api.appidentity.AppIdentityService;
 import com.google.appengine.api.appidentity.AppIdentityServiceFactory;
-import com.google.appengine.api.utils.SystemProperty;
 import com.google.apphosting.api.ApiProxy;
-
 import com.google.common.io.CharStreams;
-
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
 import java.io.OutputStreamWriter;
-
 import java.net.HttpURLConnection;
 import java.net.URL;
-
 import java.nio.charset.StandardCharsets;
-
 import java.text.SimpleDateFormat;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.logging.Logger;
-import java.util.Properties;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -40,13 +29,12 @@ public class CloudDatastoreExport extends HttpServlet {
   private static final Logger log = Logger.getLogger(CloudDatastoreExport.class.getName());
 
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response)
-      throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
     // Verify outputURL parameter
     String outputUrlPrefix = request.getParameter("output_url_prefix");
 
-    if (outputUrlPrefix == null || !outputUrlPrefix.matches("^gs://.*")){
+    if (outputUrlPrefix == null || !outputUrlPrefix.matches("^gs://.*")) {
       response.setStatus(HttpServletResponse.SC_CONFLICT);
       response.setContentType("text/plain");
       response.getWriter().println("Error: Must provide a valid output_url_prefix.");
@@ -56,18 +44,18 @@ public class CloudDatastoreExport extends HttpServlet {
       // Get project ID
       String projectId = ApiProxy.getCurrentEnvironment().getAppId();
       // Remove partition information to get plain app ID
-      String appId = projectId.replaceFirst("(.*~)","");
-
+      String appId = projectId.replaceFirst("(.*~)", "");
 
       // Get access token
       ArrayList<String> scopes = new ArrayList<String>();
       scopes.add("https://www.googleapis.com/auth/datastore");
       final AppIdentityService appIdentity = AppIdentityServiceFactory.getAppIdentityService();
-      final AppIdentityService.GetAccessTokenResult accessToken = appIdentity.getAccessToken(scopes);
+      final AppIdentityService.GetAccessTokenResult accessToken =
+          appIdentity.getAccessToken(scopes);
 
       // Read export parameters
       // If output prefix does not end with slash, add a timestamp
-      if( !outputUrlPrefix.substring(outputUrlPrefix.length() - 1).contentEquals("/") ){
+      if (!outputUrlPrefix.substring(outputUrlPrefix.length() - 1).contentEquals("/")) {
         String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
         outputUrlPrefix = outputUrlPrefix + "/" + timeStamp + "/";
       }
@@ -81,12 +69,12 @@ public class CloudDatastoreExport extends HttpServlet {
 
       JSONObject entityFilter = new JSONObject();
 
-      if (kinds != null){
+      if (kinds != null) {
         JSONArray kindsJSON = new JSONArray(kinds);
-        entityFilter.put("kinds", kinds);      
+        entityFilter.put("kinds", kinds);
       }
 
-      if (namespaces != null){
+      if (namespaces != null) {
         JSONArray namespacesJSON = new JSONArray(namespaces);
         entityFilter.put("namespaceIds", namespacesJSON);
       }
@@ -106,17 +94,19 @@ public class CloudDatastoreExport extends HttpServlet {
 
       if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
 
-      JSONTokener exportResponseTokens = new JSONTokener(connection.getInputStream());
-      JSONObject exportResponse = new JSONObject(exportResponseTokens);
+        JSONTokener exportResponseTokens = new JSONTokener(connection.getInputStream());
+        JSONObject exportResponse = new JSONObject(exportResponseTokens);
 
-      response.setContentType("text/plain");
-      response.getWriter().println("Export started:\n" + exportResponse.toString(4));
+        response.setContentType("text/plain");
+        response.getWriter().println("Export started:\n" + exportResponse.toString(4));
 
       } else {
         InputStream s = connection.getErrorStream();
         InputStreamReader r = new InputStreamReader(s, StandardCharsets.UTF_8);
-        String errorMessage = String.format("got error (%d) response %s from %s",
-                  connection.getResponseCode(), CharStreams.toString(r), connection.toString());
+        String errorMessage =
+            String.format(
+                "got error (%d) response %s from %s",
+                connection.getResponseCode(), CharStreams.toString(r), connection.toString());
         log.warning(errorMessage);
         response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         response.setContentType("text/plain");
@@ -124,5 +114,4 @@ public class CloudDatastoreExport extends HttpServlet {
       }
     }
   }
-
 }
