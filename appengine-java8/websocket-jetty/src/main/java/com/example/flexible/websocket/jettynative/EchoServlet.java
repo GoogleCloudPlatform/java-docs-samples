4d65354081248da9cc9f7cf738ae50cf702bcf4e
Averi Kitsch <akitsch@google.com>
2018-12-05 14:20:37 -0800

    Web Socket - Jetty  (#1282)
    * copy over sample
    
    * update sample URL construction
    
    * Update parent dependency

---
+++ appengine-java8/websocket-jetty/src/main/java/com/example/flexible/websocket/jettynative/EchoServlet.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.flexible.websocket.jettynative;
+
+import javax.servlet.annotation.WebServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
+import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
+
+/*
+ * Server-side WebSocket registered as /echo servlet.
+ */
+@SuppressWarnings("serial")
+@WebServlet(name = "Echo WebSocket Servlet", urlPatterns = { "/echo" })
+public class EchoServlet extends WebSocketServlet {
+  @Override
+  public void configure(WebSocketServletFactory factory) {
+    factory.register(ServerSocket.class);
+  }
+}
