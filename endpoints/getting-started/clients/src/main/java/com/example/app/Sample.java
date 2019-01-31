789d4b0692b27303d48f5931082608afe7e05f98
Dan Sanche <sanche@google.com>
2019-01-31 15:50:02 -0800

    added make request function

--- endpoints/getting-started/clients/src/main/java/com/example/app/Sample.java
+++ endpoints/getting-started/clients/src/main/java/com/example/app/Sample.java
@@ -20,6 +20,10 @@ import com.auth0.jwt.JWTCreator;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+
+import java.net.*;
+import java.io.*;
+
 public class Sample {
 
 
@@ -45,6 +49,25 @@ public class Sample {
     return token.sign(algorithm);
   }
 // [END endpoints_generate_jwt_sa]
+
+
+// [START endpoints_jwt_request]
+  public static String makeJWTRequest(String singedJWT, URL url) throws IOException, ProtocolException{
+    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+    con.setRequestMethod("GET");
+    con.setRequestProperty("Content-Type", "application/json");
+    con.setRequestProperty("Authorization", "Bearer " + singedJWT);
+
+    BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String line;
+    StringBuilder result = new StringBuilder();
+    while ((line = rd.readLine()) != null) {
+       result.append(line);
+    }
+    rd.close();
+    return result.toString();
+  }
+// [END endpoints_jwt_request]
 
   public static void main(String[] args) throws Exception {
     String jwt = generateJWT("/app/key.json", "iss", "sub", 100);
