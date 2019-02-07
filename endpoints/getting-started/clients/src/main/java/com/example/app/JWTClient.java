b15d7652c7aa1edaa7a5973c115bc2aa481165d3
Dan Sanche <sanche@google.com>
2019-02-06 17:26:57 -0800

    addressed checkstyle issues

--- endpoints/getting-started/clients/src/main/java/com/example/app/JWTClient.java
+++ endpoints/getting-started/clients/src/main/java/com/example/app/JWTClient.java
@@ -2,9 +2,6 @@ package com.example.app;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
 import java.io.FileInputStream;
 import java.security.interfaces.RSAPrivateKey;
 
@@ -27,66 +24,74 @@ import java.net.ProtocolException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+/**
+ * JWTClient shows how a client can authenticate with a Cloud Endpoints service
+ */
 public class JWTClient {
 
-
 // [START endpoints_generate_jwt_sa]
-  /*
+  /**
    * Generates a signed JSON Web Token using a Google API Service Account
-   * utilizes com.auth0.jwt
+   * utilizes com.auth0.jwt.
    */
-  public static String generateJWT(String saKeyfile, String saEmail, String audience,
-      int expiryLength) throws FileNotFoundException, IOException {
+  public static String generateJWT(final String saKeyfile, final String saEmail,
+    final String audience, final int expiryLength)
+    throws FileNotFoundException, IOException {
 
     Date now = new Date();
-    Date expiration = new Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(expiryLength));
+    Date expTime = new Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(expiryLength));
 
     // build jwt
     JWTCreator.Builder token = JWT.create()
-                                  .withIssuedAt(now)
-                                  // expires after 'expirary_length' seconds.
-                                  .withExpiresAt(expiration)
-                                  // must match 'issuer' in the security configuration in your
-                                  // swagger spec (e.g. service account email). It can be any string
-                                  .withIssuer(saEmail)
-                                  // must be either your Endpoints service name, or match the value
-                                  // specified as the 'x-google-audience' in the OpenAPI document.
-                                  .withAudience(audience)
-                                  // subject and email should match the service account's email
-                                  .withSubject(saEmail)
-                                  .withClaim("email", saEmail);
+        .withIssuedAt(now)
+        // expires after 'expirary_length' seconds.
+        .withExpiresAt(expTime)
+        // must match 'issuer' in the security configuration in your
+        // swagger spec (e.g. service account email). It can be any string
+        .withIssuer(saEmail)
+        // must be either your Endpoints service name, or match the value
+        // specified as the 'x-google-audience' in the OpenAPI document.
+        .withAudience(audience)
+        // subject and email should match the service account's email
+        .withSubject(saEmail)
+        .withClaim("email", saEmail);
 
     // sign jwt
-    GoogleCredential credential = GoogleCredential.fromStream(new FileInputStream(saKeyfile));
-    RSAPrivateKey privateKey = (RSAPrivateKey) credential.getServiceAccountPrivateKey();
-    Algorithm algorithm = Algorithm.RSA256(null, privateKey);
+    FileInputStream stream = new FileInputStream(saKeyfile);
+    GoogleCredential cred = GoogleCredential.fromStream(stream);
+    RSAPrivateKey key = (RSAPrivateKey) cred.getServiceAccountPrivateKey();
+    Algorithm algorithm = Algorithm.RSA256(null, key);
     return token.sign(algorithm);
   }
 // [END endpoints_generate_jwt_sa]
 
 
 // [START endpoints_jwt_request]
-  /*
-   * Makes an authorized request to the endpoint
+  /**
+   * Makes an authorized request to the endpoint.
    */
-  public static String makeJWTRequest(String singedJWT, URL url) throws IOException, ProtocolException{
+  public static String makeJWTRequest(final String singedJWT, final URL url)
+    throws IOException, ProtocolException {
+
     HttpURLConnection con = (HttpURLConnection) url.openConnection();
     con.setRequestMethod("GET");
     con.setRequestProperty("Content-Type", "application/json");
     con.setRequestProperty("Authorization", "Bearer " + singedJWT);
 
-    BufferedReader rd = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    InputStreamReader reader = new InputStreamReader(con.getInputStream());
+    BufferedReader buffReader = new BufferedReader(reader);
+
     String line;
     StringBuilder result = new StringBuilder();
-    while ((line = rd.readLine()) != null) {
+    while ((line = buffReader.readLine()) != null) {
        result.append(line);
     }
-    rd.close();
+    buffReader.close();
     return result.toString();
   }
 // [END endpoints_jwt_request]
 
-  public static void main(String[] args) throws Exception {
+  public static void main(final String[] args) throws Exception {
     String keyPath = args[0];
     URL host = new URL(args[1]);
     String audience = args[2];
