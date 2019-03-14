35a191f2f6293ea40dcd6b254242152f0288c01d
Frank Natividad <franknatividad@google.com>
2019-03-14 15:05:28 -0700

    Remove workaround from sample

--- storage/s3-sdk/src/main/java/ListGcsObjects.java
+++ storage/s3-sdk/src/main/java/ListGcsObjects.java
@@ -14,24 +14,18 @@
  * limitations under the License.
  */
 
-// Solution found in: https://stackoverflow.com/questions/47264520/unable-to-listobjects-from-gcs-bucket
 
 // [START storage_s3_sdk_list_objects]
-import com.amazonaws.Request;
-
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 
-import com.amazonaws.handlers.RequestHandler2;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.ListObjectsRequest;
 import com.amazonaws.services.s3.model.ObjectListing;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 import java.util.List;
-import java.util.Map;
 
 public class ListGcsObjects {
   public static ObjectListing listGcsObjects(String googleAccessKeyId, 
@@ -43,14 +37,12 @@ public class ListGcsObjects {
     // Create a new client and do the following:
     // 1. Change the endpoint URL to use the Google Cloud Storage XML API endpoint.
     // 2. Use Cloud Storage HMAC Credentials.
-    // 3. Remove "encoding-type" when using listObjects() using withRequestHandlers().
     AmazonS3 interopClient =
         AmazonS3ClientBuilder.standard()
             .withEndpointConfiguration(
                 new AwsClientBuilder.EndpointConfiguration(
                     "https://storage.googleapis.com", "auto"))
             .withCredentials(new AWSStaticCredentialsProvider(googleCreds))
-            .withRequestHandlers(new RemoveEncodingType())
             .build();
 
     // Call GCS to list current objects
@@ -67,19 +59,5 @@ public class ListGcsObjects {
 
     return objects;
   }
-
-  // Cloud Storage does not support encoding-type and must be removed
-  // from a ListObjectsRequest made by the client. 
-  // The following class removes encoding-type and is used whenever
-  // the listObjects() is called using S3 SDK.
-  private static class RemoveEncodingType extends RequestHandler2 {
-    @Override
-    public void beforeRequest(Request<?> request) {
-      if (request.getOriginalRequest() instanceof ListObjectsRequest) {
-        request.getParameters().remove("encoding-type");
-      }
-    }
-  }
-  // [END storage_s3_sdk_list_objects]
 }
-
+// [END storage_s3_sdk_list_objects]
