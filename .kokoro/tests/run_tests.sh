b452d0a64dad62f924cf42b25aaa095152172188
averikitsch <akitsch@google.com>
2019-04-18 10:03:50 -0700

    escape variables

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -55,7 +55,7 @@ for file in **/pom.xml; do
     VERSION=$(grep -oP '(?<=<maven.compiler.target>).*?(?=</maven.compiler.target>)' pom.xml)
 
     # Check for changes to the current folder
-    if [ "$PARENT" -eq 0 ] && [[ $JAVA_VERSION = *"$VERSION"* ]]; then
+    if [ "$PARENT" -eq 0 ] && [[ "$JAVA_VERSION" = *"$VERSION"* ]]; then
         echo "------------------------------------------------------------"
         echo "- testing $file"
         echo "------------------------------------------------------------"
