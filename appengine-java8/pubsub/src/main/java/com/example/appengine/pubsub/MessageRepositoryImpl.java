51730290c6cf97c4c23938186d2e1dff096e9f26
ajaaym <34161822+ajaaym@users.noreply.github.com>
2019-05-07 17:47:14 -0400

    Example for pubsub authenticated push (#1407)

--- appengine-java8/pubsub/src/main/java/com/example/appengine/pubsub/MessageRepositoryImpl.java
+++ appengine-java8/pubsub/src/main/java/com/example/appengine/pubsub/MessageRepositoryImpl.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-
 package com.example.appengine.pubsub;
 
 import com.google.cloud.datastore.Datastore;
@@ -35,6 +34,12 @@ public class MessageRepositoryImpl implements MessageRepository {
 
   private String messagesKind = "messages";
   private KeyFactory keyFactory = getDatastoreInstance().newKeyFactory().setKind(messagesKind);
+  private String claimsKind = "claims";
+  private KeyFactory claimsKindKeyFactory =
+      getDatastoreInstance().newKeyFactory().setKind(claimsKind);
+  private String tokensKind = "tokens";
+  private KeyFactory tokensKindKeyFactory =
+      getDatastoreInstance().newKeyFactory().setKind(tokensKind);
 
   @Override
   public void save(Message message) {
@@ -42,8 +47,8 @@ public class MessageRepositoryImpl implements MessageRepository {
     Datastore datastore = getDatastoreInstance();
     Key key = datastore.allocateId(keyFactory.newKey());
 
-    Entity.Builder messageEntityBuilder = Entity.newBuilder(key)
-        .set("messageId", message.getMessageId());
+    Entity.Builder messageEntityBuilder =
+        Entity.newBuilder(key).set("messageId", message.getMessageId());
 
     if (message.getData() != null) {
       messageEntityBuilder = messageEntityBuilder.set("data", message.getData());
@@ -84,15 +89,72 @@ public class MessageRepositoryImpl implements MessageRepository {
     return messages;
   }
 
+  @Override
+  public void saveClaim(String claim) {
+    // Save message to "messages"
+    Datastore datastore = getDatastoreInstance();
+    Key key = datastore.allocateId(claimsKindKeyFactory.newKey());
+
+    Entity.Builder claimEntityBuilder = Entity.newBuilder(key).set("claim", claim);
+
+    datastore.put(claimEntityBuilder.build());
+  }
+
+  @Override
+  public List<String> retrieveClaims(int limit) {
+    // Get claim saved in Datastore
+    Datastore datastore = getDatastoreInstance();
+    Query<Entity> query = Query.newEntityQueryBuilder().setKind(claimsKind).setLimit(limit).build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    List<String> claims = new ArrayList<>();
+    while (results.hasNext()) {
+      Entity entity = results.next();
+      String claim = entity.getString("claim");
+      if (claim != null) {
+        claims.add(claim);
+      }
+    }
+    return claims;
+  }
+
+  @Override
+  public void saveToken(String token) {
+    // Save message to "messages"
+    Datastore datastore = getDatastoreInstance();
+    Key key = datastore.allocateId(tokensKindKeyFactory.newKey());
+
+    Entity.Builder tokenEntityBuilder = Entity.newBuilder(key).set("token", token);
+
+    datastore.put(tokenEntityBuilder.build());
+  }
+
+  @Override
+  public List<String> retrieveTokens(int limit) {
+    // Get token saved in Datastore
+    Datastore datastore = getDatastoreInstance();
+    Query<Entity> query = Query.newEntityQueryBuilder().setKind(tokensKind).setLimit(limit).build();
+    QueryResults<Entity> results = datastore.run(query);
+
+    List<String> tokens = new ArrayList<>();
+    while (results.hasNext()) {
+      Entity entity = results.next();
+      String token = entity.getString("token");
+      if (token != null) {
+        tokens.add(token);
+      }
+    }
+    return tokens;
+  }
+
   private Datastore getDatastoreInstance() {
     return DatastoreOptions.getDefaultInstance().getService();
   }
 
-  private MessageRepositoryImpl() {
-  }
+  private MessageRepositoryImpl() {}
 
   // retrieve a singleton instance
-  public static synchronized  MessageRepositoryImpl getInstance() {
+  public static synchronized MessageRepositoryImpl getInstance() {
     if (instance == null) {
       instance = new MessageRepositoryImpl();
     }
