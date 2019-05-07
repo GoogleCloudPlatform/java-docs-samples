0942a3b8b1df950a19956f16c94eee8935ac0338
averikitsch <akitsch@google.com>
2019-05-07 15:16:27 -0700

    fix exception

--- appengine-java11/guestbook-cloud-firestore/src/main/java/com/example/guestbook/Guestbook.java
+++ appengine-java11/guestbook-cloud-firestore/src/main/java/com/example/guestbook/Guestbook.java
@@ -73,7 +73,7 @@ public class Guestbook {
       for (QueryDocumentSnapshot greeting : querySnapshot.getDocuments()) {
         greetings.add(greeting.toObject(Greeting.class));
       }
-    } catch (InterruptedException e) {
+    } catch (Exception e) {
       System.out.println(e.getMessage());
     }
 
