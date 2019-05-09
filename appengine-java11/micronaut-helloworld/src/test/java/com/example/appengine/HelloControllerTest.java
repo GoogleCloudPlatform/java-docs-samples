b101e863eac60708d5dcf844b1d103ae3a584e39
averikitsch <akitsch@google.com>
2019-05-09 11:28:35 -0700

    micronaut helloworld sample

---
+++ appengine-java11/micronaut-helloworld/src/test/java/com/example/appengine/HelloControllerTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.appengine;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+@MicronautTest
+public class HelloControllerTest {
+
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    public void testHello() throws Exception {
+        HttpRequest request = HttpRequest.GET("/");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals(body, "Hello World!");
+    }
+}
