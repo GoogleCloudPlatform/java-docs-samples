8956f1741131ea22063466dbc3e74aa1d26144d4
Andrew Ferlitsch <aferlitsch@gmail.com>
2019-06-12 16:43:34 -0700

    fix misspelling reported by user (#1451)

--- vision/automl/src/main/java/com/google/cloud/vision/samples/automl/ModelApi.java
+++ vision/automl/src/main/java/com/google/cloud/vision/samples/automl/ModelApi.java
@@ -199,9 +199,9 @@ public class ModelApi {
       System.out.println(String.format("Model display name: %s", model.getDisplayName()));
       System.out.println("Image classification model metadata:");
       System.out.println(
-          "Tranning budget: " + model.getImageClassificationModelMetadata().getTrainBudget());
+          "Training budget: " + model.getImageClassificationModelMetadata().getTrainBudget());
       System.out.println(
-          "Tranning cost:" + model.getImageClassificationModelMetadata().getTrainCost());
+          "Training cost: " + model.getImageClassificationModelMetadata().getTrainCost());
       System.out.println(
           String.format(
               "Stop reason: %s", model.getImageClassificationModelMetadata().getStopReason()));
