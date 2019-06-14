201881ac4872d9b4d166bf730c0e51c853dd18ec
averikitsch <akitsch@google.com>
2019-06-14 10:29:27 -0700

    Fix service account

--- tasks/src/test/java/com/example/task/CreateHttpTaskIT.java
+++ tasks/src/test/java/com/example/task/CreateHttpTaskIT.java
@@ -35,7 +35,7 @@ public class CreateHttpTaskIT {
   private static final String LOCATION_ID = "us-east1";
   private static final String QUEUE_ID = "default";
   private static final String EMAIL =
-      "java-docs-samples@cloud-samples-tests.iam.gserviceaccount.com";
+      "java-docs-samples-testing@java-docs-samples-testing.iam.gserviceaccount.com";
   private ByteArrayOutputStream bout;
   private PrintStream out;
 
