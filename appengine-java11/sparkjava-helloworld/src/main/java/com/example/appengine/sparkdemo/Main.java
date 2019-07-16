6cd8e2b4e932a29f73ce51c98fe7319a98087cec
i0n- <daniel@dleon.me>
2019-07-16 10:57:41 -0700

    Update Main.java

--- appengine-java11/sparkjava-helloworld/src/main/java/com/example/appengine/sparkdemo/Main.java
+++ appengine-java11/sparkjava-helloworld/src/main/java/com/example/appengine/sparkdemo/Main.java
@@ -21,12 +21,10 @@ import spark.Spark;
 public class Main {
 
   public static void main(String[] args) {
-    // Starts the webapp on localhost:8080.
-    Spark.port(
-                System.getenv().containsKey("PORT")
-                ? Integer.parseInt(System.getenv().get("PORT"))
-                : 8080
-        );
+    // Starts the webapp on localhost and the port defined by the PORT
+    // environment variable when is present, otherwise on 8080.
+    int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+    Spark.port(port);
     Spark.get("/", (req, res) -> "Hello World!");
   }
 }
