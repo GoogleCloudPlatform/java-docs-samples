4d5c361a81739969a48938681625367f31ea3cf8
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2019-07-25 18:04:26 -0500

    Update appengine-flex websocket usage (#1526)

--- appengine-java8/websocket-jetty/src/main/java/com/example/flexible/websocket/jettynative/EchoServlet.java
+++ appengine-java8/websocket-jetty/src/main/java/com/example/flexible/websocket/jettynative/EchoServlet.java
@@ -17,17 +17,27 @@
 package com.example.flexible.websocket.jettynative;
 
 import javax.servlet.annotation.WebServlet;
+
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeRequest;
+import org.eclipse.jetty.websocket.servlet.ServletUpgradeResponse;
+import org.eclipse.jetty.websocket.servlet.WebSocketCreator;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
 /*
- * Server-side WebSocket registered as /echo servlet.
+ * Server-side WebSocket upgraded on /echo servlet.
  */
 @SuppressWarnings("serial")
 @WebServlet(name = "Echo WebSocket Servlet", urlPatterns = { "/echo" })
-public class EchoServlet extends WebSocketServlet {
+public class EchoServlet extends WebSocketServlet implements WebSocketCreator {
   @Override
   public void configure(WebSocketServletFactory factory) {
-    factory.register(ServerSocket.class);
+    factory.setCreator(this);
+  }
+
+  @Override
+  public Object createWebSocket(ServletUpgradeRequest servletUpgradeRequest,
+      ServletUpgradeResponse servletUpgradeResponse) {
+    return new ServerSocket();
   }
 }
