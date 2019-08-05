f759ee14625d9db6d766ad7adbe9b19fa4321bb7
Kamal Aboul-Hosn <kamal.aboulhosn@gmail.com>
2019-08-05 16:34:52 -0400

    Add documentation and improve formatting

--- pubsub/cloud-client/src/main/java/com/example/pubsub/SubscriberExample.java
+++ pubsub/cloud-client/src/main/java/com/example/pubsub/SubscriberExample.java
@@ -26,8 +26,18 @@ import com.google.pubsub.v1.ProjectSubscriptionName;
 import com.google.pubsub.v1.PubsubMessage;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.TimeUnit;
 
 public class SubscriberExample {
+  // use the default project id
+  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
+
+  // The amount of time to wait to offer a message to the queue.
+  private static final long QUEUE_OFFER_TIMEOUT_SECONDS = 5;
+
+  private static final BlockingQueue<MessageAndConsumer> queue = new LinkedBlockingDeque<>();
+
+  // A POD object to hold a message and the consumer associated with it.
   static class MessageAndConsumer {
     PubsubMessage message;
     AckReplyConsumer consumer;
@@ -38,17 +48,22 @@ public class SubscriberExample {
     }
   }
 
-  // use the default project id
-  private static final String PROJECT_ID = ServiceOptions.getDefaultProjectId();
-
-  private static final BlockingQueue<MessageAndConsumer> messages = new LinkedBlockingDeque<>();
-
-
   static class MessageReceiverExample implements MessageReceiver {
 
     @Override
     public void receiveMessage(PubsubMessage message, AckReplyConsumer consumer) {
-      if (!messages.offer(new MessageAndConsumer(message, consumer))) {
+      // Attempt to add the message to the queue for processing. If we can't add it within
+      // QUEUE_OFFER_TIMEOUT_SECONDS, then nack it and allow it to be redelivered (to this client
+      // or to another one that might be running and have more capacity)
+      try {
+        if (!queue.offer(
+            new MessageAndConsumer(message, consumer),
+            QUEUE_OFFER_TIMEOUT_SECONDS,
+            TimeUnit.SECONDS)) {
+          consumer.nack();
+        }
+      } catch (InterruptedException e) {
+        System.out.println("Could not add message to queue: " + e);
         consumer.nack();
       }
     }
@@ -66,11 +81,12 @@ public class SubscriberExample {
       subscriber =
           Subscriber.newBuilder(subscriptionName, new MessageReceiverExample()).build();
       subscriber.startAsync().awaitRunning();
-      // Continue to listen to messages
+      // Continue to listen to queue
       while (true) {
-        MessageAndConsumer messageAndConsumer = messages.take();
+        MessageAndConsumer messageAndConsumer = queue.take();
         System.out.println("Message Id: " + messageAndConsumer.message.getMessageId());
         System.out.println("Data: " + messageAndConsumer.message.getData().toStringUtf8());
+        // Ack only once all work with the message is complete.
         messageAndConsumer.consumer.ack();
       }
     } finally {
