9df581222019c62a29ffd519104065eb161308db
averikitsch <akitsch@google.com>
2019-08-06 13:34:05 -0700

    linting

--- appengine-java8/tasks/snippets/src/main/java/com/example/task/CreateTaskWithName.java
+++ appengine-java8/tasks/snippets/src/main/java/com/example/task/CreateTaskWithName.java
@@ -18,20 +18,11 @@ package com.example.task;
 
 // [START taskqueues_naming_tasks]
 import com.google.cloud.tasks.v2.AppEngineHttpRequest;
-import com.google.cloud.tasks.v2.AppEngineRouting;
 import com.google.cloud.tasks.v2.CloudTasksClient;
 import com.google.cloud.tasks.v2.HttpMethod;
-import com.google.cloud.tasks.v2.LocationName;
-import com.google.cloud.tasks.v2.Queue;
 import com.google.cloud.tasks.v2.QueueName;
-import com.google.cloud.tasks.v2.RateLimits;
-import com.google.cloud.tasks.v2.RetryConfig;
 import com.google.cloud.tasks.v2.Task;
 import com.google.cloud.tasks.v2.TaskName;
-import com.google.cloud.tasks.v2.UpdateQueueRequest;
-import com.google.protobuf.ByteString;
-import com.google.protobuf.Duration;
-import java.nio.charset.Charset;
 
 public class CreateTaskWithName {
   public static void createTaskWithName(
--- appengine-java8/tasks/snippets/src/main/java/com/example/task/DeleteTask.java
+++ appengine-java8/tasks/snippets/src/main/java/com/example/task/DeleteTask.java
@@ -17,21 +17,8 @@
 package com.example.task;
 
 // [START taskqueues_deleting_tasks]
-import com.google.cloud.tasks.v2.AppEngineHttpRequest;
-import com.google.cloud.tasks.v2.AppEngineRouting;
 import com.google.cloud.tasks.v2.CloudTasksClient;
-import com.google.cloud.tasks.v2.HttpMethod;
-import com.google.cloud.tasks.v2.LocationName;
-import com.google.cloud.tasks.v2.Queue;
-import com.google.cloud.tasks.v2.QueueName;
-import com.google.cloud.tasks.v2.RateLimits;
-import com.google.cloud.tasks.v2.RetryConfig;
-import com.google.cloud.tasks.v2.Task;
 import com.google.cloud.tasks.v2.TaskName;
-import com.google.cloud.tasks.v2.UpdateQueueRequest;
-import com.google.protobuf.ByteString;
-import com.google.protobuf.Duration;
-import java.nio.charset.Charset;
 
 public class DeleteTask {
   public static void deleteTask(String projectId, String locationId, String queueId, String taskId)
