5e4fbfe6883f10e473ccf0d741b8f645b0d678cb
Grant Timmerman <granttimmerman@gmail.com>
2019-09-09 16:49:34 -0300

    feat: add functions_env_vars (#1551)
    * feat: add functions_env_vars
    
    * fix: Address function_ent comments around naming and tests
    
    * Comments
    
    * fix: Adds org.junit.contrib.java.lang.system.EnvironmentVariables to fix env vars
    
    * docs: Convert JavaDoc to single comment
    
    * fix: Fix lint issue

--- functions/snippets/src/test/java/SnippetsTests.java
+++ functions/snippets/src/test/java/SnippetsTests.java
@@ -32,7 +32,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.EnvironmentVariables;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
@@ -44,6 +46,10 @@ public class SnippetsTests {
 
   private ByteArrayOutputStream stdOut;
   private StringWriter responseOut;
+
+  @Rule
+  public final EnvironmentVariables environmentVariables
+      = new EnvironmentVariables();
 
   @Before
   public void beforeTest() throws Exception {
@@ -167,6 +173,13 @@ public class SnippetsTests {
     assertThat(responseOut.toString(), containsString("Hello John!"));    
   }
 
+  @Test
+  public void envTest() throws IOException {
+    environmentVariables.set("FOO", "BAR");
+    new EnvVars().envVar(request, response);
+    assertThat(responseOut.toString(), containsString("BAR"));
+  }
+  
   @Test
   public void helloExecutionCount() throws IOException {
     new Concepts().executionCount(request, response);
