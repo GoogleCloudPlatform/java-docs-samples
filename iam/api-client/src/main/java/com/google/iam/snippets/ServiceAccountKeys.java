9a3b057eb41a895ee718e44437e3c2ed8e7b7389
mdejong3 <35782177+mdejong3@users.noreply.github.com>
2019-09-11 13:27:31 -0700

    Added region tags to capture imports/variables

--- iam/api-client/src/main/java/com/google/iam/snippets/ServiceAccountKeys.java
+++ iam/api-client/src/main/java/com/google/iam/snippets/ServiceAccountKeys.java
@@ -1,4 +1,4 @@
-/* Copyright 2018 Google LLC
+/* Copyright 2019 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,47 +15,54 @@
 
 package com.google.iam.snippets;
 
+// [START iam_create_key]
+// [START iam_list_keys]
+// [START iam_delete_key]
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.iam.v1.Iam;
 import com.google.api.services.iam.v1.IamScopes;
-import com.google.api.services.iam.v1.model.CreateServiceAccountKeyRequest;
 import com.google.api.services.iam.v1.model.ServiceAccountKey;
 import java.io.IOException;
 import java.util.Collections;
+// [END iam_create_key]
+// [END iam_list_keys]
+// [END iam_delete_key]
+
+// [START iam_create_key]
+import com.google.api.services.iam.v1.model.CreateServiceAccountKeyRequest;
+// [END iam_create_key]
+
+// [START iam_list_keys]
 import java.util.List;
+// [END iam_list_keys]
+
+// [START iam_create_key]
+// [START iam_list_keys]
+// [START iam_delete_key]
 
 public class ServiceAccountKeys {
 
   private final Iam service;
 
   public ServiceAccountKeys() throws Exception {
-    GoogleCredential credential =
-        GoogleCredential.getApplicationDefault()
-            .createScoped(Collections.singleton(IamScopes.CLOUD_PLATFORM));
+    GoogleCredential credential = GoogleCredential.getApplicationDefault()
+        .createScoped(Collections.singleton(IamScopes.CLOUD_PLATFORM));
 
-    service =
-        new Iam.Builder(
-                GoogleNetHttpTransport.newTrustedTransport(),
-                JacksonFactory.getDefaultInstance(),
-                credential)
-            .setApplicationName("service-account-keys")
-            .build();
+    service = new Iam.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(),
+        credential).setApplicationName("service-account-keys").build();
   }
+
+// [END iam_create_key]
+// [END iam_list_keys]
+// [END iam_delete_key]
 
   // [START iam_create_key]
   public ServiceAccountKey createKey(String serviceAccountEmail) throws IOException {
 
-    ServiceAccountKey key =
-        service
-            .projects()
-            .serviceAccounts()
-            .keys()
-            .create(
-                "projects/-/serviceAccounts/" + serviceAccountEmail,
-                new CreateServiceAccountKeyRequest())
-            .execute();
+    ServiceAccountKey key = service.projects().serviceAccounts().keys()
+        .create("projects/-/serviceAccounts/" + serviceAccountEmail, new CreateServiceAccountKeyRequest()).execute();
 
     System.out.println("Created key: " + key.getName());
     return key;
@@ -65,14 +72,8 @@ public class ServiceAccountKeys {
   // [START iam_list_keys]
   public List<ServiceAccountKey> listKeys(String serviceAccountEmail) throws IOException {
 
-    List<ServiceAccountKey> keys =
-        service
-            .projects()
-            .serviceAccounts()
-            .keys()
-            .list("projects/-/serviceAccounts/" + serviceAccountEmail)
-            .execute()
-            .getKeys();
+    List<ServiceAccountKey> keys = service.projects().serviceAccounts().keys()
+        .list("projects/-/serviceAccounts/" + serviceAccountEmail).execute().getKeys();
 
     for (ServiceAccountKey key : keys) {
       System.out.println("Key: " + key.getName());
@@ -89,4 +90,11 @@ public class ServiceAccountKeys {
     System.out.println("Deleted key: " + fullKeyName);
   }
   // [END iam_delete_key]
+
+// [START iam_create_key]
+// [START iam_list_keys]
+// [START iam_delete_key
 }
+// [END iam_create_key]
+// [END iam_list_keys]
+// [END iam_delete_key]
