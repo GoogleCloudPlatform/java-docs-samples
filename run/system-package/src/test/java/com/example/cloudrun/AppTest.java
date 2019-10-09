d82265976b08a2bac8c9fd0764b9e7853cbc05b8
averikitsch <akitsch@google.com>
2019-10-09 16:57:12 -0700

    Fix lint

--- run/system-package/src/test/java/com/example/cloudrun/AppTest.java
+++ run/system-package/src/test/java/com/example/cloudrun/AppTest.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
- package com.example.cloudrun;
+package com.example.cloudrun;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
