695af3f82277ecf92cd863a5b8da5077fa05f676
averikitsch <akitsch@google.com>
2019-10-17 13:40:28 -0700

    draft

---
+++ run/hello-broken/src/main/java/com/example/cloudrun/App.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.cloudrun;
+
+// [START run_broken_service]
+import static spark.Spark.get;
+import static spark.Spark.port;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+
+public class App {
+
+	private static Logger logger = Logger.getLogger(App.class.getName());
+
+  public static void main(String[] args) {
+    int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+    port(port);
+
+    get("/", (req, res) -> {
+			logger.info("Hello: received request.");
+			// [START run_broken_service_problem]
+			String name = System.getenv("NAME");
+			if (name == null) {
+				// Standard error logs do not appear in Stackdriver Error Reporting.
+				System.err.println("Environment validation failed.");
+				Exception e = new NullPointerException();
+				logger.log(Level.SEVERE, "Missing required server parameter");
+				res.status(500);
+				return "Internal Server Error";
+			}
+			// [END run_broken_service_problem]
+			res.status(200);
+			return String.format("Hello %s!", name);
+		});
+
+		get("/improved", (req, res) -> {
+			logger.info("Hello: received request.");
+			// [START run_broken_service_upgrade]
+			String name = System.getenv().getOrDefault("NAME", "World");
+			if (System.getenv("NAME") == null) {
+				logger.warning(String.format("NAME not set, default to %s", name));
+			}
+			// [END run_broken_service_upgrade]
+			res.status(200);
+			return String.format("Hello %s!", name);
+		});
+  }
+}
+// [END run_broken_service]
