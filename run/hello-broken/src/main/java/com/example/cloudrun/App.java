dce008161d34aaa58792601310c6f1216f3775a6
Adam Ross <adamross@google.com>
2019-10-24 11:09:35 -0700

    Whitespace checkstyle blind fix.

--- run/hello-broken/src/main/java/com/example/cloudrun/App.java
+++ run/hello-broken/src/main/java/com/example/cloudrun/App.java
@@ -49,8 +49,7 @@ public class App {
           res.status(200);
           return String.format("Hello %s!", name);
         });
-    // [END run_broken_service]
-
+        // [END run_broken_service]
     get(
         "/improved",
         (req, res) -> {
@@ -64,6 +63,7 @@ public class App {
           res.status(200);
           return String.format("Hello %s!", name);
         });
+
   // [START run_broken_service]
   }
 }
