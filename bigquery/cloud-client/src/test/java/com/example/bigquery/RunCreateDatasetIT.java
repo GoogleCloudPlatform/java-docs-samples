49e34439d728fbff0874bae0419bc7c5f0f381c4
stephwang <stephwang@google.com>
2019-11-25 16:04:55 -0500

    bigquery_create_dataset sample

---
+++ bigquery/cloud-client/src/test/java/com/example/bigquery/RunCreateDatasetIT.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquery;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.cloud.bigquery.Dataset;
+import com.google.cloud.bigquery.testing.RemoteBigQueryHelper;
+import org.junit.Test;
+
+public class RunCreateDatasetIT {
+
+  @Test
+  public void createDataset() {
+    String generatedDatasetName = RemoteBigQueryHelper.generateDatasetName();
+    Dataset dataset = RunCreateDataset.createDataset(generatedDatasetName);
+    assertEquals(generatedDatasetName, dataset.getDatasetId().getDataset());
+  }
+}
