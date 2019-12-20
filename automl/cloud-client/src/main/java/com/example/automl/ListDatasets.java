3cbd6057857a000e90c9b8fda113dd5ef46eda7f
Noah Negrey <nnegrey@users.noreply.github.com>
2019-12-20 08:40:06 -0700

    Update ListDatasets.java

--- automl/cloud-client/src/main/java/com/example/automl/ListDatasets.java
+++ automl/cloud-client/src/main/java/com/example/automl/ListDatasets.java
@@ -48,7 +48,6 @@ class ListDatasets {
       ListDatasetsRequest request =
           ListDatasetsRequest.newBuilder()
               .setParent(projectLocation.toString())
-              .setFilter("")
               .build();
 
       // List all the datasets available in the region by applying filter.
