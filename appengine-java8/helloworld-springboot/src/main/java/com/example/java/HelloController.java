e983adbe4a6c7143bd0cb4211e40caa0cb5cace1
averikitsch <akitsch@google.com>
2020-01-24 17:55:54 -0800

    Update springboot app

--- appengine-java8/helloworld-springboot/src/main/java/com/example/java/HelloController.java
+++ appengine-java8/helloworld-springboot/src/main/java/com/example/java/HelloController.java
@@ -14,34 +14,27 @@
  * limitations under the License.
  */
 
-package com.example.java.gettingstarted;
+package com.example.java;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
 @RestController
-public class HelloworldApplication {
-  @RequestMapping("/")
-  public String home() {
-    return "Hello World!";
-  }
+public class HelloController {
+    @RequestMapping("/")
+    String index(){
+        return "Hello World!";
+    }
 
-  /**
+    /**
    * (Optional) App Engine health check endpoint mapping.
    * @see <a href="https://cloud.google.com/appengine/docs/flexible/java/how-instances-are-managed#health_checking"></a>
    * If your app does not handle health checks, a HTTP 404 response is interpreted
    *     as a successful reply.
    */
-  @RequestMapping("/_ah/health")
-  public String healthy() {
-    // Message body required though ignored
-    return "Still surviving.";
-  }
-
-  public static void main(String[] args) {
-    SpringApplication.run(HelloworldApplication.class, args);
-  }
+    @RequestMapping("/_ah/health")
+    public String healthy() {
+      // Message body required though ignored
+      return "Still surviving.";
+    }
 }
