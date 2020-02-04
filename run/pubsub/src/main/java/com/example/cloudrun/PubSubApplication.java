323020cc70f5d868bbf7aaf4e248303b9c9f6266
averikitsch <akitsch@google.com>
2020-02-04 15:49:04 -0800

    Add region tags and fix lint

--- run/pubsub/src/main/java/com/example/cloudrun/PubSubApplication.java
+++ run/pubsub/src/main/java/com/example/cloudrun/PubSubApplication.java
@@ -24,7 +24,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PubSubApplication {
   public static void main(String[] args) {
     String port = System.getenv().getOrDefault("PORT", "8080");
-    System.setProperty("server.port" , port);
+    System.setProperty("server.port", port);
     SpringApplication.run(PubSubApplication.class, args);
   }
 }
