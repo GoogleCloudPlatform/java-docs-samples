b84f11b129e955eef71b31ea9f1f7be9c349b091
ace-n <anassri@google.com>
2020-03-13 22:11:38 -0700

    Address comments

--- functions/snippets/src/main/java/com/example/functions/SlackSlashCommand.java
+++ functions/snippets/src/main/java/com/example/functions/SlackSlashCommand.java
@@ -19,7 +19,6 @@ package com.example.functions;
 import com.github.seratch.jslack.app_backend.SlackSignature;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.ArrayMap;
 import com.google.api.services.kgsearch.v1.Kgsearch;
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
@@ -30,8 +29,6 @@ import com.google.gson.JsonObject;
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.security.GeneralSecurityException;
 import java.util.HashMap;
 import java.util.List;
@@ -41,9 +38,9 @@ import java.util.stream.Collectors;
 public class SlackSlashCommand implements HttpFunction {
 
   private Kgsearch kgClient;
-  private static String API_KEY;
-  private static String SLACK_SECRET;
-  private static final Logger LOGGER = Logger.getLogger(HelloHttp.class.getName());
+  private static final String API_KEY = System.getenv("KG_API_KEY");
+  private static final String SLACK_SECRET = System.getenv("SLACK_SECRET");
+  private static final Logger LOGGER = Logger.getLogger(SlackSlashCommand.class.getName());
   private SlackSignature.Verifier verifier;
   private Gson gson = new Gson();
 
@@ -51,17 +48,10 @@ public class SlackSlashCommand implements HttpFunction {
     kgClient = new Kgsearch.Builder(
         GoogleNetHttpTransport.newTrustedTransport(), new JacksonFactory(), null).build();
 
-    // Read + parse config file
-    Path configPath = Path.of(System.getProperty("user.dir"), "config.json");
-    JsonObject configJson = (new Gson()).fromJson(Files.readString(configPath), JsonObject.class);
-
-    SLACK_SECRET = configJson.get("SLACK_SECRET").getAsString();
-    API_KEY = configJson.get("KG_API_KEY").getAsString();
-
     verifier = new SlackSignature.Verifier(new SlackSignature.Generator(SLACK_SECRET));
   }
 
-  private boolean isValidSlackWebhook(HttpRequest request, String requestBody) throws IOException {
+  boolean isValidSlackWebhook(HttpRequest request, String requestBody) throws IOException {
 
     // Check for headers
     HashMap<String, List<String>> headers = new HashMap(request.getHeaders());
@@ -69,7 +59,6 @@ public class SlackSlashCommand implements HttpFunction {
         || !headers.containsKey("X-Slack-Signature")) {
       return false;
     }
-
     return verifier.isValid(
         headers.get("X-Slack-Request-Timestamp").get(0),
         requestBody,
@@ -77,14 +66,14 @@ public class SlackSlashCommand implements HttpFunction {
         1L);
   }
 
-  private void addPropertyIfPresent(
-      JsonObject target, String targetName, ArrayMap source, String sourceName) {
-    if (source.containsKey(sourceName)) {
-      target.addProperty(targetName, source.get(sourceName).toString());
+  void addPropertyIfPresent(
+      JsonObject target, String targetName, JsonObject source, String sourceName) {
+    if (source.has(sourceName)) {
+      target.addProperty(targetName, source.get(sourceName).getAsString());
     }
   }
 
-  private String formatSlackMessage(List<Object> kgResults, String query) {
+  String formatSlackMessage(JsonObject kgResponse, String query) {
     JsonObject attachmentJson = new JsonObject();
     JsonArray attachments = new JsonArray();
 
@@ -92,8 +81,10 @@ public class SlackSlashCommand implements HttpFunction {
     responseJson.addProperty("response_type", "in_channel");
     responseJson.addProperty("text", String.format("Query: %s", query));
 
+    JsonArray entityList = kgResponse.getAsJsonArray("itemListElement");
+
     // Extract the first entity from the result list, if any
-    if (kgResults.size() == 0) {
+    if (entityList.size() == 0) {
       attachmentJson.addProperty("text","No results match your query...");
 
       attachments.add(attachmentJson);
@@ -102,23 +93,23 @@ public class SlackSlashCommand implements HttpFunction {
       return gson.toJson(responseJson);
     }
 
-    ArrayMap entity = (ArrayMap) ((ArrayMap) kgResults.get(0)).get("result");
+    JsonObject entity = entityList.get(0).getAsJsonObject().getAsJsonObject("result");
 
     // Construct Knowledge Graph response attachment
-    String title = entity.get("name").toString();
-    if (entity.containsKey("description")) {
-      title = String.format("%s: %s", title, entity.get("description").toString());
+    String title = entity.get("name").getAsString();
+    if (entity.has("description")) {
+      title = String.format("%s: %s", title, entity.get("description").getAsString());
     }
     attachmentJson.addProperty("title", title);
 
-    if (entity.containsKey("detailedDescription")) {
-      ArrayMap detailedDescJson = (ArrayMap) entity.get("detailedDescription");
+    if (entity.has("detailedDescription")) {
+      JsonObject detailedDescJson = entity.getAsJsonObject("detailedDescription");
       addPropertyIfPresent(attachmentJson, "title_link", detailedDescJson, "url");
       addPropertyIfPresent(attachmentJson, "text", detailedDescJson, "articleBody");
     }
 
-    if (entity.containsKey("image")) {
-      ArrayMap imageJson = (ArrayMap) entity.get("image");
+    if (entity.has("image")) {
+      JsonObject imageJson = entity.getAsJsonObject("image");
       addPropertyIfPresent(attachmentJson, "image_url", imageJson, "contentUrl");
     }
 
@@ -129,12 +120,12 @@ public class SlackSlashCommand implements HttpFunction {
     return gson.toJson(responseJson);
   }
 
-  private List<Object> searchKnowledgeGraph(String query) throws IOException {
+  JsonObject searchKnowledgeGraph(String query) throws IOException {
     Kgsearch.Entities.Search kgRequest = kgClient.entities().search();
     kgRequest.setQuery(query);
     kgRequest.setKey(API_KEY);
 
-    return kgRequest.execute().getItemListElement();
+    return gson.fromJson(kgRequest.execute().toString(), JsonObject.class);
   }
 
   @Override
@@ -163,10 +154,10 @@ public class SlackSlashCommand implements HttpFunction {
     String query = body.get("text").getAsString();
 
     // Call knowledge graph API
-    List<Object> kgResults = searchKnowledgeGraph(query);
+    JsonObject kgResponse = searchKnowledgeGraph(query);
 
     // Format response to Slack
     BufferedWriter writer = response.getWriter();
-    writer.write(formatSlackMessage(kgResults, query));
+    writer.write(formatSlackMessage(kgResponse, query));
   }
 }
