6a9bcf28e3713f7c41d809392c580054205f783f
Mike <45373284+munkhuushmgl@users.noreply.github.com>
2020-03-18 11:15:02 -0700

    Update video/cloud-client/src/main/java/com/example/video/LogoDetection.java
    Co-Authored-By: Noah Negrey <nnegrey@users.noreply.github.com>

--- video/cloud-client/src/main/java/com/example/video/LogoDetection.java
+++ video/cloud-client/src/main/java/com/example/video/LogoDetection.java
@@ -48,7 +48,10 @@ public class LogoDetection {
 
   // [START video_detect_logo]
   public static void detectLogo(String filePath) throws Exception {
-    try (VideoIntelligenceServiceClient client = VideoIntelligenceServiceClient.create()) {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (VideoIntelligenceServiceClient client = VideoIntelligenceServiceClient.create()) {```
       // Read file
       Path path = Paths.get(filePath);
       byte[] data = Files.readAllBytes(path);
