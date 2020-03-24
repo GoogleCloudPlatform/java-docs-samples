027ad0bdbfd85b4f294dc00f9f1f4f8592e09614
averikitsch <akitsch@google.com>
2020-03-24 12:21:05 -0700

    initialize

---
+++ run/markdown-preview/editor/src/main/java/com/example/cloudrun/EditorApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EditorApplication.class, args);
+	}
+
+}
---
+++ run/markdown-preview/renderer/src/main/java/com/example/cloudrun/RendererApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RendererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RendererApplication.class, args);
+	}
+
+}
