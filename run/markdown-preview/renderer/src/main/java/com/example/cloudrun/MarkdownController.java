0fc3563b4c0f81fabf9c22b5ed405c3ef66f9379
averikitsch <akitsch@google.com>
2020-03-30 18:36:49 -0700

    add tests and lint

--- run/markdown-preview/renderer/src/main/java/com/example/cloudrun/MarkdownController.java
+++ run/markdown-preview/renderer/src/main/java/com/example/cloudrun/MarkdownController.java
@@ -1,15 +1,29 @@
-package com.example.cloudrun;
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.commonmark.Extension;
-import org.commonmark.node.Node;
-import org.commonmark.parser.Parser;
-import org.commonmark.renderer.html.HtmlRenderer;
+package com.example.cloudrun;
 
 import java.util.Arrays;
 import java.util.List;
-
+import org.commonmark.Extension;
 import org.commonmark.ext.gfm.strikethrough.StrikethroughExtension;
 import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,23 +33,28 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MarkdownController {
 
-    @PostMapping("/")
-    public String markdownRenderer(@RequestBody String payload) {
-        // Set up HTML renderer
-        // https://github.com/atlassian/commonmark-java#extensions
-        List<Extension> extensions = Arrays.asList(TablesExtension.create(), StrikethroughExtension.create());
-        Parser parser = Parser.builder().extensions(extensions).build();
-        Node document = parser.parse(payload);
-        HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
-        // Convert Markdown to HTML
-        String converted = renderer.render(document);
+  @PostMapping("/")
+  public String markdownRenderer(@RequestBody String payload) {
+    // Set up HTML renderer
+    // https://github.com/atlassian/commonmark-java#extensions
+    List<Extension> extensions =
+        Arrays.asList(TablesExtension.create(), StrikethroughExtension.create());
+    Parser parser = Parser.builder().extensions(extensions).build();
+    Node document = parser.parse(payload);
+    HtmlRenderer renderer = HtmlRenderer.builder().extensions(extensions).build();
+    // Convert Markdown to HTML
+    String converted = renderer.render(document);
 
-        // Use prepackaged policies to sanitize HTML. Cusomized and tighter standards
-        // are recommended.
-        PolicyFactory policy = Sanitizers.FORMATTING.and(Sanitizers.BLOCKS).and(Sanitizers.LINKS).and(Sanitizers.IMAGES)
-                .and(Sanitizers.TABLES);
-        String safeHTML = policy.sanitize(converted);
+    // Use prepackaged policies to sanitize HTML. Cusomized and tighter standards
+    // are recommended.
+    PolicyFactory policy =
+        Sanitizers.FORMATTING
+            .and(Sanitizers.BLOCKS)
+            .and(Sanitizers.LINKS)
+            .and(Sanitizers.IMAGES)
+            .and(Sanitizers.TABLES);
+    String safeHTML = policy.sanitize(converted);
 
-        return safeHTML;
-    }
+    return safeHTML;
+  }
 }
--- run/markdown-preview/renderer/src/main/java/com/example/cloudrun/RendererApplication.java
+++ run/markdown-preview/renderer/src/main/java/com/example/cloudrun/RendererApplication.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.cloudrun;
 
 import java.util.Collections;
@@ -9,20 +25,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RendererApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(RendererApplication.class);
+  private static final Logger logger = LoggerFactory.getLogger(RendererApplication.class);
 
-	public static void main(String[] args) {
-		String port = System.getenv("PORT");
+  public static void main(String[] args) {
+    String port = System.getenv("PORT");
     if (port == null) {
       port = "8080";
       logger.warn("Defaulting to port " + port);
     }
-		SpringApplication app = new SpringApplication(RendererApplication.class);
+    SpringApplication app = new SpringApplication(RendererApplication.class);
     app.setDefaultProperties(Collections.singletonMap("server.port", port));
 
     // Start the Spring Boot application.
     app.run(args);
-		logger.info("Listening on port " + port);
-	}
-
+    logger.info("Listening on port " + port);
+  }
 }
