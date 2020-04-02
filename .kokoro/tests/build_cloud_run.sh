a8cd3b542a558ca98bbef15b68668e3604084cf6
averikitsch <akitsch@google.com>
2020-04-02 14:57:32 -0700

    update kokoro tests

--- .kokoro/tests/build_cloud_run.sh
+++ .kokoro/tests/build_cloud_run.sh
@@ -14,57 +14,60 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -eo pipefail
+JIB=$(grep -o '<artifactId>jib-maven-plugin</artifactId>' pom.xml)
+if [ -n "$JIB" ]; then
+  set -eo pipefail
 
-# Register post-test cleanup.
-# Only needed if deploy completed.
-function cleanup {
+  # Register post-test cleanup.
+  # Only needed if deploy completed.
+  function cleanup {
+    set -x
+    gcloud container images delete "${CONTAINER_IMAGE}" --quiet --no-user-output-enabled || true
+    gcloud run services delete ${SERVICE_NAME} \
+      --platform=managed \
+      --region="${REGION:-us-central1}" \
+      --quiet --no-user-output-enabled
+    mvn clean
+  }
+  trap cleanup EXIT
+
+  requireEnv() {
+    test "${!1}" || (echo "Environment Variable '$1' not found" && exit 1)
+  }
+  requireEnv SAMPLE_NAME
+
+  # Version is in the format <PR#>-<GIT COMMIT SHA>.
+  # Ensures PR-based triggers of the same branch don't collide if Kokoro attempts
+  # to run them concurrently.
+  export SAMPLE_VERSION="${KOKORO_GIT_COMMIT:-latest}"
+  # Builds not triggered by a PR will fall back to the commit hash then "latest".
+  SUFFIX=${KOKORO_GITHUB_PULL_REQUEST_NUMBER:-${SAMPLE_VERSION:0:12}}
+  export SERVICE_NAME="${SAMPLE_NAME}-${SUFFIX}"
+  export CONTAINER_IMAGE="gcr.io/${GOOGLE_CLOUD_PROJECT}/run-${SAMPLE_NAME}:${SAMPLE_VERSION}"
+  export SPECIAL_BASE_IMAGE="gcr.io/${GOOGLE_CLOUD_PROJECT}/imagemagick"
+  BASE_IMAGE_SAMPLES=("image-processing" "system-packages")
+
+  # Build the service
   set -x
-  gcloud container images delete "${CONTAINER_IMAGE}" --quiet --no-user-output-enabled || true
-  gcloud run services delete ${SERVICE_NAME} \
-    --platform=managed \
+
+  mvn jib:build -Dimage="${CONTAINER_IMAGE}" \
+    `if [[ "${BASE_IMAGE_SAMPLES[@]}" =~ "${SAMPLE_NAME}" ]]; then echo "-Djib.from.image=${SPECIAL_BASE_IMAGE}"; fi`
+
+  gcloud run deploy "${SERVICE_NAME}" \
+    --image="${CONTAINER_IMAGE}" \
     --region="${REGION:-us-central1}" \
-    --quiet --no-user-output-enabled
-  mvn clean
-}
-trap cleanup EXIT
-
-requireEnv() {
-  test "${!1}" || (echo "Environment Variable '$1' not found" && exit 1)
-}
-requireEnv SAMPLE_NAME
-
-# Version is in the format <PR#>-<GIT COMMIT SHA>.
-# Ensures PR-based triggers of the same branch don't collide if Kokoro attempts
-# to run them concurrently.
-export SAMPLE_VERSION="${KOKORO_GIT_COMMIT:-latest}"
-# Builds not triggered by a PR will fall back to the commit hash then "latest".
-SUFFIX=${KOKORO_GITHUB_PULL_REQUEST_NUMBER:-${SAMPLE_VERSION:0:12}}
-export SERVICE_NAME="${SAMPLE_NAME}-${SUFFIX}"
-export CONTAINER_IMAGE="gcr.io/${GOOGLE_CLOUD_PROJECT}/run-${SAMPLE_NAME}:${SAMPLE_VERSION}"
-export SPECIAL_BASE_IMAGE="gcr.io/${GOOGLE_CLOUD_PROJECT}/imagemagick"
-BASE_IMAGE_SAMPLES=("image-processing" "system-packages")
-
-# Build the service
-set -x
-
-mvn jib:build -Dimage="${CONTAINER_IMAGE}" \
-  `if [[ "${BASE_IMAGE_SAMPLES[@]}" =~ "${SAMPLE_NAME}" ]]; then echo "-Djib.from.image=${SPECIAL_BASE_IMAGE}"; fi`
-
-gcloud run deploy "${SERVICE_NAME}" \
-  --image="${CONTAINER_IMAGE}" \
-  --region="${REGION:-us-central1}" \
-  --platform=managed \
-  --quiet --no-user-output-enabled  \
-  `if [ $SAMPLE_NAME = "image-processing" ]; then echo "--memory 512M"; fi`
+    --platform=managed \
+    --quiet --no-user-output-enabled  \
+    `if [ $SAMPLE_NAME = "image-processing" ]; then echo "--memory 512M"; fi`
 
 
-set +x
+  set +x
 
-echo
-echo '---'
-echo
+  echo
+  echo '---'
+  echo
 
+  # Do not use exec to preserve trap behavior.
+  "$@"
 
-# Do not use exec to preserve trap behavior.
-"$@"
+fi
