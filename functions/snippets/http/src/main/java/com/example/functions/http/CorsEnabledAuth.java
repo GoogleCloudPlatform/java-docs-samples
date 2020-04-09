99029893f464bb279665baf6988def976ca3dda6
ace-n <anassri@google.com>
2020-04-08 20:20:16 -0700

    GCF: clean up CORS sample + add CORS auth sample

--- functions/snippets/http/src/main/java/com/example/functions/http/CorsEnabledAuth.java
+++ functions/snippets/http/src/main/java/com/example/functions/http/CorsEnabledAuth.java
@@ -16,7 +16,7 @@
 
 package com.example.functions.http;
 
-// [START functions_http_cors]
+// [START functions_http_cors_auth]
 
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
@@ -25,29 +25,30 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 
-public class CorsEnabled implements HttpFunction {
-  // corsEnabled is an example of setting CORS headers.
+public class CorsEnabledAuth implements HttpFunction {
+  // corsEnabledAuth is an example of setting CORS headers.
   // For more information about CORS and CORS preflight requests, see
   // https://developer.mozilla.org/en-US/docs/Glossary/Preflight_request.
   @Override
   public void service(HttpRequest request, HttpResponse response)
       throws IOException {
     // Set CORS headers
-    //   Allows GETs from any origin with the Content-Type
-    //   header and caches preflight response for 3600s
-    response.appendHeader("Access-Control-Allow-Origin", "*");
+    //   Allows GETs from origin https://mydomain.com
+    //   with the Authorization header present
+    response.appendHeader("Access-Control-Allow-Origin", "https://mydomain.com");
+    response.appendHeader("Access-Control-Allow-Credentials", "true");
 
     if ("OPTIONS".equals(request.getMethod())) {
       response.appendHeader("Access-Control-Allow-Methods", "GET");
-      response.appendHeader("Access-Control-Allow-Headers", "Content-Type");
+      response.appendHeader("Access-Control-Allow-Headers", "Authorization");
       response.appendHeader("Access-Control-Max-Age", "3600");
       response.setStatusCode(HttpURLConnection.HTTP_NO_CONTENT);
       return;
     }
 
-    // Handle the main request.
+    // Handle the main request
     BufferedWriter writer = response.getWriter();
     writer.write("CORS headers set successfully!");
   }
 }
-// [END functions_http_cors]
+// [END functions_http_cors_auth]
