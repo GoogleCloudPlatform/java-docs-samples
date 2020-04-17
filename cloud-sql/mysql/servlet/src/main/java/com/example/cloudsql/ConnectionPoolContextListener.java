8f8c8aa327f1e17f3e2510fe608b3c34e6aebf42
kurtisvg <31518063+kurtisvg@users.noreply.github.com>
2020-04-16 17:12:02 -0700

    Fix lint errors.

--- cloud-sql/mysql/servlet/src/main/java/com/example/cloudsql/ConnectionPoolContextListener.java
+++ cloud-sql/mysql/servlet/src/main/java/com/example/cloudsql/ConnectionPoolContextListener.java
@@ -18,28 +18,33 @@ package com.example.cloudsql;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.logging.Logger;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
 import javax.sql.DataSource;
 
+@SuppressFBWarnings(
+    value = {"HARD_CODE_PASSWORD", "WEM_WEAK_EXCEPTION_MESSAGING"},
+    justification = "Extracted from environment, Exception message adds context.")
 @WebListener("Creates a connection pool that is stored in the Servlet's context for later use.")
 public class ConnectionPoolContextListener implements ServletContextListener {
 
-  private static final Logger LOGGER = Logger.getLogger(IndexServlet.class.getName());
-
   // Saving credentials in environment variables is convenient, but not secure - consider a more
   // secure solution such as https://cloud.google.com/kms/ to help keep secrets safe.
-  private static final String CLOUD_SQL_CONNECTION_NAME = System.getenv(
-      "CLOUD_SQL_CONNECTION_NAME");
+  private static final String CLOUD_SQL_CONNECTION_NAME =
+      System.getenv("CLOUD_SQL_CONNECTION_NAME");
   private static final String DB_USER = System.getenv("DB_USER");
   private static final String DB_PASS = System.getenv("DB_PASS");
   private static final String DB_NAME = System.getenv("DB_NAME");
 
+  @SuppressFBWarnings(
+      value = "USBR_UNNECESSARY_STORE_BEFORE_RETURN",
+      justification = "Necessary for sample region tag.")
   private DataSource createConnectionPool() {
     // [START cloud_sql_mysql_servlet_create]
     // The configuration object specifies behaviors for the connection pool.
@@ -102,12 +107,13 @@ public class ConnectionPoolContextListener implements ServletContextListener {
   private void createTable(DataSource pool) throws SQLException {
     // Safely attempt to create the table schema.
     try (Connection conn = pool.getConnection()) {
-      PreparedStatement createTableStatement = conn.prepareStatement(
+      String stmt =
           "CREATE TABLE IF NOT EXISTS votes ( "
               + "vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, candidate CHAR(6) NOT NULL,"
-              + " PRIMARY KEY (vote_id) );"
-      );
-      createTableStatement.execute();
+              + " PRIMARY KEY (vote_id) );";
+      try (PreparedStatement createTableStatement = conn.prepareStatement(stmt); ) {
+        createTableStatement.execute();
+      }
     }
   }
 
@@ -124,16 +130,19 @@ public class ConnectionPoolContextListener implements ServletContextListener {
   public void contextInitialized(ServletContextEvent event) {
     // This function is called when the application starts and will safely create a connection pool
     // that can be used to connect to.
-    DataSource pool = (DataSource) event.getServletContext().getAttribute("my-pool");
+    ServletContext servletContext = event.getServletContext();
+    DataSource pool = (DataSource) servletContext.getAttribute("my-pool");
     if (pool == null) {
       pool = createConnectionPool();
-      event.getServletContext().setAttribute("my-pool", pool);
+      servletContext.setAttribute("my-pool", pool);
     }
     try {
       createTable(pool);
     } catch (SQLException ex) {
-      throw new RuntimeException("Unable to verify table schema. Please double check the steps"
-          + "in the README and try again.", ex);
+      throw new RuntimeException(
+          "Unable to verify table schema. Please double check the steps"
+              + "in the README and try again.",
+          ex);
     }
   }
 }
