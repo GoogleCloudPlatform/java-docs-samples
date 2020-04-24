0e285fe8842f545832392848603b9e6ef179a3ec
ace-n <anassri@google.com>
2020-04-23 18:05:05 -0700

    Fix checkstyle

--- functions/helloworld/hello-pub-sub/src/test/java/functions/ExampleIntegrationTest.java
+++ functions/helloworld/hello-pub-sub/src/test/java/functions/ExampleIntegrationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package functions;
 
 // [START functions_pubsub_integration_test]
@@ -65,7 +81,6 @@ public class ExampleIntegrationTest {
     String name = UUID.randomUUID().toString();
     String nameBase64 = Base64.getEncoder().encodeToString(name.getBytes(StandardCharsets.UTF_8));
 
-    String expected = String.format("Hello %s!", name);
     String jsonStr = gson.toJson(Map.of("data", Map.of("data", nameBase64)));
 
     HttpPost postRequest =  new HttpPost(URI.create(functionUrl));
@@ -91,7 +106,8 @@ public class ExampleIntegrationTest {
     stdoutBytes.write(stdoutStream.readNBytes(stdoutStream.available()));
 
     // Verify desired name value is present
-    assertThat(stdoutBytes.toString(StandardCharsets.UTF_8)).contains(expected);
+    assertThat(stdoutBytes.toString(StandardCharsets.UTF_8)).contains(
+        String.format("Hello %s!", name));
   }
 }
 // [END functions_pubsub_integration_test]
--- functions/helloworld/hello-pub-sub/src/test/java/functions/ExampleSystemTest.java
+++ functions/helloworld/hello-pub-sub/src/test/java/functions/ExampleSystemTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package functions;
 
 // [START functions_pubsub_system_test]
@@ -58,7 +74,8 @@ public class ExampleSystemTest {
     Page<LogEntry> logEntries =
         loggingClient.listLogEntries(
             Logging.EntryListOption.filter(filter),
-            Logging.EntryListOption.sortOrder(Logging.SortingField.TIMESTAMP, Logging.SortingOrder.DESCENDING)
+            Logging.EntryListOption.sortOrder(
+                Logging.SortingField.TIMESTAMP, Logging.SortingOrder.DESCENDING)
         );
 
     // Serialize Stackdriver logging entries + collect them into a single string
