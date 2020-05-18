90fc774af91f91b356250df44bf466ad0b0e95bb
Ace Nassri <anassri@google.com>
2020-05-18 16:11:16 -0700

    Update HelloWorld.java

--- functions/helloworld/helloworld/src/main/java/functions/HelloWorld.java
+++ functions/helloworld/helloworld/src/main/java/functions/HelloWorld.java
@@ -14,9 +14,8 @@
  * limitations under the License.
  */
 
-package functions;
-
 // [START functions_helloworld_get]
+package functions;
 
 import com.google.cloud.functions.HttpFunction;
 import com.google.cloud.functions.HttpRequest;
