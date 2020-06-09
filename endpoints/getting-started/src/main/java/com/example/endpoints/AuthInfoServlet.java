5b6e87107d9692698aaa13954d26a15a66675424
averikitsch <akitsch@google.com>
2020-06-09 09:34:18 -0700

    lint

--- endpoints/getting-started/src/main/java/com/example/endpoints/AuthInfoServlet.java
+++ endpoints/getting-started/src/main/java/com/example/endpoints/AuthInfoServlet.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2017 Google Inc.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -12,17 +12,14 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
-
+ */
 
 package com.example.endpoints;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
-
 import java.io.IOException;
 import java.util.Base64;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,8 +27,8 @@ import javax.servlet.http.HttpServletResponse;
 
 // [START endpoints_auth_info_backend]
 /**
- * A servlet that returns authentication information.
- * See openapi.yaml for authentication mechanisms (e.g. JWT tokens, Google ID token).
+ * A servlet that returns authentication information. See openapi.yaml for authentication mechanisms
+ * (e.g. JWT tokens, Google ID token).
  */
 @WebServlet("/auth/info/*")
 public class AuthInfoServlet extends HttpServlet {
