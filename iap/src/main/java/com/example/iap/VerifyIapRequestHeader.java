8b8783de903c5dd9b0aa3d1112753567e676b5fd
Jeff Ching <chingor@google.com>
2020-06-24 13:50:12 -0700

    docs: clarify comment about subject/email claims

--- iap/src/main/java/com/example/iap/VerifyIapRequestHeader.java
+++ iap/src/main/java/com/example/iap/VerifyIapRequestHeader.java
@@ -65,7 +65,7 @@ public class VerifyIapRequestHeader {
     try {
       JsonWebToken jsonWebToken = tokenVerifier.verify(jwtToken);
 
-      // must have subject, email
+      // Verify that the token contain subject and email claims
       JsonWebToken.Payload payload = jsonWebToken.getPayload();
       return payload.getSubject() != null && payload.get("email") != null;
     } catch (TokenVerifier.VerificationException e) {
