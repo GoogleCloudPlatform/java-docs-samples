5d083ad2a367b5b130421d22d27bc9a27c061a67
averikitsch <akitsch@google.com>
2020-07-09 09:51:46 -0700

    update Springboot to use application.properties for port

--- run/events-pubsub/src/main/java/com/example/cloudrun/Application.java
+++ run/events-pubsub/src/main/java/com/example/cloudrun/Application.java
@@ -23,8 +23,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    String port = System.getenv().getOrDefault("PORT", "8080");
-    System.setProperty("server.port", port);
     SpringApplication.run(Application.class, args);
   }
 }
