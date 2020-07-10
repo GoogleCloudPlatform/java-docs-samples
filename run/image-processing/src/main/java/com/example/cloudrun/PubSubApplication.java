36f5996d66e5fe1a50334f1d2a516e3ae8236f10
Averi Kitsch <akitsch@google.com>
2020-07-10 15:30:21 -0700

    update Springboot to use application.properties for port (#3343)

--- run/image-processing/src/main/java/com/example/cloudrun/PubSubApplication.java
+++ run/image-processing/src/main/java/com/example/cloudrun/PubSubApplication.java
@@ -22,8 +22,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PubSubApplication {
   public static void main(String[] args) {
-    String port = System.getenv().getOrDefault("PORT", "8080");
-    System.setProperty("server.port", port);
     SpringApplication.run(PubSubApplication.class, args);
   }
 }
