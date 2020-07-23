88881e535b24c09160511232dca69ccd5c8abf94
aubrey-y <aubreym.yan@gmail.com>
2020-07-23 00:05:43 -0400

    linting fixes

--- run/endpoints-v2-backend/src/main/java/com/example/endpoints/controllers/RepeatController.java
+++ run/endpoints-v2-backend/src/main/java/com/example/endpoints/controllers/RepeatController.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.endpoints.controllers;
 
 import org.springframework.http.HttpStatus;
@@ -12,15 +28,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RepeatController {
 
-    @GetMapping("/repeat")
-    public ResponseEntity<String> repeat(@RequestParam("text") String text,
-                                         @RequestParam("times") Integer times) {
-        StringBuilder response = new StringBuilder();
-        for(int i = 0; i < times - 1; i++) {
-            response.append(text).append(", ");
-        }
-        response.append(text).append("!");
-
-        return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+  @GetMapping("/repeat")
+  public ResponseEntity<String> repeat(@RequestParam("text") String text,
+                                       @RequestParam("times") Integer times) {
+    StringBuilder response = new StringBuilder();
+    for (int i = 0; i < times - 1; i++) {
+      response.append(text).append(", ");
     }
+    response.append(text).append("!");
+
+    return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+  }
 }
