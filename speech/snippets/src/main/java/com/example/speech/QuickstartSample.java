googleapis/java-video-intelligence

samples: updating to latest google-cloud-* dependencies (#723)

samples/snippets/src/main/java/com/example/video/QuickstartSample.java
@@ -18,14 +18,14 @@ package com.example.video;
 
 // [START videointelligence_quickstart]
 import com.google.api.gax.grpc.OperationFuture;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceClient;
-import com.google.cloud.videointelligence.spi.v1beta1.VideoIntelligenceServiceSettings;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoRequest;
 import com.google.cloud.videointelligence.v1beta1.AnnotateVideoResponse;
 import com.google.cloud.videointelligence.v1beta1.Feature;
 import com.google.cloud.videointelligence.v1beta1.LabelAnnotation;
 import com.google.cloud.videointelligence.v1beta1.LabelLocation;
 import com.google.cloud.videointelligence.v1beta1.VideoAnnotationResults;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceClient;
+import com.google.cloud.videointelligence.v1beta1.VideoIntelligenceServiceSettings;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;

GoogleCloudPlatform/java-docs-samples

speech/snippets/src/main/java/com/example/speech/QuickstartSample.java
@@ -18,11 +18,11 @@ package com.example.speech;
 
 // [START speech_quickstart]
 // Imports the Google Cloud client library
-import com.google.cloud.speech.spi.v1.SpeechClient;
 import com.google.cloud.speech.v1.RecognitionAudio;
 import com.google.cloud.speech.v1.RecognitionConfig;
 import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
 import com.google.cloud.speech.v1.RecognizeResponse;
+import com.google.cloud.speech.v1.SpeechClient;
 import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
 import com.google.cloud.speech.v1.SpeechRecognitionResult;
 import com.google.protobuf.ByteString;