8f465c6e4c98f9aeafd9b59889728932c03d608e
Mike Ganbold <munkhbayarg@google.com>
2020-08-21 11:34:29 -0700

    samples(test): refactor automl test

--- automl/cloud-client/src/test/java/com/example/automl/GetOperationStatusTest.java
+++ automl/cloud-client/src/test/java/com/example/automl/GetOperationStatusTest.java
@@ -19,9 +19,18 @@ package com.example.automl;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.automl.v1.AutoMlClient;
+import com.google.cloud.automl.v1.DatasetName;
+import com.google.cloud.automl.v1.GcsDestination;
+import com.google.cloud.automl.v1.OperationMetadata;
+import com.google.cloud.automl.v1.OutputConfig;
+import com.google.protobuf.Empty;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -32,7 +41,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class GetOperationStatusTest {
   private static final String PROJECT_ID = System.getenv("AUTOML_PROJECT_ID");
-  private String operationId;
+  private static final String DATASET_ID = "TRL5394674636845744128";
+  private static final String GCS_OUTPUT_PREFIX =
+      String.format(
+          "gs://translate_data_exported/TEST_OUTPUT_%s/",
+          UUID.randomUUID().toString().replace("-", "_").substring(0, 26));
+
+  private String operationFullName;
   private ByteArrayOutputStream bout;
   private PrintStream out;
 
@@ -49,15 +64,27 @@ public class GetOperationStatusTest {
   }
 
   @Before
-  public void setUp() throws IOException {
+  public void setUp() throws IOException, ExecutionException, InterruptedException {
     bout = new ByteArrayOutputStream();
     out = new PrintStream(bout);
     System.setOut(out);
 
-    ListOperationStatus.listOperationStatus(PROJECT_ID);
-    String got = bout.toString();
-    operationId = got.split("\n")[1].split(":")[1].trim();
-    assertThat(got).contains("Operation details:");
+    // start a export data into dataset and cancel it before it finishes.
+    try (AutoMlClient client = AutoMlClient.create()) {
+      // Get the complete path of the dataset.
+      DatasetName datasetFullId = DatasetName.of(PROJECT_ID, "us-central1", DATASET_ID);
+
+      GcsDestination gcsSource =
+          GcsDestination.newBuilder().setOutputUriPrefix(GCS_OUTPUT_PREFIX).build();
+
+      OutputConfig outputConfig = OutputConfig.newBuilder().setGcsDestination(gcsSource).build();
+
+      // Start the export job
+      OperationFuture<Empty, OperationMetadata> operation =
+          client.exportDataAsync(datasetFullId, outputConfig);
+
+      operationFullName = operation.getName();
+    }
 
     bout = new ByteArrayOutputStream();
     out = new PrintStream(bout);
@@ -65,13 +92,22 @@ public class GetOperationStatusTest {
   }
 
   @After
-  public void tearDown() {
+  public void tearDown() throws IOException, InterruptedException {
+    // delete the cancelled operation
+    try (AutoMlClient client = AutoMlClient.create()) {
+      // wait for the operation to be cancelled
+      while (!client.getOperationsClient().getOperation(operationFullName).getDone()) {
+        Thread.sleep(1000);
+      }
+      client.getOperationsClient().deleteOperation(operationFullName);
+    }
+
     System.setOut(null);
   }
 
   @Test
   public void testGetOperationStatus() throws IOException {
-    GetOperationStatus.getOperationStatus(operationId);
+    GetOperationStatus.getOperationStatus(operationFullName);
     String got = bout.toString();
     assertThat(got).contains("Operation details:");
   }
