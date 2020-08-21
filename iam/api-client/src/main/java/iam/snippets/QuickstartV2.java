26339506c1708b55eabec1d88bbf6934d643ad89
melaniedejong <35782177+melaniedejong@users.noreply.github.com>
2020-08-21 16:59:48 -0700

    Fix addBinding method for quickstartV2 (#3594)
    * Fix addBinding method
    
    * Update imports
    
    * Lint

--- iam/api-client/src/main/java/iam/snippets/QuickstartV2.java
+++ iam/api-client/src/main/java/iam/snippets/QuickstartV2.java
@@ -98,22 +98,27 @@ public class QuickstartV2 {
     // Gets the project's policy.
     Policy policy = getPolicy(crmService, projectId);
 
-    // If binding already exists, adds member to binding.
-    List<Binding> bindings = policy.getBindings();
-    for (Binding b : bindings) {
+    // Finds binding in policy, if it exists
+    Binding binding = null;
+    for (Binding b : policy.getBindings()) {
       if (b.getRole().equals(role)) {
-        b.getMembers().add(member);
+        binding = b; 
         break;
       }
     }
 
-    // If binding does not exist, adds binding to policy.
-    Binding binding = new Binding();
-    binding.setRole(role);
-    binding.setMembers(Collections.singletonList(member));
-    policy.getBindings().add(binding);
+    if (binding != null) {
+      // If binding already exists, adds member to binding.
+      binding.getMembers().add(member);
+    } else {
+      // If binding does not exist, adds binding to policy.
+      binding = new Binding();
+      binding.setRole(role);
+      binding.setMembers(Collections.singletonList(member));
+      policy.getBindings().add(binding);
+    }
 
-    // Set the updated policy
+    // Sets the updated policy
     setPolicy(crmService, projectId, policy);
   }
 
@@ -123,9 +128,8 @@ public class QuickstartV2 {
     Policy policy = getPolicy(crmService, projectId);
 
     // Removes the member from the role.
-    List<Binding> bindings = policy.getBindings();
     Binding binding = null;
-    for (Binding b : bindings) {
+    for (Binding b : policy.getBindings()) {
       if (b.getRole().equals(role)) {
         binding = b;
         break;
