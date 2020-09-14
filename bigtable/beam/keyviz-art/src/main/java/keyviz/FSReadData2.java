96a17ca5f777caa8589fa6f36a0ee78a101255eb
Billy Jacobson <thebilly@google.com>
2020-09-14 15:40:27 -0400

    trying new fs keyviz stuff

--- bigtable/beam/keyviz-art/src/main/java/keyviz/FSReadData2.java
+++ bigtable/beam/keyviz-art/src/main/java/keyviz/FSReadData2.java
@@ -43,7 +43,7 @@ public class FSReadData2 {
 
     Pipeline p = Pipeline.create(options);
     // Initiates a new pipeline every second
-    p.apply(GenerateSequence.from(0).withRate(1, new Duration(1)))
+    p.apply(GenerateSequence.from(0).withRate(1, new Duration(1000)))
         .apply(ParDo.of(new ReadFromTableFn()));
 
     p.run();
@@ -58,7 +58,8 @@ public class FSReadData2 {
     protected Connection connection;
     protected Datastore datastore;
     private KeyFactory keyFactory;
-    private final String kind = "BillyDF500reversed1mswithP";
+    // private final String kind = "BillyDF500reversed1mswithP";
+    private final String kind = "BillyDF500reversed1s";
 
     private static final int RANDOM_ID_BOUND = 500;
     Random rand = new Random();
@@ -93,70 +94,80 @@ public class FSReadData2 {
       String numberFormat = "%0" + maxLength + "d";
       Datastore ds = getDatastore();
 
-      CompositeFilter evenRangeFilter = CompositeFilter
-          .and(
-              PropertyFilter.lt("__key__", keyFactory.newKey("250"))
-              // PropertyFilter.le("__key__", "1"),
-              // PropertyFilter.ge("__key__", "2"), PropertyFilter.le("__key__", "3"),
-              // PropertyFilter.ge("__key__", "4"), PropertyFilter.le("__key__", "5"),
-              // PropertyFilter.ge("__key__", "6"), PropertyFilter.le("__key__", "7"),
-              // PropertyFilter.ge("__key__", "8"), PropertyFilter.le("__key__", "9")
-          );
-
-      CompositeFilter oddRangeFilter = CompositeFilter
-          .and(
-              PropertyFilter.ge("__key__", keyFactory.newKey("250"))
-              // , PropertyFilter.le("__key__", ""),
-              // PropertyFilter.ge("__key__", "3"), PropertyFilter.le("__key__", "4"),
-              // PropertyFilter.ge("__key__", "5"), PropertyFilter.le("__key__", "6"),
-              // PropertyFilter.ge("__key__", "7"), PropertyFilter.le("__key__", "8"),
-              // PropertyFilter.ge("__key__", "9")
-          );
+      // CompositeFilter evenRangeFilter = CompositeFilter
+      //     .and(
+      //         PropertyFilter.lt("__key__", keyFactory.newKey("250"))
+      //         // PropertyFilter.le("__key__", "1"),
+      //         // PropertyFilter.ge("__key__", "2"), PropertyFilter.le("__key__", "3"),
+      //         // PropertyFilter.ge("__key__", "4"), PropertyFilter.le("__key__", "5"),
+      //         // PropertyFilter.ge("__key__", "6"), PropertyFilter.le("__key__", "7"),
+      //         // PropertyFilter.ge("__key__", "8"), PropertyFilter.le("__key__", "9")
+      //     );
+      //
+      // CompositeFilter oddRangeFilter = CompositeFilter
+      //     .and(
+      //         PropertyFilter.ge("__key__", keyFactory.newKey("250"))
+      //         // , PropertyFilter.le("__key__", ""),
+      //         // PropertyFilter.ge("__key__", "3"), PropertyFilter.le("__key__", "4"),
+      //         // PropertyFilter.ge("__key__", "5"), PropertyFilter.le("__key__", "6"),
+      //         // PropertyFilter.ge("__key__", "7"), PropertyFilter.le("__key__", "8"),
+      //         // PropertyFilter.ge("__key__", "9")
+      //     );
 
       EntityQuery.Builder query = Query.newEntityQueryBuilder()
           .setKind(kind);
-      if ((count / (2 * 60 * 1000)) % 2 == 0) {
-        query.setFilter(oddRangeFilter);
-      } else {
-        query.setFilter(evenRangeFilter);
-      }
+
+      // if ((count / (2 * 60 * 1000)) % 2 == 0) {
+      //   query.setFilter(oddRangeFilter);
+      // } else {
+      //   query.setFilter(evenRangeFilter);
+      // }
       QueryResults<Entity> entityQueryResults = ds.run(query.build());
       int c = 0;
-      while (entityQueryResults.hasNext()) {
-        Entity entity = entityQueryResults.next();
-        System.out.println(entity);
-        c++;
-      }
-      System.out.println(c + " rows queried");
-
+      // while (entityQueryResults.hasNext()) {
+      //   Entity entity = entityQueryResults.next();
+      //   System.out.println(entity);
+      //   c++;
+      // }
 
       // for (int i = 0; i < maxInput; i++) {
-      //
       //   String paddedRowkey = String.format(numberFormat, i);
       //   String reversedRowkey = new StringBuilder(paddedRowkey).reverse().toString();
-      //   int rowNumber = i / rowHeight;
-      //
-      //   float p = (float) rowNumber / numRows;
-      //   System.out.printf("index: %d rowNumber %d prob %f count %d", i, rowNumber, p, count);
-      //
-      //   if (Math.random() <= p || count < 3000) {
-      //     FullEntity<com.google.cloud.datastore.Key> fullEntity =
-      //         com.google.cloud.datastore.Entity.newBuilder(keyFactory.newKey(reversedRowkey))
-      //             .set("description", getRandomStringValue())
-      //             .build();
-      //     entities.add(fullEntity);
-      //
-      //     // logger.info("writing entity" + fullEntity.getKey());
-      //
-      //   }
-      //   // 500 per write limit
-      //   if (i % 500 == 0) {
-      //     System.out.printf("500 mod 0");
-      //     ds.put(entities.toArray(new FullEntity<?>[0]));
-      //     entities = new ArrayList<>();
-      //   }
+      //   datastore.get(keyFactory.newKey(reversedRowkey));
+      //   c++;
       // }
-      // ds.put(entities.toArray(new FullEntity<?>[0]));
+      // System.out.println(c + " entities fetched");
+
+
+      for (int i = 0; i < maxInput; i++) {
+
+        String paddedRowkey = String.format(numberFormat, i);
+        String reversedRowkey = new StringBuilder(paddedRowkey).reverse().toString();
+        int rowNumber = i / rowHeight;
+
+        float p = (float) rowNumber / numRows;
+        System.out.printf("index: %d rowNumber %d prob %f count %d", i, rowNumber, p, count);
+
+        if (Math.random() <= p || count < 120) {
+          FullEntity<com.google.cloud.datastore.Key> fullEntity =
+              com.google.cloud.datastore.Entity.newBuilder(keyFactory.newKey(reversedRowkey))
+                  .set("description", getRandomStringValue())
+                  .build();
+          entities.add(fullEntity);
+          // logger.info("writing entity" + fullEntity.getKey());
+          c++;
+        }
+        // 500 per write limit
+        if (i % 500 == 0) {
+          System.out.printf("500 mod 0");
+          ds.put(entities.toArray(new FullEntity<?>[0]));
+          entities = new ArrayList<>();
+        }
+      }
+
+      System.out.println(c + " entities updated");
+
+      ds.put(entities.toArray(new FullEntity<?>[0]));
 
       // String kind = "Billy10ms1krs";
       // int maxInput = 1000;
