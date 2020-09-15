bb576d3570ce2ea655a42fb2b6bd58ca4dbeab06
Billy Jacobson <thebilly@google.com>
2020-09-15 14:51:00 -0400

    nobody

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -85,7 +85,7 @@ if [[ "$SCRIPT_DEBUG" != "true" ]]; then
 
     # Start Memcached server
     apt-get update && apt-get install -y memcached libmemcached-tools
-    memcached -U 0 &
+    memcached -u nobody &
     memcstat --servers="127.0.0.1"
 
     cd github/java-docs-samples
