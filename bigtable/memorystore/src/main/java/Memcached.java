a16ef4ef9e46ef918f29248687fc86f542c3a149
Billy Jacobson <billyjacobson@users.noreply.github.com>
2020-10-13 13:45:28 -0400

    Sample for Bigtable and Memcached (#3717)
    * memorystore WIP
    
    * adding bigtable to memorystore example
    
    * cache example working
    
    * tests written
    
    * Adding memcached to run_tests.sh
    
    * add some logs
    
    * adding logs around get from cache
    
    * try with memcached install
    
    * remove sudo
    
    * add yes
    
    * fix lint
    
    * add memcstat
    
    * install tools
    
    * run as root
    
    * -U 0
    
    * nobody
    
    * set c
    
    * remove memcstat
    
    * echo
    
    * remove debug
    
    * launch memcached docker container
    
    * logs
    
    * log attempt 2
    
    * sout stack
    
    * stack system out
    
    * cleanup and get params from system
    
    * change tests to use system props
    
    * uncomment docker start
    
    * Add README.md

---
+++ bigtable/memorystore/src/main/java/Memcached.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.models.Row;
+import java.net.InetSocketAddress;
+import java.util.Arrays;
+import net.spy.memcached.MemcachedClient;
+
+public class Memcached {
+
+  public static void main(String[] args) {
+    memcachedBigtable(System.getProperty("bigtableProjectId"),
+        System.getProperty("bigtableInstanceId"),
+        System.getProperty("bigtableTableId"),
+        System.getProperty("memcachedHostIP"));
+  }
+
+
+  public static void memcachedBigtable(String projectId, String instanceId, String tableId,
+      String hostname) {
+    // String projectId = "my-project-id";
+    // String instanceId = "my-instance-id";
+    // String tableId = "mobile-time-series";
+    // String hostname = "localhost";
+
+    try {
+      MemcachedClient mcc = new MemcachedClient(new InetSocketAddress(hostname, 11211));
+      System.out.println("Connected to Memcached successfully");
+
+      // Get value from cache
+      String rowkey = "phone#4c410523#20190501";
+      String columnFamily = "stats_summary";
+      String column = "os_build";
+      String cacheKey = String.format("%s:%s:%s", rowkey, columnFamily, column);
+
+      Object value = mcc.get(cacheKey);
+
+      if (value != null) {
+        System.out.println("Value fetched from cache: " + value);
+      } else {
+        System.out.println("didn't get value from cache");
+        // Get data from Bigtable source and add to cache for 10 seconds.
+        try (BigtableDataClient dataClient = BigtableDataClient.create(projectId, instanceId)) {
+          Row row = dataClient.readRow(tableId, rowkey);
+          String cellValue = row.getCells(columnFamily, column).get(0).getValue().toStringUtf8();
+          System.out.println("got data from bt " + cellValue);
+          // Set data into memcached server.
+          mcc.set(cacheKey, 10, cellValue);
+          System.out.println("Value fetched from Bigtable: " + cellValue);
+        } catch (Exception e) {
+          System.out.println("Could not set cache value.");
+          e.printStackTrace();
+        }
+      }
+      mcc.shutdown();
+    } catch (Exception e) {
+      System.out.println("Could not get cache value.");
+      e.printStackTrace();
+    }
+  }
+}
