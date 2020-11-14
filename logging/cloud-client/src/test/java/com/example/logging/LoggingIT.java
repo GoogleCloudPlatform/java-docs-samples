94da5a75a4f76bbdef95d1b7dc3d1f7978a797a4
Shubha Rajan <shubhadayini@google.com>
2020-11-14 01:03:26 -0800

    increase test timeout to 50000

--- logging/cloud-client/src/test/java/com/example/logging/LoggingIT.java
+++ logging/cloud-client/src/test/java/com/example/logging/LoggingIT.java
@@ -71,7 +71,7 @@ public class LoggingIT {
     assertThat(got).contains("Logged: Hello, world!");
   }
 
-  @Test(timeout = 45000)
+  @Test(timeout = 50000)
   public void testWriteAndListLogs() throws Exception {
     // write a log entry
     LogEntry entry =
