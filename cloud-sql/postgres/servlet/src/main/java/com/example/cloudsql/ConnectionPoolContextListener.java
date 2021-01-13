1f4b6532f4edbd1034bb8a2d8a7f546bb601c5f7
Shubha Rajan <shubhadayini@google.com>
2021-01-13 09:31:47 -0800

    Add tests for Cloud SQL samples (#4495)
    * add integration tests for mysql
    
    * source Cloud SQL secrets in run_tests.sh
    
    * add integration tests to postgres sample
    
    * add tests for SQL server sample
    
    * fix typo in run_tests.sh
    
    * reformat files
    
    * create table before tests
    
    * fix linting
    
    * update license headers
    
    * rename files
    
    * use POJO instead of hashmap
    
    * move TemplateData class
    
    * move TemplateData class
    
    * move TemplateData class
    
    * add cleanup method to drop table
    
    * add uuid to table name
    
    * fix linting

--- cloud-sql/postgres/servlet/src/main/java/com/example/cloudsql/ConnectionPoolContextListener.java
+++ cloud-sql/postgres/servlet/src/main/java/com/example/cloudsql/ConnectionPoolContextListener.java
@@ -66,6 +66,7 @@ public class ConnectionPoolContextListener implements ServletContextListener {
     config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
     config.addDataSourceProperty("cloudSqlInstance", CLOUD_SQL_CONNECTION_NAME);
 
+
     // The ipTypes argument can be used to specify a comma delimited list of preferred IP types 
     // for connecting to a Cloud SQL instance. The argument ipTypes=PRIVATE will force the 
     // SocketFactory to connect with an instance's associated private IP. 
