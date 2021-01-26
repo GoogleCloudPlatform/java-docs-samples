eecd0e1916f715a0dd48bdebae965cc8bb7c8b0f
Billy Jacobson <thebilly@google.com>
2021-01-26 14:32:32 -0500

    Sample for configuring Bigtable connection pool

---
+++ bigtable/hbase/snippets/src/test/java/com/example/bigtable/ConfigureConnectionPoolTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigtable;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.hamcrest.CoreMatchers;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ConfigureConnectionPoolTest {
+
+  private static final String INSTANCE_ENV = "BIGTABLE_TESTING_INSTANCE";
+  private static String projectId;
+  private static String instanceId;
+  private ByteArrayOutputStream bout;
+
+  private static String requireEnv(String varName) {
+    assertNotNull(
+        System.getenv(varName),
+        "Environment variable '%s' is required to perform these tests.".format(varName));
+    return System.getenv(varName);
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    projectId = requireEnv("GOOGLE_CLOUD_PROJECT");
+    instanceId = requireEnv(INSTANCE_ENV);
+  }
+
+  @Before
+  public void setupStream() {
+    bout = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bout));
+  }
+
+  @Test
+  public void testConfigureConnectionPool() {
+    ConfigureConnectionPool.configureConnectionPool(projectId, instanceId);
+
+    String output = bout.toString();
+    assertThat(output, CoreMatchers.containsString("Connected with pool size of 250"));
+  }
+}
