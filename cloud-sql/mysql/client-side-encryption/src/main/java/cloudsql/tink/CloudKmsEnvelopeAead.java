7a1dbf42c001d3aaaff544d3434e74476748a229
Shubha Rajan <shubhadayini@google.com>
2021-03-07 23:19:01 -0800

    fix: replace deprecated envelope encryption method in Tink sample

--- cloud-sql/mysql/client-side-encryption/src/main/java/cloudsql/tink/CloudKmsEnvelopeAead.java
+++ cloud-sql/mysql/client-side-encryption/src/main/java/cloudsql/tink/CloudKmsEnvelopeAead.java
@@ -31,17 +31,15 @@ public class CloudKmsEnvelopeAead {
 
   public static Aead get(String kmsUri) throws GeneralSecurityException {
     AeadConfig.register();
-    // Generate a new envelope key template, then generate key material.
-    KeyTemplate kmsEnvKeyTemplate = AeadKeyTemplates
-        .createKmsEnvelopeAeadKeyTemplate(kmsUri, AeadKeyTemplates.AES128_GCM);
-    KeysetHandle keysetHandle = KeysetHandle.generateNew(kmsEnvKeyTemplate);
 
-    // Register the KMS client.
-    KmsClients.add(new GcpKmsClient()
-        .withDefaultCredentials());
+    // Create a new KMS Client
+    KmsClient client = new GcpKmsClient().withDefaultCredentials();
 
-    // Create envelope AEAD primitive from keysetHandle
-    return keysetHandle.getPrimitive(Aead.class);
+    // Create an AEAD primitive using the Cloud KMS key
+    Aead gcpAead = client.getAead(kmsUri);
+
+    // Create an envelope AEAD primitive
+    return new KmsEnvelopeAead(AeadKeyTemplates.AES128_GCM, gcpAead);
   }
 }
 // [END cloud_sql_mysql_cse_key]
