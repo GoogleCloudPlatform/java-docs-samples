bd3ca1332d012d4ac55ab15942ae01ef52e7ae39
Jesse Lovelace <jesselovelace@google.com>
2021-03-09 14:32:57 -0800

    fix copyright

--- storage/storage-transfer/src/main/java/com/google/cloud/storage/storagetransfer/samples/CreateTransferClient.java
+++ storage/storage-transfer/src/main/java/com/google/cloud/storage/storagetransfer/samples/CreateTransferClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Google Inc.
+ * Copyright 2021 Google Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,8 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-// [START all]
 
 package com.google.cloud.storage.storagetransfer.samples;
 
