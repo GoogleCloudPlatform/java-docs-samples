8faccb86e86b404b15bc51694a9d7d2a1c216204
sitalakshmi <sitalakshmi@google.com>
2021-06-03 03:34:32 +0530

    Adding test file and pom.xml. Refactored Quickstart.java to include wait operation.

--- compute/cloud-client/src/main/java/compute/Quickstart.java
+++ compute/cloud-client/src/main/java/compute/Quickstart.java
@@ -1,37 +1,36 @@
+package src.main.java.compute;
+
 import com.google.cloud.compute.v1.AttachedDisk;
 import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
-import com.google.cloud.compute.v1.GlobalOperationClient;
 import com.google.cloud.compute.v1.Instance;
 import com.google.cloud.compute.v1.InstanceClient;
-import com.google.cloud.compute.v1.InstanceTemplate;
 import com.google.cloud.compute.v1.InstancesScopedList;
 import com.google.cloud.compute.v1.NetworkInterface;
 import com.google.cloud.compute.v1.Operation;
-import com.google.cloud.compute.v1.ProjectGlobalOperationName;
-import com.google.cloud.compute.v1.ProjectRegionOperationName;
 import com.google.cloud.compute.v1.ProjectZoneInstanceName;
 import com.google.cloud.compute.v1.ProjectZoneMachineTypeName;
 import com.google.cloud.compute.v1.ProjectZoneName;
 import com.google.cloud.compute.v1.ProjectZoneOperationName;
-import com.google.cloud.compute.v1.RegionOperationClient;
 import com.google.cloud.compute.v1.ZoneOperationClient;
 import java.io.IOException;
 
 public class Quickstart {
 
-//  [START compute_instances_create]
+
+  // [START compute_instances_create]
 
   /**
    * Creates a GCP Compute Instance
    * @param project Google Cloud Project id
    * @param zone    Google Cloud Project zone eg: "us-central1-a"
    * @param machineType Type of the instance that you want to create, eg: "n1-standard-1"
-   * @param machineName Name to identify the machine
+   * @param machineName Name to identify the machine/ instance
    * @param sourceImage Image to be mounted in the machine. eg: "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20150710"
    * @param diskSizeGB Size of the disk to be allocated for the machine. eg: "10" in GBs
    */
 
-  private void createInstance(String project, String zone, String machineName, String machineType, String sourceImage, String diskSizeGB) {
+  public void createInstance(String project, String zone, String machineName, String machineType, String sourceImage, String diskSizeGB)
+      throws IOException {
 
     try (InstanceClient instanceClient = InstanceClient.create()) {
       // Instance creation requires at least one persistent disk and one network interface
@@ -47,6 +46,7 @@ public class Quickstart {
       // "default" network interface is created automatically for every project
       NetworkInterface networkInterface = NetworkInterface.newBuilder().setName("default").build();
 
+
       // Bind machine_name, machine_type, disk and network interface to an instance
       Instance instanceResource =
           Instance.newBuilder()
@@ -56,17 +56,21 @@ public class Quickstart {
               .addNetworkInterfaces(networkInterface)
               .build();
 
+      // Inserting the instance in the specified project and zone
       ProjectZoneName projectZoneName = ProjectZoneName.of(project, zone);
       Operation response = instanceClient.insertInstance(projectZoneName, instanceResource);
+
+      // Waits until the insertion operation is completed
+      while(!response.getStatus().equalsIgnoreCase("DONE")) {
+        ZoneOperationClient zoneOperationClient = ZoneOperationClient.create();
+        response = zoneOperationClient.waitZoneOperation(ProjectZoneOperationName.newBuilder().setOperation(response.getName()).setProject(project).setZone(zone).build());
+      }
 
       if (response.getError() == null) {
         System.out.println("Instance creation successful ! ! ");
       } else {
         System.out.println("Instance creation failed ! ! " + response.getError());
       }
-
-    }catch (Exception e) {
-      System.out.println("Instance creation failed ! ! " + e.getCause());
     }
   }
 
@@ -84,17 +88,17 @@ public class Quickstart {
    * @param zone Google Cloud Project zone
    */
 
-  private void listInstances(String project, String zone) {
+  public void listInstances(String project, String zone) throws IOException {
     try(InstanceClient instanceClient = InstanceClient.create()) {
       // Set the project and zone to retrieve instances present in the zone
       ProjectZoneName projectZoneName = ProjectZoneName.newBuilder().setProject(project).setZone(zone).build();
+      System.out.println("Listing instances ! ! ");
       for(Instance zoneInstance : instanceClient.listInstances(projectZoneName).iterateAll()) {
         System.out.println(zoneInstance.getName());
       }
-    }catch (Exception e) {
-      System.out.println("Failed to retrieve instances ! ! " + e.getCause());
     }
   }
+
 //  [END compute_instances_list]
 
 
@@ -108,10 +112,11 @@ public class Quickstart {
    * @param project Google Cloud Project id
    */
 
-  private void listAllInstances(String project) {
+  public void listAllInstances(String project) throws IOException {
     try(InstanceClient instanceClient = InstanceClient.create()) {
       // Listing all instances for the project
-      InstanceClient.AggregatedListInstancesPagedResponse response = instanceClient.aggregatedListInstances(false, project);
+      InstanceClient.AggregatedListInstancesPagedResponse response = instanceClient.aggregatedListInstances(true, project);
+      System.out.println("Listing instances ! ! ");
       for(InstancesScopedList zoneInstances : response.iterateAll()) {
         // Scoped by each zone; Check if instances in a zone is null, else iterate and list them
         if(zoneInstances.getInstancesList() != null) {
@@ -122,10 +127,9 @@ public class Quickstart {
           }
         }
       }
-    }catch (Exception e) {
-      System.out.println("Failed to retrieve instances ! ! " + e.getCause());
     }
   }
+
 // [END compute_instances_list_all]
 
 
@@ -140,56 +144,75 @@ public class Quickstart {
    * @param zone Google Cloud Project zone
    * @param instanceName Name of the instance to be deleted
    */
-  private void deleteInstance(String project, String zone, String instanceName) {
+
+  public void deleteInstance(String project, String zone, String instanceName)
+      throws IOException, InterruptedException {
     try(InstanceClient instanceClient = InstanceClient.create()) {
+
       // Set the properties of the instance which is to be deleted
       ProjectZoneInstanceName instance = ProjectZoneInstanceName.newBuilder().setProject(project).setZone(zone).setInstance(instanceName).build();
       Operation response = instanceClient.deleteInstance(instance);
 
+
+//      [START compute_instances_operation_check]
+      ZoneOperationClient zoneOperationClient = ZoneOperationClient.create();
+      // Waits until the delete operation is completed
+      while(!response.getStatus().equalsIgnoreCase("DONE")) {
+        response = zoneOperationClient.getZoneOperation(ProjectZoneOperationName.newBuilder().setOperation(response.getId()).setProject(project).setZone(zone).build());
+        if(!response.getStatus().equalsIgnoreCase("DONE")) {
+          System.out.println("Deletion in process...");
+        }
+      }
+//      [END compute_instances_operation_check]
+
+
       if(response.getError() == null) {
         System.out.println("Instance deleted successfully ! ! ");
       }else {
-        System.out.println("Error in instance deletion ! ! " + response.getError());
+        System.out.println("Failed to delete instance ! ! " + response.getError());
       }
-    }catch (Exception e) {
-      System.out.println("Failed to delete the specified instance ! ! " + e.getCause());
     }
   }
+
 // [END compute_instances_delete]
 
 
 
 
 
-  private void initialize(String projectId, String zone, String machineName, String machineType, String sourceImage, String diskSizeGB) {
+  public void quickstart() throws IOException, InterruptedException {
+    // TODO(developer): Replace the values for below variables before running the template
+    String projectId = "your_project_id",  // Google Cloud project id
+        zone = "zone_name",                // Google Cloud zone name  eg: "us-central1-a"
+        machineName = "machine_name",      // Name to identify the machine/ instance
+        machineType = "machine_type",      // Type of the machine - "n1-standard-1"
+        sourceImage = "source_image",      // Image to be mounted on the machine - "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20150710"
+        diskSizeGB = "disk_size";          // Default size of the disk - 10
 
-    // Action 1: Create Instance
-    this.createInstance(projectId, zone, machineName, machineType, sourceImage, diskSizeGB);
-
-    // Action 2: List Instances
-    this.listInstances(projectId, zone);
-
-    // Action 3: List all Instances
-    this.listAllInstances(projectId);
-
-    // Action 4: Delete Instance
-    String instanceName = "instance_name_to_delete";
-    this.deleteInstance(projectId, zone, instanceName);
-
+    quickstart(projectId, zone, machineName, machineType, sourceImage, diskSizeGB);
   }
 
 
 
 
-  public static void main(String[] args) {
-    String projectId = args[0],  // Google Cloud project id
-        zone = args[1],          // Google Cloud zone name  eg: "us-central1-a"
-        machineName = args[2],   // Name to identify the machine eg: "instance-name"
-        machineType = args[3],   // Type of the machine eg: "n1-standard-1"
-        sourceImage = args[4],  // image to be mounted on the machine eg: "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20150710"
-        diskSizeGB = args[5];   // default size of the disk eg: "10"
 
-    new Quickstart().initialize(projectId, zone, machineName, machineType, sourceImage, diskSizeGB);
+  public void quickstart(String projectId, String zone, String machineName, String machineType, String sourceImage, String diskSizeGB)
+      throws IOException, InterruptedException {
+    Quickstart quickstart = new Quickstart();
+
+    // Action 1: Create Instance
+    quickstart.createInstance(projectId, zone, machineName, machineType, sourceImage, diskSizeGB);
+
+    // Action 2: List the created instance in that zone
+    quickstart.listInstances(projectId, zone);
+
+    // Action 3: List all instances in the given project
+    quickstart.listAllInstances(projectId);
+
+    // Action 4: Delete the created Instance
+    quickstart.deleteInstance(projectId, zone, machineName);
+
+    System.out.println("Successfully completed quickstart ! ! ");
   }
 
 }
---
+++ compute/cloud-client/src/test/java/compute/QuickstartIT.java
@@ -0,0 +1,65 @@
+package src.test.java.compute;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(JUnit4.class)
+public class QuickstartIT {
+
+  private static final String PROJECT_ID = System.getenv("GOOGLE_PROJECT_ID");
+  private static String ZONE;
+  private static String MACHINE_NAME;
+  private static String MACHINE_TYPE;
+  private static String SOURCE_IMAGE;
+  private static String DISK_GB;
+
+  private ByteArrayOutputStream stdOut;
+
+  public static void requireEnvVar(String envVarName) {
+    assertNotNull(System.getenv(envVarName));
+  }
+
+
+  @BeforeClass
+  public static void checkRequirements() {
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_PROJECT_ID");
+
+    ZONE = "us-central1-a";
+    MACHINE_NAME = "my-new-test-instance";
+    MACHINE_TYPE = "n1-standard-1";
+    SOURCE_IMAGE = "https://www.googleapis.com/compute/v1/projects/debian-cloud/global/images/debian-7-wheezy-v20150710";
+    DISK_GB = "10";
+  }
+
+
+  @Before
+  public void beforeEach() {
+    stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
+  }
+
+  @After
+  public void afterEach() {
+    stdOut = null;
+    System.setOut(null);
+  }
+
+
+  @Test public void testQuickstart() throws IOException, InterruptedException {
+    new Quickstart().quickstart(PROJECT_ID, ZONE, MACHINE_NAME, MACHINE_TYPE, SOURCE_IMAGE, DISK_GB);
+    assertThat(stdOut.toString()).contains("Successfully completed quickstart ! ! ");
+  }
+
+}
