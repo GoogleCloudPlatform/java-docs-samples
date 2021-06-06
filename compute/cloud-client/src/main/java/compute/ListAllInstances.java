32941265b1b8a7d5876cfbeff3bd5b49fd42c92d
sitalakshmis <79585041+sitalakshmis@users.noreply.github.com>
2021-06-06 23:49:12 +0530

    Google cloud client compute samples (#5253)
    * Adding initial samples to demonstrate how to use google-cloud-compute client library
    
    * fixup! Adding initial samples to demonstrate how to use google-cloud-compute client library
    
    * fixup! Adding initial samples to demonstrate how to use google-cloud-compute client library
    
    * fixup! Adding initial samples to demonstrate how to use google-cloud-compute client library
    
    * Adding test file and pom.xml. Refactored Quickstart.java to include wait operation.
    
    * Fixed lint errors and added java version in pom.xml
    
    * Fixed the version to match the diregapic
    
    * Env variable setup in test and lint fix
    
    * Adding UUID
    
    * Refactored code into separate files and performed the requested changes.
    
    * included lint changes and increased timeout
    
    * modified error checking mechanism
    
    * Update copyright compute/cloud-client/src/test/java/compute/SnippetsIT.java
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>
    
    * Update pom.xml
    
    Co-authored-by: Kurtis Van Gent <31518063+kurtisvg@users.noreply.github.com>

---
+++ compute/cloud-client/src/main/java/compute/ListAllInstances.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute;
+
+// [START compute_instances_list_all]
+
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.InstancesScopedList;
+import java.io.IOException;
+import java.util.Map;
+
+public class ListAllInstances {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample
+    String project = "your-project-id";
+    listAllInstances(project);
+  }
+
+  // lists all instances available in the specified project id
+  public static void listAllInstances(String project) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+      // Listing all instances for the project
+      InstancesClient.AggregatedListPagedResponse response = instancesClient
+          .aggregatedList(project);
+      for (Map.Entry<String, InstancesScopedList> zoneInstances : response.iterateAll()) {
+        // instances scoped by each zone
+        String zone = zoneInstances.getKey();
+        if (!zoneInstances.getValue().getInstancesList().isEmpty()) {
+          // zoneInstances.getKey() returns the fully qualified address.
+          // Hence, strip it to get the zone name only
+          System.out.println(
+              String.format("Instances at %s: ", zone.substring(zone.lastIndexOf('/') + 1)));
+          for (Instance instance : zoneInstances.getValue().getInstancesList()) {
+            System.out.println(instance.getName());
+          }
+        }
+      }
+      System.out.println("####### Listing all instances complete #######");
+    }
+  }
+
+}
+// [END compute_instances_list_all]
---
+++ compute/cloud-client/src/main/java/compute/ListInstance.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute;
+
+//  [START compute_instances_list]
+
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import java.io.IOException;
+
+public class ListInstance {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample
+    String project = "your-project-id";
+    String zone = "zone-name";
+    listInstances(project, zone);
+  }
+
+  // lists all instances that are available in the given zone in the specified project id
+  public static void listInstances(String project, String zone) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+      // Set the project and zone to retrieve instances present in the zone
+      System.out.println(String.format("Listing instances from %s in %s:", project, zone));
+      for (Instance zoneInstance : instancesClient.list(project, zone).iterateAll()) {
+        System.out.println(zoneInstance.getName());
+      }
+      System.out.println("####### Listing instances complete #######");
+    }
+  }
+}
+//  [END compute_instances_list]
