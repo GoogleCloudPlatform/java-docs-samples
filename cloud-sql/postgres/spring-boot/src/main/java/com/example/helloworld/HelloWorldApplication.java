9a4089f8905889deafcc41a5e73485892e803ecc
Eno Compton <enocom@google.com>
2021-06-30 13:10:30 -0600

    chore: add small spring boot app

---
+++ cloud-sql/postgres/spring-boot/src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.helloworld;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+    // Saving credentials in environment variables is convenient, but not secure - consider a more
+    // secure solution such as https://cloud.google.com/kms/ to help keep secrets safe.
+    private static final String CLOUD_SQL_CONNECTION_NAME =
+            System.getenv("CLOUD_SQL_CONNECTION_NAME");
+    private static final String DB_USER = System.getenv("DB_USER");
+    private static final String DB_PASS = System.getenv("DB_PASS");
+    private static final String DB_NAME = System.getenv("DB_NAME");
+
+    @Bean
+    DataSource getDataSource() {
+        // Note: For Java users, the Cloud SQL JDBC Socket Factory can provide authenticated connections
+        // which is preferred to using the Cloud SQL Proxy with Unix sockets.
+        // See https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory for details.
+
+        // The configuration object specifies behaviors for the connection pool.
+        HikariConfig config = new HikariConfig();
+
+        // The following URL is equivalent to setting the config options below:
+        // jdbc:postgresql:///<DB_NAME>?cloudSqlInstance=<CLOUD_SQL_CONNECTION_NAME>&
+        // socketFactory=com.google.cloud.sql.postgres.SocketFactory&user=<DB_USER>&password=<DB_PASS>
+        // See the link below for more info on building a JDBC URL for the Cloud SQL JDBC Socket Factory
+        // https://github.com/GoogleCloudPlatform/cloud-sql-jdbc-socket-factory#creating-the-jdbc-url
+
+        // Configure which instance and what database user to connect with.
+        config.setJdbcUrl(String.format("jdbc:postgresql:///%s", DB_NAME));
+        config.setUsername(DB_USER);
+        config.setPassword(DB_PASS);
+
+        config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.postgres.SocketFactory");
+        config.addDataSourceProperty("cloudSqlInstance", CLOUD_SQL_CONNECTION_NAME);
+
+
+        // The ipTypes argument can be used to specify a comma delimited list of preferred IP types
+        // for connecting to a Cloud SQL instance. The argument ipTypes=PRIVATE will force the
+        // SocketFactory to connect with an instance's associated private IP.
+        config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
+
+        // ... Specify additional connection properties here.
+
+        // maximumPoolSize limits the total number of concurrent connections this pool will keep. Ideal
+        // values for this setting are highly variable on app design, infrastructure, and database.
+        config.setMaximumPoolSize(5);
+        // minimumIdle is the minimum number of idle connections Hikari maintains in the pool.
+        // Additional connections will be established to meet this value unless the pool is full.
+        config.setMinimumIdle(5);
+
+        // setConnectionTimeout is the maximum number of milliseconds to wait for a connection checkout.
+        // Any attempt to retrieve a connection from this pool that exceeds the set limit will throw an
+        // SQLException.
+        config.setConnectionTimeout(10000); // 10 seconds
+        // idleTimeout is the maximum amount of time a connection can sit in the pool. Connections that
+        // sit idle for this many milliseconds are retried if minimumIdle is exceeded.
+        config.setIdleTimeout(600000); // 10 minutes
+
+        // Hikari automatically delays between failed connection attempts, eventually reaching a
+        // maximum delay of `connectionTimeout / 2` between attempts.
+
+        // [START cloud_sql_postgres_servlet_lifetime]
+        // maxLifetime is the maximum possible lifetime of a connection in the pool. Connections that
+        // live longer than this many milliseconds will be closed and reestablished between uses. This
+        // value should be several minutes shorter than the database's timeout value to avoid unexpected
+        // terminations.
+        config.setMaxLifetime(1800000); // 30 minutes
+
+        return new HikariDataSource(config);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
