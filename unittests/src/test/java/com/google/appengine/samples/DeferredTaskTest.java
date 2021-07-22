f33be79aebd1353e9f79d8b4bfc6d3eb29f51a71
averikitsch <akitsch@google.com>
2021-07-22 11:19:53 -0700

    synchronize methods

--- unittests/src/test/java/com/google/appengine/samples/DeferredTaskTest.java
+++ unittests/src/test/java/com/google/appengine/samples/DeferredTaskTest.java
@@ -33,17 +33,25 @@ import org.junit.Test;
 public class DeferredTaskTest extends BaseTestConfiguration {
 
   // Unlike CountDownLatch, TaskCountDownlatch lets us reset.
-  private final LocalTaskQueueTestConfig.TaskCountDownLatch latch =
+  private static final LocalTaskQueueTestConfig.TaskCountDownLatch latch =
       new LocalTaskQueueTestConfig.TaskCountDownLatch(1);
 
   private final LocalServiceTestHelper helper =
       new LocalServiceTestHelper(new LocalTaskQueueTestConfig()
-          .setDisableAutoTaskExecution(false)
+          .setDisableAutoTaskExecution(false) // Enable auto task execution
           .setCallbackClass(LocalTaskQueueTestConfig.DeferredTaskCallback.class)
           .setTaskExecutionLatch(latch));
 
+  private static synchronized boolean requestAwait() throws InterruptedException {
+    return latch.await(5, TimeUnit.SECONDS);
+  }
+
+  private static synchronized void requestReset() {
+    latch.reset();
+  }
+
   private static class MyTask implements DeferredTask {
-    private static boolean taskRan = false;
+    private static volatile boolean taskRan = false;
 
     @Override
     public void run() {
@@ -59,7 +67,7 @@ public class DeferredTaskTest extends BaseTestConfiguration {
   @After
   public void tearDown() {
     MyTask.taskRan = false;
-    latch.reset();
+    requestReset();
     helper.tearDown();
   }
 
@@ -67,7 +75,7 @@ public class DeferredTaskTest extends BaseTestConfiguration {
   public void testTaskGetsRun() throws InterruptedException {
     QueueFactory.getDefaultQueue().add(
         TaskOptions.Builder.withPayload(new MyTask()));
-    assertTrue(latch.await(5, TimeUnit.SECONDS));
+    assertTrue(requestAwait());
     assertTrue(MyTask.taskRan);
   }
 }
