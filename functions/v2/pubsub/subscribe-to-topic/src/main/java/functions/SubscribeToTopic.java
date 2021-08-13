702077dcfe619cc3764a213f253beaefcdb04b43
kelsk <38271546+kelsk@users.noreply.github.com>
2021-08-13 16:36:44 -0700

    New sample for GCFv2 (#5747)
    * Add GCFv2 pubsub sample
    
    * Update SubscribeToTopic with gson construction
    
    * Update copyright from 2020 to 2021
    
    * Remove publish sample
    
    * Update region tag
    
    * Fix dependencies, change package from 'functions' to 'functions.v2'
    
    * Remove unused POJO dependency
    
    * Add PubSubBody POJO to replace json destructuring
    
    * Remove unnecessary null check & imports; fix output string; nits
    
    * Add scope test to cloudevents-core dep
    
    * Remove .v2 from package name
    
    * Remove prefix from PubSubBody import
    
    * Address nit
    
    * lint
    
    Co-authored-by: averikitsch <akitsch@google.com>

---
+++ functions/v2/pubsub/subscribe-to-topic/src/main/java/functions/SubscribeToTopic.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+// [START functions_cloudevent_pubsub]
+
+import com.google.cloud.functions.CloudEventsFunction;
+import com.google.gson.Gson;
+import functions.eventpojos.PubSubBody;
+import io.cloudevents.CloudEvent;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.logging.Logger;
+
+public class SubscribeToTopic implements CloudEventsFunction {
+  private static final Logger logger = Logger.getLogger(SubscribeToTopic.class.getName());
+
+  @Override
+  public void accept(CloudEvent event) {
+    Gson gson = new Gson();
+    PubSubBody pubSubBody = gson.fromJson(new String(event.getData().toBytes()), PubSubBody.class);
+    String messageString =
+        new String(
+            Base64.getDecoder().decode(pubSubBody.getMessage().getData()), StandardCharsets.UTF_8);
+
+    logger.info(messageString);
+  }
+}
+// [END functions_cloudevent_pubsub]
---
+++ functions/v2/pubsub/subscribe-to-topic/src/test/java/functions/SubscribeToTopicTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.testing.TestLogHandler;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import java.net.URI;
+import java.util.Base64;
+import java.util.logging.Logger;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class SubscribeToTopicTest {
+  private static final Logger logger = Logger.getLogger(SubscribeToTopic.class.getName());
+  private static final TestLogHandler logHandler = new TestLogHandler();
+
+  @BeforeClass
+  public static void beforeClass() {
+    logger.addHandler(logHandler);
+  }
+
+  @Test
+  public void functionsPubsubSubscribe_shouldPrintPubsubMessage() throws Exception {
+
+    String encodedMessage = Base64.getEncoder().encodeToString("Hello World".getBytes());
+    String encodedData = new String("{\"message\": { \"data\": \"" + encodedMessage + "\"} }");
+
+    CloudEvent event =
+        CloudEventBuilder.v1()
+            .withId("0")
+            .withType("pubsub.message")
+            .withSource(URI.create("https://example.com"))
+            .withData(encodedData.getBytes())
+            .build();
+
+    new SubscribeToTopic().accept(event);
+
+    assertThat("Hello World").isEqualTo(logHandler.getStoredLogRecords().get(0).getMessage());
+  }
+}
