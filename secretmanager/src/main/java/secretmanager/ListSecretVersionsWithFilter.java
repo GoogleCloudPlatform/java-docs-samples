6e949461c2fa0cdf701ce00d932dcbe87fbb797a
gwhitehawk <gwhitehawk@gmail.com>
2021-08-31 17:46:35 +0200

    Added SecretManager filtered-listing samples (#5852)
    Co-authored-by: Miroslava Sotakova <mirka@google.com>

--- secretmanager/src/main/java/secretmanager/ListSecretVersionsWithFilter.java
+++ secretmanager/src/main/java/secretmanager/ListSecretVersionsWithFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,23 +16,28 @@
 
 package secretmanager;
 
-// [START secretmanager_list_secret_versions]
+// [START secretmanager_list_secret_versions_with_filter]
+import com.google.cloud.secretmanager.v1.ListSecretVersionsRequest;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient.ListSecretVersionsPagedResponse;
 import com.google.cloud.secretmanager.v1.SecretName;
 import java.io.IOException;
 
-public class ListSecretVersions {
+public class ListSecretVersionsWithFilter {
 
   public static void listSecretVersions() throws IOException {
     // TODO(developer): Replace these variables before running the sample.
     String projectId = "your-project-id";
     String secretId = "your-secret-id";
-    listSecretVersions(projectId, secretId);
+    // Follow https://cloud.google.com/secret-manager/docs/filtering
+    // for filter syntax and examples.
+    String filter = "create_time>2021-01-01T00:00:00Z";
+    listSecretVersions(projectId, secretId, filter);
   }
 
   // List all secret versions for a secret.
-  public static void listSecretVersions(String projectId, String secretId) throws IOException {
+  public static void listSecretVersions(String projectId, String secretId, String filter)
+      throws IOException {
     // Initialize client that will be used to send requests. This client only needs to be created
     // once, and can be reused for multiple requests. After completing all of your requests, call
     // the "close" method on the client to safely clean up any remaining background resources.
@@ -40,8 +45,14 @@ public class ListSecretVersions {
       // Build the parent name.
       SecretName secretName = SecretName.of(projectId, secretId);
 
-      // Get all versions.
-      ListSecretVersionsPagedResponse pagedResponse = client.listSecretVersions(secretName);
+      // Get filtered versions.
+      ListSecretVersionsRequest request =
+          ListSecretVersionsRequest.newBuilder()
+              .setParent(secretName.toString())
+              .setFilter(filter)
+              .build();
+
+      ListSecretVersionsPagedResponse pagedResponse = client.listSecretVersions(request);
 
       // List all versions and their state.
       pagedResponse
@@ -53,4 +64,4 @@ public class ListSecretVersions {
     }
   }
 }
-// [END secretmanager_list_secret_versions]
+// [END secretmanager_list_secret_versions_with_filter]
--- secretmanager/src/main/java/secretmanager/ListSecretsWithFilter.java
+++ secretmanager/src/main/java/secretmanager/ListSecretsWithFilter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2021 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,41 +16,50 @@
 
 package secretmanager;
 
-// [START secretmanager_list_secret_versions]
+// [START secretmanager_list_secrets_with_filter]
+import com.google.cloud.secretmanager.v1.ListSecretsRequest;
+import com.google.cloud.secretmanager.v1.ProjectName;
 import com.google.cloud.secretmanager.v1.SecretManagerServiceClient;
-import com.google.cloud.secretmanager.v1.SecretManagerServiceClient.ListSecretVersionsPagedResponse;
-import com.google.cloud.secretmanager.v1.SecretName;
+import com.google.cloud.secretmanager.v1.SecretManagerServiceClient.ListSecretsPagedResponse;
 import java.io.IOException;
 
-public class ListSecretVersions {
+public class ListSecretsWithFilter {
 
-  public static void listSecretVersions() throws IOException {
+  public static void listSecrets() throws IOException {
     // TODO(developer): Replace these variables before running the sample.
     String projectId = "your-project-id";
-    String secretId = "your-secret-id";
-    listSecretVersions(projectId, secretId);
+    // Follow https://cloud.google.com/secret-manager/docs/filtering
+    // for filter syntax and examples.
+    String filter = "name:your-secret-substring AND expire_time<2022-01-01T00:00:00Z";
+    listSecrets(projectId, filter);
   }
 
-  // List all secret versions for a secret.
-  public static void listSecretVersions(String projectId, String secretId) throws IOException {
+  // List all secrets for a project
+  public static void listSecrets(String projectId, String filter) throws IOException {
     // Initialize client that will be used to send requests. This client only needs to be created
     // once, and can be reused for multiple requests. After completing all of your requests, call
     // the "close" method on the client to safely clean up any remaining background resources.
     try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
       // Build the parent name.
-      SecretName secretName = SecretName.of(projectId, secretId);
+      ProjectName projectName = ProjectName.of(projectId);
 
-      // Get all versions.
-      ListSecretVersionsPagedResponse pagedResponse = client.listSecretVersions(secretName);
+      // Get filtered secrets.
+      ListSecretsRequest request =
+          ListSecretsRequest.newBuilder()
+              .setParent(projectName.toString())
+              .setFilter(filter)
+              .build();
 
-      // List all versions and their state.
+      ListSecretsPagedResponse pagedResponse = client.listSecrets(request);
+
+      // List all secrets.
       pagedResponse
           .iterateAll()
           .forEach(
-              version -> {
-                System.out.printf("Secret version %s, %s\n", version.getName(), version.getState());
+              secret -> {
+                System.out.printf("Secret %s\n", secret.getName());
               });
     }
   }
 }
-// [END secretmanager_list_secret_versions]
+// [END secretmanager_list_secrets_with_filter]
