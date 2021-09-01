b0df897b3ced1249f7c38fc6ec3cedc96a24128a
Ace Nassri <anassri@google.com>
2021-09-01 09:15:39 -0700

    chore(functions/logging): exclude trace code from sample (#5908)

--- run/logging-manual/src/main/java/com/example/cloudrun/App.java
+++ run/logging-manual/src/main/java/com/example/cloudrun/App.java
@@ -42,8 +42,10 @@ public class App {
         (req, res) -> {
           // [START cloudrun_manual_logging]
           // [START run_manual_logging]
+          // [START functions_manual_logging]
           // Build structured log messages as an object.
           Object globalLogFields = null;
+          // [END functions_manual_logging]
           // Add log correlation to nest all log messages beneath request log in Log Viewer.
           String traceHeader = req.headers("x-cloud-trace-context");
           if (traceHeader != null && project != null) {
@@ -53,6 +55,7 @@ public class App {
                     "logging.googleapis.com/trace",
                     String.format("projects/%s/traces/%s", project, trace));
           }
+          // [START functions_manual_logging]
           // Create a structured log entry using key value pairs.
           logger.error(
               "This is the default display field.",
@@ -61,6 +64,7 @@ public class App {
               globalLogFields);
           // [END run_manual_logging]
           // [END cloudrun_manual_logging]
+          // [END functions_manual_logging]
           res.status(200);
           return "Hello Logger!";
         });
