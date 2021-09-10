e6f6a65d24ad7711fb21519c50c49de31effa085
Averi Kitsch <akitsch@google.com>
2021-09-10 15:50:09 -0700

    Add GCFv2 Storage sample (#5972)
    Fixes #issue
    
    > It's a good idea to open an issue first for discussion.
    
    - [ ] I have followed [Sample Format Guide](https://github.com/GoogleCloudPlatform/java-docs-samples/blob/master/SAMPLE_FORMAT.md)
    - [ ] `pom.xml` parent set to latest `shared-configuration`
    - [ ] Appropriate changes to README are included in PR
    - [ ] API's need to be enabled to test (tell us)
    - [ ] Environment Variables need to be set (ask us to set them)
    - [ ] **Tests** pass:   `mvn clean verify` **required**
    - [ ] **Lint**  passes: `mvn -P lint checkstyle:check` **required**
    - [ ] **Static Analysis**:  `mvn -P lint clean compile pmd:cpd-check spotbugs:check` **advisory only**
    - [ ] Please **merge** this PR for me once it is approved.

---
+++ functions/v2/helloworld/hello-gcs/src/main/java/functions/HelloGcs.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+// [START functions_cloudevent_storage]
+import com.google.cloud.functions.CloudEventsFunction;
+import com.google.gson.Gson;
+import functions.eventpojos.GcsEvent;
+import io.cloudevents.CloudEvent;
+import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
+
+public class HelloGcs implements CloudEventsFunction {
+  private static final Logger logger = Logger.getLogger(HelloGcs.class.getName());
+
+  @Override
+  public void accept(CloudEvent event) {
+    logger.info("Event: " + event.getId());
+    logger.info("Event Type: " + event.getType());
+
+    if (event.getData() != null) {
+      String cloudEventData = new String(event.getData().toBytes(), StandardCharsets.UTF_8);
+      Gson gson = new Gson();
+      GcsEvent gcsEvent = gson.fromJson(cloudEventData, GcsEvent.class);
+      logger.info("Bucket: " + gcsEvent.getBucket());
+      logger.info("File: " + gcsEvent.getName());
+      logger.info("Metageneration: " + gcsEvent.getMetageneration());
+      logger.info("Created: " + gcsEvent.getTimeCreated());
+      logger.info("Updated: " + gcsEvent.getUpdated());
+    }
+  }
+}
+
+// [END functions_cloudevent_storage]
---
+++ functions/v2/helloworld/hello-gcs/src/test/java/functions/HelloGcsTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package functions;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.testing.TestLogHandler;
+import com.google.gson.Gson;
+import functions.eventpojos.GcsEvent;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import java.net.URI;
+import java.util.Date;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** Unit tests for main.java.com.example.functions.helloworld.HelloGcs. */
+public class HelloGcsTest {
+  private static final TestLogHandler LOG_HANDLER = new TestLogHandler();
+  private static final Logger logger = Logger.getLogger(HelloGcs.class.getName());
+
+  @Before
+  public void beforeTest() throws Exception {
+    logger.addHandler(LOG_HANDLER);
+  }
+
+  @After
+  public void afterTest() {
+    LOG_HANDLER.clear();
+  }
+
+  @Test
+  public void helloGcs_shouldPrintFileName() throws JsonProcessingException {
+    // Create event data
+    GcsEvent gcsEvent = new GcsEvent();
+    gcsEvent.setName("foo.txt");
+    gcsEvent.setBucket("gs://test-bucket");
+    gcsEvent.setMetageneration("metageneration-data");
+    gcsEvent.setTimeCreated(new Date());
+    gcsEvent.setUpdated(new Date());
+
+    // Convert event data to a JSON string
+    Gson gson = new Gson();
+    String jsonData = gson.toJson(gcsEvent);
+
+    // Construct a CloudEvent
+    CloudEvent event =
+        CloudEventBuilder.v1()
+            .withId("0")
+            .withType("google.storage.object.finalize")
+            .withSource(URI.create("https://example.com"))
+            .withData("application/json", jsonData.getBytes())
+            .build();
+
+    new HelloGcs().accept(event);
+
+    String message = LOG_HANDLER.getStoredLogRecords().get(3).getMessage();
+    assertThat(message).contains("File: foo.txt");
+  }
+}
