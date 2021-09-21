feeb7bfad8c5f98c490a824d97be0af7cf3a366f
Ace Nassri <anassri@google.com>
2021-09-21 14:52:45 -0700

    chore(functions/logging): include trace code for HTTP requests (#5964)

--- run/logging-manual/src/main/java/com/example/cloudrun/App.java
+++ run/logging-manual/src/main/java/com/example/cloudrun/App.java
@@ -42,11 +42,12 @@ public class App {
         (req, res) -> {
           // [START cloudrun_manual_logging]
           // [START run_manual_logging]
-          // [START functions_manual_logging]
           // Build structured log messages as an object.
           Object globalLogFields = null;
-          // [END functions_manual_logging]
+
           // Add log correlation to nest all log messages beneath request log in Log Viewer.
+          // TODO(developer): delete this code if you're creating a Cloud
+          //                  Function and it is *NOT* triggered by HTTP.
           String traceHeader = req.headers("x-cloud-trace-context");
           if (traceHeader != null && project != null) {
             String trace = traceHeader.split("/")[0];
@@ -55,7 +56,8 @@ public class App {
                     "logging.googleapis.com/trace",
                     String.format("projects/%s/traces/%s", project, trace));
           }
-          // [START functions_manual_logging]
+          // -- End log correlation code --
+
           // Create a structured log entry using key value pairs.
           logger.error(
               "This is the default display field.",
@@ -64,7 +66,6 @@ public class App {
               globalLogFields);
           // [END run_manual_logging]
           // [END cloudrun_manual_logging]
-          // [END functions_manual_logging]
           res.status(200);
           return "Hello Logger!";
         });
