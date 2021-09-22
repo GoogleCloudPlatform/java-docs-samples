dc6fcf2eaeec20d722f34b680a03f4bc940a1e93
SitaLakshmi <sita1996@gmail.com>
2021-09-22 18:20:54 +0530

    docs(samples): added samples to start, stop & reset and create encrypted instance.

---
+++ compute/cloud-client/src/main/java/compute/CreateEncryptedInstance.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute;
+
+// [START compute_instances_create_encrypted]
+
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDisk.Type;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.CustomerEncryptionKey;
+import com.google.cloud.compute.v1.InsertInstanceRequest;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.ZoneOperationsClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class CreateEncryptedInstance {
+
+  public static void main(String[] args)
+      throws IOException, InterruptedException, ExecutionException {
+    // TODO(developer): Replace these variables before running the sample.
+    String project = "your-project-id";
+    String zone = "zone-name";
+    String instanceName = "instance-name";
+    String diskEncryptionKey = "disk-encryption-key"; // Base64 encoded
+    createEncryptedInstance(project, zone, instanceName, diskEncryptionKey);
+  }
+
+
+  // Create a new encrypted instance with the provided "instanceName" value
+  // in the specified project and zone.
+  public static void createEncryptedInstance(String project, String zone, String instanceName, String diskEncryptionKey)
+      throws IOException, InterruptedException, ExecutionException {
+    // Below are sample values that can be replaced.
+    // machineType: machine type of the VM being created. This value uses the format zones/{zone}/machineTypes/{type_name}. For a list of machine types, see https://cloud.google.com/compute/docs/machine-types
+    // sourceImage: path to the operating system image to mount. For details about images you can mount, see https://cloud.google.com/compute/docs/images
+    // diskSizeGb: storage size of the boot disk to attach to the instance.
+    // networkName: network interface to associate with the instance.
+    String machineType = String.format("zones/%s/machineTypes/n1-standard-1", zone);
+    String sourceImage = String
+        .format("projects/debian-cloud/global/images/family/%s", "debian-10");
+    long diskSizeGb = 10L;
+    String networkName = "default";
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the `instancesClient.close()` method on the client to safely
+    // clean up any remaining background resources.
+    try (InstancesClient instancesClient = InstancesClient.create();
+        ZoneOperationsClient zoneOperationsClient = ZoneOperationsClient.create()) {
+      // Instance creation requires at least one persistent disk and one network interface.
+      AttachedDisk disk =
+          AttachedDisk.newBuilder()
+              .setBoot(true)
+              .setAutoDelete(true)
+              .setType(Type.PERSISTENT)
+              .setInitializeParams(
+                  AttachedDiskInitializeParams.newBuilder()
+                      .setSourceImage(sourceImage)
+                      .setDiskSizeGb(diskSizeGb).build())
+              .setDiskEncryptionKey(
+                  CustomerEncryptionKey.newBuilder()
+                      .setRawKey(diskEncryptionKey).build())
+              .build();
+
+      // Use the network interface provided in the networkName argument.
+      NetworkInterface networkInterface = NetworkInterface.newBuilder().setName(networkName)
+          .build();
+
+      // Bind `instanceName`, `machineType`, `disk`, and `networkInterface` to an instance.
+      Instance instanceResource =
+          Instance.newBuilder()
+              .setName(instanceName)
+              .setMachineType(machineType)
+              .addDisks(disk)
+              .addNetworkInterfaces(networkInterface)
+              .build();
+
+      System.out.println(String.format("Creating instance: %s at %s ", instanceName, zone));
+
+      // Insert the instance in the specified project and zone.
+      InsertInstanceRequest insertInstanceRequest = InsertInstanceRequest.newBuilder()
+          .setProject(project)
+          .setZone(zone)
+          .setInstanceResource(instanceResource).build();
+
+      Operation operation = instancesClient.insertCallable().futureCall(insertInstanceRequest)
+          .get();
+
+      // Wait for the operation to complete.
+      Operation response = zoneOperationsClient.wait(project, zone, operation.getName());
+
+      if (response.hasError()) {
+        System.out.println("Instance creation failed ! ! " + response);
+        return;
+      }
+      System.out.println("Operation Status: " + response.getStatus());
+    }
+  }
+}
+//  [END compute_instances_create_encrypted]
---
+++ compute/cloud-client/src/main/java/compute/StartInstance.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute;
+
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.Operation.Status;
+import com.google.cloud.compute.v1.StartInstanceRequest;
+import com.google.cloud.compute.v1.ZoneOperationsClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+// [START compute_start_instance]
+public class StartInstance {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    // TODO(developer): Replace these variables before running the sample.
+    /* project: project ID or project number of the Cloud project your instance belongs to.
+       zone: name of the zone your instance belongs to.
+       instanceName: name of the instance your want to start. */
+    String project = "your-project-id";
+    String zone = "zone-name";
+    String instanceName = "instance-name";
+
+    startInstance(project, zone, instanceName);
+  }
+
+  // Starts a stopped Google Compute Engine instance (with unencrypted disks).
+  public static void startInstance(String project, String zone, String instanceName)
+      throws IOException, ExecutionException, InterruptedException {
+    /* Initialize client that will be used to send requests. This client only needs to be created
+       once, and can be reused for multiple requests. After completing all of your requests, call
+       the `instancesClient.close()` method on the client to safely
+       clean up any remaining background resources. */
+    try (InstancesClient instancesClient = InstancesClient.create();
+        ZoneOperationsClient zoneOperationsClient = ZoneOperationsClient.create()) {
+
+      // Create the request.
+      StartInstanceRequest startInstanceRequest = StartInstanceRequest.newBuilder()
+          .setProject(project)
+          .setZone(zone)
+          .setInstance(instanceName)
+          .build();
+
+      Operation operation = instancesClient.startCallable()
+          .futureCall(startInstanceRequest)
+          .get();
+
+      // Wait for the operation to complete.
+      Operation response = zoneOperationsClient.wait(project, zone, operation.getName());
+
+      if(response.getStatus() == Status.DONE) {
+        System.out.println("Instance started successfully ! ");
+      }
+    }
+  }
+}
+// [END compute_start_instance]
