082c3d247e17a674004783ed1bb1c4ad03436eb2
averikitsch <akitsch@google.com>
2021-09-24 09:38:34 -0700

    lint

--- run/filesystem/src/test/java/com/example/filesystem/FilesystemApplicationTests.java
+++ run/filesystem/src/test/java/com/example/filesystem/FilesystemApplicationTests.java
@@ -24,7 +24,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Map;
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -42,7 +41,8 @@ public class FilesystemApplicationTests {
 
   @Autowired private MockMvc mockMvc;
 
-  private static final String systemMntDir = System.getenv().getOrDefault("MNT_DIR", "/mnt/nfs/filestore");
+  private static final String systemMntDir =
+      System.getenv().getOrDefault("MNT_DIR", "/mnt/nfs/filestore");
   private static String mntDir;
   String filename = System.getenv().getOrDefault("FILENAME", "Dockerfile");
 
