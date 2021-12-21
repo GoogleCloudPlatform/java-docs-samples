38c29a42d4a0444568e1fb07e97b269241d66e47
Billy Jacobson <billyjacobson@users.noreply.github.com>
2021-12-21 09:57:11 -0500

    docs: Bigtable Load generator tool (#6372)
    * Working workload generator pipeline
    
    * Clean up metadata and pom
    
    * Add tests, README
    
    * fix template path
    
    * Cleanup some text
    
    * fix regexes
    
    * fix shaded
    
    * Update deployment instructions
    
    * Update POM comment
    
    * Lint
    
    * Rename QPS variable
    
    * Fix name regexes
    
    * Fix new lines
    
    * Update test to use monitoring and actually run dataflow job on a table
    
    * fix lint issue: VariableDeclarationUsageDistance
    
    * Update bigtable/beam/workload-generator/README.md
    
    Co-authored-by: kolea2 <45548808+kolea2@users.noreply.github.com>
    
    * Update bigtable/beam/workload-generator/README.md
    
    Co-authored-by: kolea2 <45548808+kolea2@users.noreply.github.com>
    
    * responding to kolea2's review
    
    * new line fix
    
    Co-authored-by: kolea2 <45548808+kolea2@users.noreply.github.com>

---
+++ bigtable/beam/workload-generator/README.md
@@ -0,0 +1,100 @@
+# Bigtable workload generator
+
+This is a tool to perform a high number of reads to a Bigtable table for
+demonstration purposes. It is deployed as a Dataflow template, so it can easily
+be run as a Dataflow job.
+
+## Template
+
+### Running
+
+1. Set your environment variables
+
+    ```
+    TEMPLATE_PATH="gs://cloud-bigtable-dataflow-templates/generate-workload.json"
+    INSTANCE_ID=YOUR-INSTANCE-ID
+    TABLE_ID=YOUR-TABLE-ID
+    REGION=us-central1
+    WORKLOAD_QPS=100 # Optional
+    ```
+
+1. Run this command to start a job from dataflow template:
+
+    ```
+    JOB_NAME="generate-bigtable-workload-`date +%Y%m%d-%H%M%S`"
+    gcloud dataflow flex-template run $JOB_NAME \
+    --template-file-gcs-location "$TEMPLATE_PATH" \
+    --parameters bigtableInstanceId="$INSTANCE_ID" \
+    --parameters bigtableTableId="$TABLE_ID" \
+    --region "$REGION" \
+    --parameters workloadQPS=$WORKLOAD_QPS
+    ```
+
+1. Make sure to cancel the job once you are done.
+
+    ```
+    gcloud dataflow jobs cancel $JOB_NAME
+    ```
+
+### Deploying a template instructions
+
+These instructions are for maintenance of the workload generator, but if you 
+would like to modify this example and deploy the template yourself, you can 
+follow them to do so.
+
+1. Build the project
+
+    ```
+    mvn clean package -DskipTests
+    ```
+
+1. Set the environment variables. To deploy a version on your project, update 
+   these with your own resources as described in the [Using Flex Templates](https://cloud.google.com/dataflow/docs/guides/templates/using-flex-templates)
+   documentation.
+
+   ```
+   export TEMPLATE_PATH="gs://cloud-bigtable-dataflow-templates/generate-workload.json"
+   export TEMPLATE_IMAGE="gcr.io/cloud-bigtable-ecosystem/dataflow/generate-workload:latest"
+   export LOGS_PATH="gs://cloud-bigtable-dataflow-templates-logs/workload-generator"
+   ```
+
+1. Deploy the template
+
+   ```
+   gcloud dataflow flex-template build $TEMPLATE_PATH \
+   --image-gcr-path "$TEMPLATE_IMAGE" \
+   --sdk-language "JAVA" \
+   --flex-template-base-image JAVA11 \
+   --metadata-file "metadata.json" \
+   --jar "target/workload-generator-0.1.jar" \
+   --env FLEX_TEMPLATE_JAVA_MAIN_CLASS="bigtable.WorkloadGenerator" \
+   --gcs-log-dir="$LOGS_PATH"
+   ```
+
+   Note: Make sure your account or service account has cloudbuild and storage permissions.
+
+## Building and running
+
+If you would like to modify this and run it yourself you can use these commands:
+
+1. Create a Bigtable instance and table
+
+1. Set up the environment variables
+
+   ```
+   GOOGLE_CLOUD_PROJECT=your-project-id
+   INSTANCE_ID=your-instance-id
+   REGION=us-central1
+   TABLE_ID=your-table-id 
+   WORKLOAD_QPS=100 # Optional
+   ```
+
+1. Run the command
+
+   ```
+   mvn compile exec:java -Dexec.mainClass=WorkloadGenerator \
+   "-Dexec.args=--bigtableInstanceId=$INSTANCE_ID =--bigtableTableId=$TABLE_ID \
+   --runner=dataflow --project=$GOOGLE_CLOUD_PROJECT \
+   --region=$REGION" \
+   --workloadQPS=$WORKLOAD_QPS 
+   ```
---
+++ bigtable/beam/workload-generator/src/test/java/bigtable/WorkloadGeneratorTest.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package bigtable;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertNotNull;
+
+import bigtable.WorkloadGenerator.BigtableWorkloadOptions;
+import bigtable.WorkloadGenerator.ReadFromTableFn;
+import com.google.api.services.dataflow.model.Job;
+import com.google.bigtable.repackaged.com.google.cloud.monitoring.v3.MetricServiceClient;
+import com.google.bigtable.repackaged.com.google.cloud.monitoring.v3.MetricServiceClient.ListTimeSeriesPagedResponse;
+import com.google.bigtable.repackaged.com.google.monitoring.v3.ListTimeSeriesRequest;
+import com.google.bigtable.repackaged.com.google.monitoring.v3.ProjectName;
+import com.google.bigtable.repackaged.com.google.monitoring.v3.TimeInterval;
+import com.google.bigtable.repackaged.com.google.monitoring.v3.TimeSeries;
+import com.google.bigtable.repackaged.com.google.protobuf.util.Timestamps;
+import com.google.cloud.bigtable.beam.CloudBigtableTableConfiguration;
+import com.google.cloud.bigtable.hbase.BigtableConfiguration;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Date;
+import java.util.UUID;
+import org.apache.beam.runners.dataflow.DataflowClient;
+import org.apache.beam.runners.dataflow.DataflowPipelineJob;
+import org.apache.beam.runners.dataflow.DataflowRunner;
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Create;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WorkloadGeneratorTest {
+
+  private static final String INSTANCE_ENV = "BIGTABLE_TESTING_INSTANCE";
+  private static final String TABLE_ID =
+      "mobile-time-series-" + UUID.randomUUID().toString().substring(0, 20);
+  private static final String COLUMN_FAMILY_NAME = "stats_summary";
+
+  private static String projectId;
+  private static String instanceId;
+  private static final String REGION_ID = "us-central1";
+
+  private ByteArrayOutputStream bout;
+
+  private static String requireEnv(String varName) {
+    String value = System.getenv(varName);
+    assertNotNull(
+        String.format("Environment variable '%s' is required to perform these tests.", varName),
+        value);
+    return value;
+  }
+
+  @BeforeClass
+  public static void beforeClass() {
+    projectId = requireEnv("GOOGLE_CLOUD_PROJECT");
+    instanceId = requireEnv(INSTANCE_ENV);
+    try (Connection connection = BigtableConfiguration.connect(projectId, instanceId)) {
+      Admin admin = connection.getAdmin();
+      HTableDescriptor descriptor = new HTableDescriptor(TableName.valueOf(TABLE_ID));
+      descriptor.addFamily(new HColumnDescriptor(COLUMN_FAMILY_NAME));
+      admin.createTable(descriptor);
+    } catch (Exception e) {
+      System.out.println("Error during beforeClass: \n" + e.toString());
+    }
+  }
+
+  @Before
+  public void setupStream() {
+    bout = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(bout));
+  }
+
+  @AfterClass
+  public static void afterClass() {
+    try (Connection connection = BigtableConfiguration.connect(projectId, instanceId)) {
+      Admin admin = connection.getAdmin();
+      Table table = connection.getTable(TableName.valueOf(Bytes.toBytes(TABLE_ID)));
+      admin.disableTable(table.getName());
+      admin.deleteTable(table.getName());
+    } catch (Exception e) {
+      System.out.println("Error during afterClass: \n" + e.toString());
+    }
+  }
+
+  @Test
+  public void testGenerateWorkload() {
+    BigtableWorkloadOptions options = PipelineOptionsFactory.create()
+        .as(BigtableWorkloadOptions.class);
+    options.setBigtableInstanceId(instanceId);
+    options.setBigtableTableId(TABLE_ID);
+    options.setRegion(REGION_ID);
+
+    Pipeline p = Pipeline.create(options);
+
+    CloudBigtableTableConfiguration bigtableTableConfig =
+        new CloudBigtableTableConfiguration.Builder()
+            .withProjectId(options.getProject())
+            .withInstanceId(options.getBigtableInstanceId())
+            .withTableId(options.getBigtableTableId())
+            .build();
+
+    // Initiates a new pipeline every second
+    p.apply(Create.of(1L))
+        .apply(ParDo.of(new ReadFromTableFn(bigtableTableConfig)));
+    p.run().waitUntilFinish();
+
+    String output = bout.toString();
+    assertThat(output.contains("Connected to table"));
+  }
+
+  @Test
+  public void testPipeline() throws IOException, InterruptedException {
+    String workloadJobName = "bigtable-workload-generator-test-" + new Date().getTime();
+    final int WAIT_DURATION = 10 * 60 * 1000;
+    int rate = 1000;
+
+    BigtableWorkloadOptions options = PipelineOptionsFactory.create()
+        .as(BigtableWorkloadOptions.class);
+    options.setBigtableInstanceId(instanceId);
+    options.setBigtableTableId(TABLE_ID);
+    options.setWorkloadRate(rate);
+    options.setRegion(REGION_ID);
+    options.setRunner(DataflowRunner.class);
+    options.setJobName(workloadJobName);
+
+    final PipelineResult pipelineResult = WorkloadGenerator.generateWorkload(options);
+
+    MetricServiceClient metricServiceClient = MetricServiceClient.create();
+    ProjectName name = ProjectName.of(projectId);
+
+    // Wait X minutes and then get metrics for the X minute period.
+    Thread.sleep(WAIT_DURATION);
+    long startMillis = System.currentTimeMillis() - WAIT_DURATION;
+
+    TimeInterval interval =
+        TimeInterval.newBuilder()
+            .setStartTime(Timestamps.fromMillis(startMillis))
+            .setEndTime(Timestamps.fromMillis(System.currentTimeMillis()))
+            .build();
+
+    ListTimeSeriesRequest request =
+        ListTimeSeriesRequest.newBuilder()
+            .setName(name.toString())
+            .setFilter("metric.type=\"bigtable.googleapis.com/server/request_count\"")
+            .setInterval(interval)
+            .build();
+    ListTimeSeriesPagedResponse response = metricServiceClient.listTimeSeries(request);
+
+    long startRequestCount = 0;
+    long endRequestCount = 0;
+    for (TimeSeries ts : response.iterateAll()) {
+      startRequestCount = ts.getPoints(0).getValue().getInt64Value();
+      endRequestCount = ts.getPoints(ts.getPointsCount() - 1).getValue().getInt64Value();
+    }
+    assertThat(endRequestCount - startRequestCount > rate);
+
+    // Stop the running job.
+    String jobId = ((DataflowPipelineJob) pipelineResult).getJobId();
+    DataflowClient client = DataflowClient.create(options);
+    Job job = client.getJob(jobId);
+
+    job.setRequestedState("JOB_STATE_CANCELLED");
+    client.updateJob(jobId, job);
+  }
+}
