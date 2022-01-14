8d3ab61122c84cc461cb6bc9a9810001118c1de5
ace-n <anassri@google.com>
2022-01-13 17:22:17 -0800

    Optimize imports

--- functions/helloworld/hello-http/src/test/java/functions/ExampleSystemIT.java
+++ functions/helloworld/hello-http/src/test/java/functions/ExampleSystemIT.java
@@ -25,7 +25,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import org.apache.http.client.methods.RequestBuilder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
