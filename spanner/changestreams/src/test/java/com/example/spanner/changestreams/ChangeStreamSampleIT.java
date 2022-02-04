9894e89aa729c14b1843192a684e0bf46e278bd2
haikuo-google <haikuo@google.com>
2022-02-03 21:58:49 -0800

    Add Cloud Spanner change stream samples.
    The followings are included:
    * Sample code for creating change stream.
    * Sample code for querying change stream, basically it does an change stream initial query to get a list of partition tokens, and then use the partition tokens to query the data change records for each partition.
    * Sample code for dropping change stream.
    * ChangeStreamRecordMapper and models, these are the files that help change stream users understand how to model change stream records after receiving them from API, they are copied from org.apache.beam.sdk.io.gcp.spanner.changestreams.* with unused code being removed.
    * README for how to run tests.

---
+++ spanner/changestreams/src/test/java/com/example/spanner/changestreams/ChangeStreamSampleIT.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.spanner.changestreams;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.google.cloud.spanner.DatabaseAdminClient;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.Instance;
+import com.google.cloud.spanner.Spanner;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.connection.ConnectionOptions;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.UUID;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * Tests for ChangeStreamSample.
+ */
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
+public class ChangeStreamSampleIT {
+  private static String instanceId = System.getProperty("spanner.test.instance");
+  private static final String databaseId =
+      formatForTest(System.getProperty("spanner.sample.database", "cssample"));
+  private static final String prefix = "prefix";
+  private static DatabaseId dbId;
+  private static DatabaseAdminClient dbClient;
+
+  private ByteArrayOutputStream bout;
+  private final PrintStream stdOut = System.out;
+  private PrintStream out;
+
+  static String formatForTest(String name) {
+    return name + "-" + UUID.randomUUID().toString().substring(0, 20);
+  }
+
+  @Before
+  public void setUp() {
+    SpannerOptions options = SpannerOptions.newBuilder().build();
+    Spanner spanner = options.getService();
+    dbClient = spanner.getDatabaseAdminClient();
+    if (instanceId == null) {
+      Iterator<Instance> iterator =
+          spanner.getInstanceAdminClient().listInstances().iterateAll().iterator();
+      if (iterator.hasNext()) {
+        instanceId = iterator.next().getId().getInstance();
+      }
+    }
+    dbId = DatabaseId.of(options.getProjectId(), instanceId, databaseId);
+    dbClient.dropDatabase(dbId.getInstanceId().getInstance(), dbId.getDatabase());
+    try {
+      dbClient.createDatabase(instanceId, databaseId, Collections.emptyList()).get();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() {
+    ConnectionOptions.closeSpanner();
+    dbClient.dropDatabase(dbId.getInstanceId().getInstance(), dbId.getDatabase());
+
+    try {
+      bout.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    System.setOut(stdOut);
+  }
+
+  @Test
+  public void testChangeStreamSample() {
+    assertNotNull(instanceId);
+    assertNotNull(databaseId);
+    assertNotNull(prefix);
+    ChangeStreamSample.run(instanceId, databaseId, prefix);
+
+    String got = bout.toString();
+    Assert.assertTrue(got.contains("Received a ChildPartitionsRecord"));
+    Assert.assertTrue(got.contains("Received a DataChangeRecord"));
+    Assert.assertTrue(got.contains("mods=[Mod{keysJson={\"SingerId\":\"1\"}, "
+        + "oldValuesJson='', "
+        + "newValuesJson="
+        + "'{\"FirstName\":\"singer_1_first_name\",\"LastName\":\"singer_1_last_name\"}'}, "
+        + "Mod{keysJson={\"SingerId\":\"2\"}, "
+        + "oldValuesJson='', "
+        + "newValuesJson="
+        + "'{\"FirstName\":\"singer_2_first_name\",\"LastName\":\"singer_2_last_name\"}'}]"));
+  }
+}
