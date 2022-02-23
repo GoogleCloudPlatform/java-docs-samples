a27201df229f44f9e3d7782a7c55df252fcb2a37
Eugene Enclona <eenclona@google.com>
2022-02-23 13:38:36 -0500

    Applied google-java-format

--- support/cloud-client/src/test/java/com/example/gcpsupport/CreateCaseTest.java
+++ support/cloud-client/src/test/java/com/example/gcpsupport/CreateCaseTest.java
@@ -1,64 +1,56 @@
 /**
  * Copyright (C) 2022 Google Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * <p>Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * <p>http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
+ * <p>Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.example.gcpsupport;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-import java.io.IOException;
 import com.google.api.services.cloudsupport.v2beta.model.CloudSupportCase;
+import java.io.IOException;
+import org.junit.Test;
 
-public class CreateCaseTest 
-{
-    
-    private static CloudSupportCase csc; 
+public class CreateCaseTest {
 
-    private void createCase() {
-        if (csc != null) return;
+  private static CloudSupportCase csc;
 
-        String newCasePath = System.getenv("NEW_CASE_PATH");
-        String parentResource = System.getenv("PARENT_RESOURCE");
+  private void createCase() {
+    if (csc != null) return;
 
-        try {
-            csc = CreateCase.createCase(parentResource, newCasePath);
-        } catch (IOException e) {
-            System.out.println("IOException caught! \n" + e);
-        }
+    String newCasePath = System.getenv("NEW_CASE_PATH");
+    String parentResource = System.getenv("PARENT_RESOURCE");
+
+    try {
+      csc = CreateCase.createCase(parentResource, newCasePath);
+    } catch (IOException e) {
+      System.out.println("IOException caught! \n" + e);
     }
+  }
 
-    @Test
-    public void createsNewCase()
-    {
-        createCase();
-     
-        boolean b = csc instanceof CloudSupportCase;
-        assertTrue(b);
-       
-    }
+  @Test
+  public void createsNewCase() {
+    createCase();
 
-    @Test
-    public void createsCase_WithSameDescription()
-    {
-        createCase();
+    boolean b = csc instanceof CloudSupportCase;
+    assertTrue(b);
+  }
 
-        String caseDisplayName = csc.getDisplayName().toLowerCase();
-        String expectedDisplayName = "test case";
-        assertEquals(expectedDisplayName, caseDisplayName);
-    }
+  @Test
+  public void createsCase_WithSameDescription() {
+    createCase();
 
+    String caseDisplayName = csc.getDisplayName().toLowerCase();
+    String expectedDisplayName = "test case";
+    assertEquals(expectedDisplayName, caseDisplayName);
+  }
 }
