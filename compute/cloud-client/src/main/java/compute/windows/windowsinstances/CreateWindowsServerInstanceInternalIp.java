28b98759ef9da5b89680d2d05ef6bb819bacf525
SitaLakshmi <sita1996@gmail.com>
2022-04-11 14:23:36 +0530

    docs(compute-samples): add snippets and tests for windows instance management

---
+++ compute/cloud-client/src/main/java/compute/windows/windowsinstances/CreateWindowsServerInstanceInternalIp.java
@@ -0,0 +1,127 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compute.windows.windowsinstances;
+// [START compute_create_windows_instance_internal_ip]
+
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.InsertInstanceRequest;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class CreateWindowsServerInstanceInternalIp {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    // TODO(developer): Replace these variables before running the sample.
+    // projectId - ID or number of the project you want to use.
+    String projectId = "your-google-cloud-project-id";
+
+    // zone - Name of the zone you want to use, for example: us-west3-b
+    String zone = "europe-central2-b";
+
+    // instanceName - Name of the new machine.
+    String instanceName = "instance-name";
+
+    // networkLink - Name of the network you want the new instance to use.
+    //  *   For example: "global/networks/default" represents the network
+    //  *   named "default", which is created automatically for each project.
+    String networkLink = "global/networks/default";
+
+    // subnetworkLink - Name of the subnetwork you want the new instance to use.
+    //  *   This value uses the following format:
+    //  *   "regions/{region}/subnetworks/{subnetwork_name}"
+    String subnetworkLink = "regions/europe-central2/subnetworks/default";
+
+    createWindowsServerInstanceInternalIp(projectId, zone, instanceName, networkLink,
+        subnetworkLink);
+  }
+
+  // Creates a new Windows Server instance that has only an internal IP address.
+  public static void createWindowsServerInstanceInternalIp(String projectId, String zone,
+      String instanceName, String networkLink, String subnetworkLink)
+      throws IOException, ExecutionException, InterruptedException {
+
+    // machineType - Machine type you want to create in following format:
+    //  *    "zones/{zone}/machineTypes/{type_name}". For example:
+    //  *    "zones/europe-west3-c/machineTypes/f1-micro"
+    //  *    You can find the list of available machine types using:
+    //  *    https://cloud.google.com/sdk/gcloud/reference/compute/machine-types/list
+    String machineType = "n1-standard-1";
+    // sourceImageFamily - Name of the public image family for Windows Server or SQL Server images.
+    //  *    https://cloud.google.com/compute/docs/images#os-compute-support
+    String sourceImageFamily = "windows-2012-r2";
+
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+
+      AttachedDisk attachedDisk = AttachedDisk.newBuilder()
+          // Describe the size and source image of the boot disk to attach to the instance.
+          .setInitializeParams(AttachedDiskInitializeParams.newBuilder()
+              .setDiskSizeGb(64)
+              .setSourceImage(
+                  String.format("projects/windows-cloud/global/images/family/%s",
+                      sourceImageFamily))
+              .build())
+          .setAutoDelete(true)
+          .setBoot(true)
+          .setType(AttachedDisk.Type.PERSISTENT.toString())
+          .build();
+
+      Instance instance = Instance.newBuilder()
+          .setName(instanceName)
+          .setMachineType(String.format("zones/%s/machineTypes/%s", zone, machineType))
+          .addDisks(attachedDisk)
+          .addNetworkInterfaces(NetworkInterface.newBuilder()
+              // You must verify or configure routes and firewall rules in your VPC network
+              // to allow access to kms.windows.googlecloud.com.
+              // More information about access to kms.windows.googlecloud.com: https://cloud.google.com/compute/docs/instances/windows/creating-managing-windows-instances#kms-server
+
+              // Additionally, you must enable Private Google Access for subnets in your VPC network
+              // that contain Windows instances with only internal IP addresses.
+              // More information about Private Google Access: https://cloud.google.com/vpc/docs/configure-private-google-access#enabling
+              .setName(networkLink)
+              .setSubnetwork(subnetworkLink)
+              .build())
+          // If you chose an image that supports Shielded VM, you can optionally change the
+          // instance's Shielded VM settings.
+          // .setShieldedInstanceConfig(ShieldedInstanceConfig.newBuilder()
+          //    .setEnableSecureBoot(true)
+          //    .setEnableVtpm(true)
+          //    .setEnableIntegrityMonitoring(true)
+          //    .build())
+          .build();
+
+      InsertInstanceRequest request = InsertInstanceRequest.newBuilder()
+          .setProject(projectId)
+          .setZone(zone)
+          .setInstanceResource(instance)
+          .build();
+
+      Operation operation = instancesClient.insertAsync(request).get();
+
+      if (operation.hasError()) {
+        System.out.printf("Error in creating instance %s", operation.getError());
+        return;
+      }
+
+      System.out.printf("Instance created %s", instanceName);
+    }
+  }
+}
+// [END compute_create_windows_instance_internal_ip]
---
+++ compute/cloud-client/src/test/java/compute/windows/windowsinstances/CreatingManagingWindowsInstancesIT.java
@@ -0,0 +1,132 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compute.windows.windowsinstances;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import com.google.cloud.compute.v1.RoutesClient;
+import compute.DeleteFirewallRule;
+import compute.DeleteInstance;
+import compute.Util;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class CreatingManagingWindowsInstancesIT {
+
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private static final String ZONE = "europe-central2-b";
+  private static String INSTANCE_NAME_EXTERNAL;
+  private static String INSTANCE_NAME_INTERNAL;
+  private static String FIREWALL_RULE_NAME;
+  private static String NETWORK_NAME;
+  private static String SUBNETWORK_NAME;
+  private static String ROUTE_NAME;
+
+  private ByteArrayOutputStream stdOut;
+
+  // Check if the required environment variables are set.
+  public static void requireEnvVar(String envVarName) {
+    assertWithMessage(String.format("Missing environment variable '%s' ", envVarName))
+        .that(System.getenv(envVarName)).isNotEmpty();
+  }
+
+  @BeforeClass
+  public static void setup() throws IOException, ExecutionException, InterruptedException {
+    final PrintStream out = System.out;
+    ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
+
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_CLOUD_PROJECT");
+
+    // Cleanup existing test instances.
+    Util.cleanUpExistingInstances("windows-test-instance", PROJECT_ID, ZONE);
+
+    String uuid = UUID.randomUUID().toString().split("-")[0];
+    INSTANCE_NAME_EXTERNAL = "windows-test-instance-external-" + uuid;
+    INSTANCE_NAME_INTERNAL = "windows-test-instance-internal-" + uuid;
+    FIREWALL_RULE_NAME = "windows-test-firewall-" + uuid;
+    NETWORK_NAME = "global/networks/default-compute";
+    SUBNETWORK_NAME = "regions/europe-central2/subnetworks/default-compute";
+    ROUTE_NAME = "windows-test-route-" + uuid;
+
+    stdOut.close();
+    System.setOut(out);
+  }
+
+  public static void deleteRoute() throws IOException, ExecutionException, InterruptedException {
+    try (RoutesClient routesClient = RoutesClient.create()) {
+      routesClient.deleteAsync(PROJECT_ID, ROUTE_NAME).get();
+    }
+  }
+
+  @Before
+  public void beforeEach() {
+    stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
+  }
+
+  @After
+  public void afterEach() {
+    stdOut = null;
+    System.setOut(null);
+  }
+
+  @Test
+  public void testCreateWindowsServerInstanceExternalIp()
+      throws IOException, ExecutionException, InterruptedException {
+    // Create Windows server instance with external IP.
+    CreateWindowsServerInstanceExternalIp.createWindowsServerInstanceExternalIp(PROJECT_ID, ZONE,
+        INSTANCE_NAME_EXTERNAL);
+    assertThat(stdOut.toString()).contains("Instance created " + INSTANCE_NAME_EXTERNAL);
+
+    // Delete instance.
+    DeleteInstance.deleteInstance(PROJECT_ID, ZONE, INSTANCE_NAME_EXTERNAL);
+  }
+
+  @Test
+  public void testCreateWindowsServerInstanceInternalIp()
+      throws IOException, ExecutionException, InterruptedException {
+    // Create Windows server instance with internal IP and firewall rule.
+    CreateWindowsServerInstanceInternalIp.createWindowsServerInstanceInternalIp(PROJECT_ID, ZONE,
+        INSTANCE_NAME_INTERNAL, NETWORK_NAME, SUBNETWORK_NAME);
+    assertThat(stdOut.toString()).contains("Instance created " + INSTANCE_NAME_INTERNAL);
+    CreateFirewallRuleForWindowsActivationHost.createFirewallRuleForWindowsActivationHost(
+        PROJECT_ID, ZONE, NETWORK_NAME);
+    assertThat(stdOut.toString()).contains(
+        String.format("Firewall rule created %s", FIREWALL_RULE_NAME));
+    CreateRouteToWindowsActivationHost.createRouteToWindowsActivationHost(PROJECT_ID, ROUTE_NAME,
+        NETWORK_NAME);
+    assertThat(stdOut.toString()).contains(String.format("Route created %s", ROUTE_NAME));
+
+    // Delete Route.
+    deleteRoute();
+    // Delete Firewall.
+    DeleteFirewallRule.deleteFirewallRule(PROJECT_ID, FIREWALL_RULE_NAME);
+    // Delete Instance.
+    DeleteInstance.deleteInstance(PROJECT_ID, ZONE, INSTANCE_NAME_INTERNAL);
+  }
+}
