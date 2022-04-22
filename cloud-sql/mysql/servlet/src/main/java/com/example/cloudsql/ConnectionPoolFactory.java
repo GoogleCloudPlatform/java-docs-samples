65b4ef7405e1e2f0db7f0f2f7247b9d7f31f2e43
Shubha Rajan <shubhadayini@google.com>
2022-04-22 15:40:00 -0700

    add license header

--- cloud-sql/mysql/servlet/src/main/java/com/example/cloudsql/ConnectionPoolFactory.java
+++ cloud-sql/mysql/servlet/src/main/java/com/example/cloudsql/ConnectionPoolFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.cloudsql;
 
 import com.zaxxer.hikari.HikariConfig;
@@ -66,7 +82,7 @@ public class ConnectionPoolFactory {
 
   public static DataSource createConnectionPool() {
     if (INSTANCE_HOST != null) {
-      return createConnectionPoolWithTCP();
+      return createConnectionPoolWithTcp();
     } else {
       return createConnectionPoolWithConnector();
     }
@@ -103,7 +119,6 @@ public class ConnectionPoolFactory {
     }
     // [START cloud_sql_mysql_servlet_connect_connector]
 
-
     // The ipTypes argument can be used to specify a comma delimited list of preferred IP types
     // for connecting to a Cloud SQL instance. The argument ipTypes=PRIVATE will force the
     // SocketFactory to connect with an instance's associated private IP.
@@ -124,7 +139,7 @@ public class ConnectionPoolFactory {
   @SuppressFBWarnings(
       value = "USBR_UNNECESSARY_STORE_BEFORE_RETURN",
       justification = "Necessary for sample region tag.")
-  private static DataSource createConnectionPoolWithTCP() {
+  private static DataSource createConnectionPoolWithTcp() {
     // [START cloud_sql_mysql_servlet_connect_tcp]
     // [START cloud_sql_mysql_servlet_connect_tcp_sslcerts]
     // The configuration object specifies behaviors for the connection pool.
