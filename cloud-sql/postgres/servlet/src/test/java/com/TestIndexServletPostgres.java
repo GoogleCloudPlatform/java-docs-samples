97c6f5d5f2760dee27c612f309e36b98d4caef29
Shubha Rajan <shubhadayini@google.com>
2022-05-01 22:08:13 -0700

    remove unnecessary import

--- cloud-sql/postgres/servlet/src/test/java/com/TestIndexServletPostgres.java
+++ cloud-sql/postgres/servlet/src/test/java/com/TestIndexServletPostgres.java
@@ -22,7 +22,6 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import com.example.cloudsql.IndexServlet.TemplateData;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 import java.io.PrintWriter;
