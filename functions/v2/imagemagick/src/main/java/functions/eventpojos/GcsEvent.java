92149e6705f9442ed8a0d28bbbf9a5845d15cb95
averikitsch <akitsch@google.com>
2022-05-13 10:28:22 -0700

    update region tags

--- functions/v2/imagemagick/src/main/java/functions/eventpojos/GcsEvent.java
+++ functions/v2/imagemagick/src/main/java/functions/eventpojos/GcsEvent.java
@@ -19,7 +19,7 @@ package functions.eventpojos;
 import java.io.Serializable;
 import java.util.Date;
 
-// [START functions_helloworld_gcs_event]
+// [START cloudfunctions_helloworld_gcs_event]
 public class GcsEvent implements Serializable {
   // Cloud Functions uses GSON to populate this object.
   // Field types/names are specified by Cloud Functions
@@ -70,4 +70,4 @@ public class GcsEvent implements Serializable {
     this.updated = updated;
   }
 }
-// [END functions_helloworld_gcs_event]
+// [END cloudfunctions_helloworld_gcs_event]
