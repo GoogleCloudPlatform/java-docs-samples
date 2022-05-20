0fbf59a0b5438c30c00616e6501d06288b668161
Nick Cook <nickcook@google.com>
2022-05-20 12:01:45 -0700

    style checker errors

--- media/livestream/src/test/java/com/example/livestream/TestUtils.java
+++ media/livestream/src/test/java/com/example/livestream/TestUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.example.livestream;
 
 import com.google.cloud.video.livestream.v1.Input;
@@ -8,7 +24,7 @@ import java.time.Instant;
 
 public class TestUtils {
 
-  int DELETION_THRESHOLD_TIME_HOURS_IN_SECONDS = 10800; // 3 hours
+  private static final int DELETION_THRESHOLD_TIME_HOURS_IN_SECONDS = 10800; // 3 hours
 
   public boolean isInputStale(String projectID, String location, String inputID)
       throws IOException {
