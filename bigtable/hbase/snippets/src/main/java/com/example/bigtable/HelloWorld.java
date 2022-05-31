9eb312c0854c114f54634e4769b898fd485a8969
Kristen O'Leary <kaoleary@google.com>
2022-05-31 11:35:13 -0400

    lint

--- bigtable/hbase/snippets/src/main/java/com/example/bigtable/HelloWorld.java
+++ bigtable/hbase/snippets/src/main/java/com/example/bigtable/HelloWorld.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.bigtable;
 
 // [START bigtable_hw_imports_hbase]
--- bigtable/hbase/snippets/src/test/java/com/example/bigtable/HelloWorldTest.java
+++ bigtable/hbase/snippets/src/test/java/com/example/bigtable/HelloWorldTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.bigtable;
 
 import static org.junit.Assert.assertNotNull;
