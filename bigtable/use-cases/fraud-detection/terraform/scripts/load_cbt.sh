412d3bb2eea6865ece90476e090fcba90c6f0982
ikettaneh <ikettaneh@google.com>
2022-07-12 10:38:19 -0400

    feat: add a Cloud Bigtable fraud-detection example
    Adding a new Cloud Bigtable implementation of a fraud-detection system

--- bigtable/use-cases/fraud-detection/terraform/scripts/load_cbt.sh
+++ bigtable/use-cases/fraud-detection/terraform/scripts/load_cbt.sh
@@ -12,7 +12,7 @@ echo "CBT_INSTANCE=$CBT_INSTANCE"
 echo "CBT_TABLE=$CBT_TABLE"
 echo "GCS_BUCKET=$GCS_BUCKET"
 
-~/homebrew/bin/mvn -f ../pom.xml compile exec:java -Dexec.mainClass=com.example.dataflowpipelines.GCSToCBT -Dexec.cleanupDaemonThreads=false \
+mvn -f ../pom.xml compile exec:java -Dexec.mainClass=com.example.dataflowpipelines.GCSToCBT -Dexec.cleanupDaemonThreads=false \
 "-Dexec.args= --runner=DataflowRunner --project=$PROJECT_ID --projectID=$PROJECT_ID --region=$REGION \
 --gcpTempLocation=gs://$GCS_BUCKET/temp --CBTInstanceId=$CBT_INSTANCE --CBTTableId=$CBT_TABLE \
 --demographicsInputFile=gs://$GCS_BUCKET/training_dataset/customers.csv \
