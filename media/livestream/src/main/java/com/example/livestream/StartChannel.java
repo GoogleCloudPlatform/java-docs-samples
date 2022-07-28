92dddbfba683a7bc01300a5c0e1a16b4cbf76474
Nick Cook <nickcook@google.com>
2022-07-28 15:04:41 -0700

    docs: fix timeout for create input and start channel LROs; first call can take up to 15 minutes

--- media/livestream/src/main/java/com/example/livestream/StartChannel.java
+++ media/livestream/src/main/java/com/example/livestream/StartChannel.java
@@ -43,8 +43,8 @@ public class StartChannel {
     // the "close" method on the client to safely clean up any remaining background resources.
     try (LivestreamServiceClient livestreamServiceClient = LivestreamServiceClient.create()) {
       ChannelName name = ChannelName.of(projectId, location, channelId);
-      // First API call in a project can take up to 10 minutes.
-      livestreamServiceClient.startChannelAsync(name).get(10, TimeUnit.MINUTES);
+      // First API call in a project can take up to 15 minutes.
+      livestreamServiceClient.startChannelAsync(name).get(15, TimeUnit.MINUTES);
       System.out.println("Started channel");
     }
   }
