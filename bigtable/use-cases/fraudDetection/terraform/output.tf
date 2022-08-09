3308e81675465b160cec07232bad4fe78e1de40b
ikettaneh <ikettaneh@google.com>
2022-08-08 21:10:10 -0400

    feat: add a Cloud Bigtable fraud-detection terraform code
    This is the first PR out of three PRs that introduce Cloud Bigtable fraud-detection use-case example.
    
    This PR adds the following:
    
    1) The Terraform code that builds and destroys the infrastructure.
    2) An already-trained ML model that detects credit card fraudulent transactions.
    3) The datasets used for training the ML model.
    4) A simple Java test that builds the infrastructure.
    
    Future PRs will introduce the Dataflow jobs for preloading Cloud Bigtable and running the streaming fraud-detection pipeline.

---
+++ bigtable/use-cases/fraudDetection/terraform/output.tf
@@ -0,0 +1,49 @@
+output "random_id" {
+  description = "The random uuid used in this Terraform run"
+  value       = random_string.random_id.result
+}
+
+output "project_id" {
+  description = "The ID of the project in which to provision resources."
+  value       = var.project_id
+}
+
+output "region" {
+  description = "The region of the project in which to provision resources."
+  value       = var.region
+}
+
+output "zone" {
+  description = "The zone within the region in which to provision resources."
+  value       = var.zone
+}
+
+output "gcs_bucket" {
+  description = "The GCS bucket used in this Terraform run."
+  value       = google_storage_bucket.tf-fd-bucket.name
+}
+
+output "cbt_instance" {
+  description = "The Cloud Bigtable instance used in this Terraform run."
+  value       = google_bigtable_instance.tf-fd-instance.name
+}
+
+output "cbt_table" {
+  description = "The Cloud Bigtable table used in this Terraform run."
+  value       = google_bigtable_table.tf-fd-table.name
+}
+
+output "pubsub_input_topic" {
+  description = "The pub/sub input topic used in this Terraform run."
+  value       = google_pubsub_topic.tf-fd-pubsub-input-topic.name
+}
+
+output "pubsub_output_topic" {
+  description = "The pub/sub output topic used in this Terraform run."
+  value       = google_pubsub_topic.tf-fd-pubsub-output-topic.name
+}
+
+output "pubsub_output_subscription" {
+  description = "The pub/sub output subscription used in this Terraform run."
+  value       = google_pubsub_subscription.tf-fd-pubsub-output-subscription.name
+}
---
+++ bigtable/use-cases/fraudDetection/terraform/scripts/vertexai_build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+REGION=$1
+AI_NAME=$2
+BUCKET_NAME=$3
+
+MODEL_NAME=tf-fd-ml-model
+ENDPOINT_NAME=tf-fd-ml-model-ep
+DEPLOYED_MODEL_NAME=tf-fd-ml-deployed-model
+
+echo "REGION = $REGION"
+echo "MODEL_NAME = $MODEL_NAME"
+echo "ENDPOINT_NAME = $ENDPOINT_NAME"
+echo "DEPLOYED_MODEL_NAME = $DEPLOYED_MODEL_NAME"
+echo "BUCKET_NAME = $BUCKET_NAME"
+
+# If the model id exists, fail.
+MODEL_ID=$(gcloud ai models list   --region=$REGION   --filter=display_name=$MODEL_NAME 2>/dev/null | grep "$MODEL_NAME" | awk '{ print $1 }')
+if ! { [ -z "$MODEL_ID" ]; }; then
+  echo "ERROR, the ml model id already exists!"
+  exit 1
+fi
+
+gcloud ai models upload \
+  --region=$REGION \
+  --display-name=$MODEL_NAME \
+  --container-image-uri=us-docker.pkg.dev/vertex-ai/prediction/xgboost-cpu.0-82:latest \
+  --artifact-uri=gs://${BUCKET_NAME}/ml_model/
+MODEL_ID=$(gcloud ai models list   --region=$REGION   --filter=display_name=$MODEL_NAME 2>/dev/null | grep "$MODEL_NAME" | awk '{ print $1 }')
+
+gcloud ai endpoints create \
+  --region=$REGION \
+  --display-name=$ENDPOINT_NAME
+ENDPOINT_ID=$(gcloud ai endpoints list --region=$REGION --filter=display_name=$ENDPOINT_NAME 2>/dev/null | grep "$ENDPOINT_NAME" | awk '{ print $1 }')
+
+gcloud ai endpoints deploy-model $ENDPOINT_ID \
+  --region=$REGION \
+  --model=$MODEL_ID \
+  --display-name=$DEPLOYED_MODEL_NAME \
+  --traffic-split=0=100
+DEPLOYED_MODEL_ID=$(gcloud ai endpoints describe $ENDPOINT_ID --region=$REGION 2>/dev/null | grep "id:" | awk -F  "'" '{print $2}')
+
+# Export ENDPOINT_ID as it will be used by the dataflow job
+echo $ENDPOINT_ID > ./Scripts/ENDPOINT_ID.output
