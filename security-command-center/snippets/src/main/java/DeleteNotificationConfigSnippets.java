0c50e42a17e57d02d5a52bb8fd7c369d59a2b2aa
SitaLakshmi <sita1996@gmail.com>
2022-11-02 01:01:10 +0530

    docs(scc-sampleS): add notification config snippets and samples with new parent levels

---
+++ security-command-center/snippets/src/main/java/DeleteNotificationConfigSnippets.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START securitycenter_delete_notification_config]
+
+import com.google.cloud.securitycenter.v1.SecurityCenterClient;
+import java.io.IOException;
+
+public class DeleteNotificationConfigSnippets {
+
+  public static void main(String[] args) throws IOException {
+    // parentId: must be in one of the following formats:
+    //    "organizations/{organization_id}"
+    //    "projects/{project_id}"
+    //    "folders/{folder_id}"
+    String parentId = String.format("organizations/%s", "ORG_ID");
+
+    String notificationConfigId = "{config-id}";
+
+    deleteNotificationConfig(parentId, notificationConfigId);
+  }
+
+  // Delete a notification config.
+  public static boolean deleteNotificationConfig(String parentId, String notificationConfigId)
+      throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (SecurityCenterClient client = SecurityCenterClient.create()) {
+
+      client.deleteNotificationConfig(String.format("%s/notificationConfigs/%s",
+          parentId, notificationConfigId));
+
+      System.out.printf("Deleted Notification config: %s%n", notificationConfigId);
+    }
+    return true;
+  }
+}
+// [END securitycenter_delete_notification_config]
---
+++ security-command-center/snippets/src/test/java/NotificationConfigSnippetTests.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.UUID;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class NotificationConfigSnippetTests {
+
+  private static void createConfig(String configId) throws IOException {
+    CreateNotificationConfigSnippets.createNotificationConfig(
+        String.format("organizations/%s", getOrganizationId()), configId, getProject(),
+        getTopicName());
+  }
+
+  private static void deleteConfig(String configId) throws IOException {
+    assertTrue(
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(
+            String.format("organizations/%s", getOrganizationId()), configId));
+  }
+
+  private static String getOrganizationId() {
+    return "1081635000895";
+  }
+
+  private static String getProject() {
+    return "project-a-id";
+  }
+
+  private static String getTopicName() {
+    return "notifications-sample-topic";
+  }
+
+  @Test
+  public void testCreateNotificationConfig() throws IOException {
+    String configId = String.format("java-config-%s", UUID.randomUUID());
+    assertNotNull(
+        CreateNotificationConfigSnippets.createNotificationConfig(
+            String.format("organizations/%s", getOrganizationId()), configId, getProject(),
+            getTopicName()));
+
+    deleteConfig(configId);
+  }
+
+  @Test
+  public void testDeleteNotificationConfig() throws IOException {
+    String configId = String.format("java-config-%s", UUID.randomUUID());
+    createConfig(configId);
+
+    assertTrue(
+        DeleteNotificationConfigSnippets.deleteNotificationConfig(
+            String.format("organizations/%s", getOrganizationId()), configId));
+  }
+
+  @Test
+  public void testListNotificationConfig() throws IOException {
+    String configId = String.format("java-config-%s", UUID.randomUUID());
+    createConfig(configId);
+
+    assertNotNull(ListNotificationConfigSnippets.listNotificationConfigs(
+        String.format("organizations/%s", getOrganizationId())));
+
+    deleteConfig(configId);
+  }
+
+  @Test
+  public void testGetNotificationConfig() throws IOException {
+    String configId = String.format("java-config-%s", UUID.randomUUID());
+    createConfig(configId);
+
+    assertNotNull(
+        GetNotificationConfigSnippets.getNotificationConfig(
+            String.format("organizations/%s", getOrganizationId()), configId));
+
+    deleteConfig(configId);
+  }
+
+  @Test
+  public void testUpdateNotificationConfig() throws IOException {
+    String configId = String.format("java-config-%s", UUID.randomUUID());
+    createConfig(configId);
+
+    assertNotNull(
+        UpdateNotificationConfigSnippets.updateNotificationConfig(
+            String.format("organizations/%s", getOrganizationId()), configId, getProject(),
+            getTopicName()));
+
+    deleteConfig(configId);
+  }
+}
