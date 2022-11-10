0de87e4c00bc313b12b37bb4cfab4af2aac70ff5
shabirmean <shabirmean@google.com>
2022-11-10 15:25:50 -0500

    cleanup: fix lint errors

--- retail/interactive-tutorials/src/test/java/product/CreateProductTest.java
+++ retail/interactive-tutorials/src/test/java/product/CreateProductTest.java
@@ -41,11 +41,11 @@ public class CreateProductTest {
 
   @Before
   public void setUp() throws IOException, InterruptedException, ExecutionException {
-    String projectId = ServiceOptions.getDefaultProjectId();
-    String branchName =
+    final String projectId = ServiceOptions.getDefaultProjectId();
+    final String branchName =
         String.format(
             "projects/%s/locations/global/catalogs/default_catalog/branches/0", projectId);
-    String generatedProductId = UUID.randomUUID().toString();
+    final String generatedProductId = UUID.randomUUID().toString();
     bout = new ByteArrayOutputStream();
     PrintStream out = new PrintStream(bout);
     originalPrintStream = System.out;
