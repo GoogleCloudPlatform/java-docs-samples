c3312f1bc794a93d1b87f500825559ba527095bc
Karl Weinmeister <kweinmeister@google.com>
2022-12-13 08:12:11 -0600

    linting

--- compute/cloud-client/src/test/java/compute/FirewallIT.java
+++ compute/cloud-client/src/test/java/compute/FirewallIT.java
@@ -31,13 +31,13 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
-import org.junit.Rule;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
