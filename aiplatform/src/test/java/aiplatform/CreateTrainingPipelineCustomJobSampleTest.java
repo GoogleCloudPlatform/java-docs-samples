d246334137134106501727599b5af21dd9124149
Karl Weinmeister <kweinmeister@google.com>
2023-01-13 14:50:32 -0600

    test: add multiple attempts to aiplatform pipeline tests

--- aiplatform/src/test/java/aiplatform/CreateTrainingPipelineCustomJobSampleTest.java
+++ aiplatform/src/test/java/aiplatform/CreateTrainingPipelineCustomJobSampleTest.java
@@ -19,6 +19,7 @@ package aiplatform;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
+import com.google.cloud.testing.junit4.MultipleAttemptsRule;
 import io.grpc.StatusRuntimeException;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -30,9 +31,11 @@ import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CreateTrainingPipelineCustomJobSampleTest {
+  @Rule public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(5);
 
   private static final String PROJECT = System.getenv("UCAIP_PROJECT_ID");
   private static final String CONTAINER_IMAGE_URI =
--- aiplatform/src/test/java/aiplatform/CreateTrainingPipelineCustomTrainingManagedDatasetSampleTest.java
+++ aiplatform/src/test/java/aiplatform/CreateTrainingPipelineCustomTrainingManagedDatasetSampleTest.java
@@ -19,6 +19,7 @@ package aiplatform;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
+import com.google.cloud.testing.junit4.MultipleAttemptsRule;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
@@ -29,9 +30,12 @@ import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class CreateTrainingPipelineCustomTrainingManagedDatasetSampleTest {
+  @Rule public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(5);
+
   private static final String PROJECT = System.getenv("UCAIP_PROJECT_ID");
   private static final String DATASET_ID = System.getenv("CUSTOM_MANAGED_DATASET");
   private static final String ANNOTATION_SCHEMA_URI =
