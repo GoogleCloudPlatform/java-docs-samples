024c24a8a92086a54e6a08e958327abb40f85f95
Eric Schmidt <erschmid@google.com>
2023-01-17 17:05:08 -0800

    adds persistent phrase set

--- speech/src/test/java/com/example/speech/AdaptationPhraseSetReferenceV2Test.java
+++ speech/src/test/java/com/example/speech/AdaptationPhraseSetReferenceV2Test.java
@@ -16,6 +16,98 @@
 
 package com.example.speech;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.speech.v2.CreateRecognizerRequest;
+import com.google.cloud.speech.v2.DeleteRecognizerRequest;
+import com.google.cloud.speech.v2.OperationMetadata;
+import com.google.cloud.speech.v2.PhraseSet;
+import com.google.cloud.speech.v2.PhraseSetName;
+import com.google.cloud.speech.v2.Recognizer;
+import com.google.cloud.speech.v2.SpeechClient;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@SuppressWarnings("checkstyle:abbreviationaswordinname")
 public class AdaptationPhraseSetReferenceV2Test {
-  
+  private String recognitionAudioFile = "./resources/commercial_mono.wav";
+  private String recognizerId = String.format("rec-%s", UUID.randomUUID());
+  private String phraseSetId = String.format("phrase-%s", UUID.randomUUID());
+  private String projectId = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private String recognizerName;
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+
+  @Before
+  public void setUp() throws InterruptedException, ExecutionException,
+      TimeoutException, IOException {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+
+    // Create a recognizer for this test.
+    try (SpeechClient speechClient = SpeechClient.create()) {
+      String parent = String.format("projects/%s/locations/global", projectId);
+
+      // First, create a recognizer
+      Recognizer recognizer = Recognizer.newBuilder()
+          .setModel("latest_short")
+          .addLanguageCodes("en-US")
+          .build();
+
+      CreateRecognizerRequest createRecognizerRequest = CreateRecognizerRequest.newBuilder()
+          .setParent(parent)
+          .setRecognizerId(recognizerId)
+          .setRecognizer(recognizer)
+          .build();
+
+      OperationFuture<Recognizer, OperationMetadata> op =
+          speechClient.createRecognizerAsync(createRecognizerRequest);
+      recognizer = op.get(180, TimeUnit.SECONDS);
+      recognizerName = recognizer.getName();
+    }
+  }
+
+  @After
+  public void tearDown() throws IOException, ExecutionException, InterruptedException,
+      TimeoutException {
+    System.setOut(out);
+
+    DeleteRecognizerRequest deleteRequest = DeleteRecognizerRequest.newBuilder()
+        .setName(recognizerName)
+        .build();
+
+    try (SpeechClient speechClient = SpeechClient.create()) {
+      OperationFuture<Recognizer, OperationMetadata> op =
+          speechClient.deleteRecognizerAsync(deleteRequest);
+      op.get(180, TimeUnit.SECONDS);
+
+      OperationFuture<PhraseSet, OperationMetadata> deletePhraseOp =
+          speechClient.deletePhraseSetAsync(PhraseSetName.format(projectId, 
+          "global", phraseSetId));
+      deletePhraseOp.get(180, TimeUnit.SECONDS);
+    }
+  }
+
+  @Test
+  public void testCreatePersistentPhraseSetV2() throws IOException,
+      InterruptedException, ExecutionException {
+    AdaptationPhraseSetReferenceV2.createPersistentPhraseSetV2(projectId,
+        recognizerName, phraseSetId, recognitionAudioFile);
+    String got = bout.toString();
+    assertThat(got).contains(phraseSetId);
+    assertThat(got).contains("Chromecast");
+  }
 }
