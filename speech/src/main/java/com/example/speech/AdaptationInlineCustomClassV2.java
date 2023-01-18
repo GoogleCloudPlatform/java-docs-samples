fa5b6a3bac3678858219b491f59fcd9b27eaa54e
Eric Schmidt <erschmid@google.com>
2023-01-17 16:38:06 -0800

    adds inline custom class

--- speech/src/main/java/com/example/speech/AdaptationInlineCustomClassV2.java
+++ speech/src/main/java/com/example/speech/AdaptationInlineCustomClassV2.java
@@ -16,6 +16,91 @@
 
 package com.example.speech;
 
+import com.google.cloud.speech.v2.AutoDetectDecodingConfig;
+import com.google.cloud.speech.v2.CustomClass;
+import com.google.cloud.speech.v2.PhraseSet;
+import com.google.cloud.speech.v2.PhraseSet.Phrase;
+import com.google.cloud.speech.v2.RecognitionConfig;
+import com.google.cloud.speech.v2.RecognizeRequest;
+import com.google.cloud.speech.v2.RecognizeResponse;
+import com.google.cloud.speech.v2.SpeechAdaptation;
+import com.google.cloud.speech.v2.SpeechAdaptation.AdaptationPhraseSet;
+import com.google.cloud.speech.v2.SpeechClient;
+import com.google.cloud.speech.v2.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v2.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+
 public class AdaptationInlineCustomClassV2 {
-  
+  public static void main(String[] args) throws IOException {
+    String recognizerName = "projects/[PROJECT_ID]/locations/global/recognizers/[RECOGNIZER_ID]";
+    String audioFilePath = "path/to/audioFile";
+
+    buildInlineCustomClassV2(recognizerName, audioFilePath);
+  }
+
+  public static void buildInlineCustomClassV2(String recognizerName, String audioFilePath)
+      throws IOException {
+    try (SpeechClient speechClient = SpeechClient.create()) {
+      
+      // Create an inline phrase set to produce a more accurate transcript.
+      CustomClass.ClassItem classItem = CustomClass.ClassItem.newBuilder()
+          .setValue("Chromecast")
+          .build();
+
+      CustomClass customClass = CustomClass.newBuilder()
+          .setName("ChromeCast")
+          .addItems(classItem)
+          .build();
+
+      Phrase phrase = Phrase.newBuilder()
+          .setBoost(20)
+          .setValue("Chromecast")
+          .build();
+
+      PhraseSet phraseSet = PhraseSet.newBuilder()
+          .addPhrases(phrase)
+          .build();
+
+      AdaptationPhraseSet adaptation = AdaptationPhraseSet.newBuilder()
+          .setInlinePhraseSet(phraseSet)
+          .build();
+      
+      SpeechAdaptation speechAdaptation = SpeechAdaptation.newBuilder()
+          .addPhraseSets(adaptation)
+          .addCustomClasses(customClass)
+          .build();
+      
+      // Transcribe audio using speech adaptation
+      Path path = Paths.get(audioFilePath);
+      byte[] data = Files.readAllBytes(path);
+      ByteString audioBytes = ByteString.copyFrom(data);
+
+      RecognitionConfig recognitionConfig = RecognitionConfig.newBuilder()
+          .setAutoDecodingConfig(AutoDetectDecodingConfig.newBuilder().build())
+          .setAdaptation(speechAdaptation)
+          .build();
+
+      RecognizeRequest request = RecognizeRequest.newBuilder()
+          .setConfig(recognitionConfig)
+          .setRecognizer(recognizerName)
+          .setContent(audioBytes)
+          .build();
+
+      RecognizeResponse response = speechClient.recognize(request);
+      List<SpeechRecognitionResult> results = response.getResultsList();
+
+      for (SpeechRecognitionResult result : results) {
+        // There can be several alternative transcripts for a given chunk of speech. Just use the
+        // first (most likely) one here.
+        SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+        System.out.printf("Transcription: %s%n", alternative.getTranscript());
+      }
+    }
+  }
 }
