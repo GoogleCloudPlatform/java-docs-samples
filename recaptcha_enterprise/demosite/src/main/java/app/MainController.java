c107b0858ad7b189e75a9e8f96196fc22c62903d
SitaLakshmi <sita1996@gmail.com>
2023-02-02 14:03:29 -0500

    updated static file references

--- recaptcha_enterprise/demosite/src/main/java/app/MainController.java
+++ recaptcha_enterprise/demosite/src/main/java/app/MainController.java
@@ -29,10 +29,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import recaptcha.Recaptcha;
 
-@Controller
+@RestController
 @RequestMapping
 public class MainController {
 
