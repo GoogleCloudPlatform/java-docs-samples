86e4d0bad937afd7e0d2735595d154b381df17bc
SitaLakshmi <sita1996@gmail.com>
2023-02-09 10:55:46 -0500

    lint fix

--- compute/cloud-client/src/test/java/compute/custommachinetype/CustomMachineTypeIT.java
+++ compute/cloud-client/src/test/java/compute/custommachinetype/CustomMachineTypeIT.java
@@ -61,8 +61,8 @@ public class CustomMachineTypeIT {
   private ByteArrayOutputStream stdOut;
   private static InstancesClient instancesClient;
 
-  private final int MAX_ATTEMPT_COUNT = 3;
-  private final int INITIAL_BACKOFF_MILLIS = 300000; // 5 minutes
+  private static final int MAX_ATTEMPT_COUNT = 3;
+  private static final int INITIAL_BACKOFF_MILLIS = 300000; // 5 minutes
   @Rule
   public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(
       MAX_ATTEMPT_COUNT,
