f90a61ae338b125e3a2528bf4a328eea73229185
Averi Kitsch <akitsch@google.com>
2023-02-16 09:55:36 -0800

    Update GCF compute tests

--- functions/v2/label-compute-instance/src/test/java/functions/AutoLabelInstanceTest.java
+++ functions/v2/label-compute-instance/src/test/java/functions/AutoLabelInstanceTest.java
@@ -25,10 +25,26 @@ import io.cloudevents.CloudEvent;
 import io.cloudevents.core.builder.CloudEventBuilder;
 import java.net.URI;
 import java.util.logging.Logger;
+import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDisk.Type;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.DeleteInstanceRequest;
+import com.google.cloud.compute.v1.InsertInstanceRequest;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
 
 @RunWith(JUnit4.class)
 public class AutoLabelInstanceTest {
@@ -40,8 +56,16 @@ public class AutoLabelInstanceTest {
   private static final String INSTANCE = "lite1";
 
   @BeforeClass
-  public static void beforeClass() {
+  public static void beforeClass() throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    assertThat(PROJECT_ID).isNotNull();
+    createInstance(PROJECT_ID, ZONE, INSTANCE);
     logger.addHandler(logHandler);
+  }
+
+
+  @AfterClass
+  public static void cleanup() throws Exception {
+    deleteInstance(PROJECT_ID, ZONE, INSTANCE);
   }
 
   @Test
@@ -80,5 +104,91 @@ public class AutoLabelInstanceTest {
                 "Adding label, \"{'creator': '%s'}\", to instance, \"%s\".",
                 "test-gmail-com", INSTANCE))
         .isEqualTo(logHandler.getStoredLogRecords().get(0).getMessage());
+  }
+
+
+  public static void createInstance(String project, String zone, String instanceName)
+      throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    String machineType = String.format("zones/%s/machineTypes/n1-standard-1", zone);
+    String sourceImage = String
+        .format("projects/debian-cloud/global/images/family/%s", "debian-11");
+    long diskSizeGb = 10L;
+    String networkName = "default";
+
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+      // Instance creation requires at least one persistent disk and one network interface.
+      AttachedDisk disk =
+          AttachedDisk.newBuilder()
+              .setBoot(true)
+              .setAutoDelete(true)
+              .setType(Type.PERSISTENT.toString())
+              .setDeviceName("disk-1")
+              .setInitializeParams(
+                  AttachedDiskInitializeParams.newBuilder()
+                      .setSourceImage(sourceImage)
+                      .setDiskSizeGb(diskSizeGb)
+                      .build())
+              .build();
+
+      // Use the network interface provided in the networkName argument.
+      NetworkInterface networkInterface = NetworkInterface.newBuilder()
+          .setName(networkName)
+          .build();
+
+      // Bind `instanceName`, `machineType`, `disk`, and `networkInterface` to an instance.
+      Instance instanceResource =
+          Instance.newBuilder()
+              .setName(instanceName)
+              .setMachineType(machineType)
+              .addDisks(disk)
+              .addNetworkInterfaces(networkInterface)
+              .build();
+
+      System.out.printf("Creating instance: %s at %s %n", instanceName, zone);
+
+      // Insert the instance in the specified project and zone.
+      InsertInstanceRequest insertInstanceRequest = InsertInstanceRequest.newBuilder()
+          .setProject(project)
+          .setZone(zone)
+          .setInstanceResource(instanceResource)
+          .build();
+
+      OperationFuture<Operation, Operation> operation = instancesClient.insertAsync(
+          insertInstanceRequest);
+
+      // Wait for the operation to complete.
+      Operation response = operation.get(3, TimeUnit.MINUTES);
+
+      if (response.hasError()) {
+        System.out.println("Instance creation failed ! ! " + response);
+        return;
+      }
+      System.out.println("Operation Status: " + response.getStatus());
+    }
+  }
+
+  public static void deleteInstance(String project, String zone, String instanceName)
+      throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+
+      System.out.printf("Deleting instance: %s ", instanceName);
+
+      // Describe which instance is to be deleted.
+      DeleteInstanceRequest deleteInstanceRequest = DeleteInstanceRequest.newBuilder()
+          .setProject(project)
+          .setZone(zone)
+          .setInstance(instanceName).build();
+
+      OperationFuture<Operation, Operation> operation = instancesClient.deleteAsync(
+          deleteInstanceRequest);
+      // Wait for the operation to complete.
+      Operation response = operation.get(3, TimeUnit.MINUTES);
+
+      if (response.hasError()) {
+        System.out.println("Instance deletion failed ! ! " + response);
+        return;
+      }
+      System.out.println("Operation Status: " + response.getStatus());
+    }
   }
 }
