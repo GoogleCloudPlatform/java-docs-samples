c32503f1cbf64da7fc768cc181a635aa524eda14
Pratiksha Kap <pratikshakap@google.com>
2023-03-08 14:53:18 -0800

    lint changes

--- flexible/java-11/springboot-helloworld/src/test/java/com/example/appengine/springboot/SpringbootApplicationTest.java
+++ flexible/java-11/springboot-helloworld/src/test/java/com/example/appengine/springboot/SpringbootApplicationTest.java
@@ -13,43 +13,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.appengine.springboot;
 
 import static org.hamcrest.Matchers.containsString;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.junit.Assert.assertTrue;
 
-// [START gae_java11_helloworld]
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class SpringbootApplicationTest {
 
-    @Autowired
-    private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-    @Test
-    public void testHelloWorld() throws Exception{
-        this.mockMvc.perform(get("/"))
+  @Test
+  public void testHelloWorld() throws Exception {
+    this.mockMvc
+        .perform(get("/"))
         .andExpect(status().isOk())
         .andExpect(content().string(containsString("Hello world!")));
-    }
+  }
 }
