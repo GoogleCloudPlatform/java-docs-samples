f9931776d07f8bbca1da9a6d9d4ca9e95e6e3703
SitaLakshmi <sita1996@gmail.com>
2023-03-20 19:53:28 -0400

    add cd to original test directory

--- .kokoro/tests/run_tests.sh
+++ .kokoro/tests/run_tests.sh
@@ -114,29 +114,29 @@ if [[ ",$JAVA_VERSION," =~ "11" ]]; then
   cd ../../
 fi
 
-# Install Chrome and chrome driver for recaptcha tests
-#if [[ "$file" == *"recaptcha_enterprise/"* ]]; then
-  # Install Chrome.
-  curl https://dl-ssl.google.com/linux/linux_signing_key.pub -o /tmp/google.pub \
-    && cat /tmp/google.pub | apt-key add -; rm /tmp/google.pub \
-    && echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list \
-    && mkdir -p /usr/share/desktop-directories \
-    && apt-get -y update && apt-get install -y google-chrome-stable
+# Install Chrome and chrome driver for recaptcha tests.
+current_dir="$PWD"
+# Install Chrome.
+curl https://dl-ssl.google.com/linux/linux_signing_key.pub -o /tmp/google.pub \
+  && cat /tmp/google.pub | apt-key add -; rm /tmp/google.pub \
+  && echo 'deb http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google.list \
+  && mkdir -p /usr/share/desktop-directories \
+  && apt-get -y update && apt-get install -y google-chrome-stable
 
-  # Disable the SUID sandbox so that Chrome can launch without being in a privileged container.
-  dpkg-divert --add --rename --divert /opt/google/chrome/google-chrome.real /opt/google/chrome/google-chrome \
-    && echo "#!/bin/bash\nexec /opt/google/chrome/google-chrome.real --no-sandbox --disable-setuid-sandbox \"\$@\"" > /opt/google/chrome/google-chrome \
-    && chmod 755 /opt/google/chrome/google-chrome
+# Disable the SUID sandbox so that Chrome can launch without being in a privileged container.
+dpkg-divert --add --rename --divert /opt/google/chrome/google-chrome.real /opt/google/chrome/google-chrome \
+  && echo "#!/bin/bash\nexec /opt/google/chrome/google-chrome.real --no-sandbox --disable-setuid-sandbox \"\$@\"" > /opt/google/chrome/google-chrome \
+  && chmod 755 /opt/google/chrome/google-chrome
 
-  # Install chrome driver.
-  mkdir -p /opt/selenium \
-    && curl http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip -o /opt/selenium/chromedriver_linux64.zip \
-    && cd /opt/selenium; unzip /opt/selenium/chromedriver_linux64.zip; rm -rf chromedriver_linux64.zip; ln -fs /opt/selenium/chromedriver /usr/local/bin/chromedriver;
+# Install chrome driver.
+mkdir -p /opt/selenium \
+  && curl http://chromedriver.storage.googleapis.com/`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE`/chromedriver_linux64.zip -o /opt/selenium/chromedriver_linux64.zip \
+  && cd /opt/selenium; unzip /opt/selenium/chromedriver_linux64.zip; rm -rf chromedriver_linux64.zip; ln -fs /opt/selenium/chromedriver /usr/local/bin/chromedriver;
 
-
-  export CHROME_DRIVER_PATH="/usr/local/bin/chromedriver"
-  echo "Installing chrome and driver. Path to installation: $CHROME_DRIVER_PATH"
-#fi
+# Export the path and cd back to test directory.
+export CHROME_DRIVER_PATH="/usr/local/bin/chromedriver"
+echo "Installing chrome and driver. Path to installation: $CHROME_DRIVER_PATH"
+cd "${current_dir}"
 
 btlr_args=(
     "run"
