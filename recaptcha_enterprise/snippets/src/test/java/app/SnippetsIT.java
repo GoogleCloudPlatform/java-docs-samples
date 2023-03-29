1bb087cbccf5d9620befb21dcaa65027c44d31c5
SitaLakshmi <sita1996@gmail.com>
2023-03-29 11:31:24 -0400

    add webdriver to system path

--- recaptcha_enterprise/snippets/src/test/java/app/SnippetsIT.java
+++ recaptcha_enterprise/snippets/src/test/java/app/SnippetsIT.java
@@ -97,6 +97,7 @@ public class SnippetsIT {
 
     // Set Selenium Driver to Chrome.
     System.setProperty("webdriver.http.factory", "jdk-http-client");
+    System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
     ChromeOptions options = new ChromeOptions();
     options.addArguments("--headless");
     options.addArguments("--no-sandbox");
