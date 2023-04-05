a2d1ab06363d848bb1140de09fc151b51e0d8dba
Andrew Chasin <andrewchasin@google.com>
2023-04-05 16:26:25 -0400

    check style changes to format for linter

--- content-warehouse/src/main/java/contentwarehouse/v1/QuickStart.java
+++ content-warehouse/src/main/java/contentwarehouse/v1/QuickStart.java
@@ -18,23 +18,23 @@ package contentwarehouse.v1;
 
 // [START contentwarehouse_quickstart]
 
+
+import com.google.cloud.contentwarehouse.v1.CreateDocumentRequest;
+import com.google.cloud.contentwarehouse.v1.CreateDocumentResponse;
+import com.google.cloud.contentwarehouse.v1.CreateDocumentSchemaRequest;
+import com.google.cloud.contentwarehouse.v1.Document;
+import com.google.cloud.contentwarehouse.v1.DocumentSchema;
 import com.google.cloud.contentwarehouse.v1.DocumentSchemaServiceClient;
 import com.google.cloud.contentwarehouse.v1.DocumentSchemaServiceSettings;
 import com.google.cloud.contentwarehouse.v1.DocumentServiceClient;
 import com.google.cloud.contentwarehouse.v1.DocumentServiceSettings;
 import com.google.cloud.contentwarehouse.v1.LocationName;
 import com.google.cloud.contentwarehouse.v1.Property;
-import com.google.cloud.contentwarehouse.v1.CreateDocumentRequest;
-import com.google.cloud.contentwarehouse.v1.CreateDocumentResponse;
-import com.google.cloud.contentwarehouse.v1.CreateDocumentSchemaRequest;
-import com.google.cloud.contentwarehouse.v1.Document;
-import com.google.cloud.contentwarehouse.v1.DocumentSchema;
 import com.google.cloud.contentwarehouse.v1.PropertyDefinition;
 import com.google.cloud.contentwarehouse.v1.RequestMetadata;
 import com.google.cloud.contentwarehouse.v1.TextArray;
 import com.google.cloud.contentwarehouse.v1.TextTypeOptions;
 import com.google.cloud.contentwarehouse.v1.UserInfo;
-
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
@@ -57,66 +57,70 @@ public class QuickStart {
     DocumentSchemaServiceSettings documentSchemaServiceSettings = 
          DocumentSchemaServiceSettings.newBuilder().setEndpoint(endpoint).build(); 
   
-  // Create a Schema Service client
-  try(DocumentSchemaServiceClient documentSchemaServiceClient = DocumentSchemaServiceClient.create(documentSchemaServiceSettings)){
+    // Create a Schema Service client
+    try (DocumentSchemaServiceClient documentSchemaServiceClient =
+        DocumentSchemaServiceClient.create(documentSchemaServiceSettings)) {
+      /*  The full resource name of the location, e.g.:
+      projects/{project_number}/locations/{location} */
+      String parent = LocationName.format(projectId, location);
 
-    /*  The full resource name of the location, e.g.:
-    projects/{project_number}/locations/{location} */
-    String parent = LocationName.format(projectId, location);
+      // Create Document Schema with Text Type Property
+      DocumentSchema documentSchema = DocumentSchema.newBuilder()
+          .setDisplayName("My Test Schema")
+          .setDescription("My Test Schema's Description")
+          .addPropertyDefinitions(
+            PropertyDefinition.newBuilder()
+            .setName("stock_symbol")
+            .setDisplayName("Searchable text")
+            .setIsSearchable(true)
+            .setTextTypeOptions(TextTypeOptions.newBuilder().build())
+            .build()).build();
 
-    // Create Document Schema with Text Type Property
-    DocumentSchema documentSchema = DocumentSchema.newBuilder()
-      .setDisplayName("My Test Schema")
-      .setDescription("My Test Schema's Description")
-      .addPropertyDefinitions(
-        PropertyDefinition.newBuilder()
-        .setName("stock_symbol")
-        .setDisplayName("Searchable text")
-        .setIsSearchable(true)
-        .setTextTypeOptions(TextTypeOptions.newBuilder().build())
-        .build()).build();
+      // Define Document Schema request
+      CreateDocumentSchemaRequest createDocumentSchemaRequest =
+          CreateDocumentSchemaRequest.newBuilder()
+            .setParent(parent)
+            .setDocumentSchema(documentSchema).build();
 
-    // Define Document Schema request
-    CreateDocumentSchemaRequest createDocumentSchemaRequest = CreateDocumentSchemaRequest.newBuilder()
-      .setParent(parent)
-      .setDocumentSchema(documentSchema).build();
+      // Create Document Schema
+      DocumentSchema documentSchemaResponse =
+          documentSchemaServiceClient.createDocumentSchema(createDocumentSchemaRequest); 
+        
 
-    // Create Document Schema
-    DocumentSchema documentSchemaResponse = documentSchemaServiceClient.createDocumentSchema(createDocumentSchemaRequest); 
-    
-
-    // Create Document Service Client Settings
-    DocumentServiceSettings documentServiceSettings = 
+      // Create Document Service Client Settings
+      DocumentServiceSettings documentServiceSettings = 
           DocumentServiceSettings.newBuilder().setEndpoint(endpoint).build();
-    TextArray textArray = TextArray.newBuilder().addValues("GOOG").build();
+      TextArray textArray = TextArray.newBuilder().addValues("GOOG").build();
 
-    // Create Document Service Client and Document with relevant properties 
-      try(DocumentServiceClient documentServiceClient = DocumentServiceClient.create(documentServiceSettings)){
+      // Create Document Service Client and Document with relevant properties 
+      try (DocumentServiceClient documentServiceClient =
+          DocumentServiceClient.create(documentServiceSettings)) {
         Document document = Document.newBuilder()
-          .setDisplayName("My Test Document")
-          .setDocumentSchemaName(documentSchemaResponse.getName())
-          .setPlainText("This is a sample of a document's text.")
-          .addProperties(
-            Property.newBuilder()
-            .setName(documentSchema.getPropertyDefinitions(0).getName())
-            .setTextValues(textArray)).build();
+              .setDisplayName("My Test Document")
+              .setDocumentSchemaName(documentSchemaResponse.getName())
+              .setPlainText("This is a sample of a document's text.")
+              .addProperties(
+                Property.newBuilder()
+                  .setName(documentSchema.getPropertyDefinitions(0).getName())
+                  .setTextValues(textArray)).build();
 
         // Define Request Metadata for enforcing access control
         RequestMetadata requestMetadata = RequestMetadata.newBuilder()
-        .setUserInfo(
-          UserInfo.newBuilder()
-            .setId(userId).build()).build();
-        
+            .setUserInfo(
+            UserInfo.newBuilder()
+              .setId(userId).build()).build();
+            
         // Define Create Document Request 
         CreateDocumentRequest createDocumentRequest = CreateDocumentRequest.newBuilder()
-          .setParent(parent)
-          .setDocument(document)
-          .setRequestMetadata(requestMetadata)
-          .build();
-      
+            .setParent(parent)
+            .setDocument(document)
+            .setRequestMetadata(requestMetadata)
+            .build();
+          
         // Create Document
-        CreateDocumentResponse createDocumentResponse = documentServiceClient.createDocument(createDocumentRequest);
-        
+        CreateDocumentResponse createDocumentResponse =
+            documentServiceClient.createDocument(createDocumentRequest);
+            
         System.out.println(createDocumentResponse.getDocument().getName());
         System.out.println(documentSchemaResponse.getName());
       }
