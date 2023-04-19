ac0f59a22b743e62953929b583b29137c9c39c93
bhavika-dhanwani <bhavika.dhanwani@crestdatasys.com>
2023-04-19 18:13:54 +0530

    resolved the review comments

--- dlp/snippets/src/test/java/dlp/snippets/DeIdentificationTests.java
+++ dlp/snippets/src/test/java/dlp/snippets/DeIdentificationTests.java
@@ -28,6 +28,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -551,9 +555,23 @@ public class DeIdentificationTests extends TestBase {
   }
 
   @Test
-  public void testReIdentifyWithFpeSurrogate() throws IOException {
+  public void testReIdentifyWithFpeSurrogate() throws IOException, NoSuchAlgorithmException {
+
+    KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+    keyGenerator.init(128);
+    SecretKey secretKey = keyGenerator.generateKey();
+    byte[] keyBytes = secretKey.getEncoded();
+
+    String unwrappedKey = Base64.getEncoder().encodeToString(keyBytes);
+    String textToDeIdentify = "My phone number is 4359916731";
+
+    String textToReIdentify =
+        DeidentifyFreeTextWithFpeUsingSurrogate.deIdentifyWithFpeSurrogate(
+            PROJECT_ID, textToDeIdentify, unwrappedKey);
+
     ReidentifyFreeTextWithFpeUsingSurrogate.reIdentifyWithFpeSurrogate(
-        PROJECT_ID, "My phone number is PHONE_TOKEN(10):9617256398", "YWJjZGVmZ2hpamtsbW5vcA==");
+        PROJECT_ID, textToReIdentify, unwrappedKey);
+
     String output = bout.toString();
     assertThat(output).contains("Text after re-identification: ");
   }
