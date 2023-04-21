a1466b7aea57923b6975731f382e686dd009c454
bhavika-dhanwani <bhavika.dhanwani@crestdatasys.com>
2023-04-21 12:43:02 +0530

    Inspect Big Query Send To Scc Job Deletion after test case

--- dlp/snippets/src/test/java/dlp/snippets/JobsTests.java
+++ dlp/snippets/src/test/java/dlp/snippets/JobsTests.java
@@ -134,8 +134,11 @@ public class JobsTests extends TestBase {
     String output = bout.toString();
     assertThat(output).contains("Job created successfully");
     String dlpJobName = output.split("Job created successfully: ")[1].split("\n")[0];
+
+    // Delete the created Dlp Job
     DeleteDlpJobRequest deleteDlpJobRequest =
-            DeleteDlpJobRequest.newBuilder().setName(dlpJobName).build();
+        DeleteDlpJobRequest.newBuilder().setName(dlpJobName).build();
+
     try (DlpServiceClient client = DlpServiceClient.create()) {
       client.deleteDlpJob(deleteDlpJobRequest);
     }
