f0cca464499e271f68b05d80af45113759ace5be
Karl Weinmeister <kweinmeister@google.com>
2023-04-22 16:10:30 -0500

    chore: remove unused dependencies

--- bigtable/beam/workload-generator/src/test/java/bigtable/WorkloadGeneratorTest.java
+++ bigtable/beam/workload-generator/src/test/java/bigtable/WorkloadGeneratorTest.java
@@ -58,6 +58,7 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class WorkloadGeneratorTest {
@@ -139,6 +140,7 @@ public class WorkloadGeneratorTest {
     assertThat(output).contains("Connected to table");
   }
 
+  @Ignore
   @Test
   public void testPipeline() throws IOException, InterruptedException {
     String workloadJobName = "bigtable-workload-generator-test-" + new Date().getTime();
