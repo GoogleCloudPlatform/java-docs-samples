cf41a012c7c6902260e0aa312c7e80da511b3431
Min Zhu <zhumin@google.com>
2023-05-17 15:03:42 -0400

    chore: fix header and lint.

--- cdn/signed-urls/src/test/java/com/google/cdn/SignedUrlWithPrefixTest.java
+++ cdn/signed-urls/src/test/java/com/google/cdn/SignedUrlWithPrefixTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Google Inc.
+ * Copyright 2023 Google LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,7 +17,8 @@
 package com.google.cdn;
 
 import static com.google.cdn.SignedUrlWithPrefix.signUrlWithPrefix;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.util.Base64;
 import java.util.Date;
