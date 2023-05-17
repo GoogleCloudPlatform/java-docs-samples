7da43a5271d1d7f03ea61be002ea8924396f2fbd
Min Zhu <zhumin@google.com>
2023-05-17 10:15:47 -0400

    feat: add sample for cloudcdn sign_url_prefix.

--- cdn/signed-urls/src/main/java/com/google/cdn/SignedUrls.java
+++ cdn/signed-urls/src/main/java/com/google/cdn/SignedUrls.java
@@ -75,6 +75,60 @@ public class SignedUrls {
   }
   // [END cloudcdn_sign_url]
 
+  // [START cloudcdn_sign_url_prefix]
+  /**
+   * Creates a signed URL with a URL prefix for a Cloud CDN endpoint with the given key. Prefixes
+   * allow access to any URL with the same prefix, and can be useful for granting access broader
+   * content without signing multiple URLs.
+   *
+   * @param requestUrl URL of request
+   * @param urlPrefix URL prefix to sign as a string. urlPrefix must start with either http:// or
+   * https:// and should not include query parameters
+   * @param key url signing key uploaded to the backend service/bucket, as a 16-byte array
+   * @param keyName the name of the signing key added to the back end bucket or service
+   * @param expirationTime the date that the signed URL expires
+   * @return a properly formatted signed URL with URL prefix
+   * @throws InvalidKeyException when there is an error generating the signature for the input key
+   * @throws NoSuchAlgorithmException when HmacSHA1 algorithm is not available in the environment
+   * @throws IllegalArgumentException when urlPrefix string is malformed.
+   */
+  public static String signUrlWithPrefix(String requestUrl,
+      String urlPrefix,
+      byte[] key,
+      String keyName,
+      Date expirationTime)
+      throws InvalidKeyException, NoSuchAlgorithmException, IllegalArgumentException {
+
+    if (urlPrefix.contains("?") || urlPrefix.contains("#")) {
+      throw new IllegalArgumentException("urlPrefix must not include query params: " + urlPrefix);
+    }
+    if (!urlPrefix.startsWith("http://") && !urlPrefix.startsWith("https://")) {
+      throw new IllegalArgumentException(
+          "urlPrefix must start with either http:// or https://: " + urlPrefix);
+    }
+    final long unixTime = expirationTime.getTime() / 1000;
+
+    String encodedUrlPrefix = Base64.getUrlEncoder().encodeToString(urlPrefix.getBytes());
+    String urlToSign = "URLPrefix=" + encodedUrlPrefix
+        + "&Expires=" + unixTime
+        + "&KeyName=" + keyName;
+
+    String encoded = SignedUrls.getSignatureForUrl(key, urlToSign);
+    return requestUrl + "&" + urlToSign + "&Signature=" + encoded;
+  }
+
+  public static String getSignatureForUrl(byte[] privateKey, String input)
+      throws InvalidKeyException, NoSuchAlgorithmException {
+
+    final String algorithm = "HmacSHA1";
+    final int offset = 0;
+    Key key = new SecretKeySpec(privateKey, offset, privateKey.length, algorithm);
+    Mac mac = Mac.getInstance(algorithm);
+    mac.init(key);
+    return  Base64.getUrlEncoder().encodeToString(mac.doFinal(input.getBytes()));
+  }
+  // [END cloudcdn_sign_url_prefix]
+
   public static void main(String[] args) throws Exception {
     Calendar cal = Calendar.getInstance();
     cal.setTime(new Date());
@@ -86,7 +140,11 @@ public class SignedUrls {
     String base64String = new String(Files.readAllBytes(Paths.get(keyPath)));
     byte[] keyBytes = Base64.getUrlDecoder().decode(base64String);
 
-    String result = signUrl("http://example.com/", keyBytes, "YOUR-KEY-NAME", tomorrow);
-    System.out.println(result);
+    String signUrlResult = signUrl("https://example.com/foo?", keyBytes, "YOUR-KEY-NAME", tomorrow);
+    System.out.println(signUrlResult);
+
+    String signUrlWithPrefixResult = signUrlWithPrefix("https://media.example.com/videos/id/master.m3u8?userID=abc123&starting_profile=1",
+        "https://media.example.com/videos/", keyBytes, "YOUR-KEY-NAME", tomorrow);
+    System.out.println(signUrlWithPrefixResult);
   }
 }
