7549c4692a4b0359ba5a4e9f0a67ba8aa0c9799b
Min Zhu <zhumin@google.com>
2023-05-18 17:37:37 -0400

    fix: fix lint - import style.

--- cdn/signed-urls/src/test/java/com/google/cdn/SignedCookiesTest.java
+++ cdn/signed-urls/src/test/java/com/google/cdn/SignedCookiesTest.java
@@ -17,7 +17,8 @@
 package com.google.cdn;
 
 import static com.google.cdn.SignedCookies.signCookie;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.util.Base64;
 import java.util.Date;
