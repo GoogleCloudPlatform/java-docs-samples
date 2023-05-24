8a0553dd3bab51a308bcb95cac57b3294b54f913
Averi Kitsch <akitsch@google.com>
2023-05-23 17:21:09 -0700

    debug

--- flexible/java-11/static-files/src/test/java/com/example/staticfiles/StaticFileTest.java
+++ flexible/java-11/static-files/src/test/java/com/example/staticfiles/StaticFileTest.java
@@ -21,12 +21,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class StaticFileTest {
 
