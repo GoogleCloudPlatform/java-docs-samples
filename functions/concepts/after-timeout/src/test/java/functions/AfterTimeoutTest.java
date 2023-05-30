f369d8f4bf7084439e159337b66bddaf23b8d6b2
Karl Weinmeister <kweinmeister@google.com>
2023-05-30 08:54:14 -0500

    chore: delete duplicate jobs/v4 samples

---
+++ functions/concepts/after-timeout/src/test/java/functions/AfterTimeoutTest.java
@@ -0,0 +1,34 @@
+package functions;
+
+import com.google.cloud.functions.HttpRequest;
+import com.google.cloud.functions.HttpResponse;
+
+import java.io.IOException;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class AfterTimeoutTest {
+    /**
+     * Method under test: {@link AfterTimeout#service(HttpRequest, HttpResponse)}
+     */
+    @Test
+    @Ignore("TODO: Complete this test")
+    public void testService() throws IOException, InterruptedException {
+        // TODO: Complete this test.
+        //   Diffblue AI was unable to find a test
+
+        // Arrange
+        // TODO: Populate arranged inputs
+        AfterTimeout afterTimeout = null;
+        HttpRequest request = null;
+        HttpResponse response = null;
+
+        // Act
+        afterTimeout.service(request, response);
+
+        // Assert
+        // TODO: Add assertions on result
+    }
+}
+
