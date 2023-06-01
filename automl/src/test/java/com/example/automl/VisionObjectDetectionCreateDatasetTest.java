e5980c276fd89be31673f40a12be3954957ed7b8
Averi Kitsch <akitsch@google.com>
2023-06-01 12:06:25 -0700

    undo change

--- automl/src/test/java/com/example/automl/VisionObjectDetectionCreateDatasetTest.java
+++ automl/src/test/java/com/example/automl/VisionObjectDetectionCreateDatasetTest.java
@@ -1,15 +1,17 @@
 /*
  * Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.example.automl;
@@ -22,7 +24,6 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeoutException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -68,8 +69,7 @@ public class VisionObjectDetectionCreateDatasetTest {
   }
 
   @Test
-  public void testCreateDataset()
-      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+  public void testCreateDataset() throws IOException, ExecutionException, InterruptedException {
     // Create a random dataset name with a length of 32 characters (max allowed by AutoML)
     // To prevent name collisions when running tests in multiple java versions at once.
     // AutoML doesn't allow "-", but accepts "_"
