965c6f6c5059829e4284acd5a79f3f68ae38f5b5
Karl Weinmeister <kweinmeister@google.com>
2023-06-08 08:29:59 -0500

    chore: add retries to aiplatform prediction tests

--- aiplatform/src/test/java/aiplatform/PredictTabularClassificationSampleTest.java
+++ aiplatform/src/test/java/aiplatform/PredictTabularClassificationSampleTest.java
@@ -19,15 +19,18 @@ package aiplatform;
 import static com.google.common.truth.Truth.assertThat;
 import static junit.framework.TestCase.assertNotNull;
 
+import com.google.cloud.testing.junit4.MultipleAttemptsRule;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
 
 public class PredictTabularClassificationSampleTest {
+  @Rule public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(3);
 
   private static final String PROJECT = System.getenv("UCAIP_PROJECT_ID");
   private static final String INSTANCE =
