070945210a684927897bed75f53c77851a7bcf61
bhavika-dhanwani <bhavika.dhanwani@crestdatasys.com>
2023-06-08 14:34:43 +0530

    resolved the test case failure error

--- dlp/snippets/src/test/java/dlp/snippets/DeIdentifyStorageTests.java
+++ dlp/snippets/src/test/java/dlp/snippets/DeIdentifyStorageTests.java
@@ -31,10 +31,20 @@ import com.google.privacy.dlp.v2.InspectDataSourceDetails;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 @RunWith(PowerMockRunner.class)
+@PowerMockIgnore({
+  "javax.management.",
+  "com.sun.org.apache.xerces.",
+  "javax.xml.",
+  "org.xml.",
+  "org.w3c.dom.",
+  "com.sun.org.apache.xalan.",
+  "javax.activation.*"
+})
 @PrepareForTest({DlpServiceClient.class})
 public class DeIdentifyStorageTests extends TestBase {
 
