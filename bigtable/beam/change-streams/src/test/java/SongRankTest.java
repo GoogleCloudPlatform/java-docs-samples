2ff01f2544cbb4673853596dd1b0386a48bf02b0
Billy Jacobson <thebilly@google.com>
2023-06-12 22:54:13 -0400

    fix lint and test output

--- bigtable/beam/change-streams/src/test/java/SongRankTest.java
+++ bigtable/beam/change-streams/src/test/java/SongRankTest.java
@@ -24,7 +24,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -106,7 +105,7 @@ public class SongRankTest {
     FileInputStream fis = new FileInputStream(
         TEST_OUTPUT_LOCATION + "/song-charts/GlobalWindow-pane-0-00000-of-00001.txt");
     byte[] data = new byte[(int) fis.available()];
-    String content = new String(data, StandardCharsets.UTF_8);
+    String content = new String(data, "UTF_8");
     assertThat(content).contains("[KV{song 1, 3}, KV{song 2, 1}]");
     assertThat(content).contains("[KV{song 1, 5}, KV{song 2, 1}]");
 
