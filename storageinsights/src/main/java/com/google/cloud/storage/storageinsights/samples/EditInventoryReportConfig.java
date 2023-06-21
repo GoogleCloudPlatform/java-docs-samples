3c6a5af4ee732966a0265bbc69940bc2f3bf937d
Jesse Lovelace <jesselovelace@google.com>
2023-06-21 15:00:45 -0700

    add insights samples

---
+++ storageinsights/src/main/java/com/google/cloud/storage/storageinsights/samples/EditInventoryReportConfig.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// [START storageinsights_edit_inventory_report_config]
+
+package com.google.cloud.storage.storageinsights.samples;
+
+import com.google.cloud.storageinsights.v1.ReportConfig;
+import com.google.cloud.storageinsights.v1.ReportConfigName;
+import com.google.cloud.storageinsights.v1.StorageInsightsClient;
+import com.google.cloud.storageinsights.v1.UpdateReportConfigRequest;
+
+import java.io.IOException;
+
+public class EditInventoryReportConfig {
+
+    public static void main(String[] args) throws IOException {
+        // The ID of your Google Cloud Project
+        String projectId = "your-project-id";
+
+        // The location your bucket is in
+        String bucketLocation = "us-west-1";
+
+        // The UUID of the inventory report you want to edit
+        String inventoryReportConfigUUID = "2b90d21c-f2f4-40b5-9519-e29a78f2b09f";
+
+        editInventoryReportConfig(projectId, bucketLocation, inventoryReportConfigUUID);
+    }
+
+    public static void editInventoryReportConfig(String projectID, String location, String inventoryReportConfigUUID) throws IOException {
+        try (StorageInsightsClient storageInsightsClient = StorageInsightsClient.create()) {
+            ReportConfigName name = ReportConfigName.of(projectID, location, inventoryReportConfigUUID);
+            ReportConfig reportConfig = storageInsightsClient.getReportConfig(name);
+
+            // Set any other fields you want to update here
+            ReportConfig updatedReportConfig = reportConfig.toBuilder().setDisplayName("Updated Display Name").build();
+
+            storageInsightsClient.updateReportConfig(UpdateReportConfigRequest.newBuilder()
+                    .setReportConfig(updatedReportConfig).build());
+
+            System.out.println("Edited inventory report config with name " + name);
+        }
+    }
+}
+
+// [END storageinsights_edit_inventory_report_config]
