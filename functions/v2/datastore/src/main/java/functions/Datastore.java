9eb957bb38765533f52081f548824ca14e45360c
Tomas Morton <tlmorton@google.com>
2023-08-21 16:17:27 +0000

    Replace with existing datastore snippet

--- functions/v2/datastore/src/main/java/functions/Datastore.java
+++ functions/v2/datastore/src/main/java/functions/Datastore.java
@@ -18,27 +18,27 @@ package functions;
 
 // [START functions_cloudevent_datastore]
 import com.google.cloud.functions.CloudEventsFunction;
-import com.google.events.cloud.firestore.v1.DocumentEventData;
+import com.google.events.cloud.datastore.v1.EntityEventData;
 import com.google.protobuf.InvalidProtocolBufferException;
 import io.cloudevents.CloudEvent;
 import java.util.logging.Logger;
 
-public class FirebaseFirestore implements CloudEventsFunction {
-  private static final Logger logger = Logger.getLogger(FirebaseFirestore.class.getName());
+public class DatastoreFunction implements CloudEventsFunction {
+  private static final Logger logger = Logger.getLogger(DatastoreFunction.class.getName());
 
   @Override
   public void accept(CloudEvent event) throws InvalidProtocolBufferException {
-    DocumentEventData firestorEventData = DocumentEventData.parseFrom(event.getData().toBytes());
+    EntityEventData datastoreEventData = EntityEventData.parseFrom(event.getData().toBytes());
 
     logger.info("Function triggered by event on: " + event.getSource());
     logger.info("Event type: " + event.getType());
 
     logger.info("Old value:");
-    logger.info(firestorEventData.getOldValue().toString());
+    logger.info(datastoreEventData.getOldValue().toString());
 
     logger.info("New value:");
-    logger.info(firestorEventData.getValue().toString());
+    logger.info(datastoreEventData.getValue().toString());
   }
 }
 
-// [END functions_cloudevent_firebase_firestore]
+// [END functions_cloudevent_datastore]
