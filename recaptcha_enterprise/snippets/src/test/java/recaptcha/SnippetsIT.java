253b7fd20a682a1261e20d66028f5a4f23f2ebe4
SitaLakshmi <sita1996@gmail.com>
2023-09-25 12:34:59 -0400

    revert change and rename files

--- recaptcha_enterprise/snippets/src/test/java/recaptcha/SnippetsIT.java
+++ recaptcha_enterprise/snippets/src/test/java/recaptcha/SnippetsIT.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package app;
+package recaptcha;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
--- recaptcha_enterprise/snippets/src/test/java/recaptcha/Util.java
+++ recaptcha_enterprise/snippets/src/test/java/recaptcha/Util.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package app;
+package recaptcha;
 
 import com.google.protobuf.ByteString;
 import java.nio.charset.StandardCharsets;
