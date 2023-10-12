405dc750e60e73154a23080292d6e84648385f2b
Sita Lakshmi Sangameswaran <sitalakshmi@google.com>
2023-10-12 20:51:31 +0530

    setup secret for impersonated service account

--- auth/src/test/java/com/google/cloud/auth/samples/AccessTokenFromImpersonatedCredentialsIT.java
+++ auth/src/test/java/com/google/cloud/auth/samples/AccessTokenFromImpersonatedCredentialsIT.java
@@ -31,7 +31,7 @@ import org.junit.runners.JUnit4;
 //CHECKSTYLE OFF: AbbreviationAsWordInName
 public class AccessTokenFromImpersonatedCredentialsIT {
   //CHECKSTYLE ON: AbbreviationAsWordInName
-  private static final String impersonatedServiceAccount = "sa@project.service.gserviceaccount.com";
+  private static final String impersonatedServiceAccount = System.getenv("IMPERSONATED_SERVICE_ACCOUNT");
   private static final String scope = "https://www.googleapis.com/auth/cloud-platform";
   private ByteArrayOutputStream bout;
   private PrintStream out;
