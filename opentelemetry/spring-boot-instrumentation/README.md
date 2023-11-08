53a64c18a0a8cd9506b8b12db4f3b8adcd83e21a
Aaron Abbott <aaronabbott@google.com>
2023-11-08 19:53:59 +0000

    OpenTelemetry Spring Boot example

---
+++ opentelemetry/spring-boot-instrumentation/README.md
@@ -0,0 +1,40 @@
+# OpenTelemetry Spring Boot instrumentation example
+
+This sample is a Spring Boot application instrumented with the [OpenTelemetry java
+agent](https://opentelemetry.io/docs/instrumentation/java/automatic/). This is a java version
+of [this golang
+sample](https://github.com/GoogleCloudPlatform/golang-samples/tree/main/opentelemetry/instrumentation).
+It uses docker compose to orchestrate running the application and sending it some requests.
+
+The Java code is a basic Spring Boot application with two endpoints
+- `/multi` makes a few requests to `/single` on localhost
+- `/single` sleeps for a short time to simulate work
+
+Docker compose also runs the OpenTelemetry collector, set up to receive telemetry from the Java
+application and parse its logs from a shared volume. Finally, a loadgen container sends
+requests to the Java app.
+
+## Running the example
+
+### Cloud Shell or GCE
+
+```sh
+git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
+cd java-docs-samples/opentelemetry/spring-boot-instrumentation/
+docker compose up --abort-on-container-exit
+```
+
+### Locally with Application Default Credentials
+
+```sh
+git clone https://github.com/GoogleCloudPlatform/java-docs-samples.git
+cd java-docs-samples/opentelemetry/spring-boot-instrumentation/
+
+# Lets collector read mounted config
+USERID="$(id -u)"
+# Pass in ADC
+GOOGLE_APPLICATION_CREDENTIALS="$HOME/.config/gcloud/application_default_credentials.json"
+GOOGLE_CLOUD_PROJECT=otel-starter-project
+
+docker compose -f docker-compose.yaml -f docker-compose.adc.yaml up  --abort-on-container-exit
+```
---
+++ opentelemetry/spring-boot-instrumentation/src/main/java/com/example/demo/MultiController.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.demo;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.concurrent.ThreadLocalRandom;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MultiController {
+  Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Autowired private HttpClient client;
+
+  /**
+   * handleMulti handles an http request by making 3-7 http requests to the /single endpoint.
+   *
+   * @throws Exception
+   */
+  // [START opentelemetry_instrumentation_handle_multi]
+  @GetMapping("/multi")
+  public String index() throws Exception {
+    int subRequests = ThreadLocalRandom.current().nextInt(3, 8);
+
+    // Write a structured log with the request context, which allows the log to
+    // be linked with the trace for this request.
+    logger.atInfo().addKeyValue("subRequests", subRequests).log("handle /multi request");
+
+    // Make 3-7 http requests to the /single endpoint.
+    for (int i = 0; i < subRequests; i++) {
+      client.send(
+          HttpRequest.newBuilder(new URI("http://localhost:8080/single")).GET().build(),
+          BodyHandlers.ofString());
+    }
+
+    return "ok";
+  }
+  // [END opentelemetry_instrumentation_handle_multi]
+}
