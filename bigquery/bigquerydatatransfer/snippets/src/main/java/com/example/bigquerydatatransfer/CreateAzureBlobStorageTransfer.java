0588fe8b21644c9b1c9daa17e0055778a7d55523
Yidong Ling <yidongling21@gmail.com>
2023-11-16 23:14:43 +0000

    docs: Add create Azure Blob Storage transfer code example

---
+++ bigquery/bigquerydatatransfer/snippets/src/main/java/com/example/bigquerydatatransfer/CreateAzureBlobStorageTransfer.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquerydatatransfer;
+
+// [START bigquerydatatransfer_create_azureblobstorage_transfer]
+import com.google.api.gax.rpc.ApiException;
+import com.google.cloud.bigquery.datatransfer.v1.CreateTransferConfigRequest;
+import com.google.cloud.bigquery.datatransfer.v1.DataTransferServiceClient;
+import com.google.cloud.bigquery.datatransfer.v1.ProjectName;
+import com.google.cloud.bigquery.datatransfer.v1.TransferConfig;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+// Sample to create azure blob storage transfer config.
+public class CreateAzureBlobStorageTransfer {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    final String projectId = "MY_PROJECT_ID";
+    String datasetId = "MY_DATASET_ID";
+    String tableId = "MY_TABLE_ID";
+    String storageAccount = "MY_AZURE_STORAGE_ACCOUNT_NAME";
+    String container = "MY_AZURE_CONTAINER_NAME";
+    String dataPath = "MY_AZURE_FILE_NAME_OR_PREFIX";
+    String sasToken = "MY_AZURE_SAS_TOKEN";
+    String fileFormat = "CSV";
+    String fieldDelimiter = ",";
+    String skipLeadingRows = "1";
+    Map<String, Value> params = new HashMap<>();
+    params.put(
+        "destination_table_name_template", Value.newBuilder().setStringValue(tableId).build());
+    params.put("storage_account", Value.newBuilder().setStringValue(storageAccount).build());
+    params.put("container", Value.newBuilder().setStringValue(container).build());
+    params.put("data_path", Value.newBuilder().setStringValue(dataPath).build());
+    params.put("sas_token", Value.newBuilder().setStringValue(sasToken).build());
+    params.put("file_format", Value.newBuilder().setStringValue(fileFormat).build());
+    params.put("field_delimiter", Value.newBuilder().setStringValue(fieldDelimiter).build());
+    params.put("skip_leading_rows", Value.newBuilder().setStringValue(skipLeadingRows).build());
+    TransferConfig transferConfig =
+        TransferConfig.newBuilder()
+            .setDestinationDatasetId(datasetId)
+            .setDisplayName("Your Azure Blob Storage Transfer Config Name")
+            .setDataSourceId("azure_blob_storage")
+            .setParams(Struct.newBuilder().putAllFields(params).build())
+            .setSchedule("every 24 hours")
+            .build();
+    createAzureBlobStorageTransfer(projectId, transferConfig);
+  }
+
+  public static void createAzureBlobStorageTransfer(String projectId, TransferConfig transferConfig)
+      throws IOException {
+    try (DataTransferServiceClient client = DataTransferServiceClient.create()) {
+      ProjectName parent = ProjectName.of(projectId);
+      CreateTransferConfigRequest request =
+          CreateTransferConfigRequest.newBuilder()
+              .setParent(parent.toString())
+              .setTransferConfig(transferConfig)
+              .build();
+      TransferConfig config = client.createTransferConfig(request);
+      System.out.println("Azure Blob Storage transfer created successfully: " + config.getName());
+    } catch (ApiException ex) {
+      System.out.print("Azure Blob Storage transfer was not created." + ex.toString());
+    }
+  }
+}
+// [END bigquerydatatransfer_create_azureblobstorage_transfer]
---
+++ bigquery/bigquerydatatransfer/snippets/src/test/java/com/example/bigquerydatatransfer/CreateAzureBlobStorageTransferIT.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.bigquerydatatransfer;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertNotNull;
+
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.datatransfer.v1.TransferConfig;
+import com.google.protobuf.Struct;
+import com.google.protobuf.Value;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CreateAzureBlobStorageTransferIT {
+
+  private static final Logger LOG =
+      Logger.getLogger(CreateAzureBlobStorageTransferIT.class.getName());
+  private static final String ID = UUID.randomUUID().toString().substring(0, 8);
+  private BigQuery bigquery;
+  private ByteArrayOutputStream bout;
+  private String name;
+  private String displayName;
+  private String datasetName;
+  private String tableName;
+  private PrintStream out;
+  private PrintStream originalPrintStream;
+
+  private static final String PROJECT_ID = requireEnvVar("GOOGLE_CLOUD_PROJECT");
+  private static final String DATASET_ID = requireEnvVar("DATASET_ID");
+  private static final String TABLE_NAME = requireEnvVar("TABLE_NAME");
+  private static final String STORAGE_ACCOUNT = requireEnvVar("STORAGE_ACCOUNT");
+  private static final String CONTAINER = requireEnvVar("CONTAINER");
+  private static final String DATA_PATH = requireEnvVar("DATA_PATH");
+  private static final String SAS_TOKEN = requireEnvVar("SAS_TOKEN");
+
+  private static String requireEnvVar(String varName) {
+    String value = System.getenv(varName);
+    assertNotNull(
+        "Environment variable " + varName + " is required to perform these tests.",
+        System.getenv(varName));
+    return value;
+  }
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    originalPrintStream = System.out;
+    System.setOut(out);
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    System.out.flush();
+    System.setOut(originalPrintStream);
+    LOG.log(Level.INFO, bout.toString());
+  }
+
+  @Test
+  public void testCreateAzureBlobStorageTransfer() throws IOException {
+    Map<String, Value> params = new HashMap<>();
+    params.put(
+        "destination_table_name_template", Value.newBuilder().setStringValue(TABLE_NAME).build());
+    params.put("storage_account", Value.newBuilder().setStringValue(STORAGE_ACCOUNT).build());
+    params.put("container", Value.newBuilder().setStringValue(CONTAINER).build());
+    params.put("data_path", Value.newBuilder().setStringValue(DATA_PATH).build());
+    params.put("sas_token", Value.newBuilder().setStringValue(SAS_TOKEN).build());
+    params.put("file_format", Value.newBuilder().setStringValue("CSV").build());
+    params.put("field_delimiter", Value.newBuilder().setStringValue(",").build());
+    params.put("skip_leading_rows", Value.newBuilder().setStringValue("1").build());
+
+    TransferConfig transferConfig =
+        TransferConfig.newBuilder()
+            .setDestinationDatasetId(DATASET_ID)
+            .setDisplayName("Azure Blob Storage Transfer Code Example Test")
+            .setDataSourceId("azure_blob_storage")
+            .setParams(Struct.newBuilder().putAllFields(params).build())
+            .setSchedule("every 24 hours")
+            .build();
+    CreateAzureBlobStorageTransfer.createAzureBlobStorageTransfer(PROJECT_ID, transferConfig);
+    String result = bout.toString();
+    name = result.substring(result.indexOf(":") + 1, result.length() - 1);
+    assertThat(result).contains("Azure Blob Storage transfer created successfully: ");
+  }
+}
