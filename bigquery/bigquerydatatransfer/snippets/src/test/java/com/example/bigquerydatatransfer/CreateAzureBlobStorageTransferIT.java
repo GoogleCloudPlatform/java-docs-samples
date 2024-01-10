09a07534308647aa520a41bcaf855b52bfc2b320
yidong-l <142705855+yidong-l@users.noreply.github.com>
2024-01-10 07:45:18 -0800

    docs: Add create Azure Blob Storage transfer code example (#8885)
    * docs: Add create Azure Blob Storage transfer code example
    
    * Address code review comments
    
    * Address more comments
    
    * Make AzureBlobStorage transfer test to manage test resources in setup/teardown
    
    * Rename Env vars because they are global across all tests
    
    * Add "DTS_" prefix to test env vars to avoid collisions
    
    * Update header to 2024

--- bigquery/bigquerydatatransfer/snippets/src/test/java/com/example/bigquerydatatransfer/CreateAzureBlobStorageTransferIT.java
+++ bigquery/bigquerydatatransfer/snippets/src/test/java/com/example/bigquerydatatransfer/CreateAzureBlobStorageTransferIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2024 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,8 +29,6 @@ import com.google.cloud.bigquery.StandardTableDefinition;
 import com.google.cloud.bigquery.TableDefinition;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
-import com.google.cloud.bigquery.datatransfer.v1.TransferConfig;
-import com.google.protobuf.Struct;
 import com.google.protobuf.Value;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -45,23 +43,25 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CreateAmazonS3TransferIT {
+public class CreateAzureBlobStorageTransferIT {
 
-  private static final Logger LOG = Logger.getLogger(CreateAmazonS3TransferIT.class.getName());
+  private static final Logger LOG =
+      Logger.getLogger(CreateAzureBlobStorageTransferIT.class.getName());
   private static final String ID = UUID.randomUUID().toString().substring(0, 8);
   private BigQuery bigquery;
-  private ByteArrayOutputStream bout;
   private String name;
   private String displayName;
   private String datasetName;
   private String tableName;
+  private ByteArrayOutputStream bout;
   private PrintStream out;
   private PrintStream originalPrintStream;
 
   private static final String PROJECT_ID = requireEnvVar("GOOGLE_CLOUD_PROJECT");
-  private static final String AWS_ACCESS_KEY_ID = requireEnvVar("AWS_ACCESS_KEY_ID");
-  private static final String AWS_SECRET_ACCESS_KEY = requireEnvVar("AWS_SECRET_ACCESS_KEY");
-  private static final String AWS_BUCKET = requireEnvVar("AWS_BUCKET");
+  private static final String DTS_AZURE_STORAGE_ACCOUNT =
+      requireEnvVar("DTS_AZURE_STORAGE_ACCOUNT");
+  private static final String DTS_AZURE_BLOB_CONTAINER = requireEnvVar("DTS_AZURE_BLOB_CONTAINER");
+  private static final String DTS_AZURE_SAS_TOKEN = requireEnvVar("DTS_AZURE_SAS_TOKEN");
 
   private static String requireEnvVar(String varName) {
     String value = System.getenv(varName);
@@ -74,14 +74,14 @@ public class CreateAmazonS3TransferIT {
   @BeforeClass
   public static void checkRequirements() {
     requireEnvVar("GOOGLE_CLOUD_PROJECT");
-    requireEnvVar("AWS_ACCESS_KEY_ID");
-    requireEnvVar("AWS_SECRET_ACCESS_KEY");
-    requireEnvVar("AWS_BUCKET");
+    requireEnvVar("DTS_AZURE_STORAGE_ACCOUNT");
+    requireEnvVar("DTS_AZURE_BLOB_CONTAINER");
+    requireEnvVar("DTS_AZURE_SAS_TOKEN");
   }
 
   @Before
   public void setUp() {
-    displayName = "MY_SCHEDULE_NAME_TEST_" + ID;
+    displayName = "MY_TRANSFER_NAME_TEST_" + ID;
     datasetName = "MY_DATASET_NAME_TEST_" + ID;
     tableName = "MY_TABLE_NAME_TEST_" + ID;
     // create a temporary dataset
@@ -117,32 +117,23 @@ public class CreateAmazonS3TransferIT {
   }
 
   @Test
-  public void testCreateAmazonS3Transfer() throws IOException {
-    String sourceUri = String.format("s3://%s/*", AWS_BUCKET);
-    String fileFormat = "CSV";
-    String fieldDelimiter = ",";
-    String skipLeadingRows = "1";
+  public void testCreateAzureBlobStorageTransfer() throws IOException {
     Map<String, Value> params = new HashMap<>();
     params.put(
         "destination_table_name_template", Value.newBuilder().setStringValue(tableName).build());
-    params.put("data_path", Value.newBuilder().setStringValue(sourceUri).build());
-    params.put("access_key_id", Value.newBuilder().setStringValue(AWS_ACCESS_KEY_ID).build());
     params.put(
-        "secret_access_key", Value.newBuilder().setStringValue(AWS_SECRET_ACCESS_KEY).build());
-    params.put("file_format", Value.newBuilder().setStringValue(fileFormat).build());
-    params.put("field_delimiter", Value.newBuilder().setStringValue(fieldDelimiter).build());
-    params.put("skip_leading_rows", Value.newBuilder().setStringValue(skipLeadingRows).build());
-    TransferConfig transferConfig =
-        TransferConfig.newBuilder()
-            .setDestinationDatasetId(datasetName)
-            .setDisplayName(displayName)
-            .setDataSourceId("amazon_s3")
-            .setParams(Struct.newBuilder().putAllFields(params).build())
-            .setSchedule("every 24 hours")
-            .build();
-    CreateAmazonS3Transfer.createAmazonS3Transfer(PROJECT_ID, transferConfig);
+        "storage_account", Value.newBuilder().setStringValue(DTS_AZURE_STORAGE_ACCOUNT).build());
+    params.put("container", Value.newBuilder().setStringValue(DTS_AZURE_BLOB_CONTAINER).build());
+    params.put("data_path", Value.newBuilder().setStringValue("*").build());
+    params.put("sas_token", Value.newBuilder().setStringValue(DTS_AZURE_SAS_TOKEN).build());
+    params.put("file_format", Value.newBuilder().setStringValue("CSV").build());
+    params.put("field_delimiter", Value.newBuilder().setStringValue(",").build());
+    params.put("skip_leading_rows", Value.newBuilder().setStringValue("1").build());
+
+    CreateAzureBlobStorageTransfer.createAzureBlobStorageTransfer(
+        PROJECT_ID, displayName, datasetName, params);
     String result = bout.toString();
     name = result.substring(result.indexOf(":") + 1, result.length() - 1);
-    assertThat(result).contains("Amazon s3 transfer created successfully :");
+    assertThat(result).contains("Azure Blob Storage transfer created successfully: ");
   }
 }
