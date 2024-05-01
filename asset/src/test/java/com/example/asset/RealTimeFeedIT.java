2a6d82012ac51b86a04ac2d1c6ff0be4064cc624
Sviatoslav Sv <ssvir@softserveinc.com>
2024-05-01 18:39:41 +0300

    unignore test class, added try with resources

--- asset/src/test/java/com/example/asset/RealTimeFeedIT.java
+++ asset/src/test/java/com/example/asset/RealTimeFeedIT.java
@@ -32,7 +32,6 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -42,7 +41,6 @@ import org.junit.runners.MethodSorters;
 @RunWith(JUnit4.class)
 @SuppressWarnings("checkstyle:abbreviationaswordinname")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-@Ignore("TODO: Fix https://github.com/GoogleCloudPlatform/java-docs-samples/issues/8963")
 public class RealTimeFeedIT {
   private static final String topicId = "topicId";
   private static final String feedId = UUID.randomUUID().toString();
@@ -63,14 +61,16 @@ public class RealTimeFeedIT {
 
   @BeforeClass
   public static void createTopic() throws Exception {
-    TopicAdminClient topicAdminClient = TopicAdminClient.create();
-    topicAdminClient.createTopic(topicName);
+    try (TopicAdminClient topicAdminClient = TopicAdminClient.create()) {
+      topicAdminClient.createTopic(topicName);
+    }
   }
 
   @AfterClass
   public static void deleteTopic() throws Exception {
-    TopicAdminClient topicAdminClient = TopicAdminClient.create();
-    topicAdminClient.deleteTopic(topicName);
+    try (TopicAdminClient topicAdminClient = TopicAdminClient.create()) {
+      topicAdminClient.deleteTopic(topicName);
+    }
   }
 
   @Before
