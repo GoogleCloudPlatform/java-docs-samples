3d074a64ef00888f6e911d4d469296f22932c382
msilc <msilc@softserveinc.com>
2024-05-09 11:28:17 +0300

    AccessTests fix

--- iam/api-client/src/test/java/iam/snippets/AccessTests.java
+++ iam/api-client/src/test/java/iam/snippets/AccessTests.java
@@ -20,8 +20,7 @@ import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.assertNotNull;
 
 import com.google.cloud.iam.admin.v1.IAMClient;
-import com.google.iam.admin.v1.DeleteServiceAccountRequest;
-import com.google.iam.admin.v1.ServiceAccountName;
+import com.google.iam.admin.v1.*;
 import com.google.iam.v1.Binding;
 import com.google.iam.v1.Policy;
 import java.io.ByteArrayOutputStream;
@@ -57,7 +56,14 @@ public class AccessTests {
   public static void checkRequirementsAndInitServiceAccount() throws IOException {
     requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
     requireEnvVar("GOOGLE_CLOUD_PROJECT");
-    CreateServiceAccount.createServiceAccount(PROJECT_ID, SERVICE_ACCOUNT);
+
+    CreateServiceAccountRequest request = CreateServiceAccountRequest.newBuilder()
+            .setName(ProjectName.of(PROJECT_ID).toString())
+            .setAccountId(SERVICE_ACCOUNT)
+            .build();
+    try (IAMClient iamClient = IAMClient.create()) {
+      iamClient.createServiceAccount(request);
+    }
   }
 
   @AfterClass
