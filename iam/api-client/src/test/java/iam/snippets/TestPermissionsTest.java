929ccf9db2200005bc7b926af8e5074b51252997
Sviatoslav Sv <ssvir@softserveinc.com>
2024-05-14 14:52:05 +0300

    fix lint

--- iam/api-client/src/test/java/iam/snippets/TestPermissionsTest.java
+++ iam/api-client/src/test/java/iam/snippets/TestPermissionsTest.java
@@ -15,17 +15,19 @@
 
 package iam.snippets;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertNotNull;
+
 
 @RunWith(JUnit4.class)
 public class TestPermissionsTest {
