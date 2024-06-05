4d8afee6ff558348341819d755dece41e6ec209e
Sviatoslav Sv <ssvir@softserveinc.com>
2024-06-05 15:09:23 +0300

    delete useless code

--- compute/cloud-client/src/test/java/compute/SnippetsIT.java
+++ compute/cloud-client/src/test/java/compute/SnippetsIT.java
@@ -233,8 +233,6 @@ public class SnippetsIT {
 
     // Disable usage exports.
     boolean isDisabled = compute.SetUsageExportBucket.disableUsageExportBucket(PROJECT_ID);
-    // Wait for the settings to take place.
-    TimeUnit.SECONDS.sleep(2);
     Assert.assertFalse(isDisabled);
   }
 
