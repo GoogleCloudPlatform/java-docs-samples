a77cf4129713eb8f4eed79fd8f0f0fcdccda8ad2
Rafael Murata <rerodrigues@google.com>
2024-06-05 11:17:37 -0300

    Update security-command-center/snippets/src/main/java/vtwo/bigquery/ListBigQueryExports.java
    Co-authored-by: Sita Lakshmi Sangameswaran <sitalakshmi@google.com>

--- security-command-center/snippets/src/main/java/vtwo/bigquery/ListBigQueryExports.java
+++ security-command-center/snippets/src/main/java/vtwo/bigquery/ListBigQueryExports.java
@@ -42,8 +42,7 @@ public class ListBigQueryExports {
   public static void listBigQueryExports(String organizationId, String location)
       throws IOException {
     // Initialize client that will be used to send requests. This client only needs to be created
-    // once, and can be reused for multiple requests. After completing all of your requests, call
-    // the "close" method on the client to safely clean up any remaining background resources.
+    // once, and can be reused for multiple requests.
     try (SecurityCenterClient client = SecurityCenterClient.create()) {
       OrganizationLocationName organizationName = OrganizationLocationName.of(organizationId,
           location);
