7789c3ff14b62efdd5b153d56ecb9b62a92f19c1
Rafael Rodrigues <rerodrigues@google.com>
2024-06-06 16:43:01 +0000

    try back mockStatic

--- security-command-center/snippets/src/test/java/vtwo/BigQueryExportIT.java
+++ security-command-center/snippets/src/test/java/vtwo/BigQueryExportIT.java
@@ -17,7 +17,6 @@
 package vtwo;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -27,27 +26,21 @@ import com.google.cloud.securitycenter.v2.BigQueryExport;
 import com.google.cloud.securitycenter.v2.BigQueryExportName;
 import com.google.cloud.securitycenter.v2.CreateBigQueryExportRequest;
 import com.google.cloud.securitycenter.v2.DeleteBigQueryExportRequest;
-import com.google.cloud.securitycenter.v2.Finding.State;
 import com.google.cloud.securitycenter.v2.GetBigQueryExportRequest;
 import com.google.cloud.securitycenter.v2.ListBigQueryExportsRequest;
 import com.google.cloud.securitycenter.v2.OrganizationLocationName;
 import com.google.cloud.securitycenter.v2.SecurityCenterClient;
 import com.google.cloud.securitycenter.v2.SecurityCenterClient.ListBigQueryExportsPagedResponse;
 import com.google.cloud.securitycenter.v2.UpdateBigQueryExportRequest;
-import com.google.cloud.testing.junit4.MultipleAttemptsRule;
 import com.google.common.collect.ImmutableList;
 import com.google.protobuf.FieldMask;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.concurrent.TimeUnit;
-import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 import vtwo.bigquery.CreateBigQueryExport;
@@ -57,41 +50,25 @@ import vtwo.bigquery.ListBigQueryExports;
 import vtwo.bigquery.UpdateBigQueryExport;
 
 public class BigQueryExportIT {
-  private static final int MAX_ATTEMPT_COUNT = 3;
-  private static final int INITIAL_BACKOFF_MILLIS = 120000; // 2 minutes
+
   private static final String ORGANIZATION_ID = "test-organization-id";
   private static final String PROJECT_ID = "test-project-id";
   private static final String LOCATION = "global";
   private static final String BQ_DATASET_NAME = "test-dataset-id";
   private static final String BQ_EXPORT_ID = "test-export-id";
-  private static MockedStatic<SecurityCenterClient> clientMock;
-  @Mock
-  private static SecurityCenterClient client;
   private static ByteArrayOutputStream stdOut;
 
-  @Rule
-  public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(
-      MAX_ATTEMPT_COUNT,
-      INITIAL_BACKOFF_MILLIS);
-
   @BeforeClass
-  public static void setUp() throws InterruptedException {
+  public static void setUp() {
     final PrintStream out = System.out;
     stdOut = new ByteArrayOutputStream();
     System.setOut(new PrintStream(stdOut));
-
-    client = mock(SecurityCenterClient.class);
-    clientMock = Mockito.mockStatic(SecurityCenterClient.class);
-    clientMock.when(SecurityCenterClient::create).thenReturn(client);
-
     stdOut = null;
     System.setOut(out);
-    TimeUnit.MINUTES.sleep(1);
   }
 
   @AfterClass
   public static void cleanUp() {
-    clientMock.reset();
     stdOut = null;
     System.setOut(null);
   }
@@ -105,147 +82,177 @@ public class BigQueryExportIT {
   @Test
   public void testCreateBigQueryExport() throws IOException {
     // Mocking and test data setup.
-    String filter = "test-filter";
-    OrganizationLocationName organizationName = OrganizationLocationName.of(ORGANIZATION_ID,
-        LOCATION);
-    // Building expected BigQueryExport.
-    BigQueryExport expectedExport = BigQueryExport.newBuilder()
-        .setDescription(
-            "Export low and medium findings if the compute resource has an IAM anomalous grant")
-        .setFilter(filter)
-        .setDataset(String.format("projects/%s/datasets/%s", PROJECT_ID, BQ_DATASET_NAME))
-        .build();
-    // Building CreateBigQueryExportRequest.
-    CreateBigQueryExportRequest request = CreateBigQueryExportRequest.newBuilder()
-        .setParent(organizationName.toString())
-        .setBigQueryExport(expectedExport)
-        .setBigQueryExportId(BQ_EXPORT_ID)
-        .build();
-    // Mocking createBigQueryExport.
-    when(client.createBigQueryExport(request)).thenReturn(expectedExport);
+    SecurityCenterClient client = mock(SecurityCenterClient.class);
+    try (MockedStatic<SecurityCenterClient> clientMock = Mockito.mockStatic(
+        SecurityCenterClient.class)) {
+      clientMock.when(SecurityCenterClient::create).thenReturn(client);
+      // Mocking and test data setup.
+      String filter = "test-filter";
+      OrganizationLocationName organizationName = OrganizationLocationName.of(ORGANIZATION_ID,
+          LOCATION);
+      // Building expected BigQueryExport.
+      BigQueryExport expectedExport = BigQueryExport.newBuilder()
+          .setDescription(
+              "Export low and medium findings if the compute resource has an IAM anomalous grant")
+          .setFilter(filter)
+          .setDataset(String.format("projects/%s/datasets/%s", PROJECT_ID, BQ_DATASET_NAME))
+          .build();
+      // Building CreateBigQueryExportRequest.
+      CreateBigQueryExportRequest request = CreateBigQueryExportRequest.newBuilder()
+          .setParent(organizationName.toString())
+          .setBigQueryExport(expectedExport)
+          .setBigQueryExportId(BQ_EXPORT_ID)
+          .build();
+      // Mocking createBigQueryExport.
+      when(client.createBigQueryExport(request)).thenReturn(expectedExport);
 
-    // Calling createBigQueryExport.
-    BigQueryExport response = CreateBigQueryExport.createBigQueryExport(ORGANIZATION_ID, LOCATION,
-        PROJECT_ID, filter,
-        BQ_DATASET_NAME, BQ_EXPORT_ID);
-    // Verifying createBigQueryExport was called.
-    verify(client).createBigQueryExport(request);
+      // Calling createBigQueryExport.
+      BigQueryExport response = CreateBigQueryExport.createBigQueryExport(ORGANIZATION_ID, LOCATION,
+          PROJECT_ID, filter,
+          BQ_DATASET_NAME, BQ_EXPORT_ID);
+      // Verifying createBigQueryExport was called.
+      verify(client).createBigQueryExport(request);
 
-    // Asserts the created BigQueryExport matches the expected request.
-    assertThat(response).isEqualTo(expectedExport);
+      // Asserts the created BigQueryExport matches the expected request.
+      assertThat(response).isEqualTo(expectedExport);
+    }
   }
 
   @Test
   public void testDeleteBigQueryExport() throws IOException {
-    // Building BigQueryExportName.
-    BigQueryExportName bigQueryExportName = BigQueryExportName.of(ORGANIZATION_ID, LOCATION,
-        BQ_EXPORT_ID);
-    // Building DeleteBigQueryExportRequest.
-    DeleteBigQueryExportRequest request = DeleteBigQueryExportRequest.newBuilder()
-        .setName(bigQueryExportName.toString())
-        .build();
-    // Mocking deleteBigQueryExport.
-    doNothing().when(client).deleteBigQueryExport(request);
+    // Mocking and test data setup.
+    SecurityCenterClient client = mock(SecurityCenterClient.class);
+    try (MockedStatic<SecurityCenterClient> clientMock = Mockito.mockStatic(
+        SecurityCenterClient.class)) {
+      clientMock.when(SecurityCenterClient::create).thenReturn(client);
+      // Building BigQueryExportName.
+      BigQueryExportName bigQueryExportName = BigQueryExportName.of(ORGANIZATION_ID, LOCATION,
+          BQ_EXPORT_ID);
+      // Building DeleteBigQueryExportRequest.
+      DeleteBigQueryExportRequest request = DeleteBigQueryExportRequest.newBuilder()
+          .setName(bigQueryExportName.toString())
+          .build();
+      // Mocking deleteBigQueryExport.
+      doNothing().when(client).deleteBigQueryExport(request);
 
-    // Calling deleteBigQueryExport.
-    DeleteBigQueryExport.deleteBigQueryExport(ORGANIZATION_ID, LOCATION, BQ_EXPORT_ID);
+      // Calling deleteBigQueryExport.
+      DeleteBigQueryExport.deleteBigQueryExport(ORGANIZATION_ID, LOCATION, BQ_EXPORT_ID);
 
-    // Verifying deleteBigQueryExport was called.
-    verify(client).deleteBigQueryExport(request);
+      // Verifying deleteBigQueryExport was called.
+      verify(client).deleteBigQueryExport(request);
+    }
   }
 
   @Test
   public void testGetBigQueryExport() throws IOException {
-    // Building Expected BigQueryExport.
-    BigQueryExport expectedExport = BigQueryExport.newBuilder()
-        .setName(
-            String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
-                LOCATION, BQ_EXPORT_ID))
-        .build();
-    // Build the BigQueryExportName and request.
-    BigQueryExportName bigQueryExportName = BigQueryExportName.of(ORGANIZATION_ID, LOCATION,
-        BQ_EXPORT_ID);
-    GetBigQueryExportRequest request = GetBigQueryExportRequest.newBuilder()
-        .setName(bigQueryExportName.toString())
-        .build();
-    // Mocking getBigQueryExport.
-    when(client.getBigQueryExport(request)).thenReturn(expectedExport);
+    // Mocking and test data setup.
+    SecurityCenterClient client = mock(SecurityCenterClient.class);
+    try (MockedStatic<SecurityCenterClient> clientMock = Mockito.mockStatic(
+        SecurityCenterClient.class)) {
+      clientMock.when(SecurityCenterClient::create).thenReturn(client);
+      // Building Expected BigQueryExport.
+      BigQueryExport expectedExport = BigQueryExport.newBuilder()
+          .setName(
+              String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
+                  LOCATION, BQ_EXPORT_ID))
+          .build();
+      // Build the BigQueryExportName and request.
+      BigQueryExportName bigQueryExportName = BigQueryExportName.of(ORGANIZATION_ID, LOCATION,
+          BQ_EXPORT_ID);
+      GetBigQueryExportRequest request = GetBigQueryExportRequest.newBuilder()
+          .setName(bigQueryExportName.toString())
+          .build();
+      // Mocking getBigQueryExport.
+      when(client.getBigQueryExport(request)).thenReturn(expectedExport);
 
-    // Calling getBigQueryExport.
-    BigQueryExport response = GetBigQueryExport.getBigQueryExport(ORGANIZATION_ID, LOCATION,
-        BQ_EXPORT_ID);
-    // Verifying getBigQueryExport was called.
-    verify(client).getBigQueryExport(request);
+      // Calling getBigQueryExport.
+      BigQueryExport response = GetBigQueryExport.getBigQueryExport(ORGANIZATION_ID, LOCATION,
+          BQ_EXPORT_ID);
+      // Verifying getBigQueryExport was called.
+      verify(client).getBigQueryExport(request);
 
-    // Verifies the retrieved BigQueryExport matches the expected export.
-    assertThat(response).isEqualTo(expectedExport);
+      // Verifies the retrieved BigQueryExport matches the expected export.
+      assertThat(response).isEqualTo(expectedExport);
+    }
   }
 
   @Test
   public void testListBigQueryExports() throws IOException {
-    String exportId1 = "export-1";
-    String exportId2 = "export-2";
-    // Building Expected BigQueryExports.
-    BigQueryExport export1 = BigQueryExport.newBuilder()
-        .setName(
-            String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
-                LOCATION, exportId1))
-        .build();
-    BigQueryExport export2 = BigQueryExport.newBuilder()
-        .setName(
-            String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
-                LOCATION, exportId2))
-        .build();
-    // Mocking ListBigQueryExportsPagedResponse.
-    ListBigQueryExportsPagedResponse pagedResponse = mock(ListBigQueryExportsPagedResponse.class);
-    when(pagedResponse.iterateAll()).thenReturn(ImmutableList.of(export1, export2));
-    // Building ListBigQueryExportsRequest.
-    ListBigQueryExportsRequest request = ListBigQueryExportsRequest.newBuilder()
-        .setParent(OrganizationLocationName.of(ORGANIZATION_ID, LOCATION).toString())
-        .build();
-    // Mock the client.listBigQueryExports method to return the paged response.
-    when(client.listBigQueryExports(request)).thenReturn(pagedResponse);
+    // Mocking and test data setup.
+    SecurityCenterClient client = mock(SecurityCenterClient.class);
+    try (MockedStatic<SecurityCenterClient> clientMock = Mockito.mockStatic(
+        SecurityCenterClient.class)) {
+      clientMock.when(SecurityCenterClient::create).thenReturn(client);
+      String exportId1 = "export-1";
+      String exportId2 = "export-2";
+      // Building Expected BigQueryExports.
+      BigQueryExport export1 = BigQueryExport.newBuilder()
+          .setName(
+              String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
+                  LOCATION, exportId1))
+          .build();
+      BigQueryExport export2 = BigQueryExport.newBuilder()
+          .setName(
+              String.format("organizations/%s/locations/%s/bigQueryExports/%s", ORGANIZATION_ID,
+                  LOCATION, exportId2))
+          .build();
+      // Mocking ListBigQueryExportsPagedResponse.
+      ListBigQueryExportsPagedResponse pagedResponse = mock(ListBigQueryExportsPagedResponse.class);
+      when(pagedResponse.iterateAll()).thenReturn(ImmutableList.of(export1, export2));
+      // Building ListBigQueryExportsRequest.
+      ListBigQueryExportsRequest request = ListBigQueryExportsRequest.newBuilder()
+          .setParent(OrganizationLocationName.of(ORGANIZATION_ID, LOCATION).toString())
+          .build();
+      // Mock the client.listBigQueryExports method to return the paged response.
+      when(client.listBigQueryExports(request)).thenReturn(pagedResponse);
 
-    // Calling listBigQueryExports.
-    ListBigQueryExportsPagedResponse response = ListBigQueryExports.listBigQueryExports(
-        ORGANIZATION_ID, LOCATION);
-    // Verifying client.listBigQueryExports was called.
-    verify(client).listBigQueryExports(request);
+      // Calling listBigQueryExports.
+      ListBigQueryExportsPagedResponse response = ListBigQueryExports.listBigQueryExports(
+          ORGANIZATION_ID, LOCATION);
+      // Verifying client.listBigQueryExports was called.
+      verify(client).listBigQueryExports(request);
 
-    // Ensures the response from listBigQueryExports matches the mocked paged response.
-    assertThat(response).isEqualTo(pagedResponse);
+      // Ensures the response from listBigQueryExports matches the mocked paged response.
+      assertThat(response).isEqualTo(pagedResponse);
+    }
   }
 
   @Test
   public void testUpdateBigQueryExport() throws IOException {
-    String filter = "updated filter";
-    String name = String.format("organizations/%s/locations/%s/bigQueryExports/%s",
-        ORGANIZATION_ID,
-        LOCATION, BQ_EXPORT_ID);
-    // Building expected BigQueryExport.
-    BigQueryExport expectedExport = BigQueryExport.newBuilder()
-        .setName(name)
-        .setFilter(filter)
-        .setDescription("Updated description.")
-        .build();
-    // Building Update Parameters.
-    FieldMask updateMask = FieldMask.newBuilder().addPaths("filter").addPaths("description")
-        .build();
-    UpdateBigQueryExportRequest request = UpdateBigQueryExportRequest.newBuilder()
-        .setBigQueryExport(expectedExport)
-        .setUpdateMask(updateMask)
-        .build();
-    // Mocking updateBigQueryExport.
-    when(client.updateBigQueryExport(request)).thenReturn(expectedExport);
+    // Mocking and test data setup.
+    SecurityCenterClient client = mock(SecurityCenterClient.class);
+    try (MockedStatic<SecurityCenterClient> clientMock = Mockito.mockStatic(
+        SecurityCenterClient.class)) {
+      clientMock.when(SecurityCenterClient::create).thenReturn(client);
+      String filter = "updated filter";
+      String name = String.format("organizations/%s/locations/%s/bigQueryExports/%s",
+          ORGANIZATION_ID,
+          LOCATION, BQ_EXPORT_ID);
+      // Building expected BigQueryExport.
+      BigQueryExport expectedExport = BigQueryExport.newBuilder()
+          .setName(name)
+          .setFilter(filter)
+          .setDescription("Updated description.")
+          .build();
+      // Building Update Parameters.
+      FieldMask updateMask = FieldMask.newBuilder().addPaths("filter").addPaths("description")
+          .build();
+      UpdateBigQueryExportRequest request = UpdateBigQueryExportRequest.newBuilder()
+          .setBigQueryExport(expectedExport)
+          .setUpdateMask(updateMask)
+          .build();
+      // Mocking updateBigQueryExport.
+      when(client.updateBigQueryExport(request)).thenReturn(expectedExport);
 
-    // Calling updateBigQueryExport.
-    BigQueryExport response = UpdateBigQueryExport.updateBigQueryExport(ORGANIZATION_ID, LOCATION,
-        filter, BQ_EXPORT_ID);
-    // Verifying updateBigQueryExport was called.
-    verify(client).updateBigQueryExport(request);
+      // Calling updateBigQueryExport.
+      BigQueryExport response = UpdateBigQueryExport.updateBigQueryExport(ORGANIZATION_ID, LOCATION,
+          filter, BQ_EXPORT_ID);
+      // Verifying updateBigQueryExport was called.
+      verify(client).updateBigQueryExport(request);
 
-    // Ensures the updated BigQuery Export name matches the expected name.
-    assertThat(response.getName()).isEqualTo(name);
+      // Ensures the updated BigQuery Export name matches the expected name.
+      assertThat(response.getName()).isEqualTo(name);
+    }
   }
 
 } 
