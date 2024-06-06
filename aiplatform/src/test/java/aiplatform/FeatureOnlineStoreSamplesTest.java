ae986ed93ab307e67b205051751381c8646d20e8
Ax / Yuya Ono <yuyaono@google.com>
2024-06-06 15:03:52 +0900

    reformat

--- aiplatform/src/test/java/aiplatform/FeatureOnlineStoreSamplesTest.java
+++ aiplatform/src/test/java/aiplatform/FeatureOnlineStoreSamplesTest.java
@@ -19,83 +19,85 @@
  * the code snippet
  */
 
- package aiplatform;
+package aiplatform;
 
- import static com.google.common.truth.Truth.assertThat;
- import static junit.framework.TestCase.assertNotNull;
- 
- import com.google.cloud.aiplatform.v1beta1.FeatureOnlineStore;
- import com.google.cloud.testing.junit4.MultipleAttemptsRule;
- import java.io.IOException;
- import java.util.UUID;
- import java.util.concurrent.ExecutionException;
- import java.util.concurrent.TimeoutException;
- import org.junit.After;
- import org.junit.Before;
- import org.junit.BeforeClass;
- import org.junit.Rule;
- import org.junit.Test;
- import org.junit.runner.RunWith;
- import org.junit.runners.JUnit4;
- 
- @RunWith(JUnit4.class)
- public class FeatureOnlineStoreSamplesTest {
-   @Rule public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(3);
- 
-   private static final String PROJECT_ID = System.getenv("UCAIP_PROJECT_ID");
-   private static final int MIN_NODE_COUNT = 1;
-   private static final int MAX_NODE_COUNT = 2;
-   private static final int TARGET_CPU_UTILIZATION = 60;
-   private static final String DESCRIPTION = "Test Description";
-   private static final int MONITORING_INTERVAL_DAYS = 1;
-   private static final boolean USE_FORCE = true;
-   private static final String LOCATION = "us-central1";
-   private static final String ENDPOINT = "us-central1-aiplatform.googleapis.com:443";
-   private static final int TIMEOUT = 600;
-   private String featureOnlineStoreId;
- 
-   private static void requireEnvVar(String varName) {
-     String errorMessage =
-         String.format("Environment variable '%s' is required to perform these tests.", varName);
-     assertNotNull(errorMessage, System.getenv(varName));
-   }
- 
-   @BeforeClass
-   public static void checkRequirements() {
-     requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
-     requireEnvVar("UCAIP_PROJECT_ID");
-   }
- 
-   @Before
-   public void setUp() {
-   }
- 
-   @After
-   public void tearDown() {
-   }
- 
-   @Test
-   public void testCreateAndDeleteFeaturestore()
-       throws IOException, InterruptedException, ExecutionException, TimeoutException {
-     // Create the featureOnlineStore
-     String tempUuid = UUID.randomUUID().toString().replaceAll("-", "_").substring(0, 25);
-     String id = String.format("temp_fos_samples_test_%s", tempUuid);
-     FeatureOnlineStore featureOnlineStoreResponse;
- 
-     featureOnlineStoreResponse =
-         CreateFeatureOnlineStoreFixedNodesSample.createFeatureOnlineStoreFixedNodesSample(
-             PROJECT_ID, id, MIN_NODE_COUNT, MAX_NODE_COUNT, TARGET_CPU_UTILIZATION,
-             LOCATION, ENDPOINT, TIMEOUT);
- 
-     // Assert
-     featureOnlineStoreId =
-         featureOnlineStoreResponse.getName().split("featureOnlineStores/")[1]
-             .split("\n")[0].trim();
-     assertThat(featureOnlineStoreId).isEqualTo(id);
- 
-     // Delete the featureOnlineStore
-     DeleteFeatureOnlineStoreSample.deleteFeatureOnlineStoreSample(
-         PROJECT_ID, featureOnlineStoreId, USE_FORCE, LOCATION, ENDPOINT, TIMEOUT);
-   }
- }
- 
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.TestCase.assertNotNull;
+
+import com.google.cloud.aiplatform.v1beta1.FeatureOnlineStore;
+import com.google.cloud.testing.junit4.MultipleAttemptsRule;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FeatureOnlineStoreSamplesTest {
+  @Rule public final MultipleAttemptsRule multipleAttemptsRule = new MultipleAttemptsRule(3);
+
+  private static final String PROJECT_ID = System.getenv("UCAIP_PROJECT_ID");
+  private static final int MIN_NODE_COUNT = 1;
+  private static final int MAX_NODE_COUNT = 2;
+  private static final int TARGET_CPU_UTILIZATION = 60;
+  private static final String DESCRIPTION = "Test Description";
+  private static final int MONITORING_INTERVAL_DAYS = 1;
+  private static final boolean USE_FORCE = true;
+  private static final String LOCATION = "us-central1";
+  private static final String ENDPOINT = "us-central1-aiplatform.googleapis.com:443";
+  private static final int TIMEOUT = 600;
+  private String featureOnlineStoreId;
+
+  private static void requireEnvVar(String varName) {
+    String errorMessage =
+        String.format("Environment variable '%s' is required to perform these tests.", varName);
+    assertNotNull(errorMessage, System.getenv(varName));
+  }
+
+  @BeforeClass
+  public static void checkRequirements() {
+    // requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("UCAIP_PROJECT_ID");
+  }
+
+  @Before
+  public void setUp() {}
+
+  @After
+  public void tearDown() {}
+
+  @Test
+  public void testCreateAndDeleteFeaturestore()
+      throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    // Create the featureOnlineStore
+    String tempUuid = UUID.randomUUID().toString().replaceAll("-", "_").substring(0, 25);
+    String id = String.format("temp_fos_samples_test_%s", tempUuid);
+    FeatureOnlineStore featureOnlineStoreResponse;
+
+    featureOnlineStoreResponse =
+        CreateFeatureOnlineStoreFixedNodesSample.createFeatureOnlineStoreFixedNodesSample(
+            PROJECT_ID,
+            id,
+            MIN_NODE_COUNT,
+            MAX_NODE_COUNT,
+            TARGET_CPU_UTILIZATION,
+            LOCATION,
+            ENDPOINT,
+            TIMEOUT);
+
+    // Assert
+    featureOnlineStoreId =
+        featureOnlineStoreResponse.getName().split("featureOnlineStores/")[1].split("\n")[0].trim();
+    assertThat(featureOnlineStoreId).isEqualTo(id);
+
+    // Delete the featureOnlineStore
+    DeleteFeatureOnlineStoreSample.deleteFeatureOnlineStoreSample(
+        PROJECT_ID, featureOnlineStoreId, USE_FORCE, LOCATION, ENDPOINT, TIMEOUT);
+  }
+}
