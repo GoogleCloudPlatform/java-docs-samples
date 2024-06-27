1d6733574d871e8ed905ce3fa34b02a6f249c13e
Svyatosav <32882988+ssvir@users.noreply.github.com>
2024-06-27 22:11:33 +0300

    refactor(compute): create Certificate samples (#9371)
    * init
    
    * added secret variable
    
    * fix lint
    
    * check that PrivateKey not null
    
    * add new secret to kokoro
    
    * use env variable instead of SecretManager
    
    ---------
    
    Co-authored-by: Sita Lakshmi Sangameswaran <sitalakshmi@google.com>

---
+++ compute/load-balancing/src/main/java/compute/CreateCertificate.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute;
+
+// [START compute_certificate_create]
+
+import com.google.cloud.compute.v1.InsertSslCertificateRequest;
+import com.google.cloud.compute.v1.SslCertificate;
+import com.google.cloud.compute.v1.SslCertificatesClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class CreateCertificate {
+
+  public static void main(String[] args)
+          throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    // TODO(developer): Replace these variables before running the sample.
+    // Project ID or project number of the Google Cloud project you want to use.
+    String project = "your-project-id";
+    // The certificate you want to create in your project.
+    String certificate = "your-certificate";
+    // The private key you used to sign the certificate with.
+    String privateKey = "your-private-key";
+    // Name for the certificate once it's created in your project.
+    String certificateName = "your-certificate-name";
+
+    createCertificate(project, certificate, privateKey, certificateName);
+  }
+
+  // Create a global SSL self-signed certificate within your Google Cloud project.
+  public static SslCertificate createCertificate(String project, String certificate,
+                                                 String privateKey, String certificateName)
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (SslCertificatesClient client = SslCertificatesClient.create()) {
+      SslCertificate certificateResource = SslCertificate.newBuilder()
+              .setCertificate(certificate)
+              .setPrivateKey(privateKey)
+              .setName(certificateName)
+              .build();
+
+      InsertSslCertificateRequest request = InsertSslCertificateRequest.newBuilder()
+              .setProject(project)
+              .setSslCertificateResource(certificateResource)
+              .build();
+
+      client.insertCallable().futureCall(request).get(60, TimeUnit.SECONDS);
+
+      // Wait for server update
+      TimeUnit.SECONDS.sleep(1);
+
+      SslCertificate sslCert = client.get(project, certificateName);
+
+      System.out.printf("Certificate '%s' has been created successfully", sslCert.getName());
+
+      return sslCert;
+    }
+  }
+}
+// [END compute_certificate_create]
