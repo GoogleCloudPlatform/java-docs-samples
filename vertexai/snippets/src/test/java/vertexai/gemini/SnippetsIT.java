73809305be86737f0c038ba26c7c79cce4e344e0
Tetiana Yahodska <tyaho@softserveinc.com>
2024-08-01 11:51:28 +0200

    Fixed test

--- vertexai/snippets/src/test/java/vertexai/gemini/SnippetsIT.java
+++ vertexai/snippets/src/test/java/vertexai/gemini/SnippetsIT.java
@@ -172,8 +172,8 @@ public class SnippetsIT {
 
   @Test
   public void testQuickstart() throws IOException {
-    Quickstart.quickstart(PROJECT_ID, LOCATION, GEMINI_FLASH);
-    assertThat(bout.toString()).contains("scones");
+    String output = Quickstart.quickstart(PROJECT_ID, LOCATION, GEMINI_FLASH);
+    assertThat(output).contains("scones");
   }
 
   @Test
