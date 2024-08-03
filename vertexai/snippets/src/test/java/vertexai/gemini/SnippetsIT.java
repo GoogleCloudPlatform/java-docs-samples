148840652a5b359c9171990b1d22b5118be6ca1c
Guillaume Laforge <glaforge@gmail.com>
2024-08-03 18:55:44 +0200

    Add controlled generation support with response mime type and JSON schema (#9427)
    * feat(vertexai): Multimodal snippets using Gemini 1.5
    
    * feat(vertexai): Multimodal snippets using Gemini 1.5 (checkstyle and header tweaks)
    
    * feat(vertexai): Multimodal snippets using Gemini 1.5 (incorporating feedback, fixing region tags, and clarifying comments)
    
    * feat(vertexai): Use gemini-1.5-flash-001 instead of the preview model
    
    * Reset the initial backoff time to its original setting
    
    * feat(vertexai): Updated to use Gemini 1.5 Flash and new samples
    
    - added some advanced variants of existing snippets
    - added grounding with private data and with Google Search web results
    - added new multimodality streaming/non-streaming examples
    - updated some samples with Gemini 1.5 Flash
    - updated/added some region tags for integration in the documentation
    
    * fix assertion in the grounding with private data test case
    
    * - split the token count and grounding samples to have one file per feature
    - disable a flaky test assertion
    
    * - added some extra comments for clarity
    - updated the region tag locations
    
    * - added a couple more comments for clarity and reference
    
    * feat(vertexai): Add controlled generation (constrained decoding)
    
    * feat(vertexai): Add controlled generation (constrained decoding)
    (small typo)

--- vertexai/snippets/src/test/java/vertexai/gemini/SnippetsIT.java
+++ vertexai/snippets/src/test/java/vertexai/gemini/SnippetsIT.java
@@ -22,13 +22,17 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import com.google.cloud.testing.junit4.MultipleAttemptsRule;
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Arrays;
 import java.util.Base64;
+import java.util.stream.Collectors;
 import javax.net.ssl.HttpsURLConnection;
 import org.junit.After;
 import org.junit.Before;
@@ -44,6 +48,7 @@ public class SnippetsIT {
   private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
   private static final String LOCATION = "us-central1";
   private static final String GEMINI_FLASH = "gemini-1.5-flash-001";
+  private static final String GEMINI_PRO = "gemini-1.5-pro-001";
   private static final String DATASTORE_ID = "grounding-test-datastore_1716831150046";
   private static final int MAX_ATTEMPT_COUNT = 3;
   private static final int INITIAL_BACKOFF_MILLIS = 120000; // 2 minutes
@@ -311,5 +316,159 @@ public class SnippetsIT {
     String output = Multimodal.nonStreamingMultimodal(PROJECT_ID, LOCATION, GEMINI_FLASH);
 
     assertThat(output).ignoringCase().contains("no");
+  }
+
+  private class Recipe {
+    @SerializedName("recipe_name")
+    public String recipeName;
+  }
+
+  @Test
+  public void testControlledGenerationWithMimeType() throws Exception {
+    String output = ControlledGenerationMimeType
+        .controlGenerationWithMimeType(PROJECT_ID, LOCATION, GEMINI_FLASH);
+    Recipe[] recipes = new Gson().fromJson(output, Recipe[].class);
+
+    assertThat(recipes).isNotEmpty();
+    assertThat(recipes[0].recipeName).isNotEmpty();
+  }
+
+  @Test
+  public void testControlledGenerationWithJsonSchema() throws Exception {
+    String output = ControlledGenerationSchema
+        .controlGenerationWithJsonSchema(PROJECT_ID, LOCATION, GEMINI_PRO);
+    Recipe[] recipes = new Gson().fromJson(output, Recipe[].class);
+
+    assertThat(recipes).isNotEmpty();
+    assertThat(recipes[0].recipeName).isNotEmpty();
+  }
+
+  private class Review {
+    public int rating;
+    public String flavor;
+  }
+
+  @Test
+  public void testControlledGenerationWithJsonSchema2() throws Exception {
+    String output = ControlledGenerationSchema2
+        .controlGenerationWithJsonSchema2(PROJECT_ID, LOCATION, GEMINI_PRO);
+    Review[] recipes = new Gson().fromJson(output, Review[].class);
+
+    assertThat(recipes).hasLength(2);
+    assertThat(recipes[0].flavor).isNotEmpty();
+    assertThat(recipes[0].rating).isEqualTo(4);
+    assertThat(recipes[1].flavor).isNotEmpty();
+    assertThat(recipes[1].rating).isEqualTo(1);
+  }
+
+  private class WeatherForecast {
+    public DayForecast[] forecast;
+  }
+
+  private class DayForecast {
+    @SerializedName("Day")
+    public String day;
+    @SerializedName("Forecast")
+    public String forecast;
+    @SerializedName("Humidity")
+    public String humidity;
+    @SerializedName("Temperature")
+    public int temperature;
+    @SerializedName("Wind Speed")
+    public int windSpeed;
+  }
+
+  @Test
+  public void testControlledGenerationWithJsonSchema3() throws Exception {
+    String output = ControlledGenerationSchema3
+        .controlGenerationWithJsonSchema3(PROJECT_ID, LOCATION, GEMINI_PRO);
+    WeatherForecast weatherForecast = new Gson().fromJson(output, WeatherForecast.class);
+
+    assertThat(weatherForecast.forecast).hasLength(7);
+
+    assertThat(weatherForecast.forecast[0].day).ignoringCase().isEqualTo("Sunday");
+    assertThat(weatherForecast.forecast[0].forecast).ignoringCase().isEqualTo("Sunny");
+    assertThat(weatherForecast.forecast[0].temperature).isEqualTo(77);
+    assertThat(weatherForecast.forecast[0].humidity).ignoringCase().isEqualTo("50%");
+    assertThat(weatherForecast.forecast[0].windSpeed).isEqualTo(10);
+
+    assertThat(weatherForecast.forecast[1].day).ignoringCase().isEqualTo("Monday");
+    assertThat(weatherForecast.forecast[1].forecast).ignoringCase().isEqualTo("Partly Cloudy");
+    assertThat(weatherForecast.forecast[1].temperature).isEqualTo(72);
+    assertThat(weatherForecast.forecast[1].humidity).ignoringCase().isEqualTo("55%");
+    assertThat(weatherForecast.forecast[1].windSpeed).isEqualTo(15);
+
+    assertThat(weatherForecast.forecast[2].day).ignoringCase().isEqualTo("Tuesday");
+    assertThat(weatherForecast.forecast[2].forecast).ignoringCase().contains("Rain");
+    assertThat(weatherForecast.forecast[2].temperature).isEqualTo(64);
+    assertThat(weatherForecast.forecast[2].humidity).ignoringCase().isEqualTo("70%");
+    assertThat(weatherForecast.forecast[2].windSpeed).isEqualTo(20);
+
+    assertThat(weatherForecast.forecast[3].day).ignoringCase().isEqualTo("Wednesday");
+    assertThat(weatherForecast.forecast[3].forecast).ignoringCase().contains("Thunder");
+    assertThat(weatherForecast.forecast[3].temperature).isEqualTo(68);
+    assertThat(weatherForecast.forecast[3].humidity).ignoringCase().isEqualTo("75%");
+    assertThat(weatherForecast.forecast[3].windSpeed).isEqualTo(25);
+
+    assertThat(weatherForecast.forecast[4].day).ignoringCase().isEqualTo("Thursday");
+    assertThat(weatherForecast.forecast[4].forecast).ignoringCase().isEqualTo("Cloudy");
+    assertThat(weatherForecast.forecast[4].temperature).isEqualTo(66);
+    assertThat(weatherForecast.forecast[4].humidity).ignoringCase().isEqualTo("60%");
+    assertThat(weatherForecast.forecast[4].windSpeed).isEqualTo(18);
+
+    assertThat(weatherForecast.forecast[5].day).ignoringCase().isEqualTo("Friday");
+    assertThat(weatherForecast.forecast[5].forecast).ignoringCase().isEqualTo("Partly Cloudy");
+    assertThat(weatherForecast.forecast[5].temperature).isEqualTo(73);
+    assertThat(weatherForecast.forecast[5].humidity).ignoringCase().isEqualTo("45%");
+    assertThat(weatherForecast.forecast[5].windSpeed).isEqualTo(12);
+
+    assertThat(weatherForecast.forecast[6].day).ignoringCase().isEqualTo("Saturday");
+    assertThat(weatherForecast.forecast[6].forecast).ignoringCase().isEqualTo("Sunny");
+    assertThat(weatherForecast.forecast[6].temperature).isEqualTo(80);
+    assertThat(weatherForecast.forecast[6].humidity).ignoringCase().isEqualTo("40%");
+    assertThat(weatherForecast.forecast[6].windSpeed).isEqualTo(8);
+  }
+
+  private class Item {
+    @SerializedName("to_discard")
+    public Integer toDiscard;
+    public String subcategory;
+    @SerializedName("safe_handling")
+    public Integer safeHandling;
+    @SerializedName("item_category")
+    public String itemCategory;
+    @SerializedName("for_resale")
+    public Integer forResale;
+    public String condition;
+  }
+
+  @Test
+  public void testControlledGenerationWithJsonSchema4() throws Exception {
+    String output = ControlledGenerationSchema4
+        .controlGenerationWithJsonSchema4(PROJECT_ID, LOCATION, GEMINI_PRO);
+    Item[] items = new Gson().fromJson(output, Item[].class);
+
+    assertThat(items).isNotEmpty();
+  }
+
+  private class Obj {
+    public String object;
+  }
+
+  @Test
+  public void testControlledGenerationWithJsonSchema6() throws Exception {
+    String output = ControlledGenerationSchema6
+        .controlGenerationWithJsonSchema6(PROJECT_ID, LOCATION, GEMINI_PRO);
+
+    Obj[] objects = new Gson().fromJson(output, Obj[].class);
+    String recognizedObjects = Arrays.stream(objects)
+        .map(obj -> obj.object.toLowerCase())
+        .collect(Collectors.joining(" "));
+
+    assertThat(recognizedObjects).isNotEmpty();
+    assertThat(recognizedObjects).contains("globe");
+    assertThat(recognizedObjects).contains("keyboard");
+    assertThat(recognizedObjects).contains("passport");
+    assertThat(recognizedObjects).contains("pot");
   }
 }
