e5eb8047044f2fe4c4f91f57673765f55d740eae
Tetiana Yahodska <tyaho@softserveinc.com>
2024-08-28 12:07:48 +0200

    Implemented compute_reservation_get sample, created test

---
+++ compute/cloud-client/src/main/java/compute/reservation/GetComputeReservation.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.reservation;
+
+import com.google.cloud.compute.v1.Reservation;
+import com.google.cloud.compute.v1.ReservationsClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+// [START compute_reservation_get]
+public class GetComputeReservation {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // TODO(developer): Replace these variables before running the sample.
+
+    // Project ID or project number of the Cloud project you want to use.
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the zone in which you want to create the disk.
+    String zone = "us-central1-a";
+    // Name of the reservation you want to create.
+    String reservationName = "test-reservation-name";
+
+    getReservation(projectId, reservationName, zone);
+  }
+
+  // Describe a reservation with the given name in the given zone.
+  public static Reservation getReservation(
+      String projectId,
+      String reservationName,
+      String zone
+      )
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (ReservationsClient reservationsClient = ReservationsClient.create()) {
+
+      // Get the reservation.
+      Reservation reservation = reservationsClient.get(projectId, zone, reservationName);
+
+      System.out.println("Reservation: " + reservation.toString());
+      return reservation;
+    }
+  }
+}
+// [END compute_reservation_get]
