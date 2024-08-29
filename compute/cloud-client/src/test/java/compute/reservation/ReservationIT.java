b18ee50ce112696d15edb1024a8c896e0d5ea385
Tetiana Yahodska <tyaho@softserveinc.com>
2024-08-29 16:10:16 +0200

    Fixed test naming

--- compute/cloud-client/src/test/java/compute/reservation/ReservationIT.java
+++ compute/cloud-client/src/test/java/compute/reservation/ReservationIT.java
@@ -37,7 +37,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-public class DeleteReservationIT {
+public class ReservationIT {
 
   private static String PROJECT_ID;
   private static String ZONE;
