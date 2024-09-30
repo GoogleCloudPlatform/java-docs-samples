d6a708c7891855bee26b05723ab195ab0eb31a49
Tetiana Yahodska <tyaho@softserveinc.com>
2024-09-30 20:05:25 +0200

    Fixed test

--- compute/cloud-client/src/test/java/compute/reservation/CrudOperationResevationIT.java
+++ compute/cloud-client/src/test/java/compute/reservation/CrudOperationResevationIT.java
@@ -23,29 +23,37 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import com.google.api.gax.rpc.NotFoundException;
 import com.google.cloud.compute.v1.Reservation;
 import compute.Util;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import org.junit.Assert;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import org.junit.runners.MethodSorters;
 
 @RunWith(JUnit4.class)
 @Timeout(value = 25, unit = TimeUnit.MINUTES)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CrudOperationResevationIT {
 
   private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
   private static final String ZONE = "us-central1-a";
   private static String RESERVATION_NAME;
   private static final int NUMBER_OF_VMS = 3;
+  private ByteArrayOutputStream stdOut;
 
   // Check if the required environment variables are set.
   public static void requireEnvVar(String envVarName) {
@@ -61,15 +69,15 @@ public class CrudOperationResevationIT {
 
     // Cleanup existing stale resources.
     Util.cleanUpExistingReservations("test-reservation-", PROJECT_ID, ZONE);
-
     RESERVATION_NAME = "test-reservation-" + UUID.randomUUID();
-    CreateReservation.createReservation(
-        PROJECT_ID, RESERVATION_NAME, NUMBER_OF_VMS, ZONE);
   }
 
   @AfterAll
   public static void cleanup()
       throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    final PrintStream out = System.out;
+    ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
     // Delete all reservations created for testing.
     DeleteReservation.deleteReservation(PROJECT_ID, ZONE, RESERVATION_NAME);
 
@@ -77,10 +85,33 @@ public class CrudOperationResevationIT {
     Assertions.assertThrows(
         NotFoundException.class,
         () -> GetReservation.getReservation(PROJECT_ID, RESERVATION_NAME, ZONE));
+    System.setOut(out);
   }
 
-  @RepeatedTest(3)
-  public void testGetReservation()
+  @BeforeEach
+  public void beforeEach() {
+    stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
+  }
+
+  @AfterEach
+  public void afterEach() {
+    stdOut = null;
+    System.setOut(null);
+  }
+
+  @Test
+  public void firstCreateReservationTest()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+
+    CreateReservation.createReservation(
+        PROJECT_ID, RESERVATION_NAME, NUMBER_OF_VMS, ZONE);
+
+    assertThat(stdOut.toString()).contains("Reservation created. Operation Status: DONE");
+  }
+
+  @Test
+  public void secondGetReservationTest()
       throws IOException {
     Reservation reservation = GetReservation.getReservation(
         PROJECT_ID, RESERVATION_NAME, ZONE);
@@ -89,8 +120,8 @@ public class CrudOperationResevationIT {
     assertThat(reservation.getName()).isEqualTo(RESERVATION_NAME);
   }
 
-  @RepeatedTest(3)
-  public void testListReservation() throws IOException {
+  @Test
+  public void thirdListReservationTest() throws IOException {
     List<Reservation> reservations =
         ListReservations.listReservations(PROJECT_ID, ZONE);
 
