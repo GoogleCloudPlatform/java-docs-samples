b718949d12a14b6a8b4c3571674c3d604c246cca
Jacek Spalinski <69755075+jacspa96@users.noreply.github.com>
2024-10-10 10:05:59 +0200

    Dataplex Aspect Type snippets (#9548)
    * feat(dataplex): add pom.xml file for dataplex snippets
    
    * feat(dataplex): add sample for list Aspect Types
    
    * feat(dataplex): add sample for get Aspect Type
    
    * feat(dataplex): add sample for delete Aspect Type
    
    * feat(dataplex): add sample for create Aspect Type
    
    * feat(dataplex): add sample for update Aspect Type
    
    * feat(dataplex): add integration test for list Aspect Types
    
    * feat(dataplex): add integration test for get Aspect Type
    
    * feat(dataplex): add integration test for delete Aspect Type
    
    * feat(dataplex): add integration test for create Aspect Type
    
    * feat(dataplex): add integration test for update Aspect Type
    
    * feat(dataplex): adjust classes to pass lint check
    
    * feat(dataplex): add dataplex folder to CODEOWNERS
    
    * feat(dataplex): adjust license headers
    
    * feat(dataplex): adjust comment for update mask
    
    * feat(dataplex): adjust comment in tests
    
    * feat(dataplex): move all tests for Aspect Types to single class to reduce number of RPC calls
    
    * feat(dataplex): remove region tag from pom.xml
    
    * feat(dataplex): make aspectFields function argument and adjust comments
    
    * feat(dataplex): adjust tests after changes in code samples
    
    * feat(dataplex): add link to documentation for available locations
    
    * feat(dataplex): make snippets return values
    
    * feat(dataplex): adjust tests to check return values
    
    * feat(dataplex): add comments explaining junit annotations
    
    * feat(dataplex): adjust test methods names to adhere to naming convention
    
    ---------
    
    Co-authored-by: Jacek Spalinski <jspa@google.com>

---
+++ dataplex/snippets/src/main/java/dataplex/ListAspectTypes.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dataplex;
+
+// [START dataplex_list_aspect_types]
+import com.google.cloud.dataplex.v1.AspectType;
+import com.google.cloud.dataplex.v1.CatalogServiceClient;
+import com.google.cloud.dataplex.v1.LocationName;
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.List;
+
+// Sample to list Aspect Types
+public class ListAspectTypes {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectId = "MY_PROJECT_ID";
+    // Available locations: https://cloud.google.com/dataplex/docs/locations
+    String location = "MY_LOCATION";
+
+    LocationName locationName = LocationName.of(projectId, location);
+    List<AspectType> aspectTypes = listAspectTypes(locationName);
+    aspectTypes.forEach(
+        aspectType -> System.out.println("Aspect type name: " + aspectType.getName()));
+  }
+
+  public static List<AspectType> listAspectTypes(LocationName locationName) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources,
+    // or use "try-with-close" statement to do this automatically.
+    try (CatalogServiceClient client = CatalogServiceClient.create()) {
+      CatalogServiceClient.ListAspectTypesPagedResponse listAspectTypesResponse =
+          client.listAspectTypes(locationName);
+      return ImmutableList.copyOf(listAspectTypesResponse.iterateAll());
+    }
+  }
+}
+// [END dataplex_list_aspect_types]
