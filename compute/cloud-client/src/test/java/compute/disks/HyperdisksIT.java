c04ed28aafa3f1d6c2a2cced666998ef1bb1dfa9
Tetiana Yahodska <tyaho@softserveinc.com>
2024-10-15 08:27:11 +0200

    Cleaned Storage Pool

--- compute/cloud-client/src/test/java/compute/disks/HyperdisksIT.java
+++ compute/cloud-client/src/test/java/compute/disks/HyperdisksIT.java
@@ -32,6 +32,7 @@ import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.runner.RunWith;
@@ -72,11 +73,12 @@ public class HyperdisksIT {
        throws IOException, InterruptedException, ExecutionException, TimeoutException {
     // Delete all disks created for testing.
     DeleteDisk.deleteDisk(PROJECT_ID, ZONE, HYPERDISK_NAME);
-    DeleteDisk.deleteDisk(PROJECT_ID, ZONE, HYPERDISK_IN_POOL_NAME);
+    //DeleteDisk.deleteDisk(PROJECT_ID, ZONE, HYPERDISK_IN_POOL_NAME);
 
-    Util.deleteStoragePool(PROJECT_ID, ZONE, STORAGE_POOL_NAME);
+    //Util.deleteStoragePool(PROJECT_ID, ZONE, STORAGE_POOL_NAME);
   }
 
+  @Disabled
   @Test
   public void stage1_CreateHyperdiskTest()
       throws IOException, ExecutionException, InterruptedException, TimeoutException {
@@ -95,6 +97,7 @@ public class HyperdisksIT {
     Assert.assertTrue(hyperdisk.getZone().contains(ZONE));
   }
 
+  @Disabled
   @Test
   public void stage1_CreateHyperdiskStoragePoolTest()
       throws IOException, ExecutionException, InterruptedException, TimeoutException {
