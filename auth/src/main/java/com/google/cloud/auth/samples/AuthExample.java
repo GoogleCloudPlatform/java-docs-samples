64f6ef58991584ff28fcf822a44eea76e070e35b
ldetmer <1771267+ldetmer@users.noreply.github.com>
2024-10-15 12:28:10 -0400

    formatting fix

--- auth/src/main/java/com/google/cloud/auth/samples/AuthExample.java
+++ auth/src/main/java/com/google/cloud/auth/samples/AuthExample.java
@@ -20,20 +20,18 @@ import com.google.api.gax.paging.Page;
 import com.google.auth.appengine.AppEngineCredentials;
 import com.google.auth.oauth2.ComputeEngineCredentials;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.language.v2.AnalyzeSentimentResponse;
+import com.google.cloud.language.v2.Document;
+import com.google.cloud.language.v2.LanguageServiceClient;
+import com.google.cloud.language.v2.LanguageServiceSettings;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import com.google.common.collect.Lists;
 import java.io.FileInputStream;
 import java.io.IOException;
-import com.google.cloud.language.v2.AnalyzeSentimentResponse;
-import com.google.cloud.language.v2.Document;
-import com.google.cloud.language.v2.LanguageServiceClient;
-import com.google.cloud.language.v2.LanguageServiceSettings;
 
-/**
- * Demonstrate various ways to authenticate requests using Cloud Storage as an example call.
- */
+/** Demonstrate various ways to authenticate requests using Cloud Storage as an example call. */
 public class AuthExample {
   // [START auth_cloud_implicit]
   static void authImplicit() {
@@ -53,8 +51,9 @@ public class AuthExample {
   static void authExplicit(String jsonPath) throws IOException {
     // You can specify a credential file by providing a path to GoogleCredentials.
     // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
-    GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(jsonPath))
-          .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+    GoogleCredentials credentials =
+        GoogleCredentials.fromStream(new FileInputStream(jsonPath))
+            .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
     Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
 
     System.out.println("Buckets:");
@@ -130,13 +129,13 @@ public class AuthExample {
       return;
     }
     if ("apikey".equals(args[0])) {
-        if (args.length >= 2) {
-          authApiKey(args[1]);
-        } else {
-          throw new IllegalArgumentException("Api key is required with 'apikey'.");
-        }
-        return;
+      if (args.length >= 2) {
+        authApiKey(args[1]);
+      } else {
+        throw new IllegalArgumentException("Api key is required with 'apikey'.");
       }
+      return;
+    }
 
     authImplicit();
   }
