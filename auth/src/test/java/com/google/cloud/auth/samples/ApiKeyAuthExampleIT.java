7c2ff914820a4dbfa6551779602ff6dd3ec6611e
ldetmer <1771267+ldetmer@users.noreply.github.com>
2024-10-16 14:35:13 -0400

    added tests

---
+++ auth/src/test/java/com/google/cloud/auth/samples/ApiKeyAuthExampleIT.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.auth.samples;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+//CHECKSTYLE OFF: AbbreviationAsWordInName
+public class ApiKeyAuthExampleIT {
+  //CHECKSTYLE ON: AbbreviationAsWordInName
+  private ByteArrayOutputStream bout;
+  private PrintStream out;
+  private String apiKey;
+
+  @Before
+  public void setUp() {
+    bout = new ByteArrayOutputStream();
+    out = new PrintStream(bout);
+    System.setOut(out);
+    apiKey = System.getenv("API_KEY");
+    assertNotNull("Please set your API_KEY env var", apiKey);
+  }
+
+  @Test
+  public void testAuthApiKey() throws IOException {
+    ApiKeyAuthExample.main(new String[] {apiKey});
+    String output = bout.toString();
+    assertTrue(output.contains("magnitude:"));
+  }
+}
