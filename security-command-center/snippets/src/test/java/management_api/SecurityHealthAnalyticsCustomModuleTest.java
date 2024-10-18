db8ddf57947beb748801f5fa987bd7f2231e960c
lovenishs04 <lovenishs@google.com>
2024-10-18 11:02:38 +0000

    sample code for DeleteSecurityHealthAnalyticsCustomModule

--- security-command-center/snippets/src/test/java/management_api/SecurityHealthAnalyticsCustomModuleTest.java
+++ security-command-center/snippets/src/test/java/management_api/SecurityHealthAnalyticsCustomModuleTest.java
@@ -17,12 +17,11 @@
 package management_api;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.mock;
-
+import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.assertNotNull;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -30,31 +29,33 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
-import org.mockito.MockedStatic;
-import org.mockito.Mockito;
 
-import com.google.cloud.securitycentermanagement.v1.CreateSecurityHealthAnalyticsCustomModuleRequest;
-import com.google.cloud.securitycentermanagement.v1.CustomConfig;
-import com.google.cloud.securitycentermanagement.v1.SecurityCenterManagementClient;
 import com.google.cloud.securitycentermanagement.v1.SecurityHealthAnalyticsCustomModule;
-import com.google.cloud.securitycentermanagement.v1.CustomConfig.ResourceSelector;
-import com.google.cloud.securitycentermanagement.v1.CustomConfig.Severity;
-import com.google.cloud.securitycentermanagement.v1.SecurityHealthAnalyticsCustomModule.EnablementState;
-import com.google.type.Expr;
+
+import io.opentelemetry.api.internal.StringUtils;
 
 @RunWith(JUnit4.class)
 public class SecurityHealthAnalyticsCustomModuleTest {
 
-	private static final String ORGANIZATION_ID = "organization_id";
+	private static final String ORGANIZATION_ID = System.getenv("ORGANIZATION_ID");
 	private static final String LOCATION = "global";
-	private static final String customModuleDisplayName = "custom_module_display_name";
+	private static final String CUSTOM_MODULE_DISPLAY_NAME = "custom_module_test";
 	private static ByteArrayOutputStream stdOut;
+
+	// Check if the required environment variables are set.
+	public static void requireEnvVar(String envVarName) {
+		assertWithMessage(String.format("Missing environment variable '%s' ", envVarName))
+				.that(System.getenv(envVarName)).isNotEmpty();
+	}
 
 	@BeforeClass
 	public static void setUp() {
 		final PrintStream out = System.out;
 		stdOut = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(stdOut));
+
+		requireEnvVar("ORGANIZATION_ID");
+
 		stdOut = null;
 		System.setOut(out);
 	}
@@ -77,51 +78,72 @@ public class SecurityHealthAnalyticsCustomModuleTest {
 		System.setOut(null);
 	}
 
-	@Test
-	public void testCreateSecurityHealthAnalyticsCustomModule() throws IOException {
-		// Mocking and test data setup.
-		SecurityCenterManagementClient client = mock(SecurityCenterManagementClient.class);
-		try (MockedStatic<SecurityCenterManagementClient> clientMock = Mockito
-				.mockStatic(SecurityCenterManagementClient.class)) {
-			clientMock.when(SecurityCenterManagementClient::create).thenReturn(client);
+	// extractCustomModuleID extracts the custom module Id from the full name
+	public static String extractCustomModuleId(String customModuleFullName) {
+		if (!StringUtils.isNullOrEmpty(customModuleFullName)) {
+			String[] result = customModuleFullName.split("/");
+			if (result.length > 0) {
+				return result[result.length - 1];
+			}
+		}
+		return "";
+	}
 
-			String parent = String.format("organizations/%s/locations/%s", ORGANIZATION_ID, LOCATION);
-			String name = String.format("%s/securityHealthAnalyticsCustomModules/%s", parent, "custom_module");
-
-			Expr expr = Expr.newBuilder()
-					.setExpression("has(resource.rotationPeriod) && (resource.rotationPeriod > duration('2592000s'))")
-					.build();
-
-			ResourceSelector resourceSelector = ResourceSelector.newBuilder()
-					.addResourceTypes("cloudkms.googleapis.com/CryptoKey").build();
-
-			// Building CustomConfig
-			CustomConfig customConfig = CustomConfig.newBuilder().setPredicate(expr)
-					.setResourceSelector(resourceSelector).setSeverity(Severity.MEDIUM)
-					.setDescription("add your description here").setRecommendation("add your recommendation here").build();
-
-			// Building SecurityHealthAnalyticsCustomModule
-			SecurityHealthAnalyticsCustomModule expectedResult = SecurityHealthAnalyticsCustomModule.newBuilder()
-					.setName(name).setDisplayName(customModuleDisplayName).setEnablementState(EnablementState.ENABLED)
-					.setCustomConfig(customConfig).build();
-
-			// Building CreateSecurityHealthAnalyticsCustomModuleRequest
-			CreateSecurityHealthAnalyticsCustomModuleRequest request = CreateSecurityHealthAnalyticsCustomModuleRequest
-					.newBuilder().setParent(parent).setSecurityHealthAnalyticsCustomModule(expectedResult).build();
-
-			// Mocking createSecurityHealthAnalyticsCustomModule
-			Mockito.when(client.createSecurityHealthAnalyticsCustomModule(request)).thenReturn(expectedResult);
-
-			// Calling createSecurityHealthAnalyticsCustomModule
+	// createCustomModule method is for creating the custom module
+	public static SecurityHealthAnalyticsCustomModule createCustomModule(String parent, String customModuleDisplayName)
+			throws IOException {
+		if (!StringUtils.isNullOrEmpty(parent) && !StringUtils.isNullOrEmpty(customModuleDisplayName)) {
 			SecurityHealthAnalyticsCustomModule response = CreateSecurityHealthAnalyticsCustomModule
 					.createSecurityHealthAnalyticsCustomModule(parent, customModuleDisplayName);
-
-			// Verifying createSecurityHealthAnalyticsCustomModule was called
-			Mockito.verify(client).createSecurityHealthAnalyticsCustomModule(request);
-
-			// Asserts the created SecurityHealthAnalyticsCustomModule matches the expected
-			// request.
-			assertThat(response).isEqualTo(expectedResult);
+			return response;
 		}
+		return null;
+	}
+
+	// deleteCustomModule method is for deleting the custom module
+	public static void deleteCustomModule(String parent, String customModuleId) throws IOException {
+		if (!StringUtils.isNullOrEmpty(parent) && !StringUtils.isNullOrEmpty(customModuleId)) {
+			DeleteSecurityHealthAnalyticsCustomModule.deleteSecurityHealthAnalyticsCustomModule(parent, customModuleId);
+		}
+	}
+
+	@Test
+	public void testCreateSecurityHealthAnalyticsCustomModule() throws IOException {
+
+		String parent = String.format("organizations/%s/locations/%s", ORGANIZATION_ID, LOCATION);
+
+		// creating the custom module
+		SecurityHealthAnalyticsCustomModule response = CreateSecurityHealthAnalyticsCustomModule
+				.createSecurityHealthAnalyticsCustomModule(parent, CUSTOM_MODULE_DISPLAY_NAME);
+
+		// assert that response is not null
+		assertNotNull(response);
+
+		// assert that created module display name is matching with the name passed
+		assertThat(response.getDisplayName()).isEqualTo(CUSTOM_MODULE_DISPLAY_NAME);
+
+		// extracting the custom module id from the full name
+		String customModuleId = extractCustomModuleId(response.getName());
+
+		// deleting the custom module for cleanup
+		deleteCustomModule(parent, customModuleId);
+	}
+
+	@Test
+	public void testDeleteSecurityHealthAnalyticsCustomModule() throws IOException {
+
+		String parent = String.format("organizations/%s/locations/%s", ORGANIZATION_ID, LOCATION);
+
+		// create the custom module
+		SecurityHealthAnalyticsCustomModule response = createCustomModule(parent, CUSTOM_MODULE_DISPLAY_NAME);
+
+		// extracting the custom module id from the full name
+		String customModuleId = extractCustomModuleId(response.getName());
+
+		// delete the custom module with the custom module id
+		DeleteSecurityHealthAnalyticsCustomModule.deleteSecurityHealthAnalyticsCustomModule(parent, customModuleId);
+
+		// assert that std output is matching with the string passed
+		assertThat(stdOut.toString()).contains("SecurityHealthAnalyticsCustomModule deleted : " + customModuleId);
 	}
 }
