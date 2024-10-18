ad6d39c414a88896101456c822757238a3a48a3e
Tetiana Yahodska <tyaho@softserveinc.com>
2024-10-18 22:32:31 +0200

    Moved updateVm test to ReservationIT

--- compute/cloud-client/src/test/java/compute/reservation/CrudOperationsReservationIT.java
+++ compute/cloud-client/src/test/java/compute/reservation/CrudOperationsReservationIT.java
@@ -120,17 +120,4 @@ public class CrudOperationsReservationIT {
     assertThat(reservations).isNotNull();
     Assert.assertTrue(reservations.get(0).getName().contains("test-"));
   }
-
-  @Test
-  @Order(2)
-  public void testUpdateVmsForReservation()
-      throws IOException, ExecutionException, InterruptedException, TimeoutException {
-    int newNumberOfVms = 5;
-    UpdateVmsForReservation.updateVmsForReservation(
-        PROJECT_ID, ZONE, RESERVATION_NAME, newNumberOfVms);
-    Reservation reservation = GetReservation.getReservation(
-        PROJECT_ID, RESERVATION_NAME, ZONE);
-
-    Assert.assertEquals(newNumberOfVms, reservation.getSpecificReservation().getCount());
-  }
 }
