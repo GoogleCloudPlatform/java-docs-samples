9f14195918846450e26e7366581888cdff092d1e
Tetiana Yahodska <tyaho@softserveinc.com>
2024-10-19 22:12:19 +0200

    Implemented consume reservation samples, created tests

---
+++ compute/cloud-client/src/main/java/compute/reservation/ConsumeAnyMatchingReservation.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.reservation;
+
+// [START compute_consume_any_matching_reservation]
+
+import static com.google.cloud.compute.v1.ReservationAffinity.ConsumeReservationType.ANY_RESERVATION;
+
+import com.google.cloud.compute.v1.AllocationSpecificSKUAllocationReservedInstanceProperties;
+import com.google.cloud.compute.v1.AllocationSpecificSKUReservation;
+import com.google.cloud.compute.v1.AttachedDisk;
+import com.google.cloud.compute.v1.AttachedDiskInitializeParams;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.NetworkInterface;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.Reservation;
+import com.google.cloud.compute.v1.ReservationAffinity;
+import com.google.cloud.compute.v1.ReservationsClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class ConsumeAnyMatchingReservation {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // TODO(developer): Replace these variables before running the sample.
+    // Project ID or project number of the Cloud project you want to use.
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the zone where the reservation is located.
+    String zone = "us-central1-a";
+    // Name of the reservation you want to query.
+    String reservationName = "YOUR_RESERVATION_NAME";
+    // Name of the VM instance you want to query.
+    String instanceName = "YOUR_INSTANCE_NAME";
+    // Number of the instances.
+    int numberOfVms = 2;
+    // Machine type of the instances.
+    String machineType = "n2-standard-32";
+    // Minimum CPU platform of the instances.
+    String minCpuPlatform = "Intel Cascade Lake";
+
+    createReservation(projectId, reservationName, numberOfVms, zone, machineType, minCpuPlatform);
+    createInstance(projectId, zone, instanceName, machineType, minCpuPlatform);
+  }
+
+  // Creates reservation with properties that match the VM properties.
+  public static void createReservation(String projectId, String reservationName,
+           int numberOfVms, String zone, String machineType, String minCpuPlatform)
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (ReservationsClient reservationsClient = ReservationsClient.create()) {
+
+      Reservation reservation =
+          Reservation.newBuilder()
+              .setName(reservationName)
+              .setZone(zone)
+              .setSpecificReservation(
+                  AllocationSpecificSKUReservation.newBuilder()
+                      .setCount(numberOfVms)
+                      .setInstanceProperties(
+                          AllocationSpecificSKUAllocationReservedInstanceProperties.newBuilder()
+                              .setMachineType(machineType)
+                              .setMinCpuPlatform(minCpuPlatform)
+                              .build())
+                      .build())
+              .build();
+
+      // Wait for the create reservation operation to complete.
+      Operation response =
+          reservationsClient.insertAsync(projectId, zone, reservation)
+              .get(3, TimeUnit.MINUTES);
+
+      if (response.hasError()) {
+        System.out.println("Reservation creation failed!" + response);
+        return;
+      }
+      System.out.println("Reservation created. Operation Status: " + response.getStatus());
+    }
+  }
+
+  // Create a new instance with the provided "instanceName" value in the specified project and zone.
+  // In this consumption model, existing and new VMs automatically consume a reservation
+  // if their properties match the VM properties specified in the reservation.
+  public static void createInstance(String project, String zone, String instanceName,
+                                    String machineType, String minCpuPlatform)
+      throws IOException, InterruptedException, ExecutionException, TimeoutException {
+    // Below are sample values that can be replaced.
+    // sourceImage: path to the operating system image to mount.
+    // *   For details about images you can mount, see https://cloud.google.com/compute/docs/images
+    // diskSizeGb: storage size of the boot disk to attach to the instance.
+    // networkName: network interface to associate with the instance.
+    String sourceImage = String
+        .format("projects/debian-cloud/global/images/family/%s", "debian-11");
+    long diskSizeGb = 10L;
+    String networkName = "default";
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (InstancesClient instancesClient = InstancesClient.create()) {
+      // Instance creation requires at least one persistent disk and one network interface.
+      AttachedDisk disk =
+          AttachedDisk.newBuilder()
+              .setBoot(true)
+              .setAutoDelete(true)
+              .setType(AttachedDisk.Type.PERSISTENT.toString())
+              .setDeviceName("disk-1")
+              .setInitializeParams(
+                  AttachedDiskInitializeParams.newBuilder()
+                      .setSourceImage(sourceImage)
+                      .setDiskSizeGb(diskSizeGb)
+                      .build())
+              .build();
+
+      // Use the network interface provided in the networkName argument.
+      NetworkInterface networkInterface = NetworkInterface.newBuilder()
+          .setName(networkName)
+          .build();
+
+      // Bind `instanceName`, `machineType`, `disk`, and `networkInterface` to an instance.
+      Instance instanceResource =
+          Instance.newBuilder()
+              .setName(instanceName)
+              .setMachineType(String.format("zones/%s/machineTypes/%s", zone, machineType))
+              .addDisks(disk)
+              .addNetworkInterfaces(networkInterface)
+              .setMinCpuPlatform(minCpuPlatform)
+              // Set Reservation Affinity to "ANY"
+              .setReservationAffinity(
+                  ReservationAffinity.newBuilder()
+                      .setConsumeReservationType(
+                          ANY_RESERVATION.toString())
+                      .build())
+              .build();
+
+      System.out.printf("Creating instance: %s at %s %n", instanceName, zone);
+
+      // Wait for the operation to complete.
+      Operation response = instancesClient.insertAsync(project, zone, instanceResource)
+          .get(3, TimeUnit.MINUTES);
+      if (response.hasError()) {
+        System.out.println("Instance creation failed ! ! " + response);
+        return;
+      }
+      System.out.println("Operation Status: " + response.getStatus());
+    }
+  }
+}
+// [END compute_consume_any_matching_reservation]
---
+++ compute/cloud-client/src/test/java/compute/reservation/ConsumeReservationsIT.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.reservation;
+
+import static com.google.cloud.compute.v1.ReservationAffinity.ConsumeReservationType.ANY_RESERVATION;
+import static com.google.cloud.compute.v1.ReservationAffinity.ConsumeReservationType.SPECIFIC_RESERVATION;
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import com.google.api.gax.rpc.NotFoundException;
+import com.google.cloud.compute.v1.Instance;
+import com.google.cloud.compute.v1.InstancesClient;
+import com.google.cloud.compute.v1.ReservationsClient;
+import compute.DeleteInstance;
+import compute.Util;
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@Timeout(value = 25, unit = TimeUnit.MINUTES)
+public class ConsumeReservationsIT {
+
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private static final String ZONE = "us-central1-a";
+  private static ReservationsClient reservationsClient;
+  private static InstancesClient instancesClient;
+  private static String RESERVATION_NAME;
+  private static String RESERVATION_SHARED_NAME;
+  static String javaVersion = System.getProperty("java.version").substring(0, 2);
+  private static String INSTANCE_FOR_SPR;
+  private static  String INSTANCE_FOR_ANY_MATCHING;
+  private static  String SPECIFIC_SHARED_INSTANCE;
+  private static final int NUMBER_OF_VMS = 3;
+  private static final String MACHINE_TYPE = "n2-standard-32";
+  private static final String MIN_CPU_PLATFORM = "Intel Cascade Lake";
+
+  // Check if the required environment variables are set.
+  public static void requireEnvVar(String envVarName) {
+    assertWithMessage(String.format("Missing environment variable '%s' ", envVarName))
+        .that(System.getenv(envVarName)).isNotEmpty();
+  }
+
+  @BeforeAll
+  public static void setUp()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_CLOUD_PROJECT");
+
+    // Initialize the clients once for all tests
+    instancesClient = InstancesClient.create();
+    reservationsClient = ReservationsClient.create();
+
+    RESERVATION_SHARED_NAME = "test-reserv-shared-" + javaVersion + "-"
+        + UUID.randomUUID().toString().substring(0, 8);
+    RESERVATION_NAME = "test-reservaton-" + javaVersion + "-"
+        + UUID.randomUUID().toString().substring(0, 8);
+    INSTANCE_FOR_SPR = "test-instance-for-spr-" + javaVersion + "-"
+            + UUID.randomUUID().toString().substring(0, 8);
+    INSTANCE_FOR_ANY_MATCHING = "test-instance-" + javaVersion + "-"
+            + UUID.randomUUID().toString().substring(0, 8);
+    SPECIFIC_SHARED_INSTANCE = "test-instance-shared-" + javaVersion + "-"
+            + UUID.randomUUID().toString().substring(0, 8);
+
+    // Cleanup existing stale resources.
+    Util.cleanUpExistingInstances("test-instance-for-spr-" + javaVersion, PROJECT_ID, ZONE);
+    Util.cleanUpExistingInstances("test-instance-" + javaVersion, PROJECT_ID, ZONE);
+    Util.cleanUpExistingInstances("test-instance-shared-" + javaVersion, PROJECT_ID, ZONE);
+    Util.cleanUpExistingReservations("test-reserv-shared-" + javaVersion, PROJECT_ID, ZONE);
+    Util.cleanUpExistingReservations("test-reservaton-" + javaVersion, PROJECT_ID, ZONE);
+  }
+
+  @AfterAll
+  public static void cleanup()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Clean up the instances
+    DeleteInstance.deleteInstance(PROJECT_ID, ZONE, INSTANCE_FOR_SPR);
+    DeleteInstance.deleteInstance(PROJECT_ID, ZONE, INSTANCE_FOR_ANY_MATCHING);
+    DeleteInstance.deleteInstance(PROJECT_ID, ZONE, SPECIFIC_SHARED_INSTANCE);
+
+    // Delete all reservations created for testing.
+    DeleteReservation.deleteReservation(PROJECT_ID, ZONE, RESERVATION_NAME);
+    DeleteReservation.deleteReservation(PROJECT_ID, ZONE, RESERVATION_SHARED_NAME);
+
+    // Test that reservations are deleted
+    Assertions.assertThrows(
+        NotFoundException.class,
+        () -> GetReservation.getReservation(PROJECT_ID, RESERVATION_NAME, ZONE));
+    Assertions.assertThrows(
+        NotFoundException.class,
+        () -> GetReservation.getReservation(PROJECT_ID, RESERVATION_SHARED_NAME, ZONE));
+
+    // Close the clients after all tests
+    reservationsClient.close();
+    instancesClient.close();
+  }
+
+  @Test
+  public void testConsumeAnyMatchingReservation()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    ConsumeAnyMatchingReservation.createInstance(
+        PROJECT_ID, ZONE, INSTANCE_FOR_ANY_MATCHING, MACHINE_TYPE, MIN_CPU_PLATFORM);
+
+    Instance instance = instancesClient.get(PROJECT_ID, ZONE, INSTANCE_FOR_ANY_MATCHING);
+
+    Assert.assertEquals(ANY_RESERVATION.toString(),
+        instance.getReservationAffinity().getConsumeReservationType());
+  }
+
+  @Test
+  public void testConsumeSingleProjectReservation()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Create the reservation
+    ConsumeSingleProjectReservation.createReservation(
+        PROJECT_ID, RESERVATION_NAME, NUMBER_OF_VMS,
+        ZONE, MACHINE_TYPE, MIN_CPU_PLATFORM, true);
+
+    ConsumeSingleProjectReservation.createInstance(
+        PROJECT_ID, ZONE, INSTANCE_FOR_SPR, MACHINE_TYPE,
+        MIN_CPU_PLATFORM, RESERVATION_NAME);
+
+    Instance instance = instancesClient.get(PROJECT_ID, ZONE, INSTANCE_FOR_SPR);
+
+    assertThat(instance.getReservationAffinity().getValuesList())
+        .contains(RESERVATION_NAME);
+    Assert.assertEquals(SPECIFIC_RESERVATION.toString(),
+        instance.getReservationAffinity().getConsumeReservationType());
+  }
+
+  @Test
+  public void testConsumeSpecificSharedReservation()
+      throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    ConsumeSpecificSharedReservation.createReservation(PROJECT_ID,
+        RESERVATION_SHARED_NAME, NUMBER_OF_VMS, ZONE,
+        MACHINE_TYPE, MIN_CPU_PLATFORM, true);
+
+    Assert.assertEquals(RESERVATION_SHARED_NAME,
+        reservationsClient.get(PROJECT_ID, ZONE, RESERVATION_SHARED_NAME).getName());
+
+    ConsumeSpecificSharedReservation.createInstance(
+        PROJECT_ID, ZONE, SPECIFIC_SHARED_INSTANCE, MACHINE_TYPE,
+        MIN_CPU_PLATFORM, RESERVATION_SHARED_NAME);
+
+    // Verify that the instance was created with the correct reservation and consumeReservationType
+    Instance instance = instancesClient.get(PROJECT_ID, ZONE, SPECIFIC_SHARED_INSTANCE);
+
+    Assert.assertTrue(instance.getReservationAffinity()
+        .getValuesList().get(0).contains(RESERVATION_SHARED_NAME));
+    Assert.assertEquals(SPECIFIC_RESERVATION.toString(),
+        instance.getReservationAffinity().getConsumeReservationType());
+  }
+}
