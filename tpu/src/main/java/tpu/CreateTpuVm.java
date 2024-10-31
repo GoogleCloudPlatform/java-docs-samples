3531480719547bedb3bd554284e56a47be666c6e
Tetiana Yahodska <tyaho@softserveinc.com>
2024-10-31 12:09:46 +0100

    Fixed empty lines and tests, deleted cleanup method

--- tpu/src/main/java/tpu/CreateTpuVm.java
+++ tpu/src/main/java/tpu/CreateTpuVm.java
@@ -39,7 +39,7 @@ public class CreateTpuVm {
     // see https://cloud.google.com/tpu/docs/regions-zones
     String zone = "europe-west4-a";
     // The name for your TPU.
-    String nodeName = "YOUR_TPY_NAME";
+    String nodeName = "YOUR_TPU_NAME";
     // The accelerator type that specifies the version and size of the Cloud TPU you want to create.
     // For more information about supported accelerator types for each TPU version,
     // see https://cloud.google.com/tpu/docs/system-architecture-tpu-vm#versions.
@@ -90,9 +90,7 @@ public class CreateTpuVm {
               .setNode(tpuVm)
               .build();
 
-      Node response = tpuClient.createNodeAsync(request).get();
-      System.out.printf("TPU VM created: %s\n", response.getName());
-      return response;
+      return tpuClient.createNodeAsync(request).get();
     }
   }
 }
