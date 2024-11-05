73ecc63f115492a2adea56f5806f8a5eaa70f0d0
Tetiana Yahodska <tyaho@softserveinc.com>
2024-11-05 21:45:30 +0100

    Implemented compute_disk_start_replication and compute_disk_stop_replication samples

--- compute/cloud-client/src/main/java/compute/disks/StartDiskReplication.java
+++ compute/cloud-client/src/main/java/compute/disks/StartDiskReplication.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package compute.disks;
 
+// [START compute_disk_start_replication]
 import com.google.cloud.compute.v1.Operation;
 import com.google.cloud.compute.v1.RegionDisksClient;
 import com.google.cloud.compute.v1.RegionDisksStartAsyncReplicationRequest;
-
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 
@@ -13,50 +29,40 @@ public class StartDiskReplication {
       throws IOException, ExecutionException, InterruptedException {
     // TODO(developer): Replace these variables before running the sample.
     // The project that contains the primary disk.
-    String projectId = "tyaho-softserve-project";//"YOUR_PROJECT_ID";
-    String primaryZone = "us-central1";
-    String primaryDiskName = "primary-disk";//"PRIMARY_DISK_NAME";
-    // Name of the zone in which you want to create the secondary disk.
-    String secondaryZone = "us-central1";
-    // Name of the disk you want to create.
-    String secondaryDiskName = "second-disk";//"SECONDARY_DISK_NAME";
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the primary disk.
+    String primaryDiskName = "PRIMARY_DISK_NAME";
+    // Name of the secondary disk.
+    String secondaryDiskName = "SECONDARY_DISK_NAME";
+    // Name of the zone in which your primary disk is located.
+    // Learn more about zones and regions:
+    // https://cloud.google.com/compute/docs/disks/async-pd/about#supported_region_pairs
+    String primaryDiskRegion = "us-central1";
+    // Name of the zone in which your secondary disk is located.
+    String secondaryDiskRegion = "us-east1";
 
-    startDiskAsyncReplication(
-        projectId,
-        primaryZone,
-        primaryDiskName,
-        secondaryZone,
-        secondaryDiskName);
+    startDiskAsyncReplication(projectId, primaryDiskRegion,
+        primaryDiskName, secondaryDiskRegion, secondaryDiskName);
   }
 
   // Starts asynchronous replication for the specified disk.
-  public static void startDiskAsyncReplication(
-      String projectId,
-      String primaryZone,
-      String primaryDiskName,
-      String secondaryZone,
-      String secondaryDiskName)
+  public static void startDiskAsyncReplication(String projectId, String primaryDiskRegion,
+      String primaryDiskName, String secondaryDiskRegion, String secondaryDiskName)
       throws IOException, ExecutionException, InterruptedException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
     try (RegionDisksClient disksClient = RegionDisksClient.create()) {
 
-      String asyncSecondaryDisk = String.format(
-          "projects/%s/regions/%s/disks/%s",
-          projectId,
-          secondaryZone,
-          secondaryDiskName);
+      String asyncSecondaryDiskPath = String.format("projects/%s/regions/%s/disks/%s",
+          projectId, secondaryDiskRegion, secondaryDiskName);
 
       RegionDisksStartAsyncReplicationRequest startAsyncReplicationRequest =
           RegionDisksStartAsyncReplicationRequest.newBuilder()
-              .setAsyncSecondaryDisk(asyncSecondaryDisk)
+              .setAsyncSecondaryDisk(asyncSecondaryDiskPath)
               .build();
 
-      Operation response =
-          disksClient.startAsyncReplicationAsync(
-              projectId,
-              primaryZone,
-              primaryDiskName,
-              startAsyncReplicationRequest).get();
-
+      Operation response = disksClient.startAsyncReplicationAsync(
+          projectId, primaryDiskRegion, primaryDiskName, startAsyncReplicationRequest).get();
 
       if (response.hasError()) {
         return;
@@ -65,3 +71,4 @@ public class StartDiskReplication {
     }
   }
 }
+// [END compute_disk_start_replication]
---
+++ compute/cloud-client/src/main/java/compute/disks/StopDiskAsyncReplication.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.disks;
+
+// [START compute_disk_stop_replication]
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.RegionDisksClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+public class StopDiskAsyncReplication {
+
+  public static void main(String[] args)
+      throws IOException, ExecutionException, InterruptedException {
+    // TODO(developer): Replace these variables before running the sample.
+    // The project that contains the primary disk.
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the zone in which your secondary disk is located.
+    String secondaryDiskRegion = "us-east1";
+    // Name of the secondary disk.
+    String secondaryDiskName = "SECONDARY_DISK_NAME";
+    stopDiskAsyncReplication(projectId, secondaryDiskRegion, secondaryDiskName);
+  }
+
+  // Stops asynchronous replication for the specified disk.
+  public static void stopDiskAsyncReplication(
+      String project, String secondaryDiskRegion, String secondaryDiskName)
+      throws IOException, ExecutionException, InterruptedException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (RegionDisksClient disksClient = RegionDisksClient.create()) {
+      Operation response = disksClient.stopAsyncReplicationAsync(
+          project, secondaryDiskRegion, secondaryDiskName).get();
+
+      if (response.hasError()) {
+        return;
+      }
+      System.out.println("Async replication stopped successfully.");
+    }
+  }
+}
+// [END compute_disk_stop_replication]
