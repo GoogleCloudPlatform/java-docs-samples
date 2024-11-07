22e1c11019110bab455c7cd6d9f698cdcd476106
Jacek Spalinski <69755075+jacspa96@users.noreply.github.com>
2024-11-07 10:02:30 +0100

    feat(dataplex): link documentation for available locations (#9647)
    Co-authored-by: Jacek Spalinski <jspa@google.com>

--- dataplex/snippets/src/main/java/dataplex/ListEntries.java
+++ dataplex/snippets/src/main/java/dataplex/ListEntries.java
@@ -30,6 +30,7 @@ public class ListEntries {
   public static void main(String[] args) throws IOException {
     // TODO(developer): Replace these variables before running the sample.
     String projectId = "MY_PROJECT_ID";
+    // Available locations: https://cloud.google.com/dataplex/docs/locations
     String location = "MY_LOCATION";
     String entryGroupId = "MY_ENTRY_GROUP_ID";
 
