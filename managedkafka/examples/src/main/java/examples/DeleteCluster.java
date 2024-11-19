297ca2071c91a123fe23524c43fbe5e050cd2c79
Rohith Rajan <rohithrajan@google.com>
2024-11-19 13:55:40 -0500

    fix: increase timeout duration in Java client

--- managedkafka/examples/src/main/java/examples/DeleteCluster.java
+++ managedkafka/examples/src/main/java/examples/DeleteCluster.java
@@ -17,14 +17,21 @@
 package examples;
 
 // [START managedkafka_delete_cluster]
+
 import com.google.api.gax.longrunning.OperationFuture;
+import com.google.api.gax.longrunning.OperationSnapshot;
+import com.google.api.gax.longrunning.OperationTimedPollAlgorithm;
+import com.google.api.gax.retrying.RetrySettings;
+import com.google.api.gax.retrying.TimedRetryAlgorithm;
 import com.google.api.gax.rpc.ApiException;
 import com.google.cloud.managedkafka.v1.ClusterName;
 import com.google.cloud.managedkafka.v1.DeleteClusterRequest;
 import com.google.cloud.managedkafka.v1.ManagedKafkaClient;
+import com.google.cloud.managedkafka.v1.ManagedKafkaSettings;
 import com.google.cloud.managedkafka.v1.OperationMetadata;
 import com.google.protobuf.Empty;
 import java.io.IOException;
+import java.time.Duration;
 
 public class DeleteCluster {
 
@@ -38,13 +45,38 @@ public class DeleteCluster {
 
   public static void deleteCluster(String projectId, String region, String clusterId)
       throws Exception {
-    try (ManagedKafkaClient managedKafkaClient = ManagedKafkaClient.create()) {
+
+    // Create the settings to configure the timeout for polling operations
+    ManagedKafkaSettings.Builder settingsBuilder = ManagedKafkaSettings.newBuilder();
+    TimedRetryAlgorithm timedRetryAlgorithm = OperationTimedPollAlgorithm.create(
+        RetrySettings.newBuilder()
+            .setInitialRetryDelayDuration(Duration.ofSeconds(5L))
+            .setRetryDelayMultiplier(1.5)
+            .setMaxRetryDelayDuration(Duration.ofSeconds(30L))
+            .setInitialRpcTimeoutDuration(Duration.ZERO) // ignored for LROs
+            .setRpcTimeoutMultiplier(1.0) // ignored for LROs
+            .setMaxRpcTimeoutDuration(Duration.ZERO) // ignored for LROs
+            .setTotalTimeoutDuration(Duration.ofHours(1L))  // set polling timeout to 1 hour
+            .build());
+    settingsBuilder.deleteClusterOperationSettings()
+        .setPollingAlgorithm(timedRetryAlgorithm);
+
+    try (ManagedKafkaClient managedKafkaClient = ManagedKafkaClient.create(
+        settingsBuilder.build())) {
       DeleteClusterRequest request =
           DeleteClusterRequest.newBuilder()
               .setName(ClusterName.of(projectId, region, clusterId).toString())
               .build();
       OperationFuture<Empty, OperationMetadata> future =
           managedKafkaClient.deleteClusterOperationCallable().futureCall(request);
+
+      // Get the initial LRO and print details. CreateCluster contains sample code for polling logs.
+      OperationSnapshot operation = future.getInitialFuture().get();
+      System.out.printf("Cluster deletion started. Operation name: %s\nDone: %s\nMetadata: %s\n",
+          operation.getName(),
+          operation.isDone(),
+          future.getMetadata().get().toString());
+
       future.get();
       System.out.println("Deleted cluster");
     } catch (IOException | ApiException e) {
