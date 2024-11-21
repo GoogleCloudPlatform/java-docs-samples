b14809c81874e7e2c9205c49d0abcc1ad1d70851
Tetiana Yahodska <tyaho@softserveinc.com>
2024-11-21 14:24:51 +0100

    Fixed zone

--- compute/cloud-client/src/test/java/compute/disks/HyperdisksIT.java
+++ compute/cloud-client/src/test/java/compute/disks/HyperdisksIT.java
@@ -44,7 +44,7 @@ import org.junit.runners.JUnit4;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class HyperdisksIT {
   private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
-  private static final String ZONE = "us-central1-a";
+  private static final String ZONE = "us-west1-a";
   private static final String  HYPERDISK_NAME = "test-hyperdisk-enc-" + UUID.randomUUID();
   private static final String  HYPERDISK_IN_POOL_NAME = "test-hyperdisk-enc-" + UUID.randomUUID();
   private static final String STORAGE_POOL_NAME = "test-storage-pool-enc-" + UUID.randomUUID();
