77d892e4697b96caa6da6c80913436b6dff8dccf
Tetiana Yahodska <tyaho@softserveinc.com>
2024-11-27 14:59:50 +0100

    Implemented compute_snapshot_schedule_delete and compute_snapshot_schedule_create samples, created test

---
+++ compute/cloud-client/src/main/java/compute/snapshot/CreateSnapshotSchedule.java
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.snapshot;
+
+// [START compute_snapshot_schedule_create]
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.ResourcePoliciesClient;
+import com.google.cloud.compute.v1.ResourcePolicy;
+import com.google.cloud.compute.v1.ResourcePolicyHourlyCycle;
+import com.google.cloud.compute.v1.ResourcePolicySnapshotSchedulePolicy;
+import com.google.cloud.compute.v1.ResourcePolicySnapshotSchedulePolicyRetentionPolicy;
+import com.google.cloud.compute.v1.ResourcePolicySnapshotSchedulePolicySchedule;
+import com.google.cloud.compute.v1.ResourcePolicySnapshotSchedulePolicySnapshotProperties;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class CreateSnapshotSchedule {
+  public static void main(String[] args)
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // TODO(developer): Replace these variables before running the sample.
+    // Project ID or project number of the Cloud project you want to use.
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the region in which you want to create the snapshot schedule.
+    String region = "us-central1";
+    // Name of the snapshot schedule you want to create.
+    String scheduleName = "YOUR_SCHEDULE_NAME";
+    // Description of the snapshot schedule.
+    String scheduleDescription = "YOUR_SCHEDULE_DESCRIPTION";
+    // Maximum number of days to retain snapshots.
+    int maxRetentionDays = 10;
+    // Storage location for the snapshots.
+    // More about storage locations:
+    // https://cloud.google.com/compute/docs/disks/snapshots?authuser=0#selecting_a_storage_location
+    String storageLocation = "US";
+    // Determines what happens to your snapshots if the source disk is deleted.
+    String onSourceDiskDelete = "KEEP_AUTO_SNAPSHOTS";
+
+    createSnapshotSchedule(projectId, region, scheduleName, scheduleDescription, maxRetentionDays,
+             storageLocation, onSourceDiskDelete);
+  }
+
+  // Creates a snapshot schedule policy.
+  public static Operation.Status createSnapshotSchedule(String projectId, String region,
+            String scheduleName, String scheduleDescription, int maxRetentionDays,
+            String storageLocation, String onSourceDiskDelete)
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    String startTime = "08:00";
+    ResourcePolicySnapshotSchedulePolicySnapshotProperties snapshotProperties =
+            ResourcePolicySnapshotSchedulePolicySnapshotProperties.newBuilder()
+                    .addStorageLocations(storageLocation)
+                    .build();
+
+    // Define the hourly schedule:
+    int snapshotInterval = 10; // Create a snapshot every 10 hours
+    ResourcePolicyHourlyCycle hourlyCycle = ResourcePolicyHourlyCycle.newBuilder()
+            .setHoursInCycle(snapshotInterval)
+            .setStartTime(startTime)
+            .build();
+
+    // Define the daily schedule.
+    // ResourcePolicyDailyCycle dailySchedule =
+    //         ResourcePolicyDailyCycle.newBuilder()
+    //                 .setDaysInCycle(1)  // Every day
+    //                 .setStartTime(startTime)
+    //                 .build();
+
+    // Define the weekly schedule.
+    // List<ResourcePolicyWeeklyCycleDayOfWeek> dayOfWeeks = new ArrayList<>();
+    // ResourcePolicyWeeklyCycleDayOfWeek tuesdaySchedule =
+    //         ResourcePolicyWeeklyCycleDayOfWeek.newBuilder()
+    //                 .setDay(ResourcePolicyWeeklyCycleDayOfWeek.Day.TUESDAY.toString())
+    //                 .setStartTime(startTime)
+    //                 .build();
+    // dayOfWeeks.add(tuesdaySchedule);
+    //
+    // ResourcePolicyWeeklyCycle weeklyCycle = ResourcePolicyWeeklyCycle.newBuilder()
+    //         .addAllDayOfWeeks(dayOfWeeks)
+    //         .build();
+
+    ResourcePolicySnapshotSchedulePolicyRetentionPolicy retentionPolicy =
+            ResourcePolicySnapshotSchedulePolicyRetentionPolicy.newBuilder()
+                    .setMaxRetentionDays(maxRetentionDays)
+                    .setOnSourceDiskDelete(onSourceDiskDelete)
+            .build();
+
+    ResourcePolicySnapshotSchedulePolicy snapshotSchedulePolicy =
+            ResourcePolicySnapshotSchedulePolicy.newBuilder()
+                    .setRetentionPolicy(retentionPolicy)
+                    .setSchedule(ResourcePolicySnapshotSchedulePolicySchedule.newBuilder()
+                             // You can set only one of the following options:
+                             .setHourlySchedule(hourlyCycle)  //Set Hourly Schedule
+                             // .setDailySchedule(dailySchedule) //Set Daily Schedule
+                             // .setWeeklySchedule(weeklyCycle)    // Set Weekly Schedule
+                             .build())
+                    .setSnapshotProperties(snapshotProperties)
+                    .build();
+
+    ResourcePolicy resourcePolicy = ResourcePolicy.newBuilder()
+            .setName(scheduleName)
+            .setDescription(scheduleDescription)
+            .setSnapshotSchedulePolicy(snapshotSchedulePolicy)
+            .build();
+
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (ResourcePoliciesClient resourcePoliciesClient = ResourcePoliciesClient.create()) {
+
+      Operation response = resourcePoliciesClient.insertAsync(projectId, region, resourcePolicy)
+              .get(3, TimeUnit.MINUTES);
+
+      if (response.hasError()) {
+        System.out.printf("Snapshot schedule creation failed: %s%n", response.getError());
+        return null;
+      }
+      return response.getStatus();
+    }
+  }
+}
+// [END compute_snapshot_schedule_create]
---
+++ compute/cloud-client/src/test/java/compute/snapshot/SnapshotIT.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.snapshot;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+import com.google.cloud.compute.v1.Operation;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+@Timeout(value = 6, unit = TimeUnit.MINUTES)
+public class SnapshotIT {
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
+  private static final String ZONE = "asia-south1-a";
+  private static final String REGION = ZONE.substring(0, ZONE.lastIndexOf('-'));
+  static String templateUUID = UUID.randomUUID().toString();
+  private static final String SCHEDULE_NAME = "test-schedule-" + templateUUID;
+  private static final  String SCHEDULE_DESCRIPTION = "Test hourly snapshot schedule";
+  private static final int MAX_RETENTION_DAYS = 2;
+  private static final String STORAGE_LOCATION = "US";
+  private static final  String ON_SOURCE_DISK_DELETE = "KEEP_AUTO_SNAPSHOTS";
+
+  // Check if the required environment variables are set.
+  public static void requireEnvVar(String envVarName) {
+    assertWithMessage(String.format("Missing environment variable '%s' ", envVarName))
+            .that(System.getenv(envVarName)).isNotEmpty();
+  }
+
+  @BeforeAll
+  public static void setUp()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_CLOUD_PROJECT");
+  }
+
+  @AfterAll
+  public static void cleanup()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    ByteArrayOutputStream stdOut = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(stdOut));
+
+    // Delete snapshot schedule created for testing.
+    DeleteSnapshotSchedule.deleteSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME);
+
+    assertThat(stdOut.toString())
+            .contains("Snapshot schedule deleted successfully: " + SCHEDULE_NAME);
+
+    stdOut.close();
+  }
+
+  @Test
+  public void testCreateSnapshotScheduleHourly()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    Operation.Status status = CreateSnapshotSchedule.createSnapshotSchedule(
+            PROJECT_ID, REGION, SCHEDULE_NAME, SCHEDULE_DESCRIPTION,
+            MAX_RETENTION_DAYS, STORAGE_LOCATION, ON_SOURCE_DISK_DELETE);
+    assertThat(status).isEqualTo(Operation.Status.DONE);
+  }
+}
