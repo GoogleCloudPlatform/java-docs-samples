3f81a8c5278f87e2485c2b66587dfc5ec88b7a29
Tetiana Yahodska <tyaho@softserveinc.com>
2024-12-06 15:22:39 +0100

    Cleaned resources

--- compute/cloud-client/src/test/java/compute/snapshotschedule/SnapshotScheduleIT.java
+++ compute/cloud-client/src/test/java/compute/snapshotschedule/SnapshotScheduleIT.java
@@ -62,7 +62,7 @@ public class SnapshotScheduleIT {
           throws IOException, ExecutionException, InterruptedException, TimeoutException {
     requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
     requireEnvVar("GOOGLE_CLOUD_PROJECT");
-    RegionalDelete.deleteRegionalDisk(PROJECT_ID, REGION, "test-disk-for-consistency-727ee5d7");
+    RegionalDelete.deleteRegionalDisk(PROJECT_ID, REGION, "test-disk-for-consistency-40c43b87");
 
     List<ResourcePolicy> list = ListSnapshotSchedules.listSnapshotSchedules(
             PROJECT_ID, REGION);
