1f795553f73ac3a79738a8758cecaa71823ed6a0
Tetiana Yahodska <tyaho@softserveinc.com>
2024-12-09 22:08:59 +0100

    Fixed tests

--- compute/cloud-client/src/main/java/compute/disks/consistencygroup/ListRegionalDisksInConsistencyGroup.java
+++ compute/cloud-client/src/main/java/compute/disks/consistencygroup/ListRegionalDisksInConsistencyGroup.java
@@ -18,15 +18,11 @@ package compute.disks.consistencygroup;
 
 // [START compute_consistency_group_list_disks_regional]
 import com.google.cloud.compute.v1.Disk;
-import com.google.cloud.compute.v1.DisksClient;
-import com.google.cloud.compute.v1.ListDisksRequest;
 import com.google.cloud.compute.v1.ListRegionDisksRequest;
 import com.google.cloud.compute.v1.RegionDisksClient;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.StringJoiner;
 import java.util.concurrent.ExecutionException;
 
 public class ListRegionalDisksInConsistencyGroup {
@@ -34,9 +30,9 @@ public class ListRegionalDisksInConsistencyGroup {
           throws IOException, InterruptedException, ExecutionException {
     // TODO(developer): Replace these variables before running the sample.
     // Project ID or project number of the Cloud project you want to use.
-    String project = "tyaho-softserve-project";//"YOUR_PROJECT_ID";
+    String project = "YOUR_PROJECT_ID";
     // Name of the consistency group.
-    String consistencyGroupName = "group-name";//"CONSISTENCY_GROUP_ID";
+    String consistencyGroupName = "CONSISTENCY_GROUP_ID";
     // Region of the disk.
     String disksLocation = "us-central1";
     // Region of the consistency group.
@@ -47,29 +43,31 @@ public class ListRegionalDisksInConsistencyGroup {
   }
 
   // Lists disks in a consistency group.
-  public static List<Disk> listRegionalDisksInConsistencyGroup(String project, String consistencyGroupName,
-      String consistencyGroupLocation, String disksLocation) throws IOException {
+  public static List<Disk> listRegionalDisksInConsistencyGroup(String project,
+      String consistencyGroupName, String consistencyGroupLocation, String disksLocation)
+          throws IOException {
     String filter = String
             .format("https://www.googleapis.com/compute/v1/projects/%s/regions/%s/resourcePolicies/%s",
                     project, consistencyGroupLocation, consistencyGroupName);
     List<Disk> disksList = new ArrayList<>();
 
-      // Initialize client that will be used to send requests. This client only needs to be created
-      // once, and can be reused for multiple requests.
-      try (RegionDisksClient disksClient = RegionDisksClient.create()) {
-        ListRegionDisksRequest request =
-                ListRegionDisksRequest.newBuilder()
-                        .setProject(project)
-                        .setRegion(disksLocation)
-                        .build();
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (RegionDisksClient disksClient = RegionDisksClient.create()) {
+      ListRegionDisksRequest request =
+              ListRegionDisksRequest.newBuilder()
+                      .setProject(project)
+                      .setRegion(disksLocation)
+                      .build();
 
-        RegionDisksClient.ListPagedResponse response = disksClient.list(request);
-        for (Disk disk : response.iterateAll()) {
-          if (disk.getResourcePoliciesList().contains(filter)) {
-            disksList.add(disk);
-          }
+      RegionDisksClient.ListPagedResponse response = disksClient.list(request);
+      for (Disk disk : response.iterateAll()) {
+        if (disk.getResourcePoliciesList().contains(filter)) {
+          disksList.add(disk);
+        }
       }
     }
+    System.out.println(disksList.size());
     return disksList;
   }
 }
