f32867563fafd1881a6d059baeef6ede58ab2956
Тетяна Ягодська <49729677+TetyanaYahodska@users.noreply.github.com>
2024-12-30 17:53:19 +0100

    feat(compute): add compute snapshot schedule create/get/edit/list/delete samples (#9742)
    * Implemented compute_snapshot_schedule_delete and compute_snapshot_schedule_create samples, created test
    
    * Fixed test
    
    * Added compute_snapshot_schedule_get sample, created test
    
    * Fixed naming
    
    * Implemented compute_snapshot_schedule_edit, created test
    
    * Fixed naming
    
    * Implemented compute_snapshot_schedule_list sample, created test
    
    * Cleaned resources
    
    * Cleaned resources
    
    * Cleaned resources
    
    * Cleaned resources
    
    * Fixed test
    
    * Added comment
    
    * Fixed tests
    
    * Fixed code
    
    * Fixed code as requested in the comments

---
+++ compute/cloud-client/src/main/java/compute/snapshotschedule/DeleteSnapshotSchedule.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package compute.snapshotschedule;
+
+// [START compute_snapshot_schedule_delete]
+import com.google.cloud.compute.v1.DeleteResourcePolicyRequest;
+import com.google.cloud.compute.v1.Operation;
+import com.google.cloud.compute.v1.Operation.Status;
+import com.google.cloud.compute.v1.ResourcePoliciesClient;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public class DeleteSnapshotSchedule {
+  public static void main(String[] args)
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // TODO(developer): Replace these variables before running the sample.
+    // Project ID or project number of the Cloud project you want to use.
+    String projectId = "YOUR_PROJECT_ID";
+    // Name of the region where your snapshot schedule is located.
+    String region = "us-central1";
+    // Name of the snapshot schedule you want to delete.
+    String snapshotScheduleName = "YOUR_SCHEDULE_NAME";
+
+    deleteSnapshotSchedule(projectId, region, snapshotScheduleName);
+  }
+
+  // Deletes a snapshot schedule policy.
+  public static Status deleteSnapshotSchedule(
+          String projectId, String region, String snapshotScheduleName)
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests.
+    try (ResourcePoliciesClient resourcePoliciesClient = ResourcePoliciesClient.create()) {
+      DeleteResourcePolicyRequest request = DeleteResourcePolicyRequest.newBuilder()
+              .setProject(projectId)
+              .setRegion(region)
+              .setResourcePolicy(snapshotScheduleName)
+              .build();
+      Operation response = resourcePoliciesClient.deleteAsync(request).get(3, TimeUnit.MINUTES);
+
+      if (response.hasError()) {
+        throw new Error("Snapshot schedule deletion failed! " + response.getError());
+      }
+      return response.getStatus();
+    }
+  }
+}
+// [END compute_snapshot_schedule_delete]
--- compute/cloud-client/src/test/java/compute/snapshotschedule/SnapshotScheduleIT.java
+++ compute/cloud-client/src/test/java/compute/snapshotschedule/SnapshotScheduleIT.java
@@ -16,6 +16,8 @@
 
 package compute.snapshotschedule;
 
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -27,14 +29,24 @@ import static org.mockito.Mockito.when;
 
 import com.google.api.gax.longrunning.OperationFuture;
 import com.google.cloud.compute.v1.AddResourcePoliciesDiskRequest;
+import com.google.cloud.compute.v1.DeleteResourcePolicyRequest;
 import com.google.cloud.compute.v1.DisksClient;
+import com.google.cloud.compute.v1.GetResourcePolicyRequest;
+import com.google.cloud.compute.v1.InsertResourcePolicyRequest;
 import com.google.cloud.compute.v1.Operation;
 import com.google.cloud.compute.v1.Operation.Status;
 import com.google.cloud.compute.v1.RemoveResourcePoliciesDiskRequest;
+import com.google.cloud.compute.v1.ResourcePoliciesClient;
+import com.google.cloud.compute.v1.ResourcePoliciesClient.ListPagedResponse;
+import com.google.cloud.compute.v1.ResourcePolicy;
+import com.google.cloud.compute.v1.ResourcePolicySnapshotSchedulePolicyRetentionPolicy.OnSourceDiskDelete;
 import java.io.IOException;
+import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 import org.junit.runner.RunWith;
@@ -44,11 +56,86 @@ import org.mockito.MockedStatic;
 @RunWith(JUnit4.class)
 @Timeout(value = 6, unit = TimeUnit.MINUTES)
 public class SnapshotScheduleIT {
-  private static final String PROJECT_ID = "GOOGLE_CLOUD_PROJECT";
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
   private static final String ZONE = "asia-south1-a";
   private static final String REGION = ZONE.substring(0, ZONE.lastIndexOf('-'));
-  private static final String SCHEDULE_FOR_DISK = "test-schedule-for-disk";
   private static final String DISK_NAME = "gcloud-test-disk";
+  private static final String SCHEDULE_NAME = "test-schedule-" + UUID.randomUUID();
+  private static final String SCHEDULE_DESCRIPTION = "Test hourly snapshot schedule";
+  private static final int MAX_RETENTION_DAYS = 2;
+  private static final String STORAGE_LOCATION = "US";
+
+  // Check if the required environment variables are set.
+  public static void requireEnvVar(String envVarName) {
+    assertWithMessage(String.format("Missing environment variable '%s' ", envVarName))
+            .that(System.getenv(envVarName)).isNotEmpty();
+  }
+
+  @BeforeAll
+  public static void setUp()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    requireEnvVar("GOOGLE_APPLICATION_CREDENTIALS");
+    requireEnvVar("GOOGLE_CLOUD_PROJECT");
+    CreateSnapshotSchedule.createSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME,
+            SCHEDULE_DESCRIPTION, MAX_RETENTION_DAYS, STORAGE_LOCATION);
+  }
+
+  @AfterAll
+  public static void cleanup()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    DeleteSnapshotSchedule.deleteSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME);
+  }
+
+  @Test
+  public void testEditSnapshotSchedule()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    EditSnapshotSchedule.editSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME);
+
+    ResourcePolicy resourcePolicy = GetSnapshotSchedule
+            .getSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME);
+
+    assertThat(resourcePolicy.getDescription()).isEqualTo("Updated description");
+    assertThat(resourcePolicy.getSnapshotSchedulePolicy()
+            .getRetentionPolicy()
+            .getOnSourceDiskDelete())
+            .isEqualTo(OnSourceDiskDelete.APPLY_RETENTION_POLICY.toString());
+  }
+
+  @Test
+  public void testListSnapshotSchedules() throws IOException {
+    ListPagedResponse listPagedResponse = ListSnapshotSchedules.listSnapshotSchedules(
+            PROJECT_ID, REGION, SCHEDULE_NAME);
+
+    ResourcePolicy firstPolicy = listPagedResponse.iterateAll().iterator().next();
+
+    assertThat(listPagedResponse.iterateAll()).hasSize(1);
+    assertEquals(SCHEDULE_NAME, firstPolicy.getName());
+  }
+
+  @Test
+  public void testCreateSnapshotScheduleHourly()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    try (MockedStatic<ResourcePoliciesClient> mockedResourcePoliciesClient =
+                 mockStatic(ResourcePoliciesClient.class)) {
+      Operation operation = mock(Operation.class);
+      ResourcePoliciesClient mockClient = mock(ResourcePoliciesClient.class);
+      OperationFuture mockFuture = mock(OperationFuture.class);
+
+      mockedResourcePoliciesClient.when(ResourcePoliciesClient::create).thenReturn(mockClient);
+      when(mockClient.insertAsync(any(InsertResourcePolicyRequest.class)))
+              .thenReturn(mockFuture);
+      when(mockFuture.get(anyLong(), any(TimeUnit.class))).thenReturn(operation);
+      when(operation.getStatus()).thenReturn(Status.DONE);
+
+      Status status = CreateSnapshotSchedule.createSnapshotSchedule(PROJECT_ID, REGION,
+              SCHEDULE_NAME, SCHEDULE_DESCRIPTION, MAX_RETENTION_DAYS, STORAGE_LOCATION);
+
+      verify(mockClient, times(1))
+              .insertAsync(any(InsertResourcePolicyRequest.class));
+      verify(mockFuture, times(1)).get(anyLong(), any(TimeUnit.class));
+      assertEquals(Status.DONE, status);
+    }
+  }
 
   @Test
   public void testAttachSnapshotScheduleToDisk()
@@ -65,7 +152,7 @@ public class SnapshotScheduleIT {
       when(operation.getStatus()).thenReturn(Status.DONE);
 
       Status actualStatus = AttachSnapshotScheduleToDisk.attachSnapshotScheduleToDisk(
-              PROJECT_ID, ZONE, DISK_NAME, SCHEDULE_FOR_DISK, REGION);
+              PROJECT_ID, ZONE, DISK_NAME, SCHEDULE_NAME, REGION);
 
       verify(mockClient, times(1)).addResourcePoliciesAsync(any());
       assertEquals(Status.DONE, actualStatus);
@@ -87,10 +174,55 @@ public class SnapshotScheduleIT {
       when(operation.getStatus()).thenReturn(Status.DONE);
 
       Status actualStatus = RemoveSnapshotScheduleFromDisk.removeSnapshotScheduleFromDisk(
-            PROJECT_ID, ZONE, DISK_NAME, REGION, SCHEDULE_FOR_DISK);
+              PROJECT_ID, ZONE, DISK_NAME, REGION, SCHEDULE_NAME);
 
       verify(mockClient, times(1)).removeResourcePoliciesAsync(any());
       assertEquals(Status.DONE, actualStatus);
+    }
+  }
+
+  @Test
+  public void testGetSnapshotSchedule() throws IOException {
+    try (MockedStatic<ResourcePoliciesClient> mockedResourcePoliciesClient =
+                 mockStatic(ResourcePoliciesClient.class)) {
+      ResourcePoliciesClient mockClient = mock(ResourcePoliciesClient.class);
+      ResourcePolicy mockResourcePolicy = mock(ResourcePolicy.class);
+
+      mockedResourcePoliciesClient.when(ResourcePoliciesClient::create).thenReturn(mockClient);
+      when(mockClient.get(any(GetResourcePolicyRequest.class)))
+              .thenReturn(mockResourcePolicy);
+
+      ResourcePolicy resourcePolicy = GetSnapshotSchedule.getSnapshotSchedule(
+              PROJECT_ID, REGION, SCHEDULE_NAME);
+
+      verify(mockClient, times(1))
+              .get(any(GetResourcePolicyRequest.class));
+      assertEquals(mockResourcePolicy, resourcePolicy);
+    }
+  }
+
+  @Test
+  public void testDeleteSnapshotSchedule()
+          throws IOException, ExecutionException, InterruptedException, TimeoutException {
+    try (MockedStatic<ResourcePoliciesClient> mockedResourcePoliciesClient =
+                 mockStatic(ResourcePoliciesClient.class)) {
+      Operation operation = mock(Operation.class);
+      ResourcePoliciesClient mockClient = mock(ResourcePoliciesClient.class);
+      OperationFuture mockFuture = mock(OperationFuture.class);
+
+      mockedResourcePoliciesClient.when(ResourcePoliciesClient::create).thenReturn(mockClient);
+      when(mockClient.deleteAsync(any(DeleteResourcePolicyRequest.class)))
+              .thenReturn(mockFuture);
+      when(mockFuture.get(anyLong(), any(TimeUnit.class))).thenReturn(operation);
+      when(operation.getStatus()).thenReturn(Status.DONE);
+
+      Status status = DeleteSnapshotSchedule
+              .deleteSnapshotSchedule(PROJECT_ID, REGION, SCHEDULE_NAME);
+
+      verify(mockClient, times(1))
+              .deleteAsync(any(DeleteResourcePolicyRequest.class));
+      verify(mockFuture, times(1)).get(anyLong(), any(TimeUnit.class));
+      assertEquals(Status.DONE, status);
     }
   }
 }
