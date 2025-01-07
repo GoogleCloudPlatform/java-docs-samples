3c4c06a515f31630199a2f95ceb3855baef94010
Brian Dorsey <briandorsey@google.com>
2025-01-07 19:27:07 +0000

    fix: disable flakybot reporting

--- .kokoro/tests/run_test_java.sh
+++ .kokoro/tests/run_test_java.sh
@@ -96,11 +96,4 @@ if [[ "$file" == *"run/"* && ("$file" != *"run/filesystem"* && "$file" != *"run/
     fi
 fi
 
-# If this is a periodic build, send the test log to the FlakyBot except for Java 8
-# See https://github.com/googleapis/repo-automation-bots/tree/main/packages/flakybot.
-if [[ $JAVA_VERSION != "1.8" && $KOKORO_BUILD_ARTIFACTS_SUBDIR = *"periodic"* ]]; then
-    chmod +x $KOKORO_GFILE_DIR/linux_amd64/flakybot
-    $KOKORO_GFILE_DIR/linux_amd64/flakybot
-fi
-
 exit $RTN
