42c928ef6d3569b070b2a40739975c8b2040a1a4
alarconesparza <alarconesparza@google.com>
2025-01-14 18:47:13 -0600

    chore(job): migrate region tag to include product prefix (#9974)
    * chore(job): migrate region tag to include product prefix
    
    * chore(job): migrate region tag to include product prefix

--- jobs/v3/src/main/java/com/google/samples/AutoCompleteSample.java
+++ jobs/v3/src/main/java/com/google/samples/AutoCompleteSample.java
@@ -38,6 +38,7 @@ public final class AutoCompleteSample {
   private static CloudTalentSolution talentSolutionClient =
       JobServiceQuickstart.getTalentSolutionClient();
 
+  // [START job_auto_complete_job_title]
   // [START auto_complete_job_title]
 
   /** Auto completes job titles within given companyName. */
@@ -60,8 +61,9 @@ public final class AutoCompleteSample {
     System.out.println(results);
   }
   // [END auto_complete_job_title]
+  // [END job_auto_complete_job_title]
 
-  // [START auto_complete_default]
+  // [START job_auto_complete_default]
   /** Auto completes job titles within given companyName. */
   public static void defaultAutoComplete(String companyName, String query) throws IOException {
     Complete complete =
@@ -79,7 +81,7 @@ public final class AutoCompleteSample {
 
     System.out.println(results);
   }
-  // [END auto_complete_default]
+  // [END job_auto_complete_default]
 
   public static void main(String... args) throws Exception {
     Company companyToBeCreated = BasicCompanySample.generateCompany().setDisplayName("Google");
