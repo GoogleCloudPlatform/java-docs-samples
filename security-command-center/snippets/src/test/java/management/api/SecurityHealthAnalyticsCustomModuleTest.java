473df99b1a1cfcf2f05dedb7e7c47aaf3c43dbea
lovenishs04 <lovenishs@google.com>
2025-01-20 08:58:14 +0000

    fix(securitycenter) issue related to cleanup logic in testclass of SHA custom modules

--- security-command-center/snippets/src/test/java/management/api/SecurityHealthAnalyticsCustomModuleTest.java
+++ security-command-center/snippets/src/test/java/management/api/SecurityHealthAnalyticsCustomModuleTest.java
@@ -22,8 +22,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 import com.google.cloud.securitycentermanagement.v1.EffectiveSecurityHealthAnalyticsCustomModule;
-import com.google.cloud.securitycentermanagement.v1.ListSecurityHealthAnalyticsCustomModulesRequest;
-import com.google.cloud.securitycentermanagement.v1.SecurityCenterManagementClient;
 import com.google.cloud.securitycentermanagement.v1.SecurityCenterManagementClient.ListDescendantSecurityHealthAnalyticsCustomModulesPagedResponse;
 import com.google.cloud.securitycentermanagement.v1.SecurityCenterManagementClient.ListEffectiveSecurityHealthAnalyticsCustomModulesPagedResponse;
 import com.google.cloud.securitycentermanagement.v1.SecurityCenterManagementClient.ListSecurityHealthAnalyticsCustomModulesPagedResponse;
@@ -33,6 +31,8 @@ import com.google.cloud.securitycentermanagement.v1.SimulateSecurityHealthAnalyt
 import com.google.cloud.testing.junit4.MultipleAttemptsRule;
 import com.google.common.base.Strings;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.StreamSupport;
@@ -47,9 +47,10 @@ import org.junit.runners.JUnit4;
 public class SecurityHealthAnalyticsCustomModuleTest {
   // TODO(Developer): Replace the below variable
   private static final String PROJECT_ID = System.getenv("SCC_PROJECT_ID");
-  private static final String CUSTOM_MODULE_DISPLAY_NAME = "java_sample_custom_module_test";
+  private static final String CUSTOM_MODULE_DISPLAY_NAME = "java_sample_sha_custom_module_test";
   private static final int MAX_ATTEMPT_COUNT = 3;
   private static final int INITIAL_BACKOFF_MILLIS = 120000; // 2 minutes
+  private static List<String> createdCustomModuleIds = new ArrayList<>();
 
   @Rule
   public final MultipleAttemptsRule multipleAttemptsRule =
@@ -69,32 +70,21 @@ public class SecurityHealthAnalyticsCustomModuleTest {
   }
 
   @AfterClass
+  // Perform cleanup of all the custom modules created by the current execution of the test, after
+  // running tests
   public static void cleanUp() throws IOException {
-    // Perform cleanup after running tests
-    cleanupExistingCustomModules();
-  }
-
-  // cleanupExistingCustomModules clean up all the existing custom module
-  private static void cleanupExistingCustomModules() throws IOException {
-    try (SecurityCenterManagementClient client = SecurityCenterManagementClient.create()) {
-      ListSecurityHealthAnalyticsCustomModulesRequest request =
-          ListSecurityHealthAnalyticsCustomModulesRequest.newBuilder()
-              .setParent(String.format("projects/%s/locations/global", PROJECT_ID))
-              .build();
-      ListSecurityHealthAnalyticsCustomModulesPagedResponse response =
-          client.listSecurityHealthAnalyticsCustomModules(request);
-      // Iterate over the response and delete custom module one by one which start with
-      // java_sample_custom_module
-      for (SecurityHealthAnalyticsCustomModule module : response.iterateAll()) {
-        if (module.getDisplayName().startsWith("java_sample_custom_module")) {
-          String customModuleId = extractCustomModuleId(module.getName());
-          deleteCustomModule(PROJECT_ID, customModuleId);
-        }
+    for (String customModuleId : createdCustomModuleIds) {
+      try {
+        deleteCustomModule(PROJECT_ID, customModuleId);
+      } catch (Exception e) {
+        System.err.println("Failed to delete module: " + customModuleId);
+        e.printStackTrace();
       }
     }
   }
 
-  // extractCustomModuleID extracts the custom module Id from the full name
+  // extractCustomModuleID extracts the custom module Id from the full name and below regex will
+  // parses suffix after the last slash character.
   private static String extractCustomModuleId(String customModuleFullName) {
     if (!Strings.isNullOrEmpty(customModuleFullName)) {
       Pattern pattern = Pattern.compile(".*/([^/]+)$");
@@ -104,18 +94,6 @@ public class SecurityHealthAnalyticsCustomModuleTest {
       }
     }
     return "";
-  }
-
-  // createCustomModule method is for creating the custom module
-  private static SecurityHealthAnalyticsCustomModule createCustomModule(
-      String projectId, String customModuleDisplayName) throws IOException {
-    if (!Strings.isNullOrEmpty(projectId) && !Strings.isNullOrEmpty(customModuleDisplayName)) {
-      SecurityHealthAnalyticsCustomModule response =
-          CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
-              projectId, customModuleDisplayName);
-      return response;
-    }
-    return null;
   }
 
   // deleteCustomModule method is for deleting the custom module
@@ -132,7 +110,7 @@ public class SecurityHealthAnalyticsCustomModuleTest {
     SecurityHealthAnalyticsCustomModule response =
         CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
             PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
-
+    createdCustomModuleIds.add(extractCustomModuleId(response.getName()));
     assertNotNull(response);
     assertThat(response.getDisplayName()).isEqualTo(CUSTOM_MODULE_DISPLAY_NAME);
   }
@@ -140,7 +118,8 @@ public class SecurityHealthAnalyticsCustomModuleTest {
   @Test
   public void testDeleteSecurityHealthAnalyticsCustomModule() throws IOException {
     SecurityHealthAnalyticsCustomModule response =
-        createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
     String customModuleId = extractCustomModuleId(response.getName());
     assertTrue(
         DeleteSecurityHealthAnalyticsCustomModule.deleteSecurityHealthAnalyticsCustomModule(
@@ -149,7 +128,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
 
   @Test
   public void testListSecurityHealthAnalyticsCustomModules() throws IOException {
-    createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    createdCustomModuleIds.add(extractCustomModuleId(createCustomModuleResponse.getName()));
     ListSecurityHealthAnalyticsCustomModulesPagedResponse response =
         ListSecurityHealthAnalyticsCustomModules.listSecurityHealthAnalyticsCustomModules(
             PROJECT_ID);
@@ -161,8 +143,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
   @Test
   public void testGetSecurityHealthAnalyticsCustomModule() throws IOException {
     SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
-        createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
     String customModuleId = extractCustomModuleId(createCustomModuleResponse.getName());
+    createdCustomModuleIds.add(customModuleId);
     SecurityHealthAnalyticsCustomModule getCustomModuleResponse =
         GetSecurityHealthAnalyticsCustomModule.getSecurityHealthAnalyticsCustomModule(
             PROJECT_ID, customModuleId);
@@ -174,8 +158,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
   @Test
   public void testUpdateSecurityHealthAnalyticsCustomModule() throws IOException {
     SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
-        createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
     String customModuleId = extractCustomModuleId(createCustomModuleResponse.getName());
+    createdCustomModuleIds.add(customModuleId);
     SecurityHealthAnalyticsCustomModule response =
         UpdateSecurityHealthAnalyticsCustomModule.updateSecurityHealthAnalyticsCustomModule(
             PROJECT_ID, customModuleId);
@@ -186,8 +172,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
   @Test
   public void testGetEffectiveSecurityHealthAnalyticsCustomModule() throws IOException {
     SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
-        createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
     String customModuleId = extractCustomModuleId(createCustomModuleResponse.getName());
+    createdCustomModuleIds.add(customModuleId);
     EffectiveSecurityHealthAnalyticsCustomModule getEffectiveCustomModuleResponse =
         GetEffectiveSecurityHealthAnalyticsCustomModule
             .getEffectiveSecurityHealthAnalyticsCustomModule(PROJECT_ID, customModuleId);
@@ -200,7 +188,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
 
   @Test
   public void testListEffectiveSecurityHealthAnalyticsCustomModules() throws IOException {
-    createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    createdCustomModuleIds.add(extractCustomModuleId(createCustomModuleResponse.getName()));
     ListEffectiveSecurityHealthAnalyticsCustomModulesPagedResponse response =
         ListEffectiveSecurityHealthAnalyticsCustomModules
             .listEffectiveSecurityHealthAnalyticsCustomModules(PROJECT_ID);
@@ -211,7 +202,10 @@ public class SecurityHealthAnalyticsCustomModuleTest {
 
   @Test
   public void testListDescendantSecurityHealthAnalyticsCustomModules() throws IOException {
-    createCustomModule(PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    SecurityHealthAnalyticsCustomModule createCustomModuleResponse =
+        CreateSecurityHealthAnalyticsCustomModule.createSecurityHealthAnalyticsCustomModule(
+            PROJECT_ID, CUSTOM_MODULE_DISPLAY_NAME);
+    createdCustomModuleIds.add(extractCustomModuleId(createCustomModuleResponse.getName()));
     ListDescendantSecurityHealthAnalyticsCustomModulesPagedResponse response =
         ListDescendantSecurityHealthAnalyticsCustomModules
             .listDescendantSecurityHealthAnalyticsCustomModules(PROJECT_ID);
