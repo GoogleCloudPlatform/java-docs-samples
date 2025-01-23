774bc4fa723d8cfae7155703f594ddc92c9b6d17
Darren Ackers <ackers86@hotmail.com>
2025-01-23 15:19:46 +0000

    chore(memorstore): added file headers

--- memorystore/valkey/caching/demo/app/src/main/java/app/DataController.java
+++ memorystore/valkey/caching/demo/app/src/main/java/app/DataController.java
@@ -1,8 +1,23 @@
-/**
- * Responsible for handling the data operations.
- * Handles checking cache first, then database, and updating the cache.
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
+/**
+ * Responsible for handling the data operations. Handles checking cache first, then database, and
+ * updating the cache.
+ */
 package app;
 
 import java.util.List;
