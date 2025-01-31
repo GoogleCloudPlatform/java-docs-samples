1b8d4bbc37b42f116f9a1c67f232d79750c05bd1
Darren Ackers <ackers86@hotmail.com>
2025-01-31 18:52:32 +0000

    chore: updated file headers

--- memorystore/valkey/leaderboard/app/src/main/java/app/LeaderboardResponse.java
+++ memorystore/valkey/leaderboard/app/src/main/java/app/LeaderboardResponse.java
@@ -1,30 +1,47 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package app;
 
-import java.util.List;
 import org.json.JSONObject;
+
+import java.util.List;
 
 public class LeaderboardResponse {
 
-  private List<LeaderboardEntry> entries;
-  private int fromCache;
+    private List<LeaderboardEntry> entries;
+    private int fromCache;
 
-  public LeaderboardResponse(List<LeaderboardEntry> entries, int fromCache) {
-    this.entries = entries;
-    this.fromCache = fromCache;
-  }
+    public LeaderboardResponse(List<LeaderboardEntry> entries, int fromCache) {
+        this.entries = entries;
+        this.fromCache = fromCache;
+    }
 
-  public List<LeaderboardEntry> getEntries() {
-    return entries;
-  }
+    public List<LeaderboardEntry> getEntries() {
+        return entries;
+    }
 
-  public int getFromCache() {
-    return fromCache;
-  }
+    public int getFromCache() {
+        return fromCache;
+    }
 
-  public JSONObject toJson() {
-    JSONObject json = new JSONObject();
-    json.put("entries", entries);
-    json.put("fromCache", fromCache);
-    return json;
-  }
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("entries", entries);
+        json.put("fromCache", fromCache);
+        return json;
+    }
 }
--- memorystore/valkey/leaderboard/app/src/main/java/app/OrderByType.java
+++ memorystore/valkey/leaderboard/app/src/main/java/app/OrderByType.java
@@ -1,29 +1,45 @@
+/*
+ * Copyright 2025 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package app;
 
 public enum OrderByType {
-  HIGH_TO_LOW("h2l"),
-  LOW_TO_HIGH("l2h");
+    HIGH_TO_LOW("h2l"),
+    LOW_TO_HIGH("l2h");
 
-  private final String value;
+    private final String value;
 
-  OrderByType(String value) {
-    this.value = value;
-  }
-
-  public String getValue() {
-    return value;
-  }
-
-  public static OrderByType fromString(String text) {
-    for (OrderByType b : OrderByType.values()) {
-      if (b.value.equalsIgnoreCase(text)) {
-        return b;
-      }
+    OrderByType(String value) {
+        this.value = value;
     }
-    return null;
-  }
 
-  public static boolean isValid(String text) {
-    return fromString(text) != null;
-  }
+    public String getValue() {
+        return value;
+    }
+
+    public static OrderByType fromString(String text) {
+        for (OrderByType b : OrderByType.values()) {
+            if (b.value.equalsIgnoreCase(text)) {
+                return b;
+            }
+        }
+        return null;
+    }
+
+    public static boolean isValid(String text) {
+        return fromString(text) != null;
+    }
 }
