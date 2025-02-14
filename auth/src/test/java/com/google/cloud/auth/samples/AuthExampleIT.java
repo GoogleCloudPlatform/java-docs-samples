c9d715b36bd966ab4272f43296f5538bbb0bfe1e
ldetmer <1771267+ldetmer@users.noreply.github.com>
2025-02-14 14:52:34 -0500

    validate which key is being used/deleted

--- auth/src/test/java/com/google/cloud/auth/samples/AuthExampleIT.java
+++ auth/src/test/java/com/google/cloud/auth/samples/AuthExampleIT.java
@@ -70,6 +70,7 @@ public class AuthExampleIT {
     Key apiKey = null;
     try {
       apiKey = AuthTestUtils.createTestApiKey(projectId, keyDisplayName, service, method);
+      assertNotNull(apiKey)
       System.out.println("created key " + apiKey.getKeyString());
       String output = ApiKeyAuthExample.authenticateUsingApiKey(apiKey.getKeyString());
 
