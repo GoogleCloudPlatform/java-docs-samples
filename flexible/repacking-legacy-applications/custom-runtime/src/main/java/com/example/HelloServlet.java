46b6619204df91039971085a6a680912545ab360
i-adarsh <i.adarsh.kr@icloud.com>
2025-04-10 12:32:35 +0000

    Repackaging Java 8 application public doc update

---
+++ flexible/repacking-legacy-applications/custom-runtime/src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello, World!</h1>");
+    }
+}
