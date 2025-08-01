e984e4964c2e74a959c764624677f0312897b6e7
FRANCISCO JAVIER ALARCON ESPARZA <alarconesparza@google.com>
2025-08-01 21:20:29 +0000

    remove crossorigin

--- run/service-auth/src/main/java/com/example/serviceauth/Authentication.java
+++ run/service-auth/src/main/java/com/example/serviceauth/Authentication.java
@@ -30,7 +30,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +37,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class Authentication {
   @RestController
-  @CrossOrigin(exposedHeaders = "*", allowedHeaders = "*")
   class AuthenticationController {
 
     @Autowired private AuthenticationService authService;
