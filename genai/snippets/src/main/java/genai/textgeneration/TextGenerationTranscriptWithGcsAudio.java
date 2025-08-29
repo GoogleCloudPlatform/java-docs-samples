2ee3a227544ab4cbe7f29465e083cd0a4b216cde
Juan Dominguez <jdomin@softserveinc.com>
2025-08-29 09:51:04 -0400

    feat(genai): add new text generation samples

--- genai/snippets/src/main/java/genai/textgeneration/TextGenerationTranscriptWithGcsAudio.java
+++ genai/snippets/src/main/java/genai/textgeneration/TextGenerationTranscriptWithGcsAudio.java
@@ -33,7 +33,7 @@ public class TextGenerationTranscriptWithGcsAudio {
     generateContent(modelId);
   }
 
-  // Generates audio transcription
+  // Generates transcript with audio input
   public static String generateContent(String modelId) {
     // Initialize client that will be used to send requests. This client only needs to be created
     // once, and can be reused for multiple requests.
