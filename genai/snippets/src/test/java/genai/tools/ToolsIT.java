0a127da514cbf21ce4356bb4556f8660e490eb92
Juan Domínguez Riquelme <45468567+jdomingr@users.noreply.github.com>
2025-09-16 13:07:58 -0300

    feat(genai): add tools samples (#10147)
    * feat: add new GenAI tools samples
    
    * refactor: change some print statement in samples
    
    * refactor: introduce minor changes to improve consistency
    
    * Improve comments in toolVais samples
    
    * delete unnecessary line break
    
    * refactor: change inputs order and improve comments in some samples
    
    * delete Vertex AI tool sample to take it to a new PR
    
    * refactor(genai): use image directly in CodeExecWithTextLocalImages sample and add image source
    
    ---------
    
    Co-authored-by: Juan Dominguez <jdomin@softserveinc.com>

--- genai/snippets/src/test/java/genai/tools/ToolsIT.java
+++ genai/snippets/src/test/java/genai/tools/ToolsIT.java
@@ -20,6 +20,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import org.junit.After;
 import org.junit.Before;
@@ -32,6 +33,7 @@ import org.junit.runners.JUnit4;
 public class ToolsIT {
 
   private static final String GEMINI_FLASH = "gemini-2.5-flash";
+  private static final String PROJECT_ID = System.getenv("GOOGLE_CLOUD_PROJECT");
   private ByteArrayOutputStream bout;
   private PrintStream out;
 
@@ -57,6 +59,7 @@ public class ToolsIT {
   @After
   public void tearDown() {
     System.setOut(null);
+    bout.reset();
   }
 
   @Test
@@ -79,4 +82,27 @@ public class ToolsIT {
     assertThat(response).contains("copies_sold=350000");
     assertThat(response).contains("album_name=Echoes of the Night");
   }
+
+  @Test
+  public void testToolsCodeExecWithText() {
+    String response = ToolsCodeExecWithText.generateContent(GEMINI_FLASH);
+    assertThat(response).isNotEmpty();
+    assertThat(bout.toString()).contains("Code:");
+    assertThat(bout.toString()).contains("Outcome:");
+  }
+
+  @Test
+  public void testToolsCodeExecWithTextLocalImage() throws IOException {
+    String response = ToolsCodeExecWithTextLocalImage.generateContent(GEMINI_FLASH);
+    assertThat(response).isNotEmpty();
+    assertThat(bout.toString()).contains("Code:");
+    assertThat(bout.toString()).contains("Outcome:");
+  }
+
+  @Test
+  public void testToolsGoogleSearchWithText() {
+    String response = ToolsGoogleSearchWithText.generateContent(GEMINI_FLASH);
+    assertThat(response).isNotEmpty();
+  }
+
 }
