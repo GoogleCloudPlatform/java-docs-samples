04b2605a6306bf20aee002c5805102e13856b102
Juan Domínguez Riquelme <45468567+jdomingr@users.noreply.github.com>
2025-10-14 12:30:42 -0300

    feat(genai): add image generation samples (2) (#10173)
    * feat(genai): add new genai sdk image generation samples
    
    * refactor: change bucket prefix in tests
    
    * refactor: change output bucket in subject reference sample and change variable name in tests
    
    * refactor: apply some suggestions to the samples
    
    * add a more extensive header to each sample
    
    * refactor: change method comment
    
    ---------
    
    Co-authored-by: Juan Dominguez <jdomin@softserveinc.com>

--- genai/snippets/src/test/java/genai/imagegeneration/ImageGenerationIT.java
+++ genai/snippets/src/test/java/genai/imagegeneration/ImageGenerationIT.java
@@ -19,11 +19,16 @@ package genai.imagegeneration;
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
 
+import com.google.api.gax.paging.Page;
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import com.google.genai.types.Image;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Optional;
+import java.util.UUID;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -35,7 +40,11 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class ImageGenerationIT {
 
-  private static final String IMAGGEN_4_MODEL = "imagen-4.0-generate-001";
+  private static final String IMAGEN_3_MODEL = "imagen-3.0-capability-001";
+  private static final String BUCKET_NAME = "java-docs-samples-testing";
+  private static final String PREFIX = "genai-img-generation-" + UUID.randomUUID();
+  private static final String OUTPUT_GCS_URI = String.format("gs://%s/%s", BUCKET_NAME, PREFIX);
+  private static final String IMAGEN_4_MODEL = "imagen-4.0-generate-001";
   private static final String VIRTUAL_TRY_ON_MODEL = "virtual-try-on-preview-08-04";
 
   private ByteArrayOutputStream bout;
@@ -53,6 +62,16 @@ public class ImageGenerationIT {
     requireEnvVar("GOOGLE_CLOUD_PROJECT");
   }
 
+  @AfterClass
+  public static void cleanup() {
+    Storage storage = StorageOptions.getDefaultInstance().getService();
+    Page<Blob> blobs = storage.list(BUCKET_NAME, Storage.BlobListOption.prefix(PREFIX));
+
+    for (Blob blob : blobs.iterateAll()) {
+      storage.delete(blob.getBlobId());
+    }
+  }
+
   @Before
   public void setUp() {
     bout = new ByteArrayOutputStream();
@@ -66,10 +85,55 @@ public class ImageGenerationIT {
   }
 
   @Test
+  public void testImageGenCannyCtrlTypeWithTextAndImage() {
+    Optional<String> response =
+            ImageGenCannyCtrlTypeWithTextAndImage.cannyEdgeCustomization(
+                    IMAGEN_3_MODEL, OUTPUT_GCS_URI);
+    assertThat(response).isPresent();
+    assertThat(response.get()).isNotEmpty();
+  }
+
+  @Test
+  public void testImageGenRawReferenceWithTextAndImage() {
+    Optional<String> response =
+            ImageGenRawReferenceWithTextAndImage.styleTransferCustomization(
+                    IMAGEN_3_MODEL, OUTPUT_GCS_URI);
+    assertThat(response).isPresent();
+    assertThat(response.get()).isNotEmpty();
+  }
+
+  @Test
+  public void testImageGenScribbleCtrlTypeWithTextAndImage() {
+    Optional<String> response =
+            ImageGenScribbleCtrlTypeWithTextAndImage.scribbleCustomization(
+                    IMAGEN_3_MODEL, OUTPUT_GCS_URI);
+    assertThat(response).isPresent();
+    assertThat(response.get()).isNotEmpty();
+  }
+
+  @Test
+  public void testImageGenStyleReferenceWithTextAndImage() {
+    Optional<String> response =
+            ImageGenStyleReferenceWithTextAndImage.styleCustomization(
+                    IMAGEN_3_MODEL, OUTPUT_GCS_URI);
+    assertThat(response).isPresent();
+    assertThat(response.get()).isNotEmpty();
+  }
+
+  @Test
+  public void testImageGenSubjectReferenceWithTextAndImage() {
+    Optional<String> response =
+            ImageGenSubjectReferenceWithTextAndImage.subjectCustomization(
+                    IMAGEN_3_MODEL, OUTPUT_GCS_URI);
+    assertThat(response).isPresent();
+    assertThat(response.get()).isNotEmpty();
+  }
+
+  @Test
   public void testImageGenVirtualTryOnWithTextAndImage() throws IOException {
     Image image =
-        ImageGenVirtualTryOnWithTextAndImage.generateContent(
-            VIRTUAL_TRY_ON_MODEL, "resources/output/man_in_sweater.png");
+            ImageGenVirtualTryOnWithTextAndImage.generateContent(
+                    VIRTUAL_TRY_ON_MODEL, "resources/output/man_in_sweater.png");
 
     assertThat(image).isNotNull();
     assertThat(image.imageBytes()).isPresent();
@@ -79,10 +143,11 @@ public class ImageGenerationIT {
   @Test
   public void testImageGenWithText() throws IOException {
     Image image =
-        ImageGenWithText.generateImage(IMAGGEN_4_MODEL, "resources/output/dog_newspaper.png");
+            ImageGenWithText.generateImage(IMAGEN_4_MODEL, "resources/output/dog_newspaper.png");
 
     assertThat(image).isNotNull();
     assertThat(image.imageBytes()).isPresent();
     assertThat(image.imageBytes().get().length).isGreaterThan(0);
   }
+
 }
