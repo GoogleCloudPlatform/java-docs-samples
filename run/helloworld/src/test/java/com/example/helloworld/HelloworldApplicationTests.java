bdd728872c4674bc39dc69f2bbfef7bd9ac2914c
Kevin Gordillo <kgordillo@google.com>
2026-06-03 20:18:56 +0000

    chore(run): upgrade helloworld sample to Java 25

--- run/helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ run/helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -29,7 +29,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = HelloworldApplication.class)
 @AutoConfigureMockMvc
 public class HelloworldApplicationTests {
 
