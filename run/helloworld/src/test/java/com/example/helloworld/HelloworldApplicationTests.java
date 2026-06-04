854320751247b478ed6294a0e1e0d0e9f1ae4b7a
Kevin Gordillo <kgordillo@google.com>
2026-06-04 12:37:46 -0600

    fix(cloud-run): CloudRun/HelloWorld upgrade to Java25 along Docker (#10276)
    * chore(run): upgrade helloworld sample to Java 25
    
    * fix: kokoro btlr compatability only with Java 21 in pom.xml workaround
    
    * refactor: migrate tests from JUnit 4 to JUnit 5

--- run/helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
+++ run/helloworld/src/test/java/com/example/helloworld/HelloworldApplicationTests.java
@@ -20,15 +20,12 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloworldApplicationTests {
