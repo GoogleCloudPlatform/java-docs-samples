000eb5cc12936060c3be8edab6ad06d8e603cd36
Kevin Gordillo <kgordillo@google.com>
2026-06-19 21:46:50 +0000

    chore: skip integration tests when GCLOUD_ORGANIZATION is unset and fix minor documentation and license URL formatting

--- securitycenter/snippets/src/main/java/com/example/securitycenter/FindingSnippets.java
+++ securitycenter/snippets/src/main/java/com/example/securitycenter/FindingSnippets.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.securitycenter;
 
 import com.google.cloud.securitycenter.v1.Finding;
@@ -368,7 +369,7 @@ public class FindingSnippets {
    * Group all findings under an organization across all sources by their specified properties (e.g.
    * category).
    *
-   * @param organizationName: The organization to group all findings for.
+   * @param organizationName The organization to group all findings for.
    */
   // [START securitycenter_group_all_findings]
   static ImmutableList<GroupResult> groupFindings(OrganizationName organizationName) {
--- securitycenter/snippets/src/test/java/com/example/securitycenter/ITSourceSnippets.java
+++ securitycenter/snippets/src/test/java/com/example/securitycenter/ITSourceSnippets.java
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       https://www.apache.org/licenses/LICENSE-2.0
+ *       http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.example.securitycenter;
 
 import static junit.framework.TestCase.assertNotNull;
@@ -31,6 +32,9 @@ public class ITSourceSnippets {
 
   @BeforeClass
   public static void setUp() throws IOException {
+    org.junit.Assume.assumeTrue(
+        "Skipping tests: GCLOUD_ORGANIZATION env var is not set.",
+        System.getenv("GCLOUD_ORGANIZATION") != null);
     SOURCE_NAME = SourceName.parse(SourceSnippets.createSource(getOrganizationId()).getName());
   }
 
