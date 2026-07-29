a14ac5f6504f48e7f4b6f587d6ac41ef197e8b25
Kevin Gordillo <kgordillo@google.com>
2026-07-29 21:30:40 +0000

    refactor(gae): remove redundant snippet region tags from datastore queries test

--- appengine-java8/datastore/src/test/java/com/example/appengine/QueriesTest.java
+++ appengine-java8/datastore/src/test/java/com/example/appengine/QueriesTest.java
@@ -475,7 +475,6 @@ public class QueriesTest {
     // Act
     long minBirthYear = 1940;
     long maxBirthYear = 1980;
-    // [START gae_java8_datastore_inequality_filters_one_property_valid_1]
     Filter birthYearMinFilter =
         new FilterPredicate("birthYear", FilterOperator.GREATER_THAN_OR_EQUAL, minBirthYear);
 
@@ -486,7 +485,6 @@ public class QueriesTest {
         CompositeFilterOperator.and(birthYearMinFilter, birthYearMaxFilter);
 
     Query q = new Query("Person").setFilter(birthYearRangeFilter);
-    // [END gae_java8_datastore_inequality_filters_one_property_valid_1]
 
     // Assert
     List<Entity> results =
@@ -498,7 +496,6 @@ public class QueriesTest {
   public void queryRestrictions_compositeFilter_isInvalid() throws Exception {
     long minBirthYear = 1940;
     long maxHeight = 200;
-    // [START gae_java8_datastore_inequality_filters_one_property_invalid]
     Filter birthYearMinFilter =
         new FilterPredicate("birthYear", FilterOperator.GREATER_THAN_OR_EQUAL, minBirthYear);
 
@@ -508,7 +505,6 @@ public class QueriesTest {
     Filter invalidFilter = CompositeFilterOperator.and(birthYearMinFilter, heightMaxFilter);
 
     Query q = new Query("Person").setFilter(invalidFilter);
-    // [END gae_java8_datastore_inequality_filters_one_property_invalid]
 
     // Note: The local devserver behavior is different than the production
     // version of Cloud Datastore, so there aren't any assertions we can make
@@ -545,7 +541,6 @@ public class QueriesTest {
     String targetCity = "Somewhere";
     String targetLastName = "Someone";
 
-    // [START gae_java8_datastore_inequality_filters_one_property_valid_2]
     Filter lastNameFilter = new FilterPredicate("lastName", FilterOperator.EQUAL, targetLastName);
 
     Filter cityFilter = new FilterPredicate("city", FilterOperator.EQUAL, targetCity);
@@ -561,7 +556,6 @@ public class QueriesTest {
             lastNameFilter, cityFilter, birthYearMinFilter, birthYearMaxFilter);
 
     Query q = new Query("Person").setFilter(validFilter);
-    // [END gae_java8_datastore_inequality_filters_one_property_valid_2]
 
     // Assert
     List<Entity> results =
